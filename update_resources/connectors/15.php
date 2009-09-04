923b05d7d9a3d8e48daaf082884bed1f8581a164
pleary <pleary@baf6b65b-9124-4dc3-b1c0-01155b4af546>
2009-09-04 14:33:41 +0000

    Removed unnecessary step from Flickr connector
    git-svn-id: https://repository.eol.org/eol_php_code/trunk@10 baf6b65b-9124-4dc3-b1c0-01155b4af546

--- update_resources/connectors/15.php
+++ update_resources/connectors/15.php
@@ -20,20 +20,10 @@ $taxa = FlickrAPI::get_all_eol_photos($auth_token);
 $connection = @new SchemaConnection();
 $xml = $connection->get_taxon_xml($taxa);
 
-$old_resource_path = CONTENT_RESOURCE_LOCAL_PATH . "15.xml";
-$new_resource_path = LOCAL_ROOT . "temp/15.xml";
+$resource_path = CONTENT_RESOURCE_LOCAL_PATH . "15.xml";
 
-$OUT = fopen($new_resource_path, "w+");
+$OUT = fopen($resource_path, "w+");
 fwrite($OUT, $xml);
 fclose($OUT);
-
-if(filesize($new_resource_path) && filesize($new_resource_path)!=filesize($old_resource_path))
-{
-    shell_exec("cp ".$new_resource_path." ".$old_resource_path);
-}
-
-shell_exec("rm ".$new_resource_path);
-
-
 
 ?>
