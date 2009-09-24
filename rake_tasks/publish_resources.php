de3d83387f89db937b0e9af242627d085d481a66
pleary <pleary@baf6b65b-9124-4dc3-b1c0-01155b4af546>
2009-09-24 18:56:08 +0000

    Added publish_resources.php task
    git-svn-id: https://repository.eol.org/eol_php_code/trunk@102 baf6b65b-9124-4dc3-b1c0-01155b4af546

---
+++ rake_tasks/publish_resources.php
@@ -0,0 +1,24 @@
+#!/usr/local/bin/php
+<?php
+
+define("ENVIRONMENT", "integration");
+define('DEBUG', true);
+define('MYSQL_DEBUG', true);
+define('DEBUG_TO_FILE', true);
+
+include_once(dirname(__FILE__)."/../config/start.php");
+
+$mysqli =& $GLOBALS['mysqli_connection'];
+
+
+$resources = Resource::ready_for_publishing();
+foreach($resources as $resource)
+{
+    echo "\nPublishing $resource->title ($id)\n\n";
+    $resource->publish();
+}
+
+// shell_exec(dirname(__FILE__)."/denormalize_tables");
+// shell_exec(dirname(__FILE__)."/clear_eol_cache.php");
+
+?>
