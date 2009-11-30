ea5d4f8e7cce0250053cf51a2d20aa025d68611d
eagbayani <eagbayani@baf6b65b-9124-4dc3-b1c0-01155b4af546>
2009-11-30 14:07:36 +0000

    git-svn-id: https://repository.eol.org/eol_php_code/trunk@340 baf6b65b-9124-4dc3-b1c0-01155b4af546

--- applications/species_stat/display.php
+++ applications/species_stat/display.php
@@ -435,7 +435,9 @@ if($view == 3)
         $arr['Number of hidden and unvetted data objects'][$i]                     = number_format($row["vetted_unknown_published_notVisible_uniqueGuid"]);
         $arr['Number of hidden and unreliable data objects'][$i]                 = number_format($row["vetted_untrusted_published_notVisible_uniqueGuid"]);
         
+        /* not yet in table, temporarily commented
         $arr['Number of user submitted text data objects (published)'][$i]           = number_format($row["user_submitted_text"]);
+        */
         
         $arr2['id'][$i] = $row["id"];    // this is the page_stats!id which is an autoctr
         $i++;
