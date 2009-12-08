f13942c21edf4e1e5e1fdf4dddbd3ee9b848f69e
eagbayani <eagbayani@baf6b65b-9124-4dc3-b1c0-01155b4af546>
2009-12-08 04:32:28 +0000

    removed taxon_concept_names
    git-svn-id: https://repository.eol.org/eol_php_code/trunk@378 baf6b65b-9124-4dc3-b1c0-01155b4af546

--- rake_tasks/eol_2_gni_tcs.php
+++ rake_tasks/eol_2_gni_tcs.php
@@ -18,7 +18,6 @@ $mysqli =& $GLOBALS['mysqli_connection'];
 
 $used = array();
 
-
 /* local
 $resource_path = "..\applications\content_server\gni_tcs_files\eol_names.xml";
 */
@@ -40,52 +39,14 @@ $header="<?xml version=\"1.0\" encoding=\"utf-8\"?>
   <TaxonNames>";
 fwrite($FILE, $header);
 
-/*
-//kingdom
-$kingdoms = array();
-$result = $mysqli->query("SELECT he.id, he.lft, he.rgt, he.hierarchy_id, n.id, n.string FROM hierarchy_entries he JOIN names n ON (he.name_id=n.id) WHERE he.rank_id=".Rank::find("Kingdom"));
-while($result && $row=$result->fetch_assoc())
-{
-    $row["string"] = htmlspecialchars($row["string"]);
-    $kingdoms[] = $row;
-}
-*/
-
 $i=0;
 
-/*
-// First get preferred names from Catalogue of Life
-$qry="SELECT tc.id, n.string, ranks.label, he.hierarchy_id, he.lft, he.rgt FROM taxon_concepts tc JOIN taxon_concept_names tcn 
-ON (tc.id=tcn.taxon_concept_id) JOIN hierarchy_entries he ON (tcn.source_hierarchy_entry_id=he.id) JOIN names n 
-ON (tcn.name_id=n.id) Join ranks ON he.rank_id = ranks.id WHERE tc.published=1 
-AND tc.vetted_id IN (0, ".Vetted::find('Trusted').") AND vern=0 
-AND preferred=1 
-AND he.hierarchy_id=".Hierarchy::find_by_label('Species 2000 & ITIS Catalogue of Life: Annual Checklist 2009'); //$qry .= " limit 10000,20";
-$result = $mysqli->query($qry);
-while($result && $row=$result->fetch_assoc())
-{    
-    $i++;
-    $TaxonName = build_TaxonName($row["string"]);
-    if($TaxonName)
-    {        
-        fwrite($FILE,$TaxonName); 
-        $used[$row['id']] = 1;
-    }
-}
-*/
-
-// Then fill in the rest of the names with what is left over - ignoring the names we already used
-/*
-$qry="SELECT tc.id, n.string, ranks.label, he.hierarchy_id, he.lft, he.rgt FROM taxon_concepts tc JOIN taxon_concept_names tcn 
-ON (tc.id=tcn.taxon_concept_id) JOIN names n ON (tcn.name_id=n.id) JOIN hierarchy_entries he 
-ON (tcn.source_hierarchy_entry_id=he.id) Join ranks ON he.rank_id = ranks.id WHERE tc.published=1 
-AND tc.vetted_id IN (0, ".Vetted::find('Trusted').") AND vern=0 AND preferred=1"; //$qry .= " limit 10000,20";
-*/
-
-$qry="SELECT tc.id, n.string, ranks.label, he.hierarchy_id, he.lft, he.rgt FROM taxon_concepts tc JOIN taxon_concept_names tcn 
-ON (tc.id=tcn.taxon_concept_id) JOIN names n ON (tcn.name_id=n.id) JOIN hierarchy_entries he 
-ON (tcn.source_hierarchy_entry_id=he.id) left Join ranks ON he.rank_id = ranks.id WHERE tc.published=1 
-AND tc.vetted_id IN (0, ".Vetted::find('Trusted').") AND vern=0 "; 
+$qry="SELECT tc.id, n.string, ranks.label, he.hierarchy_id
+From hierarchy_entries he
+Inner Join names n ON n.id = he.name_id
+Inner Join taxon_concepts tc ON he.taxon_concept_id = tc.id
+Inner Join ranks ON he.rank_id = ranks.id
+WHERE tc.published=1 AND tc.vetted_id IN (0,5) "; 
 //$qry .= " limit 10";
 //$qry .= " limit 10000,20";
 //AND preferred=1
@@ -131,8 +92,7 @@ function build_TaxonName(&$str,&$label,&$id)
         </TaxonName>');
         
         return $final;
-        
-        //<dwc:Kingdom>' . get_kingdom($row["hierarchy_id"], $row["lft"], $row["rgt"]) . '</dwc:Kingdom>
+
     }
     else return false;
 }
@@ -144,20 +104,5 @@ function check_sciname($str)
     if(!$string) return false;
     return $string;
 }
-
-/*
-function get_kingdom($hierarchy_id, $lft, $rgt)
-{
-    global $kingdoms;    
-    foreach($kingdoms as $kingdom)
-    {
-        if($kingdom["hierarchy_id"] == $hierarchy_id && $kingdom["lft"] < $lft && $kingdom["rgt"] > $rgt)
-        {
-            return $kingdom["string"];
-        }
-    }    
-    return false;
-}
-*/
 
 ?>
