fc0aff0a2ee400e728d501c4d0e7fe9f6086373b
eagbayani <eagbayani@baf6b65b-9124-4dc3-b1c0-01155b4af546>
2009-12-23 23:06:52 +0000

    fix html
    git-svn-id: https://repository.eol.org/eol_php_code/trunk@453 baf6b65b-9124-4dc3-b1c0-01155b4af546

--- applications/names_lookup/index.php
+++ applications/names_lookup/index.php
@@ -1,5 +1,5 @@
 <!-- frames -->
 <frameset  cols="50%,*">
-    <frame name="form" src="form.php" marginwidth="10" marginheight="10" scrolling="auto" frameborder="no">
-    <frame name="result" src="result.php" marginwidth="10" marginheight="10" scrolling="auto" frameborder="no">
+    <frame name="form" src="form.php" marginwidth="10" marginheight="10" scrolling="auto" frameborder="0">
+    <frame name="result" src="result.php" marginwidth="10" marginheight="10" scrolling="auto" frameborder="0">
 </frameset>
