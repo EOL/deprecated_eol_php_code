8b7e2d112d9e7a54d30afc75791a7bbc60e256dd
eagbayani <eagbayani@baf6b65b-9124-4dc3-b1c0-01155b4af546>
2009-12-27 14:59:44 +0000

    git-svn-id: https://repository.eol.org/eol_php_code/trunk@465 baf6b65b-9124-4dc3-b1c0-01155b4af546

--- applications/species_stat/graph.php
+++ applications/species_stat/graph.php
@@ -117,7 +117,10 @@ function get_values_fromCSV($title)
             for ($c=0; $c < $num; $c++) 
             {        
                 if($row==0) $label[]=$data[$c];
-				else        $arr["$label[$c]"][]=$data[$c];
+                else        
+                {
+                    if(is_array($label[$c))$arr["$label[$c]"][]=$data[$c];
+                }
             }                        
             //if($row == 10)break;    
         }
