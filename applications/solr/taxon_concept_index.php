a55a02ae0fc2be51af2b722e005c65c5629f8bd5
pleary <pleary@baf6b65b-9124-4dc3-b1c0-01155b4af546>
2010-01-06 14:50:56 +0000

    Added logging to the taxon concept Solr indexer
    git-svn-id: https://repository.eol.org/eol_php_code/trunk@518 baf6b65b-9124-4dc3-b1c0-01155b4af546

--- applications/solr/taxon_concept_index.php
+++ applications/solr/taxon_concept_index.php
@@ -5,7 +5,13 @@ define('ENVIRONMENT', 'slave');
 include_once(dirname(__FILE__) . "/../../config/start.php");
 $mysqli =& $GLOBALS['mysqli_connection'];
 
+
+Functions::log("Starting TaxonConceptIndexer");
+
 $indexer = new TaxonConceptIndexer();
 $indexer->index();
+
+Functions::log("Ending TaxonConceptIndexer");
+
 
 ?>
