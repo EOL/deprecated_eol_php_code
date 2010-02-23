b4de28e52c6ba2111306b6cfe42c6df2fc3a1d4a
pleary <pleary@baf6b65b-9124-4dc3-b1c0-01155b4af546>
2010-02-23 18:11:42 +0000

    Added new simpletest
    git-svn-id: https://repository.eol.org/eol_php_code/trunk@707 baf6b65b-9124-4dc3-b1c0-01155b4af546

---
+++ classes/modules/simpletest/extensions/dom_tester/test/dom_tester_doc_test.php
@@ -0,0 +1,24 @@
+<?php
+// $Id: dom_tester_doc_test.php 1872 2009-04-15 10:43:47Z pp11 $
+require_once dirname(__FILE__) . '/../../../autorun.php';
+require_once dirname(__FILE__) . '/../../dom_tester.php';
+
+SimpleTest :: prefer(new TextReporter());
+
+class TestOfLiveCssSelectors extends DomTestCase {
+    function setUp() {
+        $this->addHeader('User-Agent: SimpleTest ' . SimpleTest::getVersion());
+    }
+    
+    function testGet() {
+        $url = 'http://simpletest.org/';
+        $this->assertTrue($this->get($url));
+        $this->assertEqual($this->getUrl(), $url);
+        $this->assertEqual($this->getElementsBySelector('h2'), array('Screenshots', 'Documentation', 'Contributing'));
+        $this->assertElementsBySelector('h2', array('Screenshots', 'Documentation', 'Contributing'));
+		$this->assertElementsBySelector('a[href="http://simpletest.org/api/"]', array('the complete API', 'documented API'));
+   		$this->assertElementsBySelector('div#content > p > strong', array('SimpleTest PHP unit tester'));
+    }
+}
+
+?>
