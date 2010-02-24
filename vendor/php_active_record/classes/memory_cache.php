1966e95c4826e92934c1b5ca01c3d50773710600
eagbayani <eagbayani@baf6b65b-9124-4dc3-b1c0-01155b4af546>
2010-02-24 23:16:19 +0000

    Uncommented MemoryCache
    git-svn-id: https://repository.eol.org/eol_php_code/trunk@732 baf6b65b-9124-4dc3-b1c0-01155b4af546

--- vendor/php_active_record/classes/memory_cache.php
+++ vendor/php_active_record/classes/memory_cache.php
@@ -1,39 +1,39 @@
 <?php
 // namespace php_active_record;
 // 
-// class MemoryCache
-// {
-//     public static function get($key)
-//     {
-//         if(!is_string($key)) return false;
-//         if(!isset($GLOBALS['memory_cache'][$key])) return false;
-//         return unserialize($GLOBALS['memory_cache'][$key]);
-//     }
-//     
-//     public static function add($key, $value, $expire = 10, $force = false)
-//     {
-//         if(!is_string($key)) return false;
-//         if(!$force && isset($GLOBALS['memory_cache'][$key])) return false;
-//         $GLOBALS['memory_cache'][$key] = serialize($value);
-//         return true;
-//     }
-//     
-//     public static function set($key, $value, $expire = 10)
-//     {
-//         if(!is_string($key)) return false;
-//         return self::add($key, $value, $expire, true);
-//     }
-//     
-//     public static function delete($key, $timeout = 0)
-//     {
-//         if(!is_string($key)) return false;
-//         if(isset($GLOBALS['memory_cache'][$key])) unset($GLOBALS['memory_cache'][$key]);
-//     }
-//     
-//     public static function flush()
-//     {
-//         $GLOBALS['memory_cache'] = array();
-//     }
-// }
+class MemoryCache
+{
+    public static function get($key)
+    {
+        if(!is_string($key)) return false;
+        if(!isset($GLOBALS['memory_cache'][$key])) return false;
+        return unserialize($GLOBALS['memory_cache'][$key]);
+    }
+    
+    public static function add($key, $value, $expire = 10, $force = false)
+    {
+        if(!is_string($key)) return false;
+        if(!$force && isset($GLOBALS['memory_cache'][$key])) return false;
+        $GLOBALS['memory_cache'][$key] = serialize($value);
+        return true;
+    }
+    
+    public static function set($key, $value, $expire = 10)
+    {
+        if(!is_string($key)) return false;
+        return self::add($key, $value, $expire, true);
+    }
+    
+    public static function delete($key, $timeout = 0)
+    {
+        if(!is_string($key)) return false;
+        if(isset($GLOBALS['memory_cache'][$key])) unset($GLOBALS['memory_cache'][$key]);
+    }
+    
+    public static function flush()
+    {
+        $GLOBALS['memory_cache'] = array();
+    }
+}
 
 ?>
