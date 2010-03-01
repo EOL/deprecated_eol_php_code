8f5a87a4b99618045eaa2a92ccc43e7f5c08f091
eagbayani <eagbayani@baf6b65b-9124-4dc3-b1c0-01155b4af546>
2010-03-01 17:39:09 +0000

    git-svn-id: https://repository.eol.org/eol_php_code/trunk@773 baf6b65b-9124-4dc3-b1c0-01155b4af546

--- applications/species_stat/display.php
+++ applications/species_stat/display.php
@@ -368,12 +368,11 @@ if($view == 3)
         if($i==17)print"<tr><td>&nbsp;</td></tr><tr><td><b>BHL Statistics</b></td></tr>";
         if($i==19)print"<tr><td>&nbsp;</td></tr><tr><td><b>Curatorial Statistics</b></td></tr>";
         
-        if($i==22)print"<tr><td>&nbsp;</td></tr><tr><td><b>LifeDesks (in EOL Content Partner Registry) Stats &nbsp;&nbsp;
-        <a target='lifedesk' href='index.php?group=5&f=results'>More info</a>
+        if($i==22)print"<tr><td>&nbsp;</td></tr><tr><td><b>LifeDesks (in EOL Content Partner Registry) Stats &nbsp;&nbsp;        
         <a target='lifedesk' href='lifedesk_moreinfo.php'>More info</a>
         
         </b></td></tr>";
-        
+        //<a target='lifedesk' href='index.php?group=5&f=results'>More info</a>
         
         
         if ($i % 2 == 0){$vcolor = 'white';}
