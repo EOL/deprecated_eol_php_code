9827677b7e3c5c79eac310c5149fc370cbf818fa
eagbayani <eagbayani@baf6b65b-9124-4dc3-b1c0-01155b4af546>
2010-03-01 14:24:43 +0000

    test for monthly run before actual run
    git-svn-id: https://repository.eol.org/eol_php_code/trunk@752 baf6b65b-9124-4dc3-b1c0-01155b4af546

--- applications/google_stats/generate_monthly_stats.php
+++ applications/google_stats/generate_monthly_stats.php
@@ -1,5 +1,6 @@
 <?php
-define("ENVIRONMENT", "staging"); 
+
+//define("ENVIRONMENT", "staging"); 
 //define("ENVIRONMENT", "eol_statistics"); 
 //define("ENVIRONMENT", "slave_32"); 
 
@@ -13,13 +14,14 @@ exit;
 
 define("MYSQL_DEBUG", false);
 define("DEBUG", true);
-include_once(dirname(__FILE__) . "/../../config/start.php");
+include_once(dirname(__FILE__) . "/../../config/environment.php");
 
 require_once('google_proc.php');
 $mysqli =& $GLOBALS['mysqli_connection'];
 
-//$mysqli2 = $mysqli; // to use in Beast
-$mysqli2 = load_mysql_environment('staging');        
+$mysqli2 = $mysqli; // to use in Beast
+exit;
+//$mysqli2 = load_mysql_environment('staging');        
 //$mysqli2 = load_mysql_environment('eol_statistics');        
 //$mysqli2 = load_mysql_environment('development'); //to be used when developing locally
 
