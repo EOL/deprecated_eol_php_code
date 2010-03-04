01be00ffafba35434f6d54729801bd2a6e00e2cd
eagbayani <eagbayani@baf6b65b-9124-4dc3-b1c0-01155b4af546>
2010-03-04 22:34:05 +0000

    in cache.php we moved function_get_args
    git-svn-id: https://repository.eol.org/eol_php_code/trunk@813 baf6b65b-9124-4dc3-b1c0-01155b4af546

--- vendor/php_active_record/classes/cache.php
+++ vendor/php_active_record/classes/cache.php
@@ -16,65 +16,70 @@ class Cache
     
     public static function get()
     {
+        $args = func_get_args();
         if(!$GLOBALS["ENV_ENABLE_CACHING"]) return false;
         if(@$GLOBALS['ENV_CACHE'] == "memory")
         {
-            return call_user_func_array(array('MemoryCache', __FUNCTION__), func_get_args());
+            return call_user_func_array(array('MemoryCache', __FUNCTION__), $args);
         }elseif(@$GLOBALS['memcached_connection'])
         {
-            return call_user_func_array(array('Memcached', __FUNCTION__), func_get_args());
+            return call_user_func_array(array('Memcached', __FUNCTION__), $args);
         }
         return false;
     }
     
     public static function add()
     {
+        $args = func_get_args();
         if(!$GLOBALS["ENV_ENABLE_CACHING"]) return false;
         if(@$GLOBALS['ENV_CACHE'] == "memory")
         {
-            return call_user_func_array(array('MemoryCache', __FUNCTION__), func_get_args());
+            return call_user_func_array(array('MemoryCache', __FUNCTION__), $args);
         }elseif(@$GLOBALS['memcached_connection'])
         {
-            return call_user_func_array(array('Memcached', __FUNCTION__), func_get_args());
+            return call_user_func_array(array('Memcached', __FUNCTION__), $args);
         }
         return false;
     }
     
     public static function set()
     {
+        $args = func_get_args();
         if(!$GLOBALS["ENV_ENABLE_CACHING"]) return false;
         if(@$GLOBALS['ENV_CACHE'] == "memory")
         {
-            return call_user_func_array(array('MemoryCache', __FUNCTION__), func_get_args());
+            return call_user_func_array(array('MemoryCache', __FUNCTION__), $args);
         }elseif(@$GLOBALS['memcached_connection'])
         {
-            return call_user_func_array(array('Memcached', __FUNCTION__), func_get_args());
+            return call_user_func_array(array('Memcached', __FUNCTION__), $args);
         }
         return false;
     }
     
     public static function delete()
     {
+        $args = func_get_args();
         if(!$GLOBALS["ENV_ENABLE_CACHING"]) return false;
         if(@$GLOBALS['ENV_CACHE'] == "memory")
         {
-            return call_user_func_array(array('MemoryCache', __FUNCTION__), func_get_args());
+            return call_user_func_array(array('MemoryCache', __FUNCTION__), $args);
         }elseif(@$GLOBALS['memcached_connection'])
         {
-            return call_user_func_array(array('Memcached', __FUNCTION__), func_get_args());
+            return call_user_func_array(array('Memcached', __FUNCTION__), $args);
         }
         return false;
     }
     
     public static function flush()
     {
+        $args = func_get_args();
         if(!$GLOBALS["ENV_ENABLE_CACHING"]) return false;
         if(@$GLOBALS['ENV_CACHE'] == "memory")
         {
-            return call_user_func_array(array('MemoryCache', __FUNCTION__), func_get_args());
+            return call_user_func_array(array('MemoryCache', __FUNCTION__), $args);
         }elseif(@$GLOBALS['memcached_connection'])
         {
-            return call_user_func_array(array('Memcached', __FUNCTION__), func_get_args());
+            return call_user_func_array(array('Memcached', __FUNCTION__), $args);
         }
         return false;
     }
