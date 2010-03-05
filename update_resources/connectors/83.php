08ff10fd792f20fb0ce40780c2bd216adf14fa33
eagbayani <eagbayani@baf6b65b-9124-4dc3-b1c0-01155b4af546>
2010-03-05 17:08:28 +0000

    Use start.php
    git-svn-id: https://repository.eol.org/eol_php_code/trunk@816 baf6b65b-9124-4dc3-b1c0-01155b4af546

--- update_resources/connectors/83.php
+++ update_resources/connectors/83.php
@@ -16,10 +16,10 @@ http://services.morphbank.net/mb3/request?method=eol&format=id
 //define("ENVIRONMENT", "development");
 //define("ENVIRONMENT", "slave_32");
 
-$GLOBALS['ENV_NAME'] = 'slave_32';
+//$GLOBALS['ENV_NAME'] = 'slave_32';
 define("MYSQL_DEBUG", false);
 define("DEBUG", false);
-include_once(dirname(__FILE__) . "/../../config/environment.php");
+include_once(dirname(__FILE__) . "/../../config/start.php");
 $mysqli =& $GLOBALS['mysqli_connection'];
 
 
