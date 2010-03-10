7b7086d8db6d5faa1976fda7f6aac3e913c59d70
pleary <pleary@baf6b65b-9124-4dc3-b1c0-01155b4af546>
2010-03-10 15:56:29 +0000

    Added two more web tests
    git-svn-id: https://repository.eol.org/eol_php_code/trunk@837 baf6b65b-9124-4dc3-b1c0-01155b4af546

---
+++ tests/web/test_google_search.php
@@ -0,0 +1,20 @@
+<?php
+
+class test_google_search extends SimpletestWebBase
+{
+    function testSearching()
+    {
+        @unlink(DOC_ROOT . "temp/google_output.txt");
+        $this->get(WEB_ROOT . 'applications/google/search.php?ENV_NAME=test');
+        $this->setField('nameList', 'Homo sapiens');
+        $this->clickSubmit('Submit');
+        $this->assertText('Homo sapiens');
+        $this->assertTrue(file_exists(DOC_ROOT . "temp/google_output.txt"), 'Results file should exist');
+        
+        $contents = file_get_contents(DOC_ROOT . "temp/google_output.txt");
+        $this->assertTrue(preg_match("/^Homo\+sapiens\thttp:/", $contents), 'Results file should contain links');
+        @unlink(DOC_ROOT . "temp/google_output.txt");
+    }
+}
+
+?>
