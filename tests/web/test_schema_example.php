811b3acda044910e61e1b04322572ba2dcc8848b
pleary <pleary@baf6b65b-9124-4dc3-b1c0-01155b4af546>
2010-03-16 19:10:27 +0000

    Removed spm, radiolaria, schema_parser applications
    git-svn-id: https://repository.eol.org/eol_php_code/trunk@860 baf6b65b-9124-4dc3-b1c0-01155b4af546

--- tests/web/test_schema_example.php
+++ tests/web/test_schema_example.php
@@ -4,7 +4,7 @@ class test_schema_example extends SimpletestWebBase
 {
     function testSchemaExampleCode()
     {
-        $this->get(WEB_ROOT . 'applications/schema_parser/schema_creation_example.php');
+        $this->get(WEB_ROOT . 'applications/schema/schema_creation_example.php');
         $this->assertPattern('/<dwc:ScientificName>Ursus maritimus Phipps, 1774<\/dwc:ScientificName>/', 'Should see the taxon name');
         $this->assertPattern('/<dc:title>Polar Bear {Ursus maritimus}<\/dc:title>/', 'Should see the text title');
         $this->assertPattern('/<agent role="author">Leary, P<\/agent>/', 'Should see the agent name');
---
+++ tests/web/test_validator_app.php
@@ -0,0 +1,21 @@
+<?php
+
+class test_validator_app extends SimpletestWebBase
+{
+    function testWithURL()
+    {
+        $this->get(WEB_ROOT . 'applications/validator/?ENV_NAME=test');
+        $this->setField('file_url', 'http://services.eol.org/eol_php_code/tests/fixtures/files/test_resource.xml');
+        $this->clickSubmit('Submit');
+        $this->assertText('http://services.eol.org/schema/content_0_2.xsd');
+        $this->assertText('This file is valid according to its defined XSD');
+        $this->assertText('but there were EOL internal validation warnings');
+    }
+    
+    function testWithUpload()
+    {
+        // SimpleTest does not handle file uploads as of current version 1.0.1
+    }
+}
+
+?>
