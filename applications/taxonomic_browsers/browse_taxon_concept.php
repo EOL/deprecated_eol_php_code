c0c3d8e537f456427fb83ab8551c16d26e2a7557
pleary <pleary@baf6b65b-9124-4dc3-b1c0-01155b4af546>
2010-03-23 15:14:03 +0000

    Updated TC browser
    git-svn-id: https://repository.eol.org/eol_php_code/trunk@892 baf6b65b-9124-4dc3-b1c0-01155b4af546

--- applications/taxonomic_browsers/browse_taxon_concept.php
+++ applications/taxonomic_browsers/browse_taxon_concept.php
@@ -9,46 +9,31 @@
 include_once(dirname(__FILE__) . "/../../config/environment.php");
 include_once("functions.php");
 
-$taxon_concept_id = @$_GET["taxon_concept_id"];
-$hierarchy_entry_id = @$_GET["hierarchy_entry_id"];
-$expand = @$_GET["expand"];
-
-$mysqli =& $GLOBALS['mysqli_connection'];
-
-$variable = "taxon_concept_id";
+$taxon_concept_id = @$_REQUEST["taxon_concept_id"];
+$expand = @$_REQUEST["expand"];
 
 
 
 if($taxon_concept_id) $node = new TaxonConcept($taxon_concept_id);
-elseif($hierarchy_entry_id) $node = new HierarchyEntry($hierarchy_entry_id);
 else exit;
 
 
-// $indent = show_ancestries($node, $variable);
-// 
-// echo show_node($node, $indent, $variable);
-// 
-// if($expand) show_all_children($node, $indent+1, array(), $variable);
-// else show_children($node, $indent+1, $variable);
-// 
-// echo "<hr>";
-// echo "<hr>";
-// echo "<hr>";
-// echo "<hr>";
-// echo "<hr>";
 
 $hierarchy_entry_ids = $node->hierarchy_entry_ids();
 foreach($hierarchy_entry_ids as $id)
 {
     $hierarchy_entry = new HierarchyEntry($id);
     
-    echo "<b>".$hierarchy_entry->hierarchy()->label."</b><br>hierarchy_entry_id: $hierarchy_entry->id<br>";
+    echo "<p align='right'><b>".$hierarchy_entry->hierarchy()->label."</b><br>hierarchy_entry_id: $hierarchy_entry->id</p>";
     
     $indent = show_ancestry_he($hierarchy_entry);
+    echo "<span style='background-color:#ccc'>";
     echo show_name_he($hierarchy_entry, $indent, 0);
+    echo "</span>";
+    echo "<div style='max-height: 250px; max-width: 700px; overflow: auto'>";
     show_children_he($hierarchy_entry, $indent+1);
     show_synonyms_he($hierarchy_entry);
-    
+    echo "</div>";
     echo "<hr>";
 }
 
