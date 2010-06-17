fc81b57242cc31fee8f2f6126204c33f52457ce9
eagbayani <eagbayani@baf6b65b-9124-4dc3-b1c0-01155b4af546>
2010-06-17 16:53:04 +0000

    Updated connectors - aquamaps, photosynth, vent larvae
    git-svn-id: https://repository.eol.org/eol_php_code/trunk@1218 baf6b65b-9124-4dc3-b1c0-01155b4af546

--- update_resources/connectors/119.php
+++ update_resources/connectors/119.php
@@ -1,9 +1,14 @@
 <?php
+/* connector for Photosynth
+estimated execution time: 
+
+This connector will use an un-official service to search the Photosynth server.
+It also scrapes the Photosynth site to get the tags entered by owners as the tags where not included in the service.
+*/
 
 include_once(dirname(__FILE__) . "/../../config/environment.php");
-require_library('PhotosynthAPI');
+require_library('connectors/PhotosynthAPI');
 $GLOBALS['ENV_DEBUG'] = false;
-
 
 $taxa = PhotosynthAPI::get_all_eol_photos();
 $xml = SchemaDocument::get_taxon_xml($taxa);
