b5174e7998046bbf5f276121687bef8d2f7b8a76
eagbayani <eagbayani@baf6b65b-9124-4dc3-b1c0-01155b4af546>
2010-06-30 20:26:15 +0000

    Connector for Afrotropical Fruit Fly Project
    git-svn-id: https://repository.eol.org/eol_php_code/trunk@1229 baf6b65b-9124-4dc3-b1c0-01155b4af546

---
+++ update_resources/connectors/138.php
@@ -0,0 +1,23 @@
+<?php
+/* connector for Afrotropical 
+estimated execution time: 
+
+This connector reads an EOL XML and converts PDF files stored in <mediaURL> into text description objects.
+
+*/
+
+include_once(dirname(__FILE__) . "/../../config/environment.php");
+require_library('connectors/AfrotropicalAPI');
+$GLOBALS['ENV_DEBUG'] = false;
+
+$taxa = AfrotropicalAPI::get_all_taxa();
+$xml = SchemaDocument::get_taxon_xml($taxa);
+
+$resource_path = CONTENT_RESOURCE_LOCAL_PATH . "138.xml";
+
+$OUT = fopen($resource_path, "w+");
+fwrite($OUT, $xml);
+fclose($OUT);
+
+echo "time: ". Functions::time_elapsed()."\n";
+?>
