949b37236397ef89dbeb82f282a8ab6e3b862b05
eagbayani <eagbayani@baf6b65b-9124-4dc3-b1c0-01155b4af546>
2010-07-01 20:51:15 +0000

    Update connector with latest suggestions from partner. Add authorship in taxa name. Improve species size text description.
    git-svn-id: https://repository.eol.org/eol_php_code/trunk@1238 baf6b65b-9124-4dc3-b1c0-01155b4af546

--- update_resources/connectors/121.php
+++ update_resources/connectors/121.php
@@ -4,6 +4,8 @@ estimated execution time: 20-25 seconds
 Connector screen scrapes the partner website.
 */
 
+$timestart = microtime(1);
+
 include_once(dirname(__FILE__) . "/../../config/environment.php");
 require_library('connectors/HydrothermalVentLarvaeAPI');
 $GLOBALS['ENV_DEBUG'] = false;
@@ -16,5 +18,13 @@ $OUT = fopen($resource_path, "w+");
 fwrite($OUT, $xml);
 fclose($OUT);
 
-echo "time: ". Functions::time_elapsed() ."\n";
+//echo "time: ". Functions::time_elapsed() ."\n";
+
+$elapsed_time_sec = microtime(1)-$timestart;
+echo "\n";
+echo "elapsed time = $elapsed_time_sec sec              \n";
+echo "elapsed time = " . $elapsed_time_sec/60 . " min   \n";
+echo "elapsed time = " . $elapsed_time_sec/60/60 . " hr \n";
+exit("\n\n Done processing.");
+
 ?>
