53ef8186d43b893e5c294ac7b5b0ad35b04c470f
eagbayani <eagbayani@baf6b65b-9124-4dc3-b1c0-01155b4af546>
2010-07-29 22:47:24 +0000

    Connector for Natural History Services, ongoing...
    git-svn-id: https://repository.eol.org/eol_php_code/trunk@1270 baf6b65b-9124-4dc3-b1c0-01155b4af546

---
+++ update_resources/connectors/145.php
@@ -0,0 +1,30 @@
+<?php
+/* connector for Natural History Services
+estimated execution time: 
+Connector screen scrapes the partner website.
+*/
+exit;
+$timestart = microtime(1);
+
+include_once(dirname(__FILE__) . "/../../config/environment.php");
+require_library('connectors/NaturalHistoryServicesAPI');
+$GLOBALS['ENV_DEBUG'] = false;
+
+$taxa = NaturalHistoryServicesAPI::get_all_taxa();
+$xml = SchemaDocument::get_taxon_xml($taxa);
+
+$resource_path = CONTENT_RESOURCE_LOCAL_PATH . "145.xml";
+$OUT = fopen($resource_path, "w+");
+fwrite($OUT, $xml);
+fclose($OUT);
+
+//echo "time: ". Functions::time_elapsed() ."\n";
+
+$elapsed_time_sec = microtime(1)-$timestart;
+echo "\n";
+echo "elapsed time = $elapsed_time_sec sec              \n";
+echo "elapsed time = " . $elapsed_time_sec/60 . " min   \n";
+echo "elapsed time = " . $elapsed_time_sec/60/60 . " hr \n";
+exit("\n\n Done processing.");
+
+?>
