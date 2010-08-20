319b6992a221f8458d640dbcdba6413bae15ae44
eagbayani <eagbayani@baf6b65b-9124-4dc3-b1c0-01155b4af546>
2010-08-20 14:18:53 +0000

    For DATA-583, made the output of the tool to point to an instruction page.
    git-svn-id: https://repository.eol.org/eol_php_code/trunk@1314 baf6b65b-9124-4dc3-b1c0-01155b4af546

--- applications/specialist_project_converter/generate.php
+++ applications/specialist_project_converter/generate.php
@@ -20,7 +20,24 @@ $filename = "xml/" . time() . ".xml";
 $OUT = fopen($filename, "w+");            
 fwrite($OUT, $xml); fclose($OUT);
 
-print"<META HTTP-EQUIV='Refresh' Content='0; URL=$filename'>";
+$domain = $_SERVER['HTTP_HOST'];
+$temp 	= $_SERVER['SCRIPT_NAME'];
+$temp 	= str_ireplace("generate.php", $filename, $temp);
+$url    = "http://$domain" . $temp;
+
+print"
+Conversion completed. <br>&nbsp;<br>
+This is the URL of your XML file: <a href='$url'>$url</a> <br>&nbsp;<br>
+You can use this as your resource URL in the EOL content partner registry (Resources section) or you can save your XML and store it elsewhere. <br>&nbsp;<br>
+This XML will be stored in our server for two (2) weeks then it will be deleted. <br>&nbsp;<br>
+Thank you.
+";
+
+
+
+
+
+//print"<META HTTP-EQUIV='Refresh' Content='0; URL=$filename'>";
 
 
 ?>
