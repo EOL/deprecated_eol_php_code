85f87071a557532e583eafac5a092d855178e6e9
eagbayani <eagbayani@baf6b65b-9124-4dc3-b1c0-01155b4af546>
2010-08-20 13:53:05 +0000

    For DATA-583, made the output of the tool to point to an XML file.
    git-svn-id: https://repository.eol.org/eol_php_code/trunk@1313 baf6b65b-9124-4dc3-b1c0-01155b4af546

--- applications/specialist_project_converter/generate.php
+++ applications/specialist_project_converter/generate.php
@@ -10,7 +10,17 @@ require_library('XLSParser');
 $parser = new XLSParser();
 $arr = $parser->convert_sheet_to_array($file);          
 $xml = $parser->create_specialist_project_xml($arr);
+
+/* working but commented because we want to point to an XML file
 header('Content-type: text/xml');    
 print $xml;
+*/
+
+$filename = "xml/" . time() . ".xml";
+$OUT = fopen($filename, "w+");            
+fwrite($OUT, $xml); fclose($OUT);
+
+print"<META HTTP-EQUIV='Refresh' Content='0; URL=$filename'>";
+
 
 ?>
