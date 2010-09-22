0c9ee5cc401076df0f845fef819a1efaf5ba96d4
eagbayani <eagbayani@baf6b65b-9124-4dc3-b1c0-01155b4af546>
2010-09-22 21:28:07 +0000

    Class for the URL look-up tool.
    git-svn-id: https://repository.eol.org/eol_php_code/trunk@1370 baf6b65b-9124-4dc3-b1c0-01155b4af546

---
+++ lib/UrlLookUp.php
@@ -0,0 +1,58 @@
+<?php
+class UrlLookUp
+{
+    function get_names($xml,$nameBankURL,$arr_4saving)
+    {       
+        $arr=array();
+        $html="<table cellpadding='3' cellspacing='0' border='0' style='font-size : small; font-family : Arial Unicode MS;'>";    
+        $i=0;
+        foreach($xml->allNames->entity as $name)
+        {        
+            if($name->score >= 0.99 and isset($name->namebankID))
+            {
+                $i++;
+                $url = $nameBankURL . $name->namebankID;
+                $html .= "<tr><td><a href='$url' target='namebank'>$name->nameString</a></td></tr>";                                      
+                $arr_4saving[]=array("name"=>$name->nameString, "url"=>$url);
+            }
+        }
+        $html.="</table>";    
+        return array($html,$i,$arr_4saving);
+    }
+    
+    function array_trim($a) 
+    { 	
+    	$b=array(); $j = 0; 
+    	for ($i = 0; $i < count($a); $i++) 
+    	{ 
+    		if (trim($a[$i]) != "") { $b[$j++] = $a[$i]; } 
+    	} 
+    	return $b; 
+    }
+    
+    function is_well_formed($xmlfile)
+    {
+    	$curl = curl_init();
+    	curl_setopt($curl, CURLOPT_URL, $xmlfile);
+    	curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    	$output = curl_exec($curl);
+    	curl_close($curl);
+    	if (simplexml_load_string($output)) {return true;} 
+    	else 								{return false;}
+    }
+    
+    function save_to_txt($arr)
+    {    
+    	$str="";        
+    	foreach($arr as $r)
+    	{
+            $str .= $r["name"] . "\t" . $r["url"] . "\n";        
+    	}
+        $fileidx = time();  
+        $filename ="temp/" . $fileidx . ".txt"; 
+    	if($fp = fopen($filename,"a")){fwrite($fp,$str);fclose($fp);}		
+        print "<hr><i>Use a spreadsheet to open the tab-delimited TXT file created for ";
+    	print "<a target='tab_delimited' href=$filename> - Download - </a></i>";        
+    }    
+}        
+?>
