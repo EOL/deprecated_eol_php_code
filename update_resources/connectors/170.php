a31b69e1a8bb1c4a560da02ef0f1fdcf364263c1
eagbayani <eagbayani@baf6b65b-9124-4dc3-b1c0-01155b4af546>
2010-10-05 21:05:10 +0000

    138: Afrotropical connector, 170: Serpent connector
    git-svn-id: https://repository.eol.org/eol_php_code/trunk@1381 baf6b65b-9124-4dc3-b1c0-01155b4af546

--- update_resources/connectors/170.php
+++ update_resources/connectors/170.php
@@ -1,31 +1,29 @@
 <?php
-/* connector for Afrotropical 
-estimated execution time: 41 mins.
-This connector reads an EOL XML and converts PDF files stored in <mediaURL> into text description objects.
+/* connector for Serpent
+estimated execution time: 23 mins.
+Connector screen scrapes the partner website.
 */
-//exit;
+
 $timestart = microtime(1);
 
 include_once(dirname(__FILE__) . "/../../config/environment.php");
-require_library('connectors/AfrotropicalAPI');
+require_library('connectors/SerpentAPI');
 $GLOBALS['ENV_DEBUG'] = false;
 
-$taxa = AfrotropicalAPI::get_all_taxa();
+$taxa = SerpentAPI::get_all_taxa();
 $xml = SchemaDocument::get_taxon_xml($taxa);
 
-$resource_path = CONTENT_RESOURCE_LOCAL_PATH . "138.xml";
-
+$resource_path = CONTENT_RESOURCE_LOCAL_PATH . "170.xml";
 $OUT = fopen($resource_path, "w+");
 fwrite($OUT, $xml);
 fclose($OUT);
 
+//echo "time: ". Functions::time_elapsed() ."\n";
 
-//echo "time: ". Functions::time_elapsed()."\n";
 $elapsed_time_sec = microtime(1)-$timestart;
 echo "\n";
 echo "elapsed time = $elapsed_time_sec sec              \n";
 echo "elapsed time = " . $elapsed_time_sec/60 . " min   \n";
 echo "elapsed time = " . $elapsed_time_sec/60/60 . " hr \n";
 exit("\n\n Done processing.");
-
 ?>
