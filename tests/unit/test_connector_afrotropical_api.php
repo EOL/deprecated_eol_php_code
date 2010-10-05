3367a038cacb44b89f10cd42b19ffce58919dd1f
eagbayani <eagbayani@baf6b65b-9124-4dc3-b1c0-01155b4af546>
2010-10-05 21:10:00 +0000

    Afrotropical and Serpent connector API tests.
    git-svn-id: https://repository.eol.org/eol_php_code/trunk@1384 baf6b65b-9124-4dc3-b1c0-01155b4af546

---
+++ tests/unit/test_connector_afrotropical_api.php
@@ -0,0 +1,33 @@
+<?php
+
+require_library('connectors/AfrotropicalAPI');
+
+class test_connector_afrotropical_api extends SimpletestUnitBase
+{
+    function testAfrotropicalAPI()
+    {        
+        $url = "http://pandanus.eol.org/public/test_resources/EOLexportFruitfly_fixed_4test.xml";
+
+        $arr = AfrotropicalAPI::get_afrotropical_taxa($url,array());
+        $taxa = $arr[0];
+        $this->assertTrue(is_array($taxa), 'Taxa should be an array');
+        
+        $taxon = $taxa[0];
+        $this->assertIsA($taxon, 'SchemaTaxon', 'Response should be an array of SchemaTaxon');
+        
+        $dataObject = $taxon->dataObjects[0];
+        $this->assertIsA($dataObject, 'SchemaDataObject', 'Taxon should have a data object');
+    
+    }
+}
+
+
+
+
+
+        
+    
+
+
+
+?>
