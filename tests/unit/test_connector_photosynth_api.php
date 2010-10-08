67faa8d5be73aee8737b4e5dab9f6a9dab44f8b7
eagbayani <eagbayani@baf6b65b-9124-4dc3-b1c0-01155b4af546>
2010-10-08 21:57:34 +0000

    Updated test for Photosynth API
    git-svn-id: https://repository.eol.org/eol_php_code/trunk@1385 baf6b65b-9124-4dc3-b1c0-01155b4af546

--- tests/unit/test_connector_photosynth_api.php
+++ tests/unit/test_connector_photosynth_api.php
@@ -5,16 +5,21 @@ require_library('connectors/PhotosynthAPI');
 class test_connector_photosynth_api extends SimpletestUnitBase
 {
     function testPhotosynthAPI()
-    {
-        $taxa = PhotosynthAPI::get_photosynth_taxa(1, 0);
-        $this->assertTrue(is_array($taxa), 'Taxa should be an array');
+    {        
+
+        $id=array();
+        $id["2631672a-0280-4fb2-83fe-1a6cbe33f666"]=true;
+        $arr = PhotosynthAPI::get_photosynth_taxa(100, 0, $id);
+        $taxa = $arr[0];        
         
-        $taxon = $taxa[0];
+        $this->assertTrue(is_array($taxa), 'Taxa should be an array');                
+        
+        $taxon = @$taxa[0];
         $this->assertIsA($taxon, 'SchemaTaxon', 'Response should be an array of SchemaTaxon');
         
         $dataObject = $taxon->dataObjects[0];
-        $this->assertIsA($dataObject, 'SchemaDataObject', 'Taxon should have a data object');
+        $this->assertIsA($dataObject, 'SchemaDataObject', 'Taxon should have a data object');        
+    
     }
 }
-
 ?>
