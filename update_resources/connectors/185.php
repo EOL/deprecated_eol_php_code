6d02209150095801b0f2d7bffb60320049a502cc
eagbayani <eagbayani@baf6b65b-9124-4dc3-b1c0-01155b4af546>
2010-10-15 18:54:50 +0000

    New connector - Turbellarian database.
    git-svn-id: https://repository.eol.org/eol_php_code/trunk@1396 baf6b65b-9124-4dc3-b1c0-01155b4af546

---
+++ update_resources/connectors/185.php
@@ -0,0 +1,30 @@
+<?php
+//exit; 
+/* connector for Turbellarian
+estimated execution time: 2 hrs.
+This connector gets data from website and rank information from a csv file.
+*/
+
+$timestart = microtime(1);
+
+include_once(dirname(__FILE__) . "/../../config/environment.php");
+require_library('connectors/TurbellarianAPI');
+$GLOBALS['ENV_DEBUG'] = false;
+
+$taxa = TurbellarianAPI::get_all_taxa();
+$xml = SchemaDocument::get_taxon_xml($taxa);
+
+$resource_path = CONTENT_RESOURCE_LOCAL_PATH . "185.xml";
+
+$OUT = fopen($resource_path, "w+");
+fwrite($OUT, $xml);
+fclose($OUT);
+
+$elapsed_time_sec = microtime(1)-$timestart;
+echo "\n";
+echo "elapsed time = $elapsed_time_sec sec              \n";
+echo "elapsed time = " . $elapsed_time_sec/60 . " min   \n";
+echo "elapsed time = " . $elapsed_time_sec/60/60 . " hr \n";
+exit("\n\n Done processing.");
+
+?>
