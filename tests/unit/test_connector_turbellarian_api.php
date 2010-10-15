cb21f2a7b1204890160a9a7e7284c64d0f2bbd5d
eagbayani <eagbayani@baf6b65b-9124-4dc3-b1c0-01155b4af546>
2010-10-15 18:54:00 +0000

    New connector - Turbellarian database.
    git-svn-id: https://repository.eol.org/eol_php_code/trunk@1394 baf6b65b-9124-4dc3-b1c0-01155b4af546

---
+++ tests/unit/test_connector_turbellarian_api.php
@@ -0,0 +1,29 @@
+<?php
+
+require_library('connectors/TurbellarianAPI');
+
+class test_connector_turbellarian_api extends SimpletestUnitBase
+{
+    function testTurbellarianAPI()
+    {
+        $arr_input = Array
+        (
+            'Proporidae',
+            'Graff, 1882',
+            '<a href="/turb2.php?action=15|code=401">diagnosis</a>',
+            '<a href="/turb2.php?action=11|code=401|syn=0">literature</a>',
+            'http://turbellaria.umaine.edu/turb2.php?action=1&code=401'
+        );
+    
+        $arr = TurbellarianAPI::get_turbellarian_taxa($arr_input,array());
+        $taxa = $arr[0];
+        $this->assertTrue(is_array($taxa), 'Taxa should be an array');
+        
+        $taxon = $taxa[0];
+        $this->assertIsA($taxon, 'SchemaTaxon', 'Response should be an array of SchemaTaxon');
+        
+        $dataObject = $taxon->dataObjects[0];
+        $this->assertIsA($dataObject, 'SchemaDataObject', 'Taxon should have a data object');
+    }
+}
+?>
