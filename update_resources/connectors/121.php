68b5860e28af6d1fe3318a111b0ff1668c835aae
eagbayani <eagbayani@baf6b65b-9124-4dc3-b1c0-01155b4af546>
2010-10-15 16:46:08 +0000

    Enable connector.
    git-svn-id: https://repository.eol.org/eol_php_code/trunk@1392 baf6b65b-9124-4dc3-b1c0-01155b4af546

--- update_resources/connectors/121.php
+++ update_resources/connectors/121.php
@@ -3,9 +3,7 @@
 estimated execution time: 16-20 seconds
 Connector screen scrapes the partner website.
 */
-exit;
 $timestart = microtime(1);
-
 include_once(dirname(__FILE__) . "/../../config/environment.php");
 require_library('connectors/HydrothermalVentLarvaeAPI');
 $GLOBALS['ENV_DEBUG'] = false;
@@ -18,13 +16,10 @@ $OUT = fopen($resource_path, "w+");
 fwrite($OUT, $xml);
 fclose($OUT);
 
-//echo "time: ". Functions::time_elapsed() ."\n";
-
 $elapsed_time_sec = microtime(1)-$timestart;
 echo "\n";
 echo "elapsed time = $elapsed_time_sec sec              \n";
 echo "elapsed time = " . $elapsed_time_sec/60 . " min   \n";
 echo "elapsed time = " . $elapsed_time_sec/60/60 . " hr \n";
 exit("\n\n Done processing.");
-
 ?>
