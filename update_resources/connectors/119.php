1b30d0c2d223a82eac28f4edadec5649c8a2f79f
eagbayani <eagbayani@baf6b65b-9124-4dc3-b1c0-01155b4af546>
2010-10-21 18:03:51 +0000

    Enable Photosynth connector.
    git-svn-id: https://repository.eol.org/eol_php_code/trunk@1404 baf6b65b-9124-4dc3-b1c0-01155b4af546

--- update_resources/connectors/119.php
+++ update_resources/connectors/119.php
@@ -1,25 +1,26 @@
 <?php
 //exit; 
 /* connector for Photosynth
-estimated execution time: 
+estimated execution time: 29 secs.
 
 This connector will use an un-official service to search the Photosynth server.
 It also scrapes the Photosynth site to get the tags entered by owners as the tags where not included in the service.
 */
 
+$timestart = microtime(1);
 include_once(dirname(__FILE__) . "/../../config/environment.php");
 require_library('connectors/PhotosynthAPI');
 $GLOBALS['ENV_DEBUG'] = false;
-
 $taxa = PhotosynthAPI::get_all_taxa();
 $xml = SchemaDocument::get_taxon_xml($taxa);
-
 $resource_path = CONTENT_RESOURCE_LOCAL_PATH . "119.xml";
-
 $OUT = fopen($resource_path, "w+");
 fwrite($OUT, $xml);
 fclose($OUT);
-
-echo "\n time: ". Functions::time_elapsed();
-
+$elapsed_time_sec = microtime(1)-$timestart;
+echo "\n";
+echo "elapsed time = $elapsed_time_sec sec              \n";
+echo "elapsed time = " . $elapsed_time_sec/60 . " min   \n";
+echo "elapsed time = " . $elapsed_time_sec/60/60 . " hr \n";
+exit("\n\n Done processing.");
 ?>
