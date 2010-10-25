e66cb2e4faba801c72356b8562a0b42ea1b29cb3
eagbayani <eagbayani@baf6b65b-9124-4dc3-b1c0-01155b4af546>
2010-10-25 17:14:01 +0000

    Filtered species without maps in www.aquamaps.org
    git-svn-id: https://repository.eol.org/eol_php_code/trunk@1407 baf6b65b-9124-4dc3-b1c0-01155b4af546

--- update_resources/connectors/123.php
+++ update_resources/connectors/123.php
@@ -1,26 +1,29 @@
 <?php
-exit;
+//exit;
 /* connector for AquaMaps
-estimated execution time: 
+estimated execution time: 1.7 hrs.
 
 This connector reads an XML (list of species with AquaMaps) then loops on each species 
-and access the external service (provide by AquaMaps) to get the distribution maps.
+and accesses the external service (provide by AquaMaps) to get the distribution maps.
 
 Maps are shown in the Distribution section.
 */
 
+$timestart = microtime(1);
 include_once(dirname(__FILE__) . "/../../config/environment.php");
 require_library('connectors/AquaMapsAPI');
 $GLOBALS['ENV_DEBUG'] = false;
-
 $taxa = AquamapsAPI::get_all_taxa();
 $xml = SchemaDocument::get_taxon_xml($taxa);
-
 $resource_path = CONTENT_RESOURCE_LOCAL_PATH . "123.xml";
-
 $OUT = fopen($resource_path, "w+");
 fwrite($OUT, $xml);
 fclose($OUT);
 
-echo "time: ". Functions::time_elapsed()."\n";
+$elapsed_time_sec = microtime(1)-$timestart;
+echo "\n";
+echo "elapsed time = $elapsed_time_sec sec              \n";
+echo "elapsed time = " . $elapsed_time_sec/60 . " min   \n";
+echo "elapsed time = " . $elapsed_time_sec/60/60 . " hr \n";
+exit("\n\n Done processing.");
 ?>
