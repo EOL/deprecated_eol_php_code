df3409d3d3799ad6f384d12cd1e6691f35237780
eagbayani <eagbayani@baf6b65b-9124-4dc3-b1c0-01155b4af546>
2010-12-03 21:11:42 +0000

    Connector now uses partner customized XML. Clean code.
    git-svn-id: https://repository.eol.org/eol_php_code/trunk@1439 baf6b65b-9124-4dc3-b1c0-01155b4af546

--- lib/connectors/NaturalHistoryServicesAPI.php
+++ lib/connectors/NaturalHistoryServicesAPI.php
@@ -1,24 +1,8 @@
 <?php
 
-//define("NHS_DOC_1", "http://127.0.0.1/eol_php_code/update_resources/connectors/files/NaturalHistoryServices/nhsjumpers_videositemap.xml");
-
-/*
-define("NHS_DOC_1", DOC_ROOT . "update_resources/connectors/files/NaturalHistoryServices/nhsjumpers_videositemap.xml");
-define("NHS_DOC_2", DOC_ROOT . "update_resources/connectors/files/NaturalHistoryServices/nhswasps_videositemap.xml");
-define("NHS_DOC_3", DOC_ROOT . "update_resources/connectors/files/NaturalHistoryServices/nhsjumpers_videositemap.xml");
-*/
-
-///*
 define("NHS_DOC_1", "http://www.rkwalton.com/nhsjumpers_videositemap.xml");
 define("NHS_DOC_2", "http://www.rkwalton.com/nhswasps_videositemap.xml");
 define("NHS_DOC_3", "http://www.rkwalton.com/nhswaspstwo_videositemap.xml");
-//*/
-
-/*
-define("NHS_DOC_1", DOC_ROOT . "update_resources/connectors/files/NaturalHistoryServices/nhsjumpers_videositemap_small.xml");
-define("NHS_DOC_2", DOC_ROOT . "update_resources/connectors/files/NaturalHistoryServices/nhswasps_videositemap.xml");
-define("NHS_DOC_3", DOC_ROOT . "update_resources/connectors/files/NaturalHistoryServices/nhsjumpers_videositemap.xml");
-*/
 
 class NaturalHistoryServicesAPI
 {
@@ -95,116 +79,8 @@ class NaturalHistoryServicesAPI
     public static function search_collections($url1,$ancestry)//this will output the raw (but structured) output from the external service
     {
         $response = self::scrape_species_page($url1,$ancestry);        
-        return $response;//structured array
+        return $response;
     }        
-
-    public static function get_location_detail($str)
-    {
-        $arr_location_detail=array();                
-        $pos = stripos($str,"Collection Points\n</h3>");
-        if(!is_numeric($pos))return $arr_location_detail; //"has no location";
-        $str = substr($str,$pos,strlen($str));
-        
-        //start special case        
-        $str = str_ireplace('<br />\n<br />' , '<br />', $str);	
-        $str = str_ireplace('© Richard K. Walton' , '', $str);	
-        //end special case        
-        
-        $str = str_ireplace('<br />' , 'xxx<br />', $str);	
-        $str = str_ireplace('xxx' , "&arr[]=", $str);	
-        $arr = array(); parse_str($str);	            
-
-        foreach($arr as $r)
-        {
-            $arr_temp = explode("-", $r);            
-            //
-            $id = strip_tags(trim(@$arr_temp[0]));        
-            $id = str_ireplace(array(" ", "\n", "\r", "\t", "\o", "\xOB","\xA0", "\xAO","\xB0", '\xa0', chr(13), chr(10), '\xaO'), '', $id);			
-            $detail = self::clean_str(strip_tags(trim(@$arr_temp[1])));
-            $arr_location_detail["$id"]=$detail;
-        }    
-        return $arr_location_detail;
-    }    
-    
-    public static function get_video_detail($str,$arr_location_detail)
-    {
-        $arr_video_detail=array();                
-        
-        //start special case        
-        $str = str_ireplace('&amp;' , '&', $str);	
-        $str = str_ireplace('36 a&b' , '36a&b', $str);	        
-        $str = str_ireplace('39 a&b' , '39a&b', $str);	        
-        $str = str_ireplace('62 a&b' , '62a&b', $str);	        
-        $str = str_ireplace('63 a&b' , '63a&b', $str);	        
-        $str = str_ireplace('64 a&b' , '64a&b', $str);	        
-        $str = str_ireplace('69 a&b' , '69a&b', $str);	        
-        //end special case        
-        
-        $str = str_ireplace('&' , '__', $str);	                
-        $str = str_ireplace('<tr' , 'xxx<tr', $str);	
-        $str = str_ireplace('xxx' , "&arr[]=", $str);	
-        $arr = array(); parse_str($str);	            
-        foreach($arr as $r)
-        {
-            $temp = str_ireplace('</td>' , '|||', $r);	
-            $temp = strip_tags($temp);
-            $temp = str_ireplace('__' , '&', $temp);            
-            $arr_temp = explode("|||", $temp);                        
-            $id = trim($arr_temp[0]);
-            $location_code = trim($arr_temp[4]);
-            $arr_video_detail["$id"]=array("date_acquired"=>trim($arr_temp[2]),
-                                           "location"=>trim($arr_temp[3]), 
-                                           "location_code"=>$location_code,
-                                           "location_detail"=>@$arr_location_detail["$location_code"]
-                                          );            
-        }
-        return $arr_video_detail;
-    }
-    
-    public static function get_video_info($str,$arr_video_detail)
-    {
-        $arr_video_info=array();        
-        //start special case        
-        $str = str_ireplace('(R:107.108)' , '(R:107,108)', $str);	                
-        $str = str_ireplace('(:' , '(', $str);	                
-        $str = str_ireplace('1a&b' , '1a,1b', $str);	        
-        $str = str_ireplace('9a&b' , '9a,9b', $str);	        
-        $str = str_ireplace('20a&b' , '20a,20b', $str);	        
-        //end special case
-        
-        $str = str_ireplace('&' , '__', $str);	        
-        $str = str_ireplace('<li>' , 'xxx<li>', $str);	
-        $str = str_ireplace('xxx' , "&arr[]=", $str);	
-        $arr = array(); parse_str($str);	            
-        $i=0;
-        foreach($arr as $r)
-        {
-            $sciname=""; $rec_nos=""; $i++;
-            if(preg_match("/<em>(.*?)<\/em>/", $r, $matches))$sciname = $matches[1];                        
-            if(preg_match("/\((.*?)\)/", $r, $matches))$rec_nos = $matches[1];                                    
-            $rec_nos = str_ireplace('R:' , '', $rec_nos);	
-            $rec_nos = str_ireplace('__' , '&', $rec_nos);	//2            
-            $arr_rec_nos = explode(",", $rec_nos);                         
-            //start <br> delimited locations
-            $location="";
-            foreach($arr_rec_nos as $rec_no)
-            {
-                $location .= "date acquired: " . @$arr_video_detail["$rec_no"]["date_acquired"] . " | "
-                             . @$arr_video_detail["$rec_no"]["location"] . ", "
-                             . @$arr_video_detail["$rec_no"]["location_detail"] 
-                             . "<br> <br>";
-            }   
-            //end <br> delimited locations            
-            
-            $arr_video_info[]=array("sciname"=>$sciname,
-                                    "rec_nos"=>$arr_rec_nos,
-                                    "locations"=>$location,
-                                    "used"=>""
-                                   );
-            
-        }
-        return $arr_video_info;
-    }
     
     public static function scrape_species_page($url1,$ancestry)
     {
@@ -276,8 +152,7 @@ class NaturalHistoryServicesAPI
         require_library('XLSParser');
         $parser = new XLSParser();
         //$arr = $parser->convert_sheet_to_array("files/NaturalHistoryServices/Acknowledgments.xls");
-        $arr = $parser->convert_sheet_to_array(DOC_ROOT . "update_resources/connectors/files/NaturalHistoryServices/Acknowledgments.xls");
-                
+        $arr = $parser->convert_sheet_to_array(DOC_ROOT . "update_resources/connectors/files/NaturalHistoryServices/Acknowledgments.xls");                
         
         $acknowledgement=array();
         $k=0;
@@ -290,7 +165,6 @@ class NaturalHistoryServicesAPI
             }
             $k++;
         }                
-        //print"<pre>";print_r($acknowledgement);print"</pre>";
         return $acknowledgement;
     }
     public static function get_acknowledgement($sciname, $arr)
@@ -307,24 +181,6 @@ class NaturalHistoryServicesAPI
         else return;
     }    
 
-    public static function get_location($arr_video_info,$sciname)
-    {
-        $location="";
-        $i=0;
-        foreach($arr_video_info as $rec)
-        {            
-            if($rec["sciname"] == $sciname && $rec["used"] != 1)
-            {
-                $location = $rec["locations"];
-                $arr_video_info[$i]["used"]=1;                
-                return array($location,$arr_video_info);
-            }
-            
-            $i++;
-        }    
-        return array($location,$arr_video_info);
-    }
-       
     public static function get_taxa_for_photo($rec)
     {
         $taxon = array();
@@ -390,26 +246,6 @@ class NaturalHistoryServicesAPI
          }
         return $data_object_parameters;
     }    
-
-    public static function clean_html($str)
-    {
-        $str = str_ireplace('&ldquo;','',$str);//special open quote
-        $str = str_ireplace('&rdquo;','',$str);//special end quote
-        $str = str_ireplace('&micro;','µ',$str);
-        $str = str_ireplace('&mu;' , 'µ', $str);	            
-        $str = str_ireplace('&ndash;','-',$str);
-        $str = str_ireplace('&deg;' , '°', $str);	        
-        $str = str_ireplace('&rsquo;' , "'", $str);	        
-        $str = str_ireplace('&gt;' , ">", $str);	        
-        $str = str_ireplace('&lt;' , "<", $str);	                
-        return $str;
-    }
     
-    public static function clean_str($str)
-    {    
-        $str = str_ireplace(array("\r", "\t", "\o"), '', $str);			
-        $str = str_ireplace(array("\n" ), ' ', $str);			        
-        return $str;
-    }
 }
 ?>
