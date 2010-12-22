ff786180882c9d0a8b2c08dd0849bc236f5a382a
eagbayani <eagbayani@baf6b65b-9124-4dc3-b1c0-01155b4af546>
2010-12-22 00:05:26 +0000

    New, MCZ Harvard connector.
    git-svn-id: https://repository.eol.org/eol_php_code/trunk@1459 baf6b65b-9124-4dc3-b1c0-01155b4af546

---
+++ update_resources/connectors/201.php
@@ -0,0 +1,34 @@
+<?php
+/* connector for MCZ Harvard
+estimated execution time: 5.3 mins.
+Partner provides a CSV file.      
+    - download the CSV from remote 
+    - add the label headers
+    - then start processing...
+
+Reminders: 
+- In CSV, there are some entries that starts with '=' the equal sign - can be mistaked to be an excel formula.
+- Connector can be improved by reading the CSV using fopen() and don't use PHPExcel library.
+*/
+//exit;
+
+$timestart = microtime(1);
+include_once(dirname(__FILE__) . "/../../config/environment.php");
+require_library('connectors/MCZHarvardAPI');
+$GLOBALS['ENV_DEBUG'] = false;
+
+$taxa = MCZHarvardAPI::get_all_taxa();
+$xml = SchemaDocument::get_taxon_xml($taxa);
+
+$resource_path = CONTENT_RESOURCE_LOCAL_PATH . "201.xml";
+$OUT = fopen($resource_path, "w+");
+fwrite($OUT, $xml);
+fclose($OUT);
+
+$elapsed_time_sec = microtime(1)-$timestart;
+echo "\n";
+echo "elapsed time = $elapsed_time_sec sec              \n";
+echo "elapsed time = " . $elapsed_time_sec/60 . " min   \n";
+echo "elapsed time = " . $elapsed_time_sec/60/60 . " hr \n";
+exit("\n\n Done processing.");
+?>
