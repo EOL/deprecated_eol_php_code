e0c99b6de616fc17601c6adad39b75a2b8f4d4fb
eagbayani <eagbayani@baf6b65b-9124-4dc3-b1c0-01155b4af546>
2010-12-31 16:18:10 +0000

    Update taxon page metrics.
    git-svn-id: https://repository.eol.org/eol_php_code/trunk@1478 baf6b65b-9124-4dc3-b1c0-01155b4af546

--- applications/taxon_page_metrics/generate_taxon_page_metrics.php
+++ applications/taxon_page_metrics/generate_taxon_page_metrics.php
@@ -1,12 +1,11 @@
 <?php
-$timestart = microtime(1);
-$temp_time = time_elapsed();            
-
 $GLOBALS['ENV_DEBUG'] = false;
 $GLOBALS['ENV_NAME'] = "integration";
 //$GLOBALS['ENV_NAME'] = "staging";
 
 include_once(dirname(__FILE__) . "/../../config/environment.php");
+$timestart = microtime(1);
+$temp_time = time_elapsed();            
 
 ini_set('memory_limit', '1500M');
 set_time_limit(0);
@@ -17,8 +16,8 @@ $stats = new SiteStatistics_v4();
 
 /* can only be run when the latest migrations are run in production */
 
-//$stats->generate_taxon_concept_with_bhl_links_textfile(); //5.4 mins
-//$stats->generate_taxon_concept_with_bhl_publications_textfile(); // singe-query approach --- should be run ahead of time
+//$stats->generate_taxon_concept_with_bhl_links_textfile(); //1.89 mins      
+$stats->generate_taxon_concept_with_bhl_publications_textfile(); // singe-query approach --- should be run ahead of time
 //$stats->generate_taxon_concept_with_bhl_publications_textfile2(); // multi-query approach --- proves to be slower than single-query approach
 //$stats->create_page_metrics_table(); //1.5 hrs
        
@@ -28,7 +27,7 @@ $time_elapsed_sec = time_elapsed() - $temp_time;
 echo "\n elapsed time = $elapsed_time_sec sec               ";
 echo "\n elapsed time = " . $elapsed_time_sec/60 . " mins   ";
 echo "\n elapsed time = " . $elapsed_time_sec/60/60 . " hrs ";
-
+echo "\n";
 echo "\n elapsed time = $time_elapsed_sec sec               ";
 echo "\n elapsed time = " . $time_elapsed_sec/60 . " mins   ";
 echo "\n elapsed time = " . $time_elapsed_sec/60/60 . " hrs ";
