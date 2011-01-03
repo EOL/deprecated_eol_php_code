219d5e11c2aa031d8a5759253a4b2e296615d7ea
pleary <pleary@baf6b65b-9124-4dc3-b1c0-01155b4af546>
2011-01-03 19:49:21 +0000

    Added two simple rake tasks from Beast
    git-svn-id: https://repository.eol.org/eol_php_code/trunk@1494 baf6b65b-9124-4dc3-b1c0-01155b4af546

---
+++ rake_tasks/compare_hierarchy_entry.php
@@ -0,0 +1,18 @@
+<?php
+
+$id = @$argv[1];
+
+if(!$id || !is_numeric($id))
+{
+    echo "\n\n\tcompare_hierarchy_entries.php [id]\n\n";
+    exit;
+}
+
+
+include_once(dirname(__FILE__) . "/../config/environment.php");
+$mysqli =& $GLOBALS['mysqli_connection'];
+
+CompareHierarchies::test_compare_single_entry($id);
+
+
+?>
---
+++ rake_tasks/partner_reports_emailer.php
@@ -0,0 +1,5 @@
+<?php
+
+file_get_contents("http://www.eol.org/email_actions/send_emails");
+
+?>
