851b2b9119f3d38384254d7800d92db99118d5f8
eagbayani <eagbayani@baf6b65b-9124-4dc3-b1c0-01155b4af546>
2011-01-05 17:38:31 +0000

    Generate taxon_concept with no. of BHL publications text file.
    git-svn-id: https://repository.eol.org/eol_php_code/trunk@1502 baf6b65b-9124-4dc3-b1c0-01155b4af546

---
+++ applications/taxon_page_metrics/generate_bhl_text_files.php
@@ -0,0 +1,30 @@
+<?php
+$GLOBALS['ENV_DEBUG'] = false;
+//$GLOBALS['ENV_NAME'] = "integration";
+//$GLOBALS['ENV_NAME'] = "staging";
+
+include_once(dirname(__FILE__) . "/../../config/environment.php");
+$timestart = microtime(1);
+$temp_time = time_elapsed();            
+
+ini_set('memory_limit', '1500M');
+set_time_limit(0);
+
+$mysqli =& $GLOBALS['mysqli_connection'];
+require_library('SiteStatistics');
+$stats = new SiteStatistics();
+
+$stats->generate_taxon_concept_with_bhl_links_textfile();           //24 mins      
+$stats->generate_taxon_concept_with_bhl_publications_textfile();    //10 hrs single-query approach --- should be run ahead of time
+       
+$elapsed_time_sec = microtime(1)-$timestart;
+$time_elapsed_sec = time_elapsed() - $temp_time;
+
+echo "\n elapsed time = $elapsed_time_sec sec               ";
+echo "\n elapsed time = " . $elapsed_time_sec/60 . " mins   ";
+echo "\n elapsed time = " . $elapsed_time_sec/60/60 . " hrs ";
+echo "\n";
+echo "\n elapsed time = $time_elapsed_sec sec               ";
+echo "\n elapsed time = " . $time_elapsed_sec/60 . " mins   ";
+echo "\n elapsed time = " . $time_elapsed_sec/60/60 . " hrs ";
+?>
--- applications/taxon_page_metrics/generate_taxon_page_metrics.php
+++ applications/taxon_page_metrics/generate_taxon_page_metrics.php
@@ -1,33 +1,23 @@
 <?php
 $GLOBALS['ENV_DEBUG'] = false;
-$GLOBALS['ENV_NAME'] = "integration";
+//$GLOBALS['ENV_NAME'] = "integration";
 //$GLOBALS['ENV_NAME'] = "staging";
 
 include_once(dirname(__FILE__) . "/../../config/environment.php");
-$timestart = microtime(1);
-$temp_time = time_elapsed();            
+$time_start = time_elapsed();            
 
 ini_set('memory_limit', '1500M');
 set_time_limit(0);
 
 $mysqli =& $GLOBALS['mysqli_connection'];
-require_library('SiteStatistics_v4');
-$stats = new SiteStatistics_v4();
+require_library('SiteStatistics');
+$stats = new SiteStatistics();
 
-/* can only be run when the latest migrations are run in production */
+/* can only be run when the latest migrations were already run in production */
 
-//$stats->generate_taxon_concept_with_bhl_links_textfile(); //1.89 mins      
-$stats->generate_taxon_concept_with_bhl_publications_textfile(); // singe-query approach --- should be run ahead of time
-//$stats->generate_taxon_concept_with_bhl_publications_textfile2(); // multi-query approach --- proves to be slower than single-query approach
-//$stats->create_page_metrics_table(); //1.5 hrs
+$stats->create_page_metrics_table(); //1.5 hrs
        
-$elapsed_time_sec = microtime(1)-$timestart;
-$time_elapsed_sec = time_elapsed() - $temp_time;
-
-echo "\n elapsed time = $elapsed_time_sec sec               ";
-echo "\n elapsed time = " . $elapsed_time_sec/60 . " mins   ";
-echo "\n elapsed time = " . $elapsed_time_sec/60/60 . " hrs ";
-echo "\n";
+$time_elapsed_sec = time_elapsed() - $time_start;
 echo "\n elapsed time = $time_elapsed_sec sec               ";
 echo "\n elapsed time = " . $time_elapsed_sec/60 . " mins   ";
 echo "\n elapsed time = " . $time_elapsed_sec/60/60 . " hrs ";
