975d6a3734cbafbdde30f939895875e12d9841ec
eagbayani <eagbayani@baf6b65b-9124-4dc3-b1c0-01155b4af546>
2011-01-12 15:04:29 +0000

    Fixed fatal memory limit error and execution time now is 2.23 hrs.
    git-svn-id: https://repository.eol.org/eol_php_code/trunk@1547 baf6b65b-9124-4dc3-b1c0-01155b4af546

--- applications/taxon_page_metrics/generate_taxon_page_metrics.php
+++ applications/taxon_page_metrics/generate_taxon_page_metrics.php
@@ -1,21 +1,18 @@
 <?php
-$GLOBALS['ENV_DEBUG'] = false;
-//$GLOBALS['ENV_NAME'] = "integration";
+$GLOBALS['ENV_DEBUG'] = true;
 //$GLOBALS['ENV_NAME'] = "staging";
 
 include_once(dirname(__FILE__) . "/../../config/environment.php");
 $time_start = time_elapsed();            
 
-ini_set('memory_limit', '2500M');
+ini_set('memory_limit', '1500M');
 set_time_limit(0);
 
 $mysqli =& $GLOBALS['mysqli_connection'];
 require_library('SiteStatistics');
 $stats = new SiteStatistics();
 
-/* can only be run when the latest migrations were already run in production */
-
-$stats->create_page_metrics_table(); //1.08 hrs
+$stats->create_page_metrics_table(); //2.23 hrs
        
 $time_elapsed_sec = time_elapsed() - $time_start;
 echo "\n elapsed time = $time_elapsed_sec sec               ";
