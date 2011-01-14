a69b559653d7903df70d483c88b553e17f94efd4
pleary <pleary@baf6b65b-9124-4dc3-b1c0-01155b4af546>
2011-01-14 20:53:36 +0000

    Added tests for new iterators
    git-svn-id: https://repository.eol.org/eol_php_code/trunk@1564 baf6b65b-9124-4dc3-b1c0-01155b4af546

---
+++ tests/unit/test_file_iterator.php
@@ -0,0 +1,53 @@
+<?php
+
+require_library('FileIterator');
+
+class test_file_iterator extends SimpletestUnitBase
+{
+    function testFileIterator()
+    {
+        $temp_filepath = temp_filepath();
+        $this->assertFalse(file_exists($temp_filepath), 'File shouldnt exist yet');
+        
+        $file_lines = array();
+        $file_lines[] = "first line";
+        $file_lines[] = "second\tline";
+        $file_lines[] = "   third  line    \t";
+        $file_lines[] = "   ";
+        $file_lines[] = "";
+        
+        $FILE = fopen($temp_filepath, "w+");
+        fwrite($FILE, implode("\n", $file_lines));
+        fclose($FILE);
+        
+        foreach(new FileIterator($temp_filepath) as $line_number => $line)
+        {
+            $this->assertTrue($line == $file_lines[$line_number], 'All lines should be the same');
+        }
+        
+        $this->assertTrue(file_exists($temp_filepath), 'File should still exist');
+        unlink($temp_filepath);
+    }
+    
+    function testFileIteratorDestroy()
+    {
+        $file_lines = array();
+        $file_lines[] = "first line";
+        $file_lines[] = "second line";
+        $file_lines[] = "third line";
+        
+        $temp_filepath = temp_filepath();
+        $FILE = fopen($temp_filepath, "w+");
+        fwrite($FILE, implode("\n", $file_lines));
+        fclose($FILE);
+        
+        foreach(new FileIterator($temp_filepath, true) as $line_number => $line)
+        {
+            $this->assertTrue($line == $file_lines[$line_number], 'All lines should be the same');
+        }
+        
+        $this->assertFalse(file_exists($temp_filepath), 'File should have been removed');
+    }
+}
+
+?>
---
+++ tests/unit/test_mysqli_iterator.php
@@ -0,0 +1,47 @@
+<?php
+
+require_library('MysqliResultIterator');
+require_library('MysqliResultFileIterator');
+
+class test_mysqli_iterator extends SimpletestUnitBase
+{
+    function testMysqliResultIterator()
+    {
+        $result = $GLOBALS['db_connection']->query("SELECT * FROM names");
+        $this->assertTrue($result, 'We should get a result');
+        $this->assertTrue($result->num_rows == 0, 'But it should be empty');
+        
+        $names_to_add = array();
+        $names_to_add[] = "Aus bus";
+        $names_to_add[] = "   Cud dus  ";
+        $names_to_add[] = "abc";
+        $names_to_add[] = "DEF";
+        $names_to_add[] = " GHI";
+        
+        foreach($names_to_add as $name)
+        {
+            $GLOBALS['db_connection']->insert("INSERT IGNORE INTO names (string) VALUES ('$name')");
+        }
+        
+        $result = $GLOBALS['db_connection']->query("SELECT * FROM names");
+        $this->assertTrue($result->num_rows == count($names_to_add), 'Should get as many rows as names added');
+        
+        foreach(new MysqliResultIterator($result) as $row_num => $row)
+        {
+            $this->assertTrue($row['string'] == $names_to_add[$row_num-1], 'Names should be the same as what was inserted');
+        }
+        
+        foreach($GLOBALS['db_connection']->iterate("SELECT * FROM names") as $row_num => $row)
+        {
+            $this->assertTrue($row['string'] == $names_to_add[$row_num-1], 'Names should be the same as what was inserted');
+        }
+        
+        foreach($GLOBALS['db_connection']->iterate_file("SELECT string FROM names") as $row_num => $row)
+        {
+            $this->assertTrue($row[0] == $names_to_add[$row_num], 'Names should be the same as what was inserted');
+        }
+        
+    }
+}
+
+?>
