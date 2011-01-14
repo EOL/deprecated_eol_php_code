f24cf254e763dba239d3674e2cfb6ec677461e10
eagbayani <eagbayani@baf6b65b-9124-4dc3-b1c0-01155b4af546>
2011-01-14 17:39:10 +0000

    Vimeo connector (new).
    git-svn-id: https://repository.eol.org/eol_php_code/trunk@1556 baf6b65b-9124-4dc3-b1c0-01155b4af546

---
+++ update_resources/connectors/214.php
@@ -0,0 +1,25 @@
+<?php
+/* connector for Vimeo 
+estimated execution time: a few seconds
+*/
+
+$timestart = microtime(1);
+include_once(dirname(__FILE__) . "/../../config/environment.php");
+require_library('connectors/VimeoAPI');
+$GLOBALS['ENV_DEBUG'] = true;
+
+$taxa = VimeoAPI::get_all_taxa();
+$xml = SchemaDocument::get_taxon_xml($taxa);
+
+$resource_path = CONTENT_RESOURCE_LOCAL_PATH . "214.xml";
+$OUT = fopen($resource_path, "w");
+fwrite($OUT, $xml);
+fclose($OUT);
+
+$elapsed_time_sec = microtime(1)-$timestart;
+echo "\n";
+echo "elapsed time = $elapsed_time_sec sec              \n";
+echo "elapsed time = " . $elapsed_time_sec/60 . " min   \n";
+echo "elapsed time = " . $elapsed_time_sec/60/60 . " hr \n";
+exit("\n\n Done processing.");
+?>
