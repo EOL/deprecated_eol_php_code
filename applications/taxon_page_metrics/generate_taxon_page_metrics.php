9d5b32e02040659bbbf8043e490345249087e4e5
eagbayani <eagbayani@baf6b65b-9124-4dc3-b1c0-01155b4af546>
2011-01-15 14:51:51 +0000

    Logged execution time 1.5 hrs.
    git-svn-id: https://repository.eol.org/eol_php_code/trunk@1567 baf6b65b-9124-4dc3-b1c0-01155b4af546

--- applications/taxon_page_metrics/generate_taxon_page_metrics.php
+++ applications/taxon_page_metrics/generate_taxon_page_metrics.php
@@ -1,12 +1,12 @@
 <?php
 $GLOBALS['ENV_DEBUG'] = true;
-
 include_once(dirname(__FILE__) . "/../../config/environment.php");
+
 $time_start = time_elapsed();
 
 require_library('TaxonPageMetrics');
 $stats = new TaxonPageMetrics();
-$stats->insert_page_metrics(); //2.23 hrs
+$stats->insert_page_metrics(); //1.5 hrs
        
 $time_elapsed_sec = time_elapsed() - $time_start;
 echo "\n elapsed time = $time_elapsed_sec sec               ";
