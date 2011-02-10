c0bbdf0ad822a21d01ad5c2e8a7249ce03c91454
eagbayani <eagbayani@baf6b65b-9124-4dc3-b1c0-01155b4af546>
2011-02-10 21:02:37 +0000

    New. Connector for SPIRE.
    git-svn-id: https://repository.eol.org/eol_php_code/trunk@1631 baf6b65b-9124-4dc3-b1c0-01155b4af546

---
+++ update_resources/connectors/219.php
@@ -0,0 +1,28 @@
+<?php
+/* connector for SPIRE
+estimated execution time: 12 mins
+
+Connector accesses the OWL (XML) files from remote server for most of the data and uses a spreadsheet for taxonomy info.
+*/
+
+include_once(dirname(__FILE__) . "/../../config/environment.php");
+require_library('connectors/SpireAPI');
+$GLOBALS['ENV_DEBUG'] = false;
+
+$timestart = time_elapsed();
+
+$taxa = SpireAPI::get_all_taxa();
+$xml = SchemaDocument::get_taxon_xml($taxa);
+
+$resource_path = CONTENT_RESOURCE_LOCAL_PATH . "219.xml";
+$OUT = fopen($resource_path, "w");
+fwrite($OUT, $xml);
+fclose($OUT);
+
+$elapsed_time_sec = time_elapsed() - $timestart;
+echo "\n";
+echo "elapsed time = $elapsed_time_sec sec              \n";
+echo "elapsed time = " . $elapsed_time_sec/60 . " min   \n";
+echo "elapsed time = " . $elapsed_time_sec/60/60 . " hr \n";
+exit("\n\n Done processing.");
+?>
