a7093568cf9c15825f0ead81ae05fdae901b13a4
eagbayani <eagbayani@baf6b65b-9124-4dc3-b1c0-01155b4af546>
2011-02-10 21:05:38 +0000

    Test for SPIRE connector API.
    git-svn-id: https://repository.eol.org/eol_php_code/trunk@1633 baf6b65b-9124-4dc3-b1c0-01155b4af546

---
+++ tests/unit/test_connector_spire_api.php
@@ -0,0 +1,39 @@
+<?php
+require_library('connectors/SpireAPI');
+class test_connector_spire_api extends SimpletestUnitBase
+{
+    function testSpireAPI()
+    {            
+        $GLOBALS['arr_taxa']["genus species"] = Array
+             (
+                 "desc" => "my species",
+                 "objects" => Array
+                     (
+                         "predator" => Array("pred 1"),
+                         "prey" => Array("prey 1")        
+                     ),
+        
+                 "ancestry" => Array
+                     (
+                         "Family" => "family",
+                         "Order" => "order",
+                         "Class" => "class",
+                         "Phylum" => "phylum"
+                     )        
+             );
+             
+        $GLOBALS['arr_taxa']["pred 1"] = Array("desc" => "my predator", "objects" => Array(), "ancestry" => Array());
+        $GLOBALS['arr_taxa']["prey 1"] = Array("desc" => "my prey", "objects" => Array(), "ancestry" => Array());             
+                
+        $arr = SpireAPI::get_spire_taxa("genus species",array());
+        $taxa = $arr[0];
+        $this->assertTrue(is_array($taxa), 'Taxa should be an array');
+        
+        $taxon = $taxa[0];
+        $this->assertIsA($taxon, 'SchemaTaxon', 'Response should be an array of SchemaTaxon');
+        
+        $dataObject = $taxon->dataObjects[0];
+        $this->assertIsA($dataObject, 'SchemaDataObject', 'Taxon should have a data object');                            
+    }
+}
+?>
