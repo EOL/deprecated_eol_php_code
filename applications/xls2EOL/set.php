7d2b22cf89dd9a9cb3b62782971af7fb5ecba52d
eagbayani <eagbayani@baf6b65b-9124-4dc3-b1c0-01155b4af546>
2011-02-23 16:16:15 +0000

    For some reason the new marine stats were not appended to table 'page_stats_marine'. This is to manually add the new stats.
    git-svn-id: https://repository.eol.org/eol_php_code/trunk@1651 baf6b65b-9124-4dc3-b1c0-01155b4af546

---
+++ applications/xls2EOL/set.php
@@ -0,0 +1,35 @@
+<?php
+//use to set values in the database
+
+include_once(dirname(__FILE__) . "/../../config/environment.php");
+$mysqli =& $GLOBALS['mysqli_connection'];
+
+$wrap = "\n\n";
+
+/*
+$id     = 201; //MCZ
+$fld    = "resource_status_id";
+$value  = 11; //Publish Pending
+
+$id     = 214;  //Vimeo
+$fld    = "service_type_id";
+$value  = 2;
+
+$id     = 214; //Vimeo
+$fld    = "auto_publish";
+$value  = 1;
+
+$query="Select resources.$fld, resources.title, resources.id From resources Where resources.id = $id"; $result = $mysqli->query($query);    
+while($row=$result->fetch_assoc()){print "$row[$fld] -- $row[title] -- $wrap";}
+$query="update resources set $fld = '$value' Where id = $id"; $update = $mysqli->query($query);    
+$query="Select resources.$fld, resources.title, resources.id From resources Where resources.id = $id"; $result = $mysqli->query($query);    
+while($row=$result->fetch_assoc()){print "$row[$fld] -- $row[title] -- $wrap";}
+*/
+
+$date_created = date('Y-m-d');
+$time_created = date('H:i:s');
+$query="insert into page_stats_marine(names_from_xml,names_in_eol,marine_pages,pages_with_objects,pages_with_vetted_objects,date_created,time_created,active) select 207877,176007,176256,107302,100912,'$date_created','$time_created','n'";
+$update = $mysqli->query($qry);
+
+print "\n\n --end-- \n\n";
+?>
