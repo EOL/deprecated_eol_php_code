abee1d5968401b82e931fa78a57a8979697b492d
eagbayani <eagbayani@baf6b65b-9124-4dc3-b1c0-01155b4af546>
2011-02-25 22:16:23 +0000

    Cleaned some code but needs more cleaning. WIP.
    git-svn-id: https://repository.eol.org/eol_php_code/trunk@1656 baf6b65b-9124-4dc3-b1c0-01155b4af546

--- applications/names_stat/result.php
+++ applications/names_stat/result.php
@@ -7,16 +7,16 @@ $func = new NameStat();
 
 if(!isset($_REQUEST['return']))exit;
 
-$report         = 'list';	//original functionality
-$returns 	    = $_REQUEST['return'];
+$report         = 'list';    //original functionality
+$returns        = $_REQUEST['return'];
 $sort_order     = $_REQUEST['sort'];
 //$vetted         = $_REQUEST['vetted'];
 $sciname_4color = "";
 
-$list 			= $_REQUEST['list'];
-$separator 		= $_REQUEST['separator'];
-$choice 		= $_REQUEST['choice'];
-$withCSV 		= $_REQUEST['withCSV'];
+$list           = $_REQUEST['list'];
+$separator      = $_REQUEST['separator'];
+$choice         = $_REQUEST['choice'];
+$withCSV        = $_REQUEST['withCSV'];
 
 if(trim($choice) == "")
 {
@@ -24,37 +24,35 @@ if(trim($choice) == "")
     exit;
 }
 
-$rd	    = "";	//row data
-$cr 	= "\n";
+$rd     = "";    //row data
+$cr     = "\n";
 $tab    = chr(9);
 
 if($separator == '')
 {
-	switch (true)
-	{
-	case $choice == 1:  $separator = chr(13);break;
-	case $choice == 2:  $separator = chr(10);break;	
-	case $choice == 3:  $separator = chr(9);break;	
-	case $choice == 4:  $separator = ',';break;	
-	default:break;
-	}	
-}	
-$list = $separator . $list;	//weird behavior - first char must be the separator
-$arr = explode("$separator", $list);	
-$orig_lenth_of_arr = count($arr);	
+    switch (true)
+    {
+    case $choice == 1:  $separator = chr(13);break;
+    case $choice == 2:  $separator = chr(10);break;    
+    case $choice == 3:  $separator = chr(9);break;    
+    case $choice == 4:  $separator = ',';break;    
+    default:break;
+    }    
+}    
+$list = $separator . $list;    //weird behavior - first char must be the separator
+$arr = explode("$separator", $list);    
+$orig_lenth_of_arr = count($arr);    
 $arr = array_unique($arr);
-$arr = $func->array_trim($arr,$orig_lenth_of_arr);	// $orig_lenth_of_arr --- this is the length of array after explode function
+$arr = $func->array_trim($arr,$orig_lenth_of_arr);    // $orig_lenth_of_arr --- this is the length of array after explode function
 
 print "<font size='2' face='courier'>Total no. of names submitted: " . " " . count($arr) . "</font>";
 if(count($arr) == 0){exit;}
 print"<table cellpadding='3' cellspacing='0' border='1' style='font-size : small; font-family : Arial Unicode MS;'>";
-$us = "&#153;";	//unique separator
+$us = "&#153;";    //unique separator
 $value_list="";
 
-$domain="www.eol.org";
-
-$api_put_species="http://$domain/api/search/";
-$api_put_taxid_1="http://$domain/api/pages/";
+$api_put_species="http://www.eol.org/api/search/";
+$api_put_taxid_1="http://www.eol.org/api/pages/";
 $api_put_taxid_2="?images=75&text=75&subjects=all";
 //$api_put_taxid_2 .= "&vetted=$vetted";    
 
@@ -71,9 +69,11 @@ foreach($arr as $sciname)
 {
     $file = $api_put_species . urlencode($sciname);
     $xml = Functions::get_hashed_response($file);
-    $arr_details = $func->get_details($xml,$sciname);
+    print"<pre>";
+    $arr_details = $func->get_details($xml,$sciname);  
     $arr_details = $func->sort_details($arr_details,$returns);
     $arr_table = array_merge($arr_details,$arr_table);    
+    print"</pre>";
 }
 $arr_table = $func->sort_by_key($arr_table,"orig_sciname",$sort_order);
 $func->show_table($arr_table);
