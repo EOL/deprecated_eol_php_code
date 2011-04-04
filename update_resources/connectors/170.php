217ae318b4eae72781a679495019f52c2e1bc08e
eagbayani <eagbayani@baf6b65b-9124-4dc3-b1c0-01155b4af546>
2011-04-04 21:25:53 +0000

    Set Serpent connector to force_harvest when connector is done.
    git-svn-id: https://repository.eol.org/eol_php_code/trunk@1727 baf6b65b-9124-4dc3-b1c0-01155b4af546

--- update_resources/connectors/170.php
+++ update_resources/connectors/170.php
@@ -1,26 +1,33 @@
 <?php
-/* connector for Serpent
+/* connector for SERPENT
 estimated execution time: 23 mins.
 Connector screen scrapes the partner website.
 */
-$timestart = microtime(1);
 
 include_once(dirname(__FILE__) . "/../../config/environment.php");
+$timestart = time_elapsed();
+
 require_library('connectors/SerpentAPI');
 $GLOBALS['ENV_DEBUG'] = false;
 
 $taxa = SerpentAPI::get_all_taxa();
 $xml = SchemaDocument::get_taxon_xml($taxa);
 
-$resource_path = CONTENT_RESOURCE_LOCAL_PATH . "170.xml";
+$resource_id = 170;
+$resource_path = CONTENT_RESOURCE_LOCAL_PATH . $resource_id . ".xml";
 $OUT = fopen($resource_path, "w+");
 fwrite($OUT, $xml);
 fclose($OUT);
 
-$elapsed_time_sec = microtime(1)-$timestart;
+// set SERPENT to force harvest
+if(filesize(CONTENT_RESOURCE_LOCAL_PATH . $resource_id . ".xml"))
+{
+    $GLOBALS['db_connection']->update("UPDATE resources SET resource_status_id=" . ResourceStatus::insert('Force Harvest') . " WHERE id=" . $resource_id);
+}
+
+$elapsed_time_sec = time_elapsed() - $timestart;
 echo "\n";
-echo "elapsed time = $elapsed_time_sec sec              \n";
-echo "elapsed time = " . $elapsed_time_sec/60 . " min   \n";
-echo "elapsed time = " . $elapsed_time_sec/60/60 . " hr \n";
+echo "elapsed time = " . $elapsed_time_sec/60 . " minutes \n";
+echo "elapsed time = " . $elapsed_time_sec/60/60 . " hours \n";
 exit("\n\n Done processing.");
 ?>
