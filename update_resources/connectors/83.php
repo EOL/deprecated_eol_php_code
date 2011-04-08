f082f9095b2ca6c99d6c6bfd7ca581259a670790
eagbayani <eagbayani@baf6b65b-9124-4dc3-b1c0-01155b4af546>
2011-04-08 14:05:03 +0000

    Data-734, Data-756: Filter Bioimages Vanderbilt images from Morphbank feed to avoid duplication. Update MorphBank connector to use latest MorphBank service.
    git-svn-id: https://repository.eol.org/eol_php_code/trunk@1731 baf6b65b-9124-4dc3-b1c0-01155b4af546

--- update_resources/connectors/83.php
+++ update_resources/connectors/83.php
@@ -1,124 +1,116 @@
 <?php
-/* MorphBank connector 
+/* MorphBank connector
 estimated execution time: 6 hours
 Partner provides a list of URL's and each URL will list ID's.
 Then connector uses their service to read each ID and get the information needed.
 */
 
-$timestart = microtime(1);
-
-/* Temporarily not being used
-$url_id_list = "http://www.morphbank.net/eolids.xml";
-$url_id_list = "http://127.0.0.1/eol_php_code/update_resources/connectors/files/MorphBank/eolids.xml";
-$url_id_list = "http://pandanus.eol.org/public/content_partners/MorphBank/eolids.xml";
+/* Temporarily not being used, but maybe revived, depends on MorphBank
+$url_list_of_group_ids = "http://www.morphbank.net/eolids.xml";
 */
 
 include_once(dirname(__FILE__) . "/../../config/environment.php");
+$timestart = time_elapsed();
 $mysqli =& $GLOBALS['mysqli_connection'];
 $resource_id = 83;
-$details_method_prefix = "http://services.morphbank.net/mb/request?method=id&format=svc&limit=2&id=";
+$details_method_prefix = "http://services.morphbank.net/mb3/request?method=id&format=svc&limit=2&id=";
 $image_ids = array();
 $schema_taxa = array();
 $used_taxa = array();
-$url="http://services.morphbank.net/mb2/request?method=eol&format=id&limit=-1";
+$url_list_of_image_ids = "http://services.morphbank.net/mb3/request?method=eol&format=id&limit=-1";
 
-/* Excluded IDs as suggested by BioImages Vanderbuilt */
+/* Excludes MorphBank IDs as suggested by BioImages Vanderbuilt */
 $excluded_MorphBank_IDs = prepare_excluded_ids();
 
-if($url)
+if($url_list_of_image_ids)
 {
-    print "\n [$url] \n";
-    $url = trim($url);
-    if($image_id_xml = simplexml_load_file($url)){}
-    else continue;
-    foreach($image_id_xml->id as $id)
-    {
-        $image_ids[] = $id;
-    }
+    print "\n [$url_list_of_image_ids] \n";
+    if(!$image_id_xml = simplexml_load_file($url_list_of_image_ids)) continue;
+    foreach($image_id_xml->id as $id) $image_ids[] = $id;    
 }
+
 $total_image_ids = count($image_ids);
 print "\n count of image ID's = $total_image_ids";
 
 /* loop through image ids */
-$k=0;
+$k = 0;
 foreach($image_ids as $image_id)
 {
-    $k++;    
-    print "\n $image_id  [$k of $total_image_ids]";
-    $image_details_url = $details_method_prefix . $image_id;
-    if($xml = simplexml_load_file($image_details_url)){}
-    else continue;
-    $dwc = $xml->specimen->children("http://rs.tdwg.org/dwc/dwcore/");
-    $dwcc = $xml->specimen->children("http://rs.tdwg.org/dwc/curatorial/");
-    $dwcg = $xml->specimen->children("http://rs.tdwg.org/dwc/geospatial/");
-    
-    $dwc_Kingdom = trim($dwc->Kingdom);
-    $dwc_Phylum = trim($dwc->Phylum);
-    $dwc_Class = trim($dwc->Class);
-    $dwc_Order = trim($dwc->Order);
-    $dwc_Family = trim($dwc->Family);
-    $dwc_Genus = trim($dwc->Genus);
-    $dwc_ScientificName = trim($dwc->ScientificName);
-    
-    $taxon_identifier = str_replace(" ", "_", $dwc_ScientificName);
-    if(@$used_taxa[$taxon_identifier])
-    {
-        $taxon_parameters = $used_taxa[$taxon_identifier];
-    }
-    else
-    {
-        $taxon_parameters = array();
-        $taxon_parameters["identifier"] = $taxon_identifier;
-        $taxon_parameters["kingdom"] = $dwc_Kingdom;
-        $taxon_parameters["phylum"] = $dwc_Phylum;
-        $taxon_parameters["class"] = $dwc_Class;
-        $taxon_parameters["order"] = $dwc_Order;
-        $taxon_parameters["family"] = $dwc_Family;
-        $taxon_parameters["genus"] = $dwc_Genus;
-        $taxon_parameters["scientificName"]= $dwc_ScientificName;
-        $taxon_parameters["source"] = "http://www.morphbank.net/Browse/ByImage/?tsnKeywords=" . urlencode($dwc_ScientificName) . "&spKeywords=&viewKeywords=&localityKeywords=&activeSubmit=2";
-        $taxon_parameters["dataObjects"]= array();
+    $k++;
+    print "\n $image_id [$k of $total_image_ids]";
+    print "\n " . $details_method_prefix . $image_id . " \n";
+    if(!$xml = simplexml_load_file($details_method_prefix . $image_id)) continue;
+
+    foreach($xml->object as $object)
+    {           
+        if(!$object["type"] == "Image") break;
+        $dwc = $object->children("http://rs.tdwg.org/dwc/dwcore/");
+        $dwc_Kingdom = trim($dwc->Kingdom);
+        $dwc_Phylum = trim($dwc->Phylum);
+        $dwc_Class = trim($dwc->Class);
+        $dwc_Order = trim($dwc->Order);
+        $dwc_Family = trim($dwc->Family);
+        $dwc_Genus = trim($dwc->Genus);
+        $dwc_ScientificName = trim($dwc->ScientificName);
+        $taxon_identifier = str_replace(" ", "_", $dwc_ScientificName);
+        if(@$used_taxa[$taxon_identifier]) $taxon_parameters = $used_taxa[$taxon_identifier];
+        else
+        {
+            $taxon_parameters = array();
+            $taxon_parameters["identifier"] = $taxon_identifier;
+            $taxon_parameters["kingdom"] = $dwc_Kingdom;
+            $taxon_parameters["phylum"] = $dwc_Phylum;
+            $taxon_parameters["class"] = $dwc_Class;
+            $taxon_parameters["order"] = $dwc_Order;
+            $taxon_parameters["family"] = $dwc_Family;
+            $taxon_parameters["genus"] = $dwc_Genus;
+            $taxon_parameters["scientificName"] = $dwc_ScientificName;
+            $taxon_parameters["source"] = "http://www.morphbank.net/Browse/ByImage/?tsnKeywords=" . urlencode($dwc_ScientificName) . "&spKeywords=&viewKeywords=&localityKeywords=&activeSubmit=2";
+            $taxon_parameters["dataObjects"] = array();
+            $used_taxa[$taxon_identifier] = $taxon_parameters;
+        }
+
+        /* start dataobject - image */
+        if(!in_array(trim($object->sourceId->morphbank), $excluded_MorphBank_IDs))
+        {
+            $dc_identifier = trim($object->sourceId->morphbank);
+            $dcterms_created = trim($object->dateCreated);
+            $dcterms_modified = trim($object->dateLastModified);
+            $thumbnailURL = trim($object->thumbUrl);
+            $dc_source = trim($object->detailPageUrl);
+
+            $agent = array();
+            $agent[] = array("role" => "source", "homepage" => "http://www.morphbank.net/?id=" . $object->submittedBy->userId, trim($object->submittedBy->uin));
+
+            $image_type = trim($object->imageType);
+            $copyright_text = trim($object->copyrightText);
+            $license_text = trim($object->creativeCommons);
+
+            $license = null;
+            if(preg_match("/(http:\/\/creativecommons\.org\/licenses\/[^\/]+\/[^\/]+\/)/", $license_text, $arr)){$license = $arr[1];}
+            if(stripos($license, "publicdomain") != "") $license = "http://creativecommons.org/licenses/publicdomain/";
+            if(!$license) continue;
+
+            $desc = null;
+            if($object->dateCreated)$desc .= "<br>Date created: " . $object->dateCreated;
+            if($object->dateLastModified)$desc .= "<br>Date modified: " . $object->dateLastModified;
+            if($object->dateToPublish)$desc .= "<br>Date published: " . $object->dateToPublish;
+            if($object->submittedBy)$desc .= "<br>Submitted by: " . $object->submittedBy->uin;
+            if($object->copyrightText)$desc .= "<br>Copyright: " . $object->copyrightText;
+            if($object->viewAngle)$desc .= "<br>View angle: " . $object->viewAngle;
+            if($object->imagingTechnique)$desc .= "<br>Imaging technique: " . $object->imagingTechnique;
+            if($object->imagingPreparationTechnique)$desc .= "<br>Imaging preparation technique: " . $object->imagingPreparationTechnique;
+            if($object->specimenPart)$desc .= "<br>Specimen part: " . $object->specimenPart;
+            if($object->developmentalStage)$desc .= "<br>Developmental stage: " . $object->developmentalStage;
+            if($object->sex)$desc .= "<br>Sex: " . $object->sex;
+            if($object->form)$desc .= "<br>Form: " . $object->form;            
+            if($desc) $desc = substr($desc, 4, strlen($desc));
+            $data_object_parameters = get_data_object($dc_identifier, $dcterms_created, $dcterms_modified, $copyright_text, $license, $agent, $desc, "image");
+            $taxon_parameters["dataObjects"][] = new SchemaDataObject($data_object_parameters);
+        }
+        /* end dataobject - image */
         $used_taxa[$taxon_identifier] = $taxon_parameters;
     }
-    
-    /* start dataobject - image */
-    if(!in_array(trim($xml->image->sourceId->morphbank),$excluded_MorphBank_IDs))
-    {
-        $dc_identifier = trim($xml->image->sourceId->morphbank);
-        $dcterms_created = trim($xml->image->dateCreated);
-        $dcterms_modified = trim($xml->image->dateLastModified);
-        $thumbnailURL = trim($xml->image->thumbUrl);
-        $dc_source = trim($xml->image->detailPageUrl);
-        $agent_name = trim($xml->image->submittedBy);
-        $image_type = trim($xml->image->imageType);
-        $copyright_text = trim($xml->image->copyrightText);
-        $license_text = trim($xml->image->creativeCommons);
-    
-        $license = null;
-        if(preg_match("/(http:\/\/creativecommons\.org\/licenses\/[^\/]+\/[^\/]+\/)/", $license_text, $arr)){$license = $arr[1];}
-        if(stripos($license,"publicdomain") != "")$license="http://creativecommons.org/licenses/publicdomain/";
-        if(!$license) continue;
-    
-        $desc = null;
-        if($xml->view->dateCreated)$desc .= "<br>Date created: " . $xml->view->dateCreated;
-        if($xml->view->dateLastModified)$desc .= "<br>Date modified: " . $xml->view->dateLastModified;
-        if($xml->view->dateToPublish)$desc .= "<br>Date published: " . $xml->view->dateToPublish;
-        if($xml->view->submittedBy)$desc .= "<br>Submitted by: " . $xml->view->submittedBy;
-        if($xml->image->copyrightText)$desc .= "<br>Copyright: " . $xml->image->copyrightText;
-        if($xml->view->viewAngle)$desc .= "<br>View angle: " . $xml->view->viewAngle;
-        if($xml->view->imagingTechnique)$desc .= "<br>Imaging technique: " . $xml->view->imagingTechnique;
-        if($xml->view->imagingPreparationTechnique)$desc .= "<br>Imaging preparation technique: " . $xml->view->imagingPreparationTechnique;
-        if($xml->view->specimenPart)$desc .= "<br>Specimen part: " . $xml->view->specimenPart;
-        if($xml->view->developmentalStage)$desc .= "<br>Developmental stage: " . $xml->view->developmentalStage;
-        if($xml->view->sex)$desc .= "<br>Sex: " . $xml->view->sex;
-        if($xml->view->form)$desc .= "<br>Form: " . $xml->view->form;
-        if($desc)$desc = substr($desc,4,strlen($desc));
-        $data_object_parameters = get_data_object($dc_identifier, $dcterms_created, $dcterms_modified, $copyright_text, $license, $agent_name, $desc, "image");
-        $taxon_parameters["dataObjects"][] = new SchemaDataObject($data_object_parameters);
-    }
-    /* end dataobject - image */    
-    $used_taxa[$taxon_identifier] = $taxon_parameters;
-    //if($k == 3)break; //debug    
 }
 
 foreach($used_taxa as $taxon_parameters)
@@ -126,7 +118,7 @@ foreach($used_taxa as $taxon_parameters)
     $schema_taxa[] = new SchemaTaxon($taxon_parameters);
 }
 $new_resource_xml = SchemaDocument::get_taxon_xml($schema_taxa);
-$old_resource_path = CONTENT_RESOURCE_LOCAL_PATH . $resource_id .".xml";
+$old_resource_path = CONTENT_RESOURCE_LOCAL_PATH . $resource_id . ".xml";
 $OUT = fopen($old_resource_path, "w+");
 fwrite($OUT, $new_resource_xml);
 fclose($OUT);
@@ -134,23 +126,21 @@ fclose($OUT);
 // set MorphBank to force harvest
 if(filesize(CONTENT_RESOURCE_LOCAL_PATH . $resource_id . ".xml"))
 {
-    $GLOBALS['db_connection']->update("UPDATE resources SET resource_status_id=".ResourceStatus::insert('Force Harvest')." WHERE id=".$resource_id);
+    $GLOBALS['db_connection']->update("UPDATE resources SET resource_status_id=" . ResourceStatus::insert('Force Harvest') . " WHERE id=" . $resource_id);
 }
-print "\n --end-- ";
 
-$elapsed_time_sec = microtime(1)-$timestart;
+$elapsed_time_sec = time_elapsed() - $timestart;
 echo "\n";
-echo "elapsed time = $elapsed_time_sec sec              \n";
-echo "elapsed time = " . $elapsed_time_sec/60 . " min   \n";
-echo "elapsed time = " . $elapsed_time_sec/60/60 . " hr \n";
+echo "elapsed time = " . $elapsed_time_sec/60 . " minutes   \n";
+echo "elapsed time = " . $elapsed_time_sec/60/60 . " hours  \n";
 exit("\n\n Done processing.");
 
 //==========================================================================================
-function get_data_object($id, $created, $modified, $rightsHolder, $license, $agent_name ,$description, $type)
+function get_data_object($id, $created, $modified, $rightsHolder, $license, $agent, $description, $type)
 {
     $dataObjectParameters = array();
     if($type == "text")
-    {   
+    {
         $dataObjectParameters["identifier"] = "txt_" . $id;
         $dataObjectParameters["title"] = "Specimen Info";
         $dataObjectParameters["subjects"] = array();
@@ -168,7 +158,7 @@ function get_data_object($id, $created, $modified, $rightsHolder, $license, $age
         $dataObjectParameters["thumbnailURL"] = "http://www.morphbank.net/?id=" . $id . "&imgType=thumb";
         $dataObjectParameters["mediaURL"] = "http://www.morphbank.net/?id=" . $id . "&imgType=jpg";
     }
-    
+
     $dataObjectParameters["description"] = $description;
     $dataObjectParameters["created"] = $created;
     $dataObjectParameters["modified"] = $modified;
@@ -177,11 +167,17 @@ function get_data_object($id, $created, $modified, $rightsHolder, $license, $age
     $dataObjectParameters["license"] = $license;
     $dataObjectParameters["source"] = "http://www.morphbank.net/?id=" . $id;
 
-    $agentParameters = array();
-    $agentParameters["role"] = "source";
-    $agentParameters["fullName"] = $agent_name;
-    $agents[] = new SchemaAgent($agentParameters);
-    $dataObjectParameters["agents"] = $agents;
+    $agents = array();
+    foreach($agent as $agent)
+    {  
+        $agentParameters = array();
+        $agentParameters["role"]     = $agent["role"];
+        $agentParameters["homepage"] = $agent["homepage"];
+        $agentParameters["logoURL"]  = "";        
+        $agentParameters["fullName"] = $agent[0];
+        $agents[] = new SchemaAgent($agentParameters);
+    }
+    $dataObjectParameters["agents"] = $agents;    
 
     $dataObjectParameters["audiences"] = array();
     $audienceParameters = array();
