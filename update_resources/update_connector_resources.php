02c8224e1eb666ffc792dc7afd0058bb41c6281d
eagbayani <eagbayani@baf6b65b-9124-4dc3-b1c0-01155b4af546>
2011-04-08 16:47:46 +0000

    Added MorphBank (83) to be excluded during harvest.
    git-svn-id: https://repository.eol.org/eol_php_code/trunk@1732 baf6b65b-9124-4dc3-b1c0-01155b4af546

--- update_resources/update_connector_resources.php
+++ update_resources/update_connector_resources.php
@@ -9,27 +9,26 @@ $mysqli =& $GLOBALS['mysqli_connection'];
 
 $manager = new ContentManager();
 
-
 $connectors = Functions::get_files_in_dir(dirname(__FILE__) . "/connectors");
 foreach($connectors as $file)
 {
     if(!preg_match("/^(.*)\.php$/", $file, $arr)) continue;
-    
+
     $resource = new Resource($arr[1]);
     if(!@$resource->id) continue;
     if(!$resource->ready_to_update()) continue;
-    
+
     // resources to skip as they are scheduled seperately
     if($resource->id==15) continue; // Flickr
     if($resource->id==71) continue; // Wikimedia Commons
     if($resource->id==80) continue; // Wikipedia
     if($resource->id==211) continue; // IUCN Redlist
     if($resource->id==31) continue; // BioPix
-    
+    if($resource->id==83) continue; // MorphBank
+
     echo "$file...\n";
     shell_exec(PHP_BIN_PATH . dirname(__FILE__) . "/connectors/". $file." ENV_NAME=slave");
 }
 
 $log->finished();
-
 ?>
