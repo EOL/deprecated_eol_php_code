325722600618e61cf7356d51cc38457f25ac82dd
eagbayani <eagbayani@baf6b65b-9124-4dc3-b1c0-01155b4af546>
2011-04-12 18:22:40 +0000

    Set warnings off.
    git-svn-id: https://repository.eol.org/eol_php_code/trunk@1734 baf6b65b-9124-4dc3-b1c0-01155b4af546

--- update_resources/connectors/100.php
+++ update_resources/connectors/100.php
@@ -7,6 +7,7 @@ The connector loops to this list and compiles each XML to 1 final XML for EOL in
 
 include_once(dirname(__FILE__) . "/../../config/environment.php");
 $timestart = time_elapsed();
+error_reporting(E_ALL ^ E_WARNING);
 require_library('connectors/ConabioAPI');
 $resource_id = 100;
 ConabioAPI::combine_all_xmls($resource_id);
