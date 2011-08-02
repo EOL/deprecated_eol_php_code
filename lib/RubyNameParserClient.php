b7869fb496122e9cbb15d902c9fcce438ca7a671
Patrick Leary <pleary@mbl.edu>
2011-08-02 13:01:11 -0400

    Added name parser client

---
+++ lib/RubyNameParserClient.php
@@ -0,0 +1,109 @@
+<?php
+namespace php_active_record;
+
+class RubyNameParserClient
+{
+    private $socket;
+    private $socket_ip;
+    private $socket_port;
+    private $connected;
+    
+    function __construct($params = array())
+    {
+        $this->connect($params);
+    }
+    
+    private static function start_parserver()
+    {
+        $maximum_attempts = 3;
+        $reconnect_attempts = 0;
+        while(!self::is_parserver_running() && $reconnect_attempts <= $maximum_attempts)
+        {
+            shell_exec('parserver --output=canonical_with_rank > /dev/null 2>/dev/null &');
+            sleep(5);
+            $reconnect_attempts++;
+        }
+        if(!self::is_parserver_running())
+        {
+            trigger_error("NameParserGemClient:: Unable to start parserver", E_USER_WARNING);
+        }
+    }
+    
+    public static function is_parserver_running()
+    {
+        $processlist = explode("\n", shell_exec('ps -e'));
+        foreach($processlist as $line)
+        {
+            if(preg_match("/parserver --output=canonical_with_rank$/", trim($line))) return true;
+        }
+        return false;
+    }
+    
+    private function connect($params = array())
+    {
+        self::start_parserver();
+        if(@!$params['ip']) $params['ip'] = 'localhost';
+        if(@!$params['port']) $params['port'] = '4334';
+        $this->timeout_retry_seconds = 120;
+        $this->socket_ip = $this->socket_ip ?: $params['ip'];
+        $this->socket_port = $this->socket_port ?: $params['port'];
+        $this->timeout_time = null;
+        $this->fsock = fsockopen($this->socket_ip, $this->socket_port, $this->errno, $this->errstr, 10);
+        if($this->fsock === false)
+        {
+            trigger_error("NameParserGemClient:: Problem with socket connection: [$this->errno] $this->errstr", E_USER_WARNING);
+            $this->connected = false;
+        }else
+        {
+            $this->connected = true;
+            stream_set_timeout($this->fsock, 2);
+        }
+    }
+    
+    public function connected()
+    {
+        return true;
+    }
+    
+    public function lookup_string($string)
+    {
+        if(!$this->fsock)
+        {
+            if($this->timeout_time && (microtime(true) - $this->timeout_time) >= $this->timeout_retry_seconds)
+            {
+                $this->connect();
+                $this->lookup_string($string);
+            }
+            return false;
+        }
+        
+        $string = str_replace("\n", " ", $string);
+        $string = str_replace("\r", " ", $string);
+        $string = preg_replace("/\s/", " ", $string);
+        
+        $string = trim($string) . "\n";
+        if(fwrite($this->fsock, $string) === false)
+        {
+            $errorcode = socket_last_error();
+            $errormsg = socket_strerror($errorcode);
+            trigger_error("NameParserGemClient:: Problem with socket connection: [$this->errno] $this->errstr", E_USER_WARNING);
+        }
+        
+        if($out = fread($this->fsock, 2048))
+        {
+            return rtrim($out, "\n");
+        }
+        
+        $info = stream_get_meta_data($this->fsock);
+        if($info['timed_out'])
+        {
+            $this->timeout_time = microtime(true);
+            fclose($this->fsock);
+            $this->fsock = false;
+        }
+        
+        return false;
+    }
+}
+
+?>
