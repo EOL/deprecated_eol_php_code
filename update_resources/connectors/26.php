43614c0a59c0cbdee54495b377114084c87c5480
Eli Agbayani <eli@eol.org>
2011-09-19 15:46:16 -0400

    Make WORMS connector to work in V2.

--- update_resources/connectors/26.php
+++ update_resources/connectors/26.php
@@ -1,11 +1,12 @@
 <?php
+namespace php_active_record;
 /* connector for WORMS
-Provider provides a service to get their list of IDs and another service to use the id to get each taxon information.
+Partner provides a service to get their list of IDs and another service to use the id to get each taxon information.
 estimated execution time:
-sample api: http://www.marinespecies.org/aphia.php?p=eol&action=taxdetails&id=571925
+sample API: http://www.marinespecies.org/aphia.php?p=eol&action=taxdetails&id=571925
 */
-$timestart = microtime(1);
 include_once(dirname(__FILE__) . "/../../config/environment.php");
+$timestart = time_elapsed();
 require_library('connectors/WormsAPI');
 $resource_id = 26;
 
@@ -13,7 +14,7 @@ if(isset($argv[1])) $call_multiple_instance = false;
 else $call_multiple_instance = true;
 
 WormsAPI::start_process($resource_id, $call_multiple_instance);
-$elapsed_time_sec = microtime(1)-$timestart;
+$elapsed_time_sec = time_elapsed() - $timestart;
 echo "\n";
 echo "elapsed time = " . $elapsed_time_sec/60 . " minutes \n";
 echo "elapsed time = " . $elapsed_time_sec/60/60 . " hours \n";
