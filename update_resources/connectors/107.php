a8ee957b693ae439b0013ab1f003c6c226f6b94c
Eli Agbayani <eli@eol.org>
2011-09-21 12:03:35 -0400

    Make Encyclopedia of Marine Life - Britain, Ireland connector to work in V2

--- update_resources/connectors/107.php
+++ update_resources/connectors/107.php
@@ -1,192 +1,133 @@
 <?php
+namespace php_active_record;
+
 /*connector for Encyclopedia of Marine Life of Britain and Ireland
 estimated execution time: 28-30 mins for 868 species.
 
 Partner provides a list of URL's for its individual species XML.
-The connector loops to this list and compiles individual XML's to 1 final XML for EOL ingestion.
-
+The connector loops to this list and compiles it to one final resource for EOL ingestion.
 */
 
 $timestart = microtime(1);
-
 include_once(dirname(__FILE__) . "/../../config/environment.php");
 $mysqli =& $GLOBALS['mysqli_connection'];
 
-$bad_id=""; //not well formed XML
-// /*
-$start=0;
-$file_number=1;
-// */
+$start = 0;
+$resource_id = 107; //Encyclopedia of Marine Life of Britain and Ireland 
+$main_count = 0;
 
-$resource = new Resource(107);//Encyclopedia of Marine Life of Britain and Ireland 
-//exit("[$resource->id]");
+$file_number = 1;
+$path = DOC_ROOT . "update_resources/connectors/files/britain_ireland_" . $file_number . ".xml";
+if(!$OUT = fopen($path, "w+")) print "\n Cannot create file $path";
 
-$main_count=0;
-//====================================================================================
 $main_id_list = array();
-//$id_processed = array();
 $main_id_list = get_main_id_list();
 $total_taxid_count = count($main_id_list);
-echo "\n total taxid count = " . $total_taxid_count . "\n\n";;
-//exit;
-//====================================================================================
-$i=1;
-$bad=0;
-    echo "-x- \n";    
-    for ($i = $start; $i < $total_taxid_count; $i++)     
+print "\n total taxid count = " . $total_taxid_count . "\n\n";
+$i = 1;
+print "\n";
+for ($i = $start; $i < $total_taxid_count; $i++)
+{
+    $taxid = $main_id_list[$i];
+    if($contents = process($taxid))
     {
-        $taxid = $main_id_list[$i];
+        print " -ok- ";
+        fwrite($OUT, $contents);
+    }
+    else print "\n no write";
+    print $i+1 . ". of $total_taxid_count \n";
+}
+$str = "</response>";
+fwrite($OUT, $str);
+fclose($OUT);
 
-            if($i % 10000 == 0) //working
-            {   
-                //start new file                
-                if(isset($OUT))fclose($OUT);
-                //$old_resource_path = CONTENT_RESOURCE_LOCAL_PATH . "temp/britain_ireland_" . $file_number .".xml";
-                $old_resource_path = "files/britain_ireland_" . $file_number .".xml";
-                $OUT = fopen($old_resource_path, "w+");            
-                $file_number++;
-            }                        
-            if($contents=process($taxid))            
-            {
-                echo " -ok- ";
-                fwrite($OUT, $contents);
-            }
-            else
-            {
-                echo " -bad- $taxid "; $bad++;
-            }
-            echo $i+1 . ". of $total_taxid_count [bad=$bad] \n";            
-    }    
-
-
-//print_r($main_id_list);print_r($id_processed);
-//====================================================================================
-$str = "</response>";fwrite($OUT, $str);fclose($OUT);
-//====================================================================================
 //start compiling all britain_ireland_?.xml 
-$old_resource_path = CONTENT_RESOURCE_LOCAL_PATH . $resource->id .".xml";
+$old_resource_path = CONTENT_RESOURCE_LOCAL_PATH . $resource_id .".xml";
 $OUT = fopen($old_resource_path, "w+");
 $str = "<?xml version='1.0' encoding='utf-8' ?>\n";
 $str .= "<response\n";
-$str .= "  xmlns='http://www.eol.org/transfer/content/0.3'\n";           
+$str .= "  xmlns='http://www.eol.org/transfer/content/0.3'\n";
 $str .= "  xmlns:xsd='http://www.w3.org/2001/XMLSchema'\n";
-$str .= "  xmlns:dc='http://purl.org/dc/elements/1.1/'\n";           
-$str .= "  xmlns:dcterms='http://purl.org/dc/terms/'\n";           
-$str .= "  xmlns:geo='http://www.w3.org/2003/01/geo/wgs84_pos#'\n";           
-$str .= "  xmlns:dwc='http://rs.tdwg.org/dwc/dwcore/'\n";           
-$str .= "  xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance'\n";                      
+$str .= "  xmlns:dc='http://purl.org/dc/elements/1.1/'\n";
+$str .= "  xmlns:dcterms='http://purl.org/dc/terms/'\n";
+$str .= "  xmlns:geo='http://www.w3.org/2003/01/geo/wgs84_pos#'\n";
+$str .= "  xmlns:dwc='http://rs.tdwg.org/dwc/dwcore/'\n";
+$str .= "  xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance'\n";
 $str .= "  xsi:schemaLocation='http://www.eol.org/transfer/content/0.3 http://services.eol.org/schema/content_0_3.xsd'>\n";
 fwrite($OUT, $str);
-$i=0;
-
-//while($i <= $total_taxid_count)
+$i = 0;
 while(true)
 {
     $i++; print "$i ";
-    //$file = CONTENT_RESOURCE_LOCAL_PATH . "temp/britain_ireland_" . $i .".xml";
-    $file = "files/britain_ireland_" . $i .".xml";
-    $str = Functions::get_remote_file($file);
-    if($str)
+    $file = DOC_ROOT . "/update_resources/connectors/files/britain_ireland_" . $i .".xml";
+    if (file_exists($file))
     {
-        fwrite($OUT, $str);
-        unlink($file);
-    }            
-    else break;    
-    //new
-    //if($i <= $total_taxid_count)unlink($file);    
+        if($str = Functions::get_remote_file($file))
+        {
+            fwrite($OUT, $str);
+            unlink($file);
+        }
+    }
+    else break;
 }
-if($bad_id != "")print "\n not well-formed XML = $bad_id \n";
+
 print "\n --end-- \n";
 fclose($OUT);
 
-$OUT = fopen("bad_id.txt", "w+");            
-fwrite($OUT, $bad_id);fclose($OUT);
-
-//end
-
 
 $elapsed_time_sec = microtime(1)-$timestart;
-echo "\n";
-echo "elapsed time = $elapsed_time_sec sec              \n";
-echo "elapsed time = " . $elapsed_time_sec/60 . " min   \n";
-echo "elapsed time = " . $elapsed_time_sec/60/60 . " hr \n";
-
+print "\n";
+print "elapsed time = $elapsed_time_sec sec              \n";
+print "elapsed time = " . $elapsed_time_sec/60 . " min   \n";
+print "elapsed time = " . $elapsed_time_sec/60/60 . " hr \n";
 exit("\n\n Done processing.");
 
-//######################################################################################################################
-//######################################################################################################################
-//######################################################################################################################
-
-
-//====================================================================================
 //start functions #################################################################################################
 function process($id)
-{   
-    global $bad_id;
-    
-    //$file = "http://www.marinespecies.org/aphia.php?p=eol&action=taxdetails&id=$id";
-    $file = $id;    
-    
-    if($xml = Functions::get_hashed_response($file)){}
-    else
+{
+    $file = $id;
+    if($contents = Functions::get_remote_file($file))
     {
-        $bad_id .= $id . ",";
-        return false;
+        if($contents)
+        {
+            $pos1 = stripos($contents,"<taxon>");
+            $pos2 = stripos($contents,"</taxon>");
+            if($pos1 != "" and $pos2 != "")
+            {
+                $contents = trim(substr($contents, $pos1, $pos2 - $pos1 + 8));
+                return $contents;
+            }
+        }
     }
-    
-    $contents = Functions::get_remote_file($file);
-    //$contents = get_file_contents($file);
-    if($contents)
-    {
-    	$pos1 = stripos($contents,"<taxon>");
-    	$pos2 = stripos($contents,"</taxon>");			
-    	if($pos1 != "" and $pos2 != "")
-    	{
-    		$contents = trim(substr($contents,$pos1,$pos2-$pos1+8));
-            //fwrite($OUT, $contents);
-            return $contents;
-            //return true;
-    	}
-    }    
     return false;
-}//end process() 
+}
+
 function get_main_id_list()
-{    
-    /*
-    $url[] = "http://services.eol.org/eol_php_code/update_resources/connectors/files/WORMS/2007.xml";
-    $url[] = "http://services.eol.org/eol_php_code/update_resources/connectors/files/WORMS/2008.xml";
-    $url[] = "http://services.eol.org/eol_php_code/update_resources/connectors/files/WORMS/2009.xml";
-    $url[] = "http://services.eol.org/eol_php_code/update_resources/connectors/files/WORMS/2010.xml";
-    */    
-    
+{
     $url[] = "http://www.habitas.org.uk/marinelife/specieslist_xml.asp";
-    
-    
-    echo "\n URLs = " . sizeof($url) . "\n";
-    $no_of_urls = sizeof($url);        
-    $arr = array(); 
-    $jj=0;
-    for ($i = 0; $i < count($url); $i++) 
+    print "\n URLs = " . sizeof($url) . "\n";
+    $no_of_urls = sizeof($url);
+    $arr = array();
+    for ($i = 0; $i < count($url); $i++)
     {
-        $j=0;        
-        //if($xml = @simplexml_load_file($url[$i]))        
-        if($xml = Functions::get_hashed_response($url[$i]))        
-        {   
+        $j = 0;
+        if($xml = Functions::get_hashed_response($url[$i]))
+        {
             $no_of_taxdetail = count($xml->taxdetail);
             foreach($xml->id as $taxdetail)
             {
                 $temp = @$taxdetail;
-                echo "\n" . $temp;
+                print "\n" . $temp;
                 $arr["$temp"]=true;
-                $j++; $jj++;
-            }    
+                $j++;
+            }
             print "\n";
         }
-        echo "\n" . $i+1 . " of " . $no_of_urls . " URLs | taxid count = " . $j . "\n";     
+        print "\n" . $i+1 . " of " . $no_of_urls . " URLs | taxid count = " . $j . "\n";
     }
     print " \n";
     $arr = array_keys($arr);
     return $arr;
-}//get_main_id_list()
+}
 
 ?>
