6cfa9a1d9630da34908902847b1fbc97beb593b1
Eli Agbayani <eli@eol.org>
2011-10-03 18:42:38 -0400

    Data-793 US Fish and Wildlife additional spreadsheets.

--- update_resources/connectors/266.php
+++ update_resources/connectors/266.php
@@ -1,27 +1,22 @@
 <?php
 namespace php_active_record;
 /* connector for US Fish and Wildlife Services - Endangered Species Program
-estimated execution time: 18 minutes
+estimated execution time: 4.17 hours (taxa = 2233)
+Taxa list comes from 3 spreadsheets and content is scraped.
 */
-
 include_once(dirname(__FILE__) . "/../../config/environment.php");
 $timestart = time_elapsed();
-
 require_library('connectors/USFishWildlifeAPI');
-
 $resource_id = 266;
 USFishWildlifeAPI::get_all_taxa_keys($resource_id);
-
 // set to force harvest
 if(filesize(CONTENT_RESOURCE_LOCAL_PATH . $resource_id . ".xml"))
 {
     $GLOBALS['db_connection']->update("UPDATE resources SET resource_status_id=" . ResourceStatus::force_harvest()->id . " WHERE id=" . $resource_id);
 }
-
 $elapsed_time_sec = time_elapsed() - $timestart;
 echo "\n";
-echo "elapsed time = $elapsed_time_sec sec              \n";
-echo "elapsed time = " . $elapsed_time_sec/60 . " min   \n";
-echo "elapsed time = " . $elapsed_time_sec/60/60 . " hr \n";
+echo "elapsed time = " . $elapsed_time_sec/60 . " minutes \n";
+echo "elapsed time = " . $elapsed_time_sec/60/60 . " hours \n";
 exit("\n\n Done processing.");
 ?>
