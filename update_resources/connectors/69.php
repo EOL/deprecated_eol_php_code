dc5098b747bc72be1b755bca4c0cf8ea4f410d9c
Eli Agbayani <eli@eol.org>
2011-11-23 10:34:31 -0500

    Tropical lichens connector.

---
+++ update_resources/connectors/69.php
@@ -0,0 +1,27 @@
+<?php
+namespace php_active_record;
+/* Tropical Lichens connector
+estimated execution time: 8 seconds
+Partner provides a service that resembles an EOL XML.
+*/
+
+include_once(dirname(__FILE__) . "/../../config/environment.php");
+$timestart = time_elapsed();
+$resource_id = 69;
+$url = 'http://www.tropicallichens.net/eolclient.aspx';
+if($xml_content = Functions::get_remote_file($url)) 
+{
+    $resource_path = CONTENT_RESOURCE_LOCAL_PATH . $resource_id . ".xml";
+    $OUT = fopen($resource_path, "w");
+    fwrite($OUT, $xml_content);
+    fclose($OUT);
+}
+else print "\n no contents $i";
+
+$elapsed_time_sec = time_elapsed() - $timestart;
+echo "\n";
+echo "elapsed time = " . $elapsed_time_sec . " seconds   \n";
+echo "elapsed time = " . $elapsed_time_sec/60 . " minutes   \n";
+echo "elapsed time = " . $elapsed_time_sec/60/60 . " hours  \n";
+exit("\n\n Done processing.");
+?>
