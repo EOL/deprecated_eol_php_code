e51590ea30ca7c7b7bf47346b00629f75965003a
Eli Agbayani <eli@eol.org>
2011-11-23 16:02:56 -0500

    Fix typo.

--- update_resources/connectors/kill_connector.php
+++ update_resources/connectors/kill_connector.php
@@ -11,9 +11,9 @@ Where resource_id param is numeric.
 include_once(dirname(__FILE__) . "/../../config/environment.php");
 
 if(isset($argv[1])) $resource_id = $argv[1];
-else exit("\n Wrong parameter. Should be: \n     php kill_connectors.php resource_id\n The resource_id param is numeric.");
+else exit("\n Wrong parameter. Should be: \n     php kill_connector.php resource_id\n The resource_id param is numeric.");
 
 if(is_numeric($resource_id)) Functions::kill_running_connectors($resource_id);
-else exit("\n Wrong parameter. Should be: \n     php kill_connectors.php resource_id\n The resource_id param is numeric.");
+else exit("\n Wrong parameter. Should be: \n     php kill_connector.php resource_id\n The resource_id param is numeric.");
 
 ?>
