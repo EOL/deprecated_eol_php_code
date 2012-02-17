c2218e1d6c59f2fb736dbaa56c906fd2f6e2e036
Eli Agbayani <eli@eol.org>
2012-02-17 15:08:51 -0500

    Make EOL stats run automatically after taxon metrics.

--- applications/taxon_page_metrics/generate_taxon_page_metrics.php
+++ applications/taxon_page_metrics/generate_taxon_page_metrics.php
@@ -7,7 +7,10 @@ $time_start = time_elapsed();
 require_library('TaxonPageMetrics');
 $stats = new TaxonPageMetrics();
 $stats->insert_page_metrics(); // 1.36 hours
-       
+
+sleep(20);
+shell_exec("php " . DOC_ROOT . "applications/taxon_page_metrics/generate_eol_stats.php");
+
 $time_elapsed_sec = time_elapsed() - $time_start;
 echo "\n elapsed time = $time_elapsed_sec sec               ";
 echo "\n elapsed time = " . $time_elapsed_sec/60 . " mins   ";
