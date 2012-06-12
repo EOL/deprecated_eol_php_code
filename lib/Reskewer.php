e13525cf0e49ae9971b11062f79554f593d1e900
JRice <jrice@eol.org>
2012-06-12 15:23:53 -0600

    QUEUE=php APP_INCLUDE=lib/Reskewer.php php vendor/php_resque/resque.php for the worker, and php rake_tasks/reskew.php split for an example of its output.  Of course, we want this to be a great bit more clever, but... this is a start.

--- lib/Reskewer.php
+++ lib/Reskewer.php
@@ -1,19 +1,23 @@
 <?php
-date_default_timezone_set('America/Denver');
-
+date_default_timezone_set('America/Denver');  // TODO - This should be moved to the config file, I think.
 include_once(dirname(__FILE__) . "/../config/environment.php");
+
+# Needed for work:
+php_active_record\require_library("SplitConceptHandler");
 
 class Reskewer
 {
-    
-    public function perform()
-    {
-      // WORK HERE
-      echo "\n\nWorking on: ";
-      echo $this->args['foo'];
-      echo "\n\n";
+
+  public function perform()
+  {
+    echo "++ Reskewer Performing...\n";
+    if ($this->args['cmd'] == 'split') {
+      SplitConceptHandler::split_concept($this->args);
+    } else {
+      echo "++ No command available for " . $this->args['cmd'] . "\n\n";
     }
-    
+  }
+
 }
 
 ?>
--- rake_tasks/split_concept.php
+++ rake_tasks/split_concept.php
@@ -1,15 +1,14 @@
 <?php
-namespace php_active_record;
 
 include_once(dirname(__FILE__) . "/../config/environment.php");
 include_once(dirname(__FILE__) . "/../lib/SplitConceptHandler.php");
 
-split_concept_handler(array('taxon_concept_id_from'        => @$argv[1],
-                            'hierarchy_entry_id'           => @$argv[2],
-                            'taxon_concept_id_to'          => @$argv[3],
-                            'bad_match_hierarchy_entry_id' => @$argv[4],
-                            'confirmed'                    => @$argv[5],
-                            'reindex'                      => @trim($argv[6])));
+SplitConceptHandler::split_concept(array('taxon_concept_id_from'        => @$argv[1],
+                                         'hierarchy_entry_id'           => @$argv[2],
+                                         'taxon_concept_id_to'          => @$argv[3],
+                                         'bad_match_hierarchy_entry_id' => @$argv[4],
+                                         'confirmed'                    => @$argv[5],
+                                         'reindex'                      => @trim($argv[6])));
 
 
 ?>
