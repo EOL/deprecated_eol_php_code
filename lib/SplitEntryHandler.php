ba0d95f7bca46ddbbf16fe98b83bef69a4578fef
JRice <jrice@eol.org>
2012-07-02 14:51:30 -0600

    WIP - Code Bridge first draft.  :)

--- lib/SplitEntryHandler.php
+++ lib/SplitEntryHandler.php
@@ -1,8 +1,53 @@
 <?php
 include_once(dirname(__FILE__) . "/../config/environment.php");
 
-class SplitConceptHandler
+class SplitEntryHandler
 {
+
+  function split_entry($args)
+  {
+
+    if(!$args['hierarchy_entry_id'] || !is_numeric($args['hierarchy_entry_id']) || !$args['bad_match_hierarchy_entry_id'] || !is_numeric($args['bad_match_hierarchy_entry_id']))
+    {
+        echo "\n\n\tsplit_entry.php [hierarchy_entry_id] [bad_match_hierarchy_entry_id] [confirmed]\n\n";
+        exit;
+    }
+
+    $he = HierarchyEntry::find($args['hierarchy_entry_id']);
+    $bad_he = HierarchyEntry::find($args['bad_match_hierarchy_entry_id']);
+
+    if(!$he->id || !$bad_he->id)
+    {
+        echo "\n\nInvalid ID\n";
+        exit;
+    }
+
+    if($he->taxon_concept_id != $bad_he->taxon_concept_id)
+    {
+        echo "\n\nThe bad match ID isn't from the same concept\n";
+        exit;
+    }
+
+    if($args['confirmed'] == 'confirmed')
+    {
+        $user_id = 13;  # 13 is Patrick's user ID
+        $update_caches = true;
+        echo HierarchyEntry::split_from_concept_static($args['hierarchy_entry_id'], $update_caches)."\n";
+        $GLOBALS['db_connection']->query("INSERT IGNORE INTO curated_hierarchy_entry_relationships VALUES (" .$args['hierarchy_entry_id'] . ", " . $args['bad_match_hierarchy_entry_id'] . ", $user_id, 0)");
+    }else
+    {
+        echo "\n\n";
+        echo "Removing:\n";
+        print_r($he);
+        echo "Name: ".$he->name->string."\n\n";
+        echo "From:\n";
+        print_r($he->taxon_concept);
+        
+        $descendant_objects = TaxonConcept::count_descendants_objects($he->taxon_concept_id);
+        echo "\n\nDescendant Objects:  $descendant_objects\n\n";
+    }
+
+//OLD:
 
   function split_concept($args)
   {
