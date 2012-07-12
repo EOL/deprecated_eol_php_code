9e11ae08287647824da0bcd215ec4c4efe6102db
JRice <jrice@eol.org>
2012-07-12 09:47:59 -0600

    Namespace has to be first.

--- lib/SplitEntryHandler.php
+++ lib/SplitEntryHandler.php
@@ -1,6 +1,6 @@
 <?php
-include_once(dirname(__FILE__) . "/../config/environment.php");
 namespace php_active_record;
+include_once(dirname(__FILE__) . "/../config/environment.php");
 
 class SplitEntryHandler
 {
