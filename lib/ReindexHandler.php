0c022d6ead2ef78ef4c14920db36c23d408bd451
JRice <jrice@eol.org>
2012-11-15 14:15:30 -0700

    Fixing some typos... should work now

--- lib/ReindexHandler.php
+++ lib/ReindexHandler.php
@@ -13,9 +13,9 @@ class ReindexHandler
       $he = new FlattenHierarchies();
       $he->flatten_hierarchies_from_concept_id($taxon_concept_id);
     }
-    php_active_record\TaxonConcept::reindex_descendants_objects($args['taxon_concept_id']);
-    php_active_record\TaxonConcept::reindex_for_search($args['taxon_concept_id']);
-    php_active_record\TaxonConcept::unlock_classifications_by_id($args['taxon_concept_id']);
+    TaxonConcept::reindex_descendants_objects($args['taxon_concept_id']);
+    TaxonConcept::reindex_for_search($args['taxon_concept_id']);
+    TaxonConcept::unlock_classifications_by_id($args['taxon_concept_id']);
 
   }
 
