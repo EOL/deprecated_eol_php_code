feb167587b3ea2dfeeaa60d31cf2584c3dade2ec
Eli Agbayani <eli@eol.org>
2012-12-18 07:35:43 -0500

    DATA-984 Update BioImages connector to use a tab-delimited text file and the original resource XML (168.xml) to update image and text objects respectively.

--- update_resources/connectors/168.php
+++ update_resources/connectors/168.php
@@ -1,32 +1,32 @@
 <?php
-/* connector for Bioimages
-estimated execution time: 34 hours
-Connector screen scrapes the partner website. 
-Ancestry information comes from 2 spreadsheet files.
+namespace php_active_record;
+/* estimated execution time: 6 minutes */
 
-                taxa    dataObject  references
-as of 18Oct     19714   90655       27753
-
-*/
-exit;
-
-$timestart = microtime(1);
 include_once(dirname(__FILE__) . "/../../config/environment.php");
 require_library('connectors/BioImagesAPI');
-$GLOBALS['ENV_DEBUG'] = false;
 
-$taxa = BioImagesAPI::get_all_taxa();
-$xml = SchemaDocument::get_taxon_xml($taxa);
+$timestart = time_elapsed();
 
-$resource_path = CONTENT_RESOURCE_LOCAL_PATH . "168.xml";
-$OUT = fopen($resource_path, "w+");
-fwrite($OUT, $xml);
-fclose($OUT);
+$func = new BioImagesAPI;
+$func->get_all_taxa();
+$partner = "bioimages";
+$resource_id = 168;
 
-$elapsed_time_sec = microtime(1)-$timestart;
-echo "\n";
-echo "elapsed time = $elapsed_time_sec sec              \n";
-echo "elapsed time = " . $elapsed_time_sec/60 . " min   \n";
-echo "elapsed time = " . $elapsed_time_sec/60/60 . " hr \n";
-exit("\n\n Done processing.");
+if(filesize(CONTENT_RESOURCE_LOCAL_PATH . $partner . "_working/taxon.tab") > 1000)
+{
+    if(is_dir(CONTENT_RESOURCE_LOCAL_PATH . $partner))
+    {
+        recursive_rmdir(CONTENT_RESOURCE_LOCAL_PATH . $partner . "_previous");
+        rename(CONTENT_RESOURCE_LOCAL_PATH . $partner, CONTENT_RESOURCE_LOCAL_PATH . $partner . "_previous");
+    }
+    rename(CONTENT_RESOURCE_LOCAL_PATH . $partner . "_working", CONTENT_RESOURCE_LOCAL_PATH . $partner);
+    Functions::set_resource_status_to_force_harvest($resource_id);
+}
+
+$elapsed_time_sec = time_elapsed() - $timestart;
+echo "\n\n";
+echo "elapsed time = " . $elapsed_time_sec/60 . " minutes \n";
+echo "elapsed time = " . $elapsed_time_sec/60/60 . " hours \n";
+exit("\nDone processing.\n");
+
 ?>
