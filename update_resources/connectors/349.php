8c76a1da32f546b673a0d851164ecd9f261c705d
Eli Agbayani <eli@eol.org>
2013-01-17 00:50:07 -0500

    Fix IABin, InBIO, Tucanes, and some code clean-up.

--- update_resources/connectors/349.php
+++ update_resources/connectors/349.php
@@ -10,23 +10,18 @@ $timestart = time_elapsed();
 require_library('connectors/INBioAPI');
 require_library('connectors/TucanesAPI');
 $resource_id = 349; 
-$taxa = TucanesAPI::get_all_taxa($resource_id);
-$xml = \SchemaDocument::get_taxon_xml($taxa);
-$xml = INBioAPI::assign_eol_subjects($xml);
-$resource_path = CONTENT_RESOURCE_LOCAL_PATH . $resource_id . ".xml";
-$OUT = fopen($resource_path, "w");
-fwrite($OUT, $xml);
-fclose($OUT);
-
-// set to force harvest
-if(filesize(CONTENT_RESOURCE_LOCAL_PATH . $resource_id . ".xml") > 600)
+if($taxa = TucanesAPI::get_all_taxa($resource_id))
 {
-    $GLOBALS['db_connection']->update("UPDATE resources SET resource_status_id=" . ResourceStatus::force_harvest()->id . " WHERE id=" . $resource_id);
+    $xml = \SchemaDocument::get_taxon_xml($taxa);
+    $xml = INBioAPI::assign_eol_subjects($xml);
+    $resource_path = CONTENT_RESOURCE_LOCAL_PATH . $resource_id . ".xml";
+    $OUT = fopen($resource_path, "w");
+    fwrite($OUT, $xml);
+    fclose($OUT);
+    Functions::set_resource_status_to_force_harvest($resource_id);
 }
-
 $elapsed_time_sec = time_elapsed() - $timestart;
-echo "\n";
-echo "elapsed time = " . $elapsed_time_sec . " seconds   \n";
-echo "elapsed time = " . $elapsed_time_sec/60 . " minutes   \n";
-echo "\n\n Done processing.";
+echo "\nelapsed time = " . $elapsed_time_sec . " seconds";
+echo "\nelapsed time = " . $elapsed_time_sec/60 . " minutes";
+echo "\nDone processing.";
 ?>
