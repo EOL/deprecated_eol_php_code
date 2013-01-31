806cec251aca55f06ff0b1a995408df61a72369e
Eli Agbayani <eli@eol.org>
2013-01-31 00:09:04 -0500

    Fix failing test for MCZHarvard connector.

--- tests/unit/test_connector_mczharvard_api.php
+++ tests/unit/test_connector_mczharvard_api.php
@@ -1,57 +1,19 @@
 <?php
 namespace php_active_record;
-
 require_library('connectors/MCZHarvardAPI');
-
 class test_connector_mczharvard_api extends SimpletestUnitBase
 {
     function testMCZHarvardAPI()
     {
-        $taxon = Array
-                (
-                    "GUID" => "MCZ:Herp:A-28293",
-                    "SCIENTIFIC_NAME" => "Phrynobatrachus maculatus",
-                    "FULL_TAXON_NAME" => "Chordata Amphibia Lissamphibia Anura Ranoidea Ranidae Phrynobatrachus maculatus",
-                    "PHYLCLASS" => "Amphibia",
-                    "KINGDOM" => "",
-                    "PHYLUM" => "Chordata",
-                    "PHYLORDER" => "Anura",
-                    "FAMILY" => "Ranidae",
-                    "GENUS" => "Phrynobatrachus",
-                    "SPECIES" => "maculatus",
-                    "SUBSPECIES" => "",
-                    "INFRASPECIFIC_RANK" => "",
-                    "AUTHOR_TEXT" => "",
-                    "taxon_id" => "MCZ:Herp:A-28293"
-                );
-        $images[0] = Array
-                    (
-                        "GUID" => "MCZ:Herp:A-28293",
-                        "MEDIA_ID" => "1515",
-                        "MEDIA_URI" => "http://mczbase.mcz.harvard.edu/herpimages/large/A28293_P_maculatus_P_d.jpg",
-                        "MIME_TYPE" => "image/jpeg",
-                        "SPEC_LOCALITY" => "Kasane, Bechuanaland",
-                        "HIGHER_GEOG" => "Africa, Botswana, Chobe",
-                        "TYPESTATUS" => "Paratype",
-                        "PARTS" => "whole animal (ethanol)",
-                        "COLLECTING_METHOD" => "",
-                        "COLLECTORS" => "HLang",
-                        "IDENTIFIEDBY" => "FitzSimons",
-                        "created" => "04 dec 2008",
-                        "LAST_EDIT_DATE" => "25-JUL-01",
-                        "SPECIMENDETAILURL" => "http://mczbase.mcz.harvard.edu/SpecimenDetail.cfm?collection_object_id=194811",
-                        "AGENT" => "Allen Iverson"
-                    );
-    
-        $arr = MCZHarvardAPI::get_MCZHarvard_taxa($taxon,$images,array());
-        $taxa = $arr[0];
-        $this->assertTrue(is_array($taxa), 'Taxa should be an array');
-        
-        $taxon = $taxa[0];
-        $this->assertIsA($taxon, 'SchemaTaxon', 'Response should be an array of SchemaTaxon');
-        
-        $dataObject = $taxon->dataObjects[0];
-        $this->assertIsA($dataObject, 'SchemaDataObject', 'Taxon should have a data object');
+        $data_dump_url = "http://dl.dropbox.com/u/7597512/MCZHarvard/MCZimages_small_2013.csv";
+        $archive_folder = "test_mczharvard";
+        $connector = new MCZHarvardAPI($archive_folder);
+        $connector->get_all_taxa($data_dump_url);
+        $this->assertTrue(count($connector->taxa) == 1, 'We should have 1 taxon');
+        $taxon = array_shift($connector->taxa);
+        $this->assertTrue($taxon->taxonID == "MCZ_Mantidactylus_tripunctatus", 'We should get the right taxon id');
+        $this->assertTrue($taxon->scientificName == "Mantidactylus tripunctatus", 'We should get the right scientific name');
+        $this->assertTrue($taxon->furtherInformationURL == "http://mczbase.mcz.harvard.edu/SpecimenDetail.cfm?collection_object_id=125329", 'We should get the right furtherInformationURL');
     }
 }
 ?>
