28b0f3a25879be295353f9d3837c7c18a0eaf114
Eli Agbayani <eli@eol.org>
2013-04-01 08:49:58 -0400

    Flora of Brazil connector

---
+++ update_resources/connectors/487.php
@@ -0,0 +1,31 @@
+<?php
+namespace php_active_record;
+/* estimated execution time:   */
+
+include_once(dirname(__FILE__) . "/../../config/environment.php");
+require_library('connectors/SciELOAPI');
+
+$timestart = time_elapsed();
+$resource_id = "487";
+
+$scielo_connector = new SciELOAPI($resource_id);
+$scielo_connector->get_all_taxa();
+
+if(filesize(CONTENT_RESOURCE_LOCAL_PATH . $resource_id . "_working/taxon.tab") > 1000)
+{
+    if(is_dir(CONTENT_RESOURCE_LOCAL_PATH . $resource_id))
+    {
+        recursive_rmdir(CONTENT_RESOURCE_LOCAL_PATH . $resource_id . "_previous");
+        rename(CONTENT_RESOURCE_LOCAL_PATH . $resource_id, CONTENT_RESOURCE_LOCAL_PATH . $resource_id . "_previous");
+    }
+    rename(CONTENT_RESOURCE_LOCAL_PATH . $resource_id . "_working", CONTENT_RESOURCE_LOCAL_PATH . $resource_id);
+    Functions::set_resource_status_to_force_harvest($resource_id);
+}
+
+
+
+$elapsed_time_sec = time_elapsed() - $timestart;
+echo "\n\n elapsed time = " . $elapsed_time_sec/60 . " minutes";
+echo "\n elapsed time = " . $elapsed_time_sec/60/60 . " hours";
+exit("\n Done processing.");
+?>
