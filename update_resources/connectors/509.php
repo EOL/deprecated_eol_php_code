d71d3242e98d0344d4b85f89392d03ef12c8177e
Eli Agbayani <eli@eol.org>
2013-04-11 08:46:35 -0400

    USDA Forest Fire service connector.

---
+++ update_resources/connectors/509.php
@@ -0,0 +1,30 @@
+<?php
+namespace php_active_record;
+include_once(dirname(__FILE__) . "/../../config/environment.php");
+require_library('connectors/USDAfsfeisAPI');
+$timestart = time_elapsed();
+
+$resource_id = "509";
+$group["Animalia"] = "animals";
+
+$func = new USDAfsfeisAPI($resource_id, $group);
+$func->get_all_taxa();
+
+if(filesize(CONTENT_RESOURCE_LOCAL_PATH . $resource_id . "_working/taxon.tab") > 1000)
+{
+    if(is_dir(CONTENT_RESOURCE_LOCAL_PATH . $resource_id))
+    {
+        recursive_rmdir(CONTENT_RESOURCE_LOCAL_PATH . $resource_id . "_previous");
+        rename(CONTENT_RESOURCE_LOCAL_PATH . $resource_id, CONTENT_RESOURCE_LOCAL_PATH . $resource_id . "_previous");
+    }
+    rename(CONTENT_RESOURCE_LOCAL_PATH . $resource_id . "_working", CONTENT_RESOURCE_LOCAL_PATH . $resource_id);
+    Functions::set_resource_status_to_force_harvest($resource_id);
+}
+
+$elapsed_time_sec = time_elapsed() - $timestart;
+echo "\n\n";
+echo "\n elapsed time = $elapsed_time_sec seconds";
+echo "\n elapsed time = " . $elapsed_time_sec/60 . " minutes";
+echo "\n elapsed time = " . $elapsed_time_sec/60/60 . " hours";
+echo "\n Done processing.";
+?>
