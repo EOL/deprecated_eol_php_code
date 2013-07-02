9865ed4057666cb5ee2ccf91137e27d0d911b55b
Patrick Leary <pleary@mbl.edu>
2013-07-02 16:13:52 -0400

    Some refactoring of ContentManager based on work by hyanwong

--- app/models/DataObjects.php
+++ app/models/DataObjects.php
@@ -206,9 +206,9 @@ class DataObject extends ActiveRecord
             if(preg_match("/^http:\/\//",$this->object_url) || preg_match("/^https:\/\//",$this->object_url))
             {
                 // TODO - hardcoded exception to make the Biopix images smaller
-                if($resource->title == "Biopix") $large_thumbnail_dimensions = "300x300";
-                else $large_thumbnail_dimensions = CONTENT_IMAGE_LARGE;
-                $this->object_cache_url = $content_manager->grab_file($this->object_url, 0, "image", $large_thumbnail_dimensions);
+                $thumbnail_options = array();
+                if($resource->title == "Biopix") $thumbnail_options = array('large_image_dimensions' => array(300, 300));
+                $this->object_cache_url = $content_manager->grab_file($this->object_url, 0, "image", $thumbnail_options);
                 if(@!$this->object_cache_url) return false;
             }else return false;
         }
