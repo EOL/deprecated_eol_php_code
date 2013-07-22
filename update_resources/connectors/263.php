610e59a698917acd08112a143372767a32dfa897
Eli Agbayani <eli@eol.org>
2013-07-22 03:19:59 -0400

    NatureServe will not set a force-harvest if main file with names and uids is not available.

--- update_resources/connectors/263.php
+++ update_resources/connectors/263.php
@@ -55,15 +55,10 @@ fclose($resource_file);
 @rename(CONTENT_RESOURCE_LOCAL_PATH . "263.xml", CONTENT_RESOURCE_LOCAL_PATH . "263_previous.xml");
 rename(CONTENT_RESOURCE_LOCAL_PATH . "263_temp.xml", CONTENT_RESOURCE_LOCAL_PATH . "263.xml");
 
-if(filesize(CONTENT_RESOURCE_LOCAL_PATH . "263.xml") > 600)
+if(filesize(CONTENT_RESOURCE_LOCAL_PATH . "263.xml") > 200000)
 {
     $GLOBALS['db_connection']->update("UPDATE resources SET resource_status_id=".ResourceStatus::find_or_create_by_translated_label('Force Harvest')->id." WHERE id=263");
 }
-
-
-
-
-
 
 function lookup_taxa($taxon, $parameters)
 {
