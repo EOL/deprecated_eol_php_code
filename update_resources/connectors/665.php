74832473d3981d91af42948efbabf5fd2b81fa5d
Eli Agbayani <eli@eol.org>
2013-09-30 08:32:29 -0400

    DATA-1234 PESI taxonomy as browsing hierarchy, source for synonyms and vernaculars.

---
+++ update_resources/connectors/665.php
@@ -0,0 +1,31 @@
+<?php
+namespace php_active_record;
+/* PESI
+estimated execution time:
+    4.5 hours - generation of taxa list text file
+    2 days - generation of 5 processed text files - save_data_to_text()
+    2 days - generation of 5 archive files - process_text_file()
+*/
+include_once(dirname(__FILE__) . "/../../config/environment.php");
+require_library('connectors/PesiAPI');
+$timestart = time_elapsed();
+$resource_id = "665";
+$func = new PesiAPI($resource_id);
+$func->get_all_taxa();
+if(filesize(CONTENT_RESOURCE_LOCAL_PATH . $resource_id . "_working/taxon.tab") > 1000)
+{
+    if(is_dir(CONTENT_RESOURCE_LOCAL_PATH . $resource_id))
+    {
+        recursive_rmdir(CONTENT_RESOURCE_LOCAL_PATH . $resource_id . "_previous");
+        rename(CONTENT_RESOURCE_LOCAL_PATH . $resource_id, CONTENT_RESOURCE_LOCAL_PATH . $resource_id . "_previous");
+    }
+    rename(CONTENT_RESOURCE_LOCAL_PATH . $resource_id . "_working", CONTENT_RESOURCE_LOCAL_PATH . $resource_id);
+    rename(CONTENT_RESOURCE_LOCAL_PATH . $resource_id . "_working.tar.gz", CONTENT_RESOURCE_LOCAL_PATH . $resource_id . ".tar.gz");
+    Functions::set_resource_status_to_force_harvest($resource_id);
+}
+$elapsed_time_sec = time_elapsed() - $timestart;
+echo "\n\n";
+echo "elapsed time = " . $elapsed_time_sec/60 . " minutes \n";
+echo "elapsed time = " . $elapsed_time_sec/60/60 . " hours \n";
+echo "\nDone processing.\n";
+?>
