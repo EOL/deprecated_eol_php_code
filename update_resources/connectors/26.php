5bd708517957e165edc701de0085b6c56964eae3
Eli Agbayani <eli@eol.org>
2013-12-30 21:08:07 -0500

    DATA-1378 Ingest new DwC-A export from WoRMS in place of the xml resource. Set Distribution as structured data.

--- update_resources/connectors/26.php
+++ update_resources/connectors/26.php
@@ -1,55 +1,30 @@
 <?php
 namespace php_active_record;
-/* connector for WORMS
-Partner provides a service to get their list of IDs and another service to use the id to get each taxon information.
-estimated execution time: 15 days
-sample API: http://www.marinespecies.org/aphia.php?p=eol&action=taxdetails&id=571925
-
-Connector can still run but takes at least two weeks to process about 201k taxa. For the meantime the connector is disabled.
-Soon this connector will be obsolete as WORMS agreed to provide a DWC-A resource. No timetable yet though.
+/*
+WORMS archive
+Now partner provides/hosts a DWC-A file. Connector also converts Distribution text into structured data.
+estimated execution time:
 */
-
-// include_once(dirname(__FILE__) . "/../../config/environment.php");
-// 
-// define('DOWNLOAD_WAIT_TIME', '200000');
-// include_once(dirname(__FILE__) . "/../../config/environment.php");
-// $timestart = time_elapsed();
-// require_library('connectors/WormsAPI');
-// $resource_id = 26;
-// 
-// $worms = new WormsAPI();
-// $worms->initialize_text_files();
-// // Functions::kill_running_connectors($resource_id);
-// $worms->start_process($resource_id, false);
-// 
-// $elapsed_time_sec = time_elapsed() - $timestart;
-// echo "\n";
-// echo "elapsed time = " . $elapsed_time_sec/60/60 . " hours \n";
-// echo "elapsed time = " . $elapsed_time_sec/60/60/24 . " days \n";
-// echo date('Y-m-d h:i:s a', time())."\n";
-// echo "\n\n Done processing.";
-
-
-function worms_check()
+include_once(dirname(__FILE__) . "/../../config/environment.php");
+require_library('connectors/WormsArchiveAPI');
+$timestart = time_elapsed();
+$resource_id = 26;
+$func = new WormsArchiveAPI($resource_id);
+$func->get_all_taxa();
+if(filesize(CONTENT_RESOURCE_LOCAL_PATH . $resource_id . "_working/taxon.tab") > 1000) // orig 1000
 {
-    $path = "http://localhost/~eolit/eli/eol_php_code/applications/content_server/resources/26.xml";
-    print "\n xml file: [$path] \n";
-    $reader = new \XMLReader();
-    $reader->open($path);
-    $i = 0;
-    while(@$reader->read())
+    if(is_dir(CONTENT_RESOURCE_LOCAL_PATH . $resource_id))
     {
-        if($reader->nodeType == \XMLReader::ELEMENT && $reader->name == "taxon")
-        {
-            $string = $reader->readOuterXML();
-            if($xml = simplexml_load_string($string)) print " ok ";
-            else 
-            {
-                print " bad "; $i++;
-            }
-        }
+        recursive_rmdir(CONTENT_RESOURCE_LOCAL_PATH . $resource_id . "_previous");
+        rename(CONTENT_RESOURCE_LOCAL_PATH . $resource_id, CONTENT_RESOURCE_LOCAL_PATH . $resource_id . "_previous");
     }
-    print "\n\n invalid xml: $i";
+    rename(CONTENT_RESOURCE_LOCAL_PATH . $resource_id . "_working", CONTENT_RESOURCE_LOCAL_PATH . $resource_id);
+    rename(CONTENT_RESOURCE_LOCAL_PATH . $resource_id . "_working.tar.gz", CONTENT_RESOURCE_LOCAL_PATH . $resource_id . ".tar.gz");
+    Functions::set_resource_status_to_force_harvest($resource_id);
 }
-
+$elapsed_time_sec = time_elapsed() - $timestart;
+echo "\n\n";
+echo "\n elapsed time = " . $elapsed_time_sec/60 . " minutes";
+echo "\n elapsed time = " . $elapsed_time_sec/60/60 . " hours";
+echo "\n Done processing.\n";
 ?>
