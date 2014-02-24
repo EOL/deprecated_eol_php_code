7cb204f347ffb71f45dd02a10e3fb934ae90fc4a
Eli Agbayani <eli@eol.org>
2014-02-23 19:46:00 -0500

    DATA-1406 Convert relevant parts of IUCN harvest to TraitBank

---
+++ update_resources/connectors/737.php
@@ -0,0 +1,34 @@
+<?php
+namespace php_active_record;
+/*
+execution time: 
+*/
+
+include_once(dirname(__FILE__) . "/../../config/environment.php");
+require_library('connectors/IUCNRedlistDataConnector');
+
+$timestart = time_elapsed();
+$resource_id = 737;
+$func = new IUCNRedlistDataConnector($resource_id);
+
+$func->generate_IUCN_data();
+
+if(filesize(CONTENT_RESOURCE_LOCAL_PATH . $resource_id . "_working/taxon.tab") > 1000)
+{
+    if(is_dir(CONTENT_RESOURCE_LOCAL_PATH . $resource_id))
+    {
+        recursive_rmdir(CONTENT_RESOURCE_LOCAL_PATH . $resource_id . "_previous");
+        rename(CONTENT_RESOURCE_LOCAL_PATH . $resource_id, CONTENT_RESOURCE_LOCAL_PATH . $resource_id . "_previous");
+    }
+    rename(CONTENT_RESOURCE_LOCAL_PATH . $resource_id . "_working", CONTENT_RESOURCE_LOCAL_PATH . $resource_id);
+    rename(CONTENT_RESOURCE_LOCAL_PATH . $resource_id . "_working.tar.gz", CONTENT_RESOURCE_LOCAL_PATH . $resource_id . ".tar.gz");
+    Functions::set_resource_status_to_force_harvest($resource_id);
+}
+
+$elapsed_time_sec = time_elapsed() - $timestart;
+echo "\n\n";
+echo "\n elapsed time = " . $elapsed_time_sec . " seconds";
+echo "\n elapsed time = " . $elapsed_time_sec/60 . " minutes";
+echo "\n elapsed time = " . $elapsed_time_sec/60/60 . " hours";
+echo "\n Done processing.\n";
+?>
