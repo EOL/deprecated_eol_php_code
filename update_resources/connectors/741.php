7a09b989c3ad775fee9c440a5b2c7ccdecadf889
Eli Agbayani <eli@eol.org>
2014-03-19 07:32:02 -0400

    DATA-1409, DATA-1410 IRMNG classification and structured data.

---
+++ update_resources/connectors/741.php
@@ -0,0 +1,36 @@
+<?php
+namespace php_active_record;
+/* IRMNG data
+   execution time: 2.23 hours; 2,032,569 records to process
+*/
+
+include_once(dirname(__FILE__) . "/../../config/environment.php");
+require_library('connectors/IrmngAPI');
+$timestart = time_elapsed();
+$resource_id = 741;
+$func = new IrmngAPI($resource_id);
+// $func->get_taxa_without_status_but_with_eol_page(); //utility
+$func->get_all_taxa("data");
+
+if(filesize(CONTENT_RESOURCE_LOCAL_PATH . $resource_id . "_working/taxon.tab") > 1000)
+{
+    if(is_dir(CONTENT_RESOURCE_LOCAL_PATH . $resource_id))
+    {
+        recursive_rmdir(CONTENT_RESOURCE_LOCAL_PATH . $resource_id . "_previous");
+        rename(CONTENT_RESOURCE_LOCAL_PATH . $resource_id, CONTENT_RESOURCE_LOCAL_PATH . $resource_id . "_previous");
+    }
+    rename(CONTENT_RESOURCE_LOCAL_PATH . $resource_id . "_working", CONTENT_RESOURCE_LOCAL_PATH . $resource_id);
+    rename(CONTENT_RESOURCE_LOCAL_PATH . $resource_id . "_working.tar.gz", CONTENT_RESOURCE_LOCAL_PATH . $resource_id . ".tar.gz");
+    Functions::set_resource_status_to_force_harvest($resource_id);
+}
+
+$command_line = "tar -czf " . CONTENT_RESOURCE_LOCAL_PATH . $resource_id . ".tar.gz --directory=" . CONTENT_RESOURCE_LOCAL_PATH . $resource_id . " .";
+$output = shell_exec($command_line);
+
+$elapsed_time_sec = time_elapsed() - $timestart;
+echo "\n\n";
+echo "\n elapsed time = " . $elapsed_time_sec . " seconds";
+echo "\n elapsed time = " . $elapsed_time_sec/60 . " minutes";
+echo "\n elapsed time = " . $elapsed_time_sec/60/60 . " hours";
+echo "\n Done processing.\n";
+?>
---
+++ update_resources/connectors/752.php
@@ -0,0 +1,33 @@
+<?php
+namespace php_active_record;
+/* IRMNG classification
+   execution time: 46 minutes; 2million+ records to process
+*/
+
+include_once(dirname(__FILE__) . "/../../config/environment.php");
+require_library('connectors/IrmngAPI');
+
+$timestart = time_elapsed();
+$resource_id = 752;
+$func = new IrmngAPI($resource_id);
+$func->get_all_taxa("classification");
+
+if(filesize(CONTENT_RESOURCE_LOCAL_PATH . $resource_id . "_working/taxon.tab") > 1000)
+{
+    if(is_dir(CONTENT_RESOURCE_LOCAL_PATH . $resource_id))
+    {
+        recursive_rmdir(CONTENT_RESOURCE_LOCAL_PATH . $resource_id . "_previous");
+        rename(CONTENT_RESOURCE_LOCAL_PATH . $resource_id, CONTENT_RESOURCE_LOCAL_PATH . $resource_id . "_previous");
+    }
+    rename(CONTENT_RESOURCE_LOCAL_PATH . $resource_id . "_working", CONTENT_RESOURCE_LOCAL_PATH . $resource_id);
+    rename(CONTENT_RESOURCE_LOCAL_PATH . $resource_id . "_working.tar.gz", CONTENT_RESOURCE_LOCAL_PATH . $resource_id . ".tar.gz");
+    Functions::set_resource_status_to_force_harvest($resource_id);
+}
+
+$elapsed_time_sec = time_elapsed() - $timestart;
+echo "\n\n";
+echo "\n elapsed time = " . $elapsed_time_sec . " seconds";
+echo "\n elapsed time = " . $elapsed_time_sec/60 . " minutes";
+echo "\n elapsed time = " . $elapsed_time_sec/60/60 . " hours";
+echo "\n Done processing.\n";
+?>
