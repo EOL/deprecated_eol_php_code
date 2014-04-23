9903d4b3971c8406a2be9c7211f73110902ca111
Eli Agbayani <eli@eol.org>
2014-04-22 23:46:16 -0400

    Quickfix. Avoid 'Notice' message if param 'cache' does not exist.

--- lib/Functions.php
+++ lib/Functions.php
@@ -222,7 +222,7 @@ class Functions
         if(isset($options['file_extension'])) $temp_path .= "." . $options['file_extension'];
         debug("\n\n Saving remote file: " . $url);
         debug("\n\n Temporary file: " . $temp_path);
-        if($options['cache']) $file_contents = self::lookup_with_cache($url, $options);
+        if(@$options['cache']) $file_contents = self::lookup_with_cache($url, $options);
         else $file_contents = self::get_remote_file($url, $options);
         if($file_contents)
         {
