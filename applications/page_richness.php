2cb575494bde7c7e2d3e6c5dc0fa765be86d99bc
Patrick Leary <pleary@mbl.edu>
2014-04-23 14:12:03 -0400

    Fixed page richness calculator

--- applications/page_richness.php
+++ applications/page_richness.php
@@ -45,38 +45,38 @@ namespace php_active_record;
           </tr>
           <tr>
             <td>Images:</td>
-            <td><input type='text' size='5' name='IMAGE_BREADTH_MAX' value='<?= $IMAGE_BREADTH_MAX; ?>'/></td>
-            <td><input type='text' size='5' name='IMAGE_BREADTH_WEIGHT' value='<?= $IMAGE_BREADTH_WEIGHT; ?>'/></td>
+            <td><input type='text' size='5' name='IMAGE_BREADTH_MAX' value='<?php echo $IMAGE_BREADTH_MAX; ?>'/></td>
+            <td><input type='text' size='5' name='IMAGE_BREADTH_WEIGHT' value='<?php echo $IMAGE_BREADTH_WEIGHT; ?>'/></td>
           </tr>
           <tr>
             <td>InfoItems:</td>
-            <td><input type='text' size='5' name='INFO_ITEM_BREADTH_MAX' value='<?= $INFO_ITEM_BREADTH_MAX; ?>'/></td>
-            <td><input type='text' size='5' name='INFO_ITEM_BREADTH_WEIGHT' value='<?= $INFO_ITEM_BREADTH_WEIGHT; ?>'/></td>
+            <td><input type='text' size='5' name='INFO_ITEM_BREADTH_MAX' value='<?php echo $INFO_ITEM_BREADTH_MAX; ?>'/></td>
+            <td><input type='text' size='5' name='INFO_ITEM_BREADTH_WEIGHT' value='<?php echo $INFO_ITEM_BREADTH_WEIGHT; ?>'/></td>
           </tr>
           <tr>
             <td>References:</td>
-            <td><input type='text' size='5' name='REFERENCE_BREADTH_MAX' value='<?= $REFERENCE_BREADTH_MAX; ?>'/></td>
-            <td><input type='text' size='5' name='REFERENCE_BREADTH_WEIGHT' value='<?= $REFERENCE_BREADTH_WEIGHT; ?>'/></td>
+            <td><input type='text' size='5' name='REFERENCE_BREADTH_MAX' value='<?php echo $REFERENCE_BREADTH_MAX; ?>'/></td>
+            <td><input type='text' size='5' name='REFERENCE_BREADTH_WEIGHT' value='<?php echo $REFERENCE_BREADTH_WEIGHT; ?>'/></td>
           </tr>
           <tr>
             <td>Maps:</td>
-            <td><input type='text' size='5' name='MAP_BREADTH_MAX' value='<?= $MAP_BREADTH_MAX; ?>'/></td>
-            <td><input type='text' size='5' name='MAP_BREADTH_WEIGHT' value='<?= $MAP_BREADTH_WEIGHT; ?>'/></td>
+            <td><input type='text' size='5' name='MAP_BREADTH_MAX' value='<?php echo $MAP_BREADTH_MAX; ?>'/></td>
+            <td><input type='text' size='5' name='MAP_BREADTH_WEIGHT' value='<?php echo $MAP_BREADTH_WEIGHT; ?>'/></td>
           </tr>
           <tr>
             <td>Videos:</td>
-            <td><input type='text' size='5' name='VIDEO_BREADTH_MAX' value='<?= $VIDEO_BREADTH_MAX; ?>'/></td>
-            <td><input type='text' size='5' name='VIDEO_BREADTH_WEIGHT' value='<?= $VIDEO_BREADTH_WEIGHT; ?>'/></td>
+            <td><input type='text' size='5' name='VIDEO_BREADTH_MAX' value='<?php echo $VIDEO_BREADTH_MAX; ?>'/></td>
+            <td><input type='text' size='5' name='VIDEO_BREADTH_WEIGHT' value='<?php echo $VIDEO_BREADTH_WEIGHT; ?>'/></td>
           </tr>
           <tr>
             <td>Sounds:</td>
-            <td><input type='text' size='5' name='SOUND_BREADTH_MAX' value='<?= $SOUND_BREADTH_MAX; ?>'/></td>
-            <td><input type='text' size='5' name='SOUND_BREADTH_WEIGHT' value='<?= $SOUND_BREADTH_WEIGHT; ?>'/></td>
+            <td><input type='text' size='5' name='SOUND_BREADTH_MAX' value='<?php echo $SOUND_BREADTH_MAX; ?>'/></td>
+            <td><input type='text' size='5' name='SOUND_BREADTH_WEIGHT' value='<?php echo $SOUND_BREADTH_WEIGHT; ?>'/></td>
           </tr>
           <tr>
             <td>IUCN:</td>
-            <td><input type='text' size='5' name='IUCN_BREADTH_MAX' value='<?= $IUCN_BREADTH_MAX; ?>'/></td>
-            <td><input type='text' size='5' name='IUCN_BREADTH_WEIGHT' value='<?= $IUCN_BREADTH_WEIGHT; ?>'/></td>
+            <td><input type='text' size='5' name='IUCN_BREADTH_MAX' value='<?php echo $IUCN_BREADTH_MAX; ?>'/></td>
+            <td><input type='text' size='5' name='IUCN_BREADTH_WEIGHT' value='<?php echo $IUCN_BREADTH_WEIGHT; ?>'/></td>
           </tr>
         </table>
       </td>
@@ -91,13 +91,13 @@ namespace php_active_record;
           </tr>
           <tr>
             <td>#Words per text:</td>
-            <td><input type='text' size='5' name='TEXT_AVERAGE_MAX' value='<?= $TEXT_AVERAGE_MAX; ?>'/></td>
-            <td><input type='text' size='5' name='TEXT_AVERAGE_WEIGHT' value='<?= $TEXT_AVERAGE_WEIGHT; ?>'/></td>
+            <td><input type='text' size='5' name='TEXT_AVERAGE_MAX' value='<?php echo $TEXT_AVERAGE_MAX; ?>'/></td>
+            <td><input type='text' size='5' name='TEXT_AVERAGE_WEIGHT' value='<?php echo $TEXT_AVERAGE_WEIGHT; ?>'/></td>
           </tr>
           <tr>
             <td>#Words total:</td>
-            <td><input type='text' size='5' name='TEXT_TOTAL_MAX' value='<?= $TEXT_TOTAL_MAX; ?>'/></td>
-            <td><input type='text' size='5' name='TEXT_TOTAL_WEIGHT' value='<?= $TEXT_TOTAL_WEIGHT; ?>'/></td>
+            <td><input type='text' size='5' name='TEXT_TOTAL_MAX' value='<?php echo $TEXT_TOTAL_MAX; ?>'/></td>
+            <td><input type='text' size='5' name='TEXT_TOTAL_WEIGHT' value='<?php echo $TEXT_TOTAL_WEIGHT; ?>'/></td>
           </tr>
         </table>
       </td>
@@ -111,32 +111,32 @@ namespace php_active_record;
           </tr>
           <tr>
             <td>Partners:</td>
-            <td><input type='text' size='5' name='PARTNERS_DIVERSITY_MAX' value='<?= $PARTNERS_DIVERSITY_MAX; ?>'/></td>
-            <td><input type='text' size='5' name='PARTNERS_DIVERSITY_WEIGHT' value='<?= $PARTNERS_DIVERSITY_WEIGHT; ?>'/></td>
+            <td><input type='text' size='5' name='PARTNERS_DIVERSITY_MAX' value='<?php echo $PARTNERS_DIVERSITY_MAX; ?>'/></td>
+            <td><input type='text' size='5' name='PARTNERS_DIVERSITY_WEIGHT' value='<?php echo $PARTNERS_DIVERSITY_WEIGHT; ?>'/></td>
           </tr>
         </table>
       </td>
     </tr>
     <tr>
-      <td class='category_weight'>Category Weight: <input type='text' size='5' name='BREADTH_WEIGHT' value='<?= $BREADTH_WEIGHT; ?>'/></td>
-      <td class='category_weight'>Category Weight: <input type='text' size='5' name='DEPTH_WEIGHT' value='<?= $DEPTH_WEIGHT; ?>'/></td>
-      <td class='category_weight'>Category Weight: <input type='text' size='5' name='DIVERSITY_WEIGHT' value='<?= $DIVERSITY_WEIGHT; ?>'/></td>
+      <td class='category_weight'>Category Weight: <input type='text' size='5' name='BREADTH_WEIGHT' value='<?php echo $BREADTH_WEIGHT; ?>'/></td>
+      <td class='category_weight'>Category Weight: <input type='text' size='5' name='DEPTH_WEIGHT' value='<?php echo $DEPTH_WEIGHT; ?>'/></td>
+      <td class='category_weight'>Category Weight: <input type='text' size='5' name='DIVERSITY_WEIGHT' value='<?php echo $DIVERSITY_WEIGHT; ?>'/></td>
     </tr>
     <tr>
-      <td colspan='3' class='category_weight'>Vetted Factor: <input type='text' size='5' name='VETTED_FACTOR' value='<?= $VETTED_FACTOR; ?>'/></td>
+      <td colspan='3' class='category_weight'>Vetted Factor: <input type='text' size='5' name='VETTED_FACTOR' value='<?php echo $VETTED_FACTOR; ?>'/></td>
     </tr>
   </table>
   <br/><br/>
   <table class='dual_input'><tr><td>
-      PageID to evaluate: <input type='text' size='20' name='taxon_concept_id' value='<?= $taxon_concept_id ?>'/>
+      PageID to evaluate: <input type='text' size='20' name='taxon_concept_id' value='<?php echo $taxon_concept_id ?>'/>
       <input type='submit' value='Calculate'/>
-      <input type='hidden' name='ENV_NAME' value='<?= $GLOBALS['ENV_NAME']; ?>'/>
+      <input type='hidden' name='ENV_NAME' value='<?php echo $GLOBALS['ENV_NAME']; ?>'/>
       <hr/>
       <? show_results_for($taxon_concept_id); ?>
   </td><td class='second'>
-      PageID to evaluate: <input type='text' size='20' name='taxon_concept_id2' value='<?= $taxon_concept_id2 ?>'/>
+      PageID to evaluate: <input type='text' size='20' name='taxon_concept_id2' value='<?php echo $taxon_concept_id2 ?>'/>
       <input type='submit' value='Calculate'/>
-      <input type='hidden' name='ENV_NAME' value='<?= $GLOBALS['ENV_NAME']; ?>'/>
+      <input type='hidden' name='ENV_NAME' value='<?php echo $GLOBALS['ENV_NAME']; ?>'/>
       </form>
       <hr/>
       <? show_results_for($taxon_concept_id2); ?>
@@ -164,69 +164,70 @@ function show_results_for($taxon_concept_id)
         $scores = $metric->scores();
         
         ?>
-        <h3><a href='http://eol.org/pages/<?= $taxon_concept_id; ?>/overview' target='_blank'><?= $name; ?></a></h3>
+        <h3><a href='http://eol.org/pages/<?php echo $taxon_concept_id; ?>/overview' target='_blank'><?php echo $name; ?></a></h3>
         <table class='results'>
-          <tr><td>Breadth:</td><td><?= round($scores['breadth'] / $metric->BREADTH_WEIGHT, 4); ?></td></tr>
-          <tr><td>Depth:</td><td><?= round($scores['depth'] / $metric->DEPTH_WEIGHT, 4); ?></td></tr>
-          <tr><td>Diversity:</td><td><?= round($scores['diversity'] / $metric->DIVERSITY_WEIGHT, 4); ?></td></tr>
-          <tr><td>Total:</td><td><?= round($scores['total'], 4); ?></td></tr>
+          <tr><td>Breadth:</td><td><?php echo round($scores['breadth'] / $metric->BREADTH_WEIGHT, 4); ?></td></tr>
+          <tr><td>Depth:</td><td><?php echo round($scores['depth'] / $metric->DEPTH_WEIGHT, 4); ?></td></tr>
+          <tr><td>Diversity:</td><td><?php echo round($scores['diversity'] / $metric->DIVERSITY_WEIGHT, 4); ?></td></tr>
+          <tr><td>Total:</td><td><?php echo round($scores['total'], 4); ?></td></tr>
         </table>
         <hr/>
         <table class='results'>
           <tr><th>Stat</th><th>Value</th><th>Max</th><th>Impact on Score</th><th>Max</th></tr>
-          <tr><td>Images:</td><td><?= $metric->weighted_images(); ?></td>
-            <td class='max_score'><?= $metric->IMAGE_BREADTH_MAX; ?></td>
-            <td><?= $metric->image_score(); ?></td>
-            <td class='max_score'>/<?= $metric->IMAGE_BREADTH_WEIGHT * $metric->BREADTH_WEIGHT; ?></td></tr>
+          <tr><td>Images:</td><td><?php echo $metric->weighted_images(); ?></td>
+            <td class='max_score'><?php echo $metric->IMAGE_BREADTH_MAX; ?></td>
+            <td><?php echo $metric->image_score(); ?></td>
+            <td class='max_score'>/<?php echo $metric->IMAGE_BREADTH_WEIGHT * $metric->BREADTH_WEIGHT; ?></td></tr>
           
-          <tr><td>InfoItems:</td><td><?= $metric->info_items; ?></td>
-            <td class='max_score'><?= $metric->INFO_ITEM_BREADTH_MAX; ?></td>
-            <td><?= $metric->info_items_score(); ?></td>
-            <td class='max_score'>/<?= $metric->INFO_ITEM_BREADTH_WEIGHT * $metric->BREADTH_WEIGHT; ?></td></tr>
+          <tr><td>InfoItems:</td><td><?php echo $metric->info_items; ?></td>
+            <td class='max_score'><?php echo $metric->INFO_ITEM_BREADTH_MAX; ?></td>
+            <td><?php echo $metric->info_items_score(); ?></td>
+            <td class='max_score'>/<?php echo $metric->INFO_ITEM_BREADTH_WEIGHT * $metric->BREADTH_WEIGHT; ?></td></tr>
           
-          <tr><td>References:</td><td><?= $metric->references(); ?></td>
-            <td class='max_score'><?= $metric->REFERENCE_BREADTH_MAX; ?></td>
-            <td><?= $metric->references_score(); ?></td>
-            <td class='max_score'>/<?= $metric->REFERENCE_BREADTH_WEIGHT * $metric->BREADTH_WEIGHT; ?></td></tr>
+          <tr><td>References:</td><td><?php echo $metric->references(); ?></td>
+            <td class='max_score'><?php echo $metric->REFERENCE_BREADTH_MAX; ?></td>
+            <td><?php echo $metric->references_score(); ?></td>
+            <td class='max_score'>/<?php echo $metric->REFERENCE_BREADTH_WEIGHT * $metric->BREADTH_WEIGHT; ?></td></tr>
           
-          <tr><td>Maps:</td><td><?= $metric->has_GBIF_map; ?></td>
-            <td class='max_score'><?= $metric->MAP_BREADTH_MAX; ?></td>
-            <td><?= $metric->maps_score(); ?></td>
-            <td class='max_score'>/<?= $metric->MAP_BREADTH_WEIGHT * $metric->BREADTH_WEIGHT; ?></td></tr>
+          <tr><td>Maps:</td><td><?php echo $metric->has_GBIF_map; ?></td>
+            <td class='max_score'><?php echo $metric->MAP_BREADTH_MAX; ?></td>
+            <td><?php echo $metric->maps_score(); ?></td>
+            <td class='max_score'>/<?php echo $metric->MAP_BREADTH_WEIGHT * $metric->BREADTH_WEIGHT; ?></td></tr>
           
-          <tr><td>Videos:</td><td><?= $metric->weighted_videos(); ?></td>
-            <td class='max_score'><?= $metric->VIDEO_BREADTH_MAX; ?></td>
-            <td><?= $metric->videos_score(); ?></td>
-            <td class='max_score'>/<?= $metric->VIDEO_BREADTH_WEIGHT * $metric->BREADTH_WEIGHT; ?></td></tr>
+          <tr><td>Videos:</td><td><?php echo $metric->weighted_videos(); ?></td>
+            <td class='max_score'><?php echo $metric->VIDEO_BREADTH_MAX; ?></td>
+            <td><?php echo $metric->videos_score(); ?></td>
+            <td class='max_score'>/<?php echo $metric->VIDEO_BREADTH_WEIGHT * $metric->BREADTH_WEIGHT; ?></td></tr>
           
-          <tr><td>Sounds:</td><td><?= $metric->weighted_sounds(); ?></td>
-            <td class='max_score'><?= $metric->SOUND_BREADTH_MAX; ?></td>
-            <td><?= $metric->sounds_score(); ?></td>
-            <td class='max_score'>/<?= $metric->SOUND_BREADTH_WEIGHT * $metric->BREADTH_WEIGHT; ?></td></tr>
+          <tr><td>Sounds:</td><td><?php echo $metric->weighted_sounds(); ?></td>
+            <td class='max_score'><?php echo $metric->SOUND_BREADTH_MAX; ?></td>
+            <td><?php echo $metric->sounds_score(); ?></td>
+            <td class='max_score'>/<?php echo $metric->SOUND_BREADTH_WEIGHT * $metric->BREADTH_WEIGHT; ?></td></tr>
           
-          <tr><td>IUCN:</td><td><?= $metric->iucn_total; ?></td>
-            <td class='max_score'><?= $metric->IUCN_BREADTH_MAX; ?></td>
-            <td><?= $metric->iucn_score(); ?></td>
-            <td class='max_score'>/<?= $metric->IUCN_BREADTH_WEIGHT * $metric->BREADTH_WEIGHT; ?></td></tr>
+          <tr><td>IUCN:</td><td><?php echo $metric->iucn_total; ?></td>
+            <td class='max_score'><?php echo $metric->IUCN_BREADTH_MAX; ?></td>
+            <td><?php echo $metric->iucn_score(); ?></td>
+            <td class='max_score'>/<?php echo $metric->IUCN_BREADTH_WEIGHT * $metric->BREADTH_WEIGHT; ?></td></tr>
           
-          <tr><td>Average #Words:</td><td><?= round($metric->average_words_weighted()); ?></td>
-            <td class='max_score'><?= $metric->TEXT_AVERAGE_MAX; ?></td>
-            <td><?= $metric->average_words_score(); ?></td>
-            <td class='max_score'>/<?= $metric->TEXT_AVERAGE_WEIGHT * $metric->DEPTH_WEIGHT; ?></td></tr>
+          <tr><td>Average #Words:</td><td><?php echo round($metric->average_words_weighted()); ?></td>
+            <td class='max_score'><?php echo $metric->TEXT_AVERAGE_MAX; ?></td>
+            <td><?php echo $metric->average_words_score(); ?></td>
+            <td class='max_score'>/<?php echo $metric->TEXT_AVERAGE_WEIGHT * $metric->DEPTH_WEIGHT; ?></td></tr>
           
-          <tr><td>Total #Words:</td><td><?= round($metric->weighted_text_words()); ?></td>
-            <td class='max_score'><?= $metric->TEXT_TOTAL_MAX; ?></td>
-            <td><?= $metric->total_words_score(); ?></td>
-            <td class='max_score'>/<?= $metric->TEXT_TOTAL_WEIGHT * $metric->DEPTH_WEIGHT; ?></td></tr>
+          <tr><td>Total #Words:</td><td><?php echo round($metric->weighted_text_words()); ?></td>
+            <td class='max_score'><?php echo $metric->TEXT_TOTAL_MAX; ?></td>
+            <td><?php echo $metric->total_words_score(); ?></td>
+            <td class='max_score'>/<?php echo $metric->TEXT_TOTAL_WEIGHT * $metric->DEPTH_WEIGHT; ?></td></tr>
           
-          <tr><td>Content Partners:</td><td><?= $metric->content_partners(); ?></td>
-            <td class='max_score'><?= $metric->PARTNERS_DIVERSITY_MAX; ?></td>
-            <td><?= $metric->content_partners_score(); ?></td>
-            <td class='max_score'>/<?= $metric->PARTNERS_DIVERSITY_WEIGHT * $metric->DIVERSITY_WEIGHT; ?></td></tr>
+          <tr><td>Content Partners:</td><td><?php echo $metric->content_partners(); ?></td>
+            <td class='max_score'><?php echo $metric->PARTNERS_DIVERSITY_MAX; ?></td>
+            <td><?php echo $metric->content_partners_score(); ?></td>
+            <td class='max_score'>/<?php echo $metric->PARTNERS_DIVERSITY_WEIGHT * $metric->DIVERSITY_WEIGHT; ?></td></tr>
         </table>
-        <?
+        <?php
     }
 }
 
 
 
+?>
