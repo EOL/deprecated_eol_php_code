1abbb64aeb2cbda50bc2f10849a1997ee3b6e349
Patrick Leary <pleary@mbl.edu>
2014-06-09 16:03:55 -0400

    Updated sample ENVs; updated Flickr API test

--- config/environments/development_sample.php
+++ config/environments/development_sample.php
@@ -1,15 +1,16 @@
 <?php
 
+ini_set('display_errors', true);
+
 // make sure you define the proper URL to the root directory of this installation
 define('WEB_ROOT', 'http://localhost/eol_php_code/');
 
-define("FLICKR_API_KEY", "xxx");
-define("FLICKR_SHARED_SECRET", "xxx");
-define("FLICKR_AUTH_TOKEN", "xxx");
-define("FLICKR_PLEARY_USER_ID", "xxx");
+define('FLICKR_API_KEY', 'xxx');
+define('FLICKR_SHARED_SECRET', 'xxx');
+define('FLICKR_AUTH_TOKEN', 'xxx');
 
 define('SOLR_SERVER', 'http://localhost:8983/solr/');
 
-define("LOGGING_DB", "eol_logging_development");
+define('LOGGING_DB', 'eol_logging_development');
 
 ?>
--- config/environments/test_sample.php
+++ config/environments/test_sample.php
@@ -1,17 +1,19 @@
 <?php
 
+ini_set('display_errors', true);
+
+$GLOBALS['ENV_DEBUG'] = false;
+$GLOBALS['ENV_DEBUG_TO_FILE'] = true;
+
 // make sure you define the proper URL to the root directory of this installation
 define('WEB_ROOT', 'http://localhost/eol_php_code/');
 
-$GLOBALS['ENV_DEBUG'] = false;
-
-define("FLICKR_API_KEY", "xxx");
-define("FLICKR_SHARED_SECRET", "xxx");
-define("FLICKR_AUTH_TOKEN", "xxx");
-define("FLICKR_PLEARY_USER_ID", "xxx");
+define('FLICKR_API_KEY', 'xxx');
+define('FLICKR_SHARED_SECRET', 'xxx');
+define('FLICKR_AUTH_TOKEN', 'xxx');
 
 define('SOLR_SERVER', 'http://localhost:8983/solr/');
 
-define("LOGGING_DB", "eol_logging_test");
+define('LOGGING_DB', 'eol_logging_test');
 
 ?>
