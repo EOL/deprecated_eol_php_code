200c1a8a7be3f60390007a13a0c80ac47e8b852f
Nathan Wilson <nathan@collectivesource.com>
2014-08-08 18:13:37 +0000

    PHP tests now pass without eol-private
    Currently it essentially turns off the Flickr tests since they rely on
    resolving Flickr URLs.  Ideally this would be handled with web mocks,
    but the current PHP testing infrastructure we are using, SimpleTest,
    doesn't support this.  We should either change to PHPUnit or convert
    all the harvesting code over to Ruby.

---
+++ config/environments/test.php
@@ -0,0 +1,12 @@
+<?php
+
+ini_set('display_errors', true);
+
+$GLOBALS['ENV_DEBUG'] = false;
+$GLOBALS['ENV_DEBUG_TO_FILE'] = true;
+
+define('SOLR_SERVER', 'http://localhost:8983/solr/');
+
+define("LOGGING_DB", "eol_logging_test");
+
+?>
--- tests/unit/test_flickr_api.php
+++ tests/unit/test_flickr_api.php
@@ -7,66 +7,73 @@ define("FLICKR_PLEARY_USER_ID", "11571226@N04");
 
 class test_flickr_api extends SimpletestUnitBase
 {
+    function hasFlickrAPIKey()
+    {
+        if (defined('FLICKR_API_KEY'))
+        {
+            return true;
+        } else {
+            echo "      **SKIPPING FLICKR TEST. NEED TO DEFINE FLICKR_API_KEY, FLICKR_EOL_GROUP_ID ETC.**\n";
+            return false;
+        }
+    }
+
     function testPoolsGetPhotos()
     {
-        if(!defined('FLICKR_API_KEY') || FLICKR_API_KEY == 'xxx')
+        if(self::hasFlickrAPIKey())
         {
-            echo "      **SKIPPING FLICKR TEST. NEED TO DEFINE FLICKR_API_KEY, FLICKR_EOL_GROUP_ID ETC.**\n";
-            return;
+	          $response = FlickrAPI::pools_get_photos(FLICKR_EOL_GROUP_ID, "", 1, 1, FLICKR_AUTH_TOKEN);
+            $this->assertTrue($response->stat == 'ok', 'pools_get_photos should return a stat of OK');
+            $this->assertTrue($response->photos->pages > 45000, 'EOL pool should have more than 45000 photos');
         }
-        $response = FlickrAPI::pools_get_photos(FLICKR_EOL_GROUP_ID, "", 1, 1, FLICKR_AUTH_TOKEN);
-        $this->assertTrue($response->stat == 'ok', 'pools_get_photos should return a stat of OK');
-        $this->assertTrue($response->photos->pages > 45000, 'EOL pool should have more than 45000 photos');
     }
     
     function testGetEOLPhotos()
     {
-        if(!defined('FLICKR_API_KEY') || FLICKR_API_KEY == 'xxx')
+        if(self::hasFlickrAPIKey())
         {
-            echo "      **SKIPPING FLICKR TEST. NEED TO DEFINE FLICKR_API_KEY, FLICKR_EOL_GROUP_ID ETC.**\n";
-            return;
+	          $response = FlickrAPI::pools_get_photos(FLICKR_EOL_GROUP_ID, "", 5, 1, FLICKR_AUTH_TOKEN, FLICKR_PLEARY_USER_ID);
+            $this->assertTrue($response->photos->total == 4, 'Patrick should have submitted only 4 photos');
+        
+            $first_photo = $response->photos->photo[3];
+            $taxa = FlickrApi::get_taxa_for_photo($first_photo->id, $first_photo->secret, FLICKR_AUTH_TOKEN);
+            $this->assertTrue(is_array($taxa), 'Taxa should be an array');
+        
+            $taxon = $taxa[0];
+            $this->assertIsA($taxon, 'SchemaTaxon', 'Response should be an array of SchemaTaxon');
+            $this->assertTrue($taxon->kingdom == 'Animalia', 'Taxon should have the right kingdom');
+            $this->assertTrue($taxon->phylum == 'Chordata', 'Taxon should have the right phylum');
+            $this->assertTrue($taxon->class == 'Aves', 'Taxon should have the right class');
+            $this->assertTrue($taxon->order == 'Ciconiiformes', 'Taxon should have the right order');
+            $this->assertTrue($taxon->family == 'Ardeidae', 'Taxon should have the right family');
+            $this->assertTrue($taxon->genus == 'Ardea', 'Taxon should have the right genus');
+            $this->assertTrue($taxon->scientificName == 'Ardea herodias', 'Taxon should have the right name');
+        
+            $this->assertIsA($taxon->commonNames[0], 'SchemaCommonName', 'Taxon should have a common name');
+            $this->assertTrue($taxon->commonNames[0]->name == 'Great Blue Heron', 'Taxon should have the right common name');
+        
+            $dataObject = $taxon->dataObjects[0];
+            $this->assertIsA($dataObject, 'SchemaDataObject', 'Taxon should have a data object');
+            $this->assertTrue($dataObject->identifier == '2687301130', 'Data object should have the right identifier');
+            $this->assertTrue($dataObject->dataType == 'http://purl.org/dc/dcmitype/StillImage', 'Data object should have the right dataType');
+            $this->assertTrue($dataObject->mimeType == 'image/jpeg', 'Data object should have the right mimeType');
+            $this->assertTrue($dataObject->created == '2008-06-21 07:08:49', 'Data object should have the right created');
+            $this->assertTrue($dataObject->title == 'P1030143', 'Data object should have the right title');
+            $this->assertTrue($dataObject->license == 'http://creativecommons.org/licenses/by-nc-sa/2.0/', 'Data object should have the right license');
+            $this->assertTrue($dataObject->source == 'https://www.flickr.com/photos/pleary/2687301130/', "Data object source $dataObject->source should be https://www.flickr.com/photos/pleary/2687301130/");
+            $this->assertTrue($dataObject->mediaURL == 'https://farm4.staticflickr.com/3057/2687301130_c12f33ac24_o.jpg', 'Data object should have the right mediaURL');
+            // 3.15.13 - it seems Flickr moved place names to a separate place API
+            // $this->assertTrue($dataObject->location == 'Copenhagen, Hovedstaden, Denmark', 'Data object should have the right location');
+        
+            $this->assertIsA($dataObject->agents[0], 'SchemaAgent', 'Data object should have an agent');
+            $this->assertTrue($dataObject->agents[0]->fullName == 'pleary', 'Agent should have the right fullName');
+            $this->assertTrue($dataObject->agents[0]->homepage == 'http://www.flickr.com/photos/11571226@N04', 'Agent should have the right homepage');
+            $this->assertTrue($dataObject->agents[0]->role == 'photographer', 'Agent should have the right role');
+        
+            $this->assertIsA($dataObject->point, 'SchemaPoint', 'Data object should have an point');
+            $this->assertTrue($dataObject->point->latitude == '55.670282', 'Point should have the right latitude');
+            $this->assertTrue($dataObject->point->longitude == '12.583293', 'Point should have the right longitude');
         }
-        $response = FlickrAPI::pools_get_photos(FLICKR_EOL_GROUP_ID, "", 5, 1, FLICKR_AUTH_TOKEN, FLICKR_PLEARY_USER_ID);
-        $this->assertTrue($response->photos->total == 4, 'Patrick should have submitted only 4 photos');
-        
-        $first_photo = $response->photos->photo[3];
-        $taxa = FlickrApi::get_taxa_for_photo($first_photo->id, $first_photo->secret, FLICKR_AUTH_TOKEN);
-        $this->assertTrue(is_array($taxa), 'Taxa should be an array');
-        
-        $taxon = $taxa[0];
-        $this->assertIsA($taxon, 'SchemaTaxon', 'Response should be an array of SchemaTaxon');
-        $this->assertTrue($taxon->kingdom == 'Animalia', 'Taxon should have the right kingdom');
-        $this->assertTrue($taxon->phylum == 'Chordata', 'Taxon should have the right phylum');
-        $this->assertTrue($taxon->class == 'Aves', 'Taxon should have the right class');
-        $this->assertTrue($taxon->order == 'Ciconiiformes', 'Taxon should have the right order');
-        $this->assertTrue($taxon->family == 'Ardeidae', 'Taxon should have the right family');
-        $this->assertTrue($taxon->genus == 'Ardea', 'Taxon should have the right genus');
-        $this->assertTrue($taxon->scientificName == 'Ardea herodias', 'Taxon should have the right name');
-        
-        $this->assertIsA($taxon->commonNames[0], 'SchemaCommonName', 'Taxon should have a common name');
-        $this->assertTrue($taxon->commonNames[0]->name == 'Great Blue Heron', 'Taxon should have the right common name');
-        
-        $dataObject = $taxon->dataObjects[0];
-        $this->assertIsA($dataObject, 'SchemaDataObject', 'Taxon should have a data object');
-        $this->assertTrue($dataObject->identifier == '2687301130', 'Data object should have the right identifier');
-        $this->assertTrue($dataObject->dataType == 'http://purl.org/dc/dcmitype/StillImage', 'Data object should have the right dataType');
-        $this->assertTrue($dataObject->mimeType == 'image/jpeg', 'Data object should have the right mimeType');
-        $this->assertTrue($dataObject->created == '2008-06-21 07:08:49', 'Data object should have the right created');
-        $this->assertTrue($dataObject->title == 'P1030143', 'Data object should have the right title');
-        $this->assertTrue($dataObject->license == 'http://creativecommons.org/licenses/by-nc-sa/2.0/', 'Data object should have the right license');
-        $this->assertTrue($dataObject->source == 'https://www.flickr.com/photos/pleary/2687301130/', "Data object source $dataObject->source should be https://www.flickr.com/photos/pleary/2687301130/");
-        $this->assertTrue($dataObject->mediaURL == 'https://farm4.staticflickr.com/3057/2687301130_c12f33ac24_o.jpg', 'Data object should have the right mediaURL');
-        // 3.15.13 - it seems Flickr moved place names to a separate place API
-        // $this->assertTrue($dataObject->location == 'Copenhagen, Hovedstaden, Denmark', 'Data object should have the right location');
-        
-        $this->assertIsA($dataObject->agents[0], 'SchemaAgent', 'Data object should have an agent');
-        $this->assertTrue($dataObject->agents[0]->fullName == 'pleary', 'Agent should have the right fullName');
-        $this->assertTrue($dataObject->agents[0]->homepage == 'http://www.flickr.com/photos/11571226@N04', 'Agent should have the right homepage');
-        $this->assertTrue($dataObject->agents[0]->role == 'photographer', 'Agent should have the right role');
-        
-        $this->assertIsA($dataObject->point, 'SchemaPoint', 'Data object should have an point');
-        $this->assertTrue($dataObject->point->latitude == '55.670282', 'Point should have the right latitude');
-        $this->assertTrue($dataObject->point->longitude == '12.583293', 'Point should have the right longitude');
     }
     
 }
