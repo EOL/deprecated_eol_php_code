88e1d32bec537883561bc4465d4c1f7065ea6448
Nathan Wilson <nathan@collectivesource.com>
2014-08-08 20:13:34 +0000

    Cleaned up some sample files

--- config/environments/test_sample.php
+++ config/environments/test_sample.php
@@ -2,18 +2,14 @@
 
 ini_set('display_errors', true);
 
-$GLOBALS['ENV_DEBUG'] = false;
-$GLOBALS['ENV_DEBUG_TO_FILE'] = true;
-
 // make sure you define the proper URL to the root directory of this installation
 define('WEB_ROOT', 'http://localhost/eol_php_code/');
 
-define('FLICKR_API_KEY', 'xxx');
-define('FLICKR_SHARED_SECRET', 'xxx');
-define('FLICKR_AUTH_TOKEN', 'xxx');
+$GLOBALS['ENV_DEBUG'] = false;
+$GLOBALS['ENV_DEBUG_TO_FILE'] = true;
 
 define('SOLR_SERVER', 'http://localhost:8983/solr/');
 
-define('LOGGING_DB', 'eol_logging_test');
+define("LOGGING_DB", "eol_logging_test");
 
 ?>
