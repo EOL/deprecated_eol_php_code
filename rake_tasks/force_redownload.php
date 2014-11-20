917b5c99449b175662ef61d797b7a3d1ea985890
Yan Wong <yan@yanwong.me>
2014-11-20 17:08:27 +0000

    Remove redundant check of crop parameters when force_downloading(). If we give the data_object_id, this should be done automatically anyway.

--- rake_tasks/force_redownload.php
+++ rake_tasks/force_redownload.php
@@ -43,12 +43,8 @@ if($result && $row=$result->fetch_assoc())
         }
     }else
     {
-        //must impose old crop locations on the new image to grab
-        $crop = $mysqli->query("SELECT crop_x_pct, crop_y_pct, crop_width_pct, crop_height_pct FROM image_sizes WHERE data_object_id=$data_object_id LIMIT 1");
-        if($crop && $row=$crop->fetch_row()) {
-            $options = array('crop_pct' => $row, 'data_object_id' = $data_object_id);
-        } else $options = NULL;
-        if($new_object_cache_url = $content_manager->grab_file($row["object_url"], "image", $options)))
+        //give data_object_id so that previously stored crop locations are reused when grabbing the new image
+        if($new_object_cache_url = $content_manager->grab_file($row["object_url"], "image", array('data_object_id' = $data_object_id)))
         {
             //echo "UPDATE data_objects SET object_cache_url=$object_cache_url WHERE object_cache_url=$object_cache_url";
             $mysqli->query("UPDATE data_objects SET object_cache_url=$new_object_cache_url WHERE id=$data_object_id");
