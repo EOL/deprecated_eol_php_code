ce01312d92bc7019d964e6de81696790a108e3a6
Eli Agbayani <eli@eol.org>
2015-01-06 20:52:10 -0500

    WEB-5844 Ori Fragman, plant photos from TrekNature

---
+++ update_resources/connectors/895.php
@@ -0,0 +1,37 @@
+<?php
+namespace php_active_record;
+/* estimated execution time:  minutes 
+
+        31Dec   5Jan
+Taxa:   2056    1951
+Image:  3353    3293
+agent   1       1
+*/
+
+include_once(dirname(__FILE__) . "/../../config/environment.php");
+require_library('connectors/TrekNatureAPI');
+
+$timestart = time_elapsed();
+$resource_id = 895;
+$func = new TrekNatureAPI($resource_id);
+$func->get_all_taxa();
+
+if(filesize(CONTENT_RESOURCE_LOCAL_PATH . $resource_id . "_working/taxon.tab") > 1000)
+{
+    if(is_dir(CONTENT_RESOURCE_LOCAL_PATH . $resource_id))
+    {
+        recursive_rmdir(CONTENT_RESOURCE_LOCAL_PATH . $resource_id . "_previous");
+        rename(CONTENT_RESOURCE_LOCAL_PATH . $resource_id, CONTENT_RESOURCE_LOCAL_PATH . $resource_id . "_previous");
+    }
+    rename(CONTENT_RESOURCE_LOCAL_PATH . $resource_id . "_working", CONTENT_RESOURCE_LOCAL_PATH . $resource_id);
+    rename(CONTENT_RESOURCE_LOCAL_PATH . $resource_id . "_working.tar.gz", CONTENT_RESOURCE_LOCAL_PATH . $resource_id . ".tar.gz");
+    Functions::set_resource_status_to_force_harvest($resource_id);
+    Functions::count_resource_tab_files($resource_id);
+}
+
+$elapsed_time_sec = time_elapsed() - $timestart;
+echo "\n\n";
+echo "elapsed time = " . $elapsed_time_sec/60 . " minutes \n";
+echo "elapsed time = " . $elapsed_time_sec/60/60 . " hours \n";
+echo "\n\nDone processing.";
+?>
