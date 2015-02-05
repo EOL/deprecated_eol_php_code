43d4e1c45aedd96c885c00fda5053d44f4611f17
Eli Agbayani <eli@eol.org>
2015-02-05 04:50:30 -0500

    Follow-up WEB-5843 Import Smithsonian type specimen data to TraitBank

--- update_resources/connectors/891.php
+++ update_resources/connectors/891.php
@@ -2,21 +2,30 @@
 namespace php_active_record;
 /* WEB-5843 Import Smithsonian type specimen data to TraitBank
 
-                    23Dec
-measurement_or_fact 4768256
-occurrence          468454
+                    14Jan
+measurement_or_fact 4,768,256
+occurrence          468,454
 taxon               295903
+
+10k records:        9Jan
+measurement_or_fact 9218
+occurrence          1708
+taxon               716
 */
 
 include_once(dirname(__FILE__) . "/../../config/environment.php");
 require_library('connectors/NMNHTypeRecordAPI');
 $timestart = time_elapsed();
 
-$params["dwca_file"]    = "http://collections.mnh.si.edu/ipt/archive.do?r=nmnhdwca";
+//local source
 $params["dwca_file"]    = "http://localhost/~eolit/cp/NMNH/type_specimen_resource/dwca-nmnhdwca.zip";
-$params["dwca_file"]    = "https://dl.dropboxusercontent.com/u/7597512/NMNH/type_specimen_resource/dwca-nmnhdwca.zip";
 $params["uri_file"]     = "http://localhost/~eolit/cp/NMNH/type_specimen_resource/nmnh mappings.xlsx";
-$params["uri_file"]     = "https://dl.dropboxusercontent.com/u/7597512/NMNH/type_specimen_resource/nmnh%20mappings.xlsx";
+
+//remote source
+$params["dwca_file"]    = "https://dl.dropboxusercontent.com/u/7597512/NMNH/type_specimen_resource/dwca-nmnhdwca.zip";
+$params["dwca_file"]    = "http://collections.mnh.si.edu/ipt/archive.do?r=nmnhdwca"; // true value - working
+$params["uri_file"]     = "https://dl.dropboxusercontent.com/u/7597512/NMNH/type_specimen_resource/nmnh mappings.xlsx";
+
 $params["dataset"]      = "NMNH";
 $params["type"]         = "structured data";
 $params["resource_id"]  = 891;
