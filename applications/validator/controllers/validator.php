a3681a816fd6c37d76f92797e6566394690f8a63
JRice <jrice@eol.org>
2015-03-02 14:19:03 -0500

    Fixing another archive type error.

--- applications/validator/controllers/validator.php
+++ applications/validator/controllers/validator.php
@@ -6,12 +6,12 @@ class validator_controller extends ControllerBase
     public static function index($parameters)
     {
         extract($parameters);
-        
+
         $errors = array();
         $eol_errors = array();
         $eol_warnings = array();
-        
-        
+
+
         $xml_file = @$file_url;
         $downloaded_file = false;
         $suffix = null;
@@ -20,7 +20,7 @@ class validator_controller extends ControllerBase
             $xml_file = $xml_upload['tmp_name'];
             if(preg_match("/\.([^\.]+)$/", $xml_upload['name'], $arr)) $suffix = strtolower(trim($arr[1]));
         }
-        if($temp_dir = ContentManager::download_temp_file_and_assign_extension($xml_file, array('suffix' => $suffix)))
+        if($temp_dir = ContentManager::download_temp_file_and_assign_extension($xml_file, $suffix, array('suffix' => $suffix)))
         {
             $downloaded_file = true;
             if(is_dir($temp_dir))
@@ -31,16 +31,16 @@ class validator_controller extends ControllerBase
                 $xml_file = $temp_dir;
             }
         }
-        
+
         if($xml_file)
         {
             // determine the xsd, and whether this is an EOL resource file
             $xsd = SchemaValidator::get_schema_location($xml_file);
             if(preg_match("/^http:\/\/services\.eol\.org\/schema\/content_[0-1]_[0-3]/", $xsd)) $is_eol_schema = true;
             else $is_eol_schema = false;
-            
+
             $valid = SchemaValidator::validate($xml_file);
-            
+
             if($valid !== true)
             {
                 $errors = $valid;
@@ -52,7 +52,7 @@ class validator_controller extends ControllerBase
                 else $is_eol_schema = false;
             }
         }
-        
+
         if($downloaded_file)
         {
             if(is_dir($temp_dir))
@@ -63,7 +63,7 @@ class validator_controller extends ControllerBase
                 unlink($temp_dir);
             }
         }
-        
+
         render_template("validator/index", array("file_url" => @$file_url, "file_upload" => @$xml_upload['name'], "is_eol_schema" => @$is_eol_schema, "xsd" => @$xsd, "errors" => @$errors, "eol_errors" => @$eol_errors, "eol_warnings" => @$eol_warnings));
     }
 }
