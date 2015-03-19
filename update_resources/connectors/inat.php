fa2c99ccd6f9a0d85d3b1a1e59df920ae648d578
Eli Agbayani <eli@eol.org>
2015-03-19 00:49:26 -0400

    DATA-1594 reverse connector, EOL data back to iNat

---
+++ update_resources/connectors/inat.php
@@ -0,0 +1,15 @@
+<?php
+namespace php_active_record;
+/*connector for iNaturalist: DATA-1594 reverse connector, EOL data back to iNat.
+This doesn't create a resource for ingestion but rather a text file of URLs to be given back to the partner
+estimated execution time:
+*/
+include_once(dirname(__FILE__) . "/../../config/environment.php");
+$timestart = time_elapsed();
+require_library('connectors/INaturalistAPI');
+$func = new INaturalistAPI();
+$func->generate_link_backs();
+$elapsed_time_sec = time_elapsed() - $timestart;
+echo "\n\nelapsed time = " . $elapsed_time_sec/60 . " minutes \n";
+echo "elapsed time = " . $elapsed_time_sec/60/60 . " hours \n";
+?>
