0eb20001cd0ae221230dba0341cb1a2289f9fdcf
Eli Agbayani <eli@eol.org>
2015-04-23 03:26:06 -0400

    Follow-up DATA-1598 tweak to Rosario Beach Marine Lab connector

--- update_resources/connectors/221.php
+++ update_resources/connectors/221.php
@@ -15,13 +15,19 @@ images              = 1540
 New DWC-A resource:
                 Apr6
 agent           [31]
-media_resource  [14682]
-reference       [58]
+media_resource  [4189]
+reference       [376]
 taxon           [854]
 vernacular_name [779]
 
+text/html   : 2294
+image/jpeg  : 1888
+image/gif   : 6
+
 - we are now getting synonyms
 - we now have separated 'habitat' from 'depth range'
+- we are now getting other photographer names as agents, before we only had David Cowles as photographer.
+- we are also now getting Scientific Articles as references
 */
 
 include_once(dirname(__FILE__) . "/../../config/environment.php");
