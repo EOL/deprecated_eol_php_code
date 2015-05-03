4b2d6b86252d385ff600c52acdc091d6ba60d42e
YoustinaAtef <youstina.atef@bibalex.org>
2015-05-03 12:42:12 +0300

    fix silent return

--- rake_tasks/chuncks.bat
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-php /home/ba/work/eol_php_code/rake_tasks/add_normalized_measurements.php http://eol.org/resources/10 5000 0
-php /home/ba/work/eol_php_code/rake_tasks/add_normalized_measurements.php http://eol.org/resources/10 5000 5000
-php /home/ba/work/eol_php_code/rake_tasks/add_normalized_measurements.php http://eol.org/resources/10 5000 10000
-php /home/ba/work/eol_php_code/rake_tasks/add_normalized_measurements.php http://eol.org/resources/10 5000 15000
-php /home/ba/work/eol_php_code/rake_tasks/add_normalized_measurements.php http://eol.org/resources/8 5000 0
-php /home/ba/work/eol_php_code/rake_tasks/add_normalized_measurements.php http://eol.org/resources/8 5000 5000
-php /home/ba/work/eol_php_code/rake_tasks/add_normalized_measurements.php http://eol.org/resources/8 5000 10000
-php /home/ba/work/eol_php_code/rake_tasks/add_normalized_measurements.php http://eol.org/resources/8 5000 15000
