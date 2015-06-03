05771929da8d4d0286d233144122463989ca5e1a
eliagbayani <eagbayani@eol.org>
2015-06-03 12:32:44 +0800

    DATA-1618 internationalize wikipedia! Test case: German Wikipedia

---
+++ lib/connectors/DWCADiagnoseAPI.php
@@ -0,0 +1,75 @@
+<?php
+namespace php_active_record;
+/* This will contain functions to diagnose EOL DWC-A files */
+class DWCADiagnoseAPI
+{
+    function __construct()
+    {
+        $this->file['taxon'] 			= "http://rs.tdwg.org/dwc/terms/taxonID";
+        $this->file['occurrence'] 		= "http://rs.tdwg.org/dwc/terms/occurrenceID";
+        $this->file['reference'] 		= "http://purl.org/dc/terms/identifier";
+        $this->file['document'] 		= "http://purl.org/dc/terms/identifier";
+        $this->file['agent'] 			= "http://purl.org/dc/terms/identifier";
+        $this->file['vernacularname']	= "http://rs.tdwg.org/dwc/terms/vernacularName";
+    }
+
+    function check_unique_ids($resource_id, $file_extension = ".tab")
+    {
+        $harvester = new ContentArchiveReader(NULL, CONTENT_RESOURCE_LOCAL_PATH . $resource_id . "/");
+        $tables = $harvester->tables;
+        $tables = array_keys($tables);
+        $tables = array_diff($tables, array("http://rs.tdwg.org/dwc/terms/measurementorfact")); //exclude measurementorfact
+        print_r($tables);
+        foreach($tables as $table) self::process_fields($harvester->process_row_type($table), pathinfo($table, PATHINFO_BASENAME));
+    }
+
+    private function process_fields($records, $class)
+    {
+        $temp_ids = array();
+        echo "\n[$class]";
+        foreach($records as $rec)
+        {
+            $keys = array_keys($rec);
+            if(!($field_index_key = @$this->file[$class]))
+            {
+                echo "\nnot yet defined [$class]\n";
+                print_r($keys);
+                print_r($rec);
+                return false;
+            }
+
+            if(!isset($temp_ids[$rec[$field_index_key]])) $temp_ids[$rec[$field_index_key]] = '';
+            else
+            {
+                echo " -- not unique ID in [$class] - " . $rec[$field_index_key] . " - [$field_index_key]" . "\n";
+                return false;
+            }
+            
+        }
+        echo " -- OK\n";
+        return true;
+    }
+
+    function cannot_delete() // a utility
+    {
+        $final = array();
+        foreach(new FileIterator(DOC_ROOT . "/public/tmp/cant_delete.txt") as $line => $r) $final[pathinfo($r, PATHINFO_DIRNAME)] = '';
+        $final = array_keys($final);
+        asort($final);
+        foreach($final as $e) echo "\n $e";
+        echo "\n";
+    }
+
+    function get_undefined_uris() // a utility
+    {
+        $ids = array("872", "886", "887", "892", "893", "894", "885", "42");
+        foreach($ids as $id)
+        {
+            echo "\nprocessing id [$id]";
+            if($undefined_uris = Functions::get_undefined_uris_from_resource($id)) print_r($undefined_uris);
+            echo "\nundefined uris: " . count($undefined_uris) . "\n";
+        }
+    }
+
+}
+?>
---
+++ update_resources/connectors/957.php
@@ -0,0 +1,38 @@
+<?php
+namespace php_active_record;
+/* DATA-1618 internationalize wikipedia! Test case: German Wikipedia
+					2015Jun2
+media_resource.tab 	54605
+taxon.tab 			27300
+*/
+include_once(dirname(__FILE__) . "/../../config/environment.php");
+require_library('connectors/WikipediaRegionalAPI');
+$timestart = time_elapsed();
+$resource_id = 957;
+$func = new WikipediaRegionalAPI($resource_id, 'de');
+$func->generate_archive();
+if(filesize(CONTENT_RESOURCE_LOCAL_PATH . $resource_id . "_working/taxon.tab") > 1000)
+{
+    if(is_dir(CONTENT_RESOURCE_LOCAL_PATH . $resource_id))
+    {
+        recursive_rmdir(CONTENT_RESOURCE_LOCAL_PATH . $resource_id . "_previous");
+        rename(CONTENT_RESOURCE_LOCAL_PATH . $resource_id, CONTENT_RESOURCE_LOCAL_PATH . $resource_id . "_previous");
+    }
+    rename(CONTENT_RESOURCE_LOCAL_PATH . $resource_id . "_working", CONTENT_RESOURCE_LOCAL_PATH . $resource_id);
+    rename(CONTENT_RESOURCE_LOCAL_PATH . $resource_id . "_working.tar.gz", CONTENT_RESOURCE_LOCAL_PATH . $resource_id . ".tar.gz");
+    Functions::set_resource_status_to_force_harvest($resource_id);
+    Functions::count_resource_tab_files($resource_id);
+	if($undefined_uris = Functions::get_undefined_uris_from_resource($resource_id)) print_r($undefined_uris);
+    echo "\nUndefined URIs: " . count($undefined_uris) . "\n";
+}
+// /* just for local use
+require_library('connectors/DWCADiagnoseAPI');
+$func = new DWCADiagnoseAPI();
+$func->check_unique_ids($resource_id);
+// */
+$elapsed_time_sec = time_elapsed() - $timestart;
+echo "\n\n";
+echo "elapsed time = " . $elapsed_time_sec/60 . " minutes \n";
+echo "elapsed time = " . $elapsed_time_sec/60/60 . " hours \n";
+echo "\nDone processing.\n";
+?>
