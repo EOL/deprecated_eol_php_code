eaf9bbf41811d9a3d069b783e6cad32e727dc5a4
eliagbayani <eagbayani@eol.org>
2015-07-05 04:05:20 -0400

    Quick fix Functions lib.

--- lib/Functions.php
+++ lib/Functions.php
@@ -362,6 +362,27 @@ class Functions
 		foreach($callers as $caller) debug($caller['file'] . ":" . $caller['line']);
 		debug($msg);
 	}
+	
+	public static function finalize_dwca_resource($resource_id)
+	{
+		if(filesize(CONTENT_RESOURCE_LOCAL_PATH . $resource_id . "_working/taxon.tab") > 1000)
+		{
+		    if(is_dir(CONTENT_RESOURCE_LOCAL_PATH . $resource_id))
+		    {
+		        recursive_rmdir(CONTENT_RESOURCE_LOCAL_PATH . $resource_id . "_previous");
+		        Functions::file_rename(CONTENT_RESOURCE_LOCAL_PATH . $resource_id, CONTENT_RESOURCE_LOCAL_PATH . $resource_id . "_previous");
+		    }
+		    Functions::file_rename(CONTENT_RESOURCE_LOCAL_PATH . $resource_id . "_working", CONTENT_RESOURCE_LOCAL_PATH . $resource_id);
+		    Functions::file_rename(CONTENT_RESOURCE_LOCAL_PATH . $resource_id . "_working.tar.gz", CONTENT_RESOURCE_LOCAL_PATH . $resource_id . ".tar.gz");
+		    Functions::set_resource_status_to_force_harvest($resource_id);
+		    Functions::count_resource_tab_files($resource_id);
+			if($undefined_uris = Functions::get_undefined_uris_from_resource($resource_id)) print_r($undefined_uris);
+		    echo "\nUndefined URIs: " . count($undefined_uris) . "\n";
+			require_library('connectors/DWCADiagnoseAPI');
+			$func = new DWCADiagnoseAPI();
+			$func->check_unique_ids($resource_id);
+		}		
+	}
     
     public static function get_undefined_uris_from_resource($resource_id)
     {
