8ff1f177934bff38fe6112d8385bb20f71a93f8b
eliagbayani <eli@eol.org>
2016-02-01 02:05:29 -0500

    Flickr UserAlbum API

--- update_resources/connectors/flickr_user_album.php
+++ update_resources/connectors/flickr_user_album.php
@@ -1,21 +1,19 @@
 <?php
 namespace php_active_record;
-/* This is a connector that uses the Flickr API. Parameters passed were the Flickr user_id and album id, and then generates the EOL DWC-A file for it. - DATA-1625
-execution time: 
-                            Jul-6
-agent.tab             [2]        2
-media_resource.tab  [5809]    5611
-taxon.tab           [2070]    2068
+/*
+This is a connector that uses the Flickr API. Parameters passed were the Flickr user_id and album id, and then generates the EOL DWC-A file for it.
+First client is 958.php (DATA-1625)
 */
-return; //this is one-time for this specific resource, others in the future maybe periodic.
 include_once(dirname(__FILE__) . "/../../config/environment.php");
 require_library('connectors/FlickrUserAlbumAPI');
 $timestart = time_elapsed();
+return;
+/* change values here:
+$params['flickr_user_id']   = '56006259@N06';
+$params['photoset_id']      = '72157655302770261'; //this is the album id
+$resource_id                = "feller_flora_helvetica";
+*/
 
-$params['flickr_user_id']     = '56006259@N06';
-$params['photoset_id']        = '72157655302770261'; //this is the album id
-
-$resource_id = "feller_flora_helvetica";
 $func = new FlickrUserAlbumAPI($resource_id);
 $func->convert_to_dwca($params);
 Functions::finalize_dwca_resource($resource_id);
