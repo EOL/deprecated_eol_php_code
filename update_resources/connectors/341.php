7527472ee7d8651e80edd83e343e9e2109c197ac
eliagbayani <eli@eol.org>
2016-07-12 18:13:41 -0400

    Audio from SI is not playing. Fixed.

--- update_resources/connectors/341.php
+++ update_resources/connectors/341.php
@@ -13,6 +13,7 @@ require_library('ResourceDataObjectElementsSetting');
 $timestart = time_elapsed();
 $resource_id = 341; 
 $resource_path = "http://collections.mnh.si.edu/services/eol/nmnh-birds-response.xml.gz"; //Birds resource
+// $resource_path = "http://localhost/cp/NMNH/resources/nmnh-birds-response.xml.gz"; //local copy
 
 $result = $GLOBALS['db_connection']->select("SELECT accesspoint_url FROM resources WHERE id=$resource_id");
 $row = $result->fetch_row();
@@ -28,7 +29,23 @@ $xml = INBioAPI::assign_eol_subjects($xml);
 
 $xml = $nmnh->replace_data_object_element_value("mimeType", "audio/wav", "audio/x-wav", $xml);
 $xml = $nmnh->remove_data_object_of_certain_element_value("mimeType", "audio/x-wav", $xml); // to exclude <dataObject>'s of this element and value
+
+//from https://github.com/EOL/tramea/issues/296
+$newpath = "https://dl.dropboxusercontent.com/u/5763406/resources/341/mp3/";
+$xml = $nmnh->replace_data_object_element_value("mediaURL", "http://collections.mnh.si.edu/services/media.php?env=birds&irn=10027486", $newpath."MZ000134.MP3", $xml);
+$xml = $nmnh->replace_data_object_element_value("mediaURL", "http://collections.mnh.si.edu/services/media.php?env=birds&irn=10027485", $newpath."MZ000133.MP3", $xml);
+$xml = $nmnh->replace_data_object_element_value("mediaURL", "http://collections.mnh.si.edu/services/media.php?env=birds&irn=10027484", $newpath."MZ000132.MP3", $xml);
+$xml = $nmnh->replace_data_object_element_value("mediaURL", "http://collections.mnh.si.edu/services/media.php?env=birds&irn=10027490", $newpath."MZ000171.MP3", $xml);
+$xml = $nmnh->replace_data_object_element_value("mediaURL", "http://collections.mnh.si.edu/services/media.php?env=birds&irn=10027489", $newpath."MZ000170.MP3", $xml);
+$xml = $nmnh->replace_data_object_element_value("mediaURL", "http://collections.mnh.si.edu/services/media.php?env=birds&irn=10027488", $newpath."MZ000169.MP3", $xml);
+$xml = $nmnh->replace_data_object_element_value("mediaURL", "http://collections.mnh.si.edu/services/media.php?env=birds&irn=10027487", $newpath."MZ000168.MP3", $xml);
+$xml = $nmnh->replace_data_object_element_value("mediaURL", "http://collections.mnh.si.edu/services/media.php?env=birds&irn=10027509", $newpath."MZ000203.MP3", $xml);
+$xml = $nmnh->replace_data_object_element_value("mediaURL", "http://collections.mnh.si.edu/services/media.php?env=birds&irn=10027508", $newpath."MZ000202.MP3", $xml);
+$xml = $nmnh->replace_data_object_element_value("mediaURL", "http://collections.mnh.si.edu/services/media.php?env=birds&irn=10027507", $newpath."MZ000201.MP3", $xml);
+$xml = $nmnh->replace_data_object_element_value("mediaURL", "http://collections.mnh.si.edu/services/media.php?env=birds&irn=10027506", $newpath."MZ000200.MP3", $xml);
+
 $nmnh->save_resource_document($xml);
+Functions::gzip_resource_xml($resource_id);
 Functions::set_resource_status_to_force_harvest($resource_id);
 $elapsed_time_sec = time_elapsed() - $timestart;
 echo "\n";
