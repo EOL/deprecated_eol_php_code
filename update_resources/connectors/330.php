98b07d1b53aea7e6a39852a4e9b966b1f6118253
eliagbayani <eli@eol.org>
2016-08-25 06:28:38 -0400

    follow-up CalPhotos resources

--- update_resources/connectors/330.php
+++ update_resources/connectors/330.php
@@ -2,6 +2,13 @@
 namespace php_active_record;
 /* connector for CalPhotos - Moorea Biocode EoL XML resource -- https://eol-jira.bibalex.org/browse/DATA-1618
 execution time: 
+
+Moorea Biocode (ID = 330)
+Numbers from the partners XML:
+taxa	images
+4521	21047	(last harvest)
+4528	21047	(next harvest)
+
 */
 
 include_once(dirname(__FILE__) . "/../../config/environment.php");
@@ -13,12 +20,12 @@ $xml_resource = "http://calphotos.berkeley.edu/eol_biocode.xml.gz";
 // $xml_resource = "http://localhost/cp/CalPhotos/eol_biocode.xml.gz"; //local debug only
 
 $func = new INBioAPI();
-$info = $func->extract_archive_file($xml_resource, "eol_biocode.xml");
+$info = $func->extract_archive_file($xml_resource, "eol_biocode.xml", array('timeout' => 172800, 'expire_seconds' => 432000)); //expires in 5 days
 if(!$info) return;
 print_r($info);
 $temp_dir = $info['temp_dir'];
 
-$xml_string = Functions::lookup_with_cache($temp_dir . "eol_biocode.xml", array('expire_seconds' => 432000)); //expires in 5 days
+$xml_string = Functions::get_remote_file($temp_dir . "eol_biocode.xml");
 $xml_string = Functions::remove_invalid_bytes_in_XML($xml_string);
 $resource_path = CONTENT_RESOURCE_LOCAL_PATH . $resource_id . ".xml";
 if(!($WRITE = Functions::file_open($resource_path, "w"))) return;
