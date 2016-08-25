39cccbed8dab08f54d3f85d42ccd191d4c898e98
eliagbayani <eli@eol.org>
2016-08-25 05:39:18 -0400

    CalPhotos resources maintenance.

--- update_resources/connectors/267.php
+++ update_resources/connectors/267.php
@@ -10,7 +10,7 @@ $timestart = time_elapsed();
 $resource_id = 267;
 
 $xml_resource = "http://calphotos.berkeley.edu/eol.xml.gz";
-$xml_resource = "http://localhost/cp/CalPhotos/eol.xml.gz";
+// $xml_resource = "http://localhost/cp/CalPhotos/eol.xml.gz"; //local debug only
 
 $func = new INBioAPI();
 $info = $func->extract_archive_file($xml_resource, "eol.xml");
@@ -18,8 +18,8 @@ if(!$info) return;
 print_r($info);
 $temp_dir = $info['temp_dir'];
 
-$xml_string = Functions::get_remote_file($temp_dir . "eol.xml");
-$xml_string = remove_invalid_bytes_in_XML($xml_string);
+$xml_string = Functions::lookup_with_cache($temp_dir . "eol.xml", array('expire_seconds' => 432000)); //expires in 5 days
+$xml_string = Functions::remove_invalid_bytes_in_XML($xml_string);
 $resource_path = CONTENT_RESOURCE_LOCAL_PATH . $resource_id . ".xml";
 if(!($WRITE = Functions::file_open($resource_path, "w"))) return;
 fwrite($WRITE, $xml_string);
@@ -36,11 +36,4 @@ echo "elapsed time = $elapsed_time_sec seconds             \n";
 echo "elapsed time = " . $elapsed_time_sec/60 . " minutes  \n";
 echo "elapsed time = " . $elapsed_time_sec/60/60 . " hours \n";
 echo "\n\n Done processing.";
-
-function remove_invalid_bytes_in_XML($string)
-{
-    $string = preg_replace('/[\x00-\x1F\x80-\xFF]/', '', $string);
-    return $string;
-}
-
 ?>
