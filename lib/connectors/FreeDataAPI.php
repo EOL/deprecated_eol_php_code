856d29f6adf9a38b2a27188b4b03521b85a65bdb
eliagbayani <eli@eol.org>
2016-09-21 01:43:55 -0400

    debug create dir

--- lib/connectors/FreeDataAPI.php
+++ lib/connectors/FreeDataAPI.php
@@ -26,13 +26,14 @@ class FreeDataAPI
         
         echo "\nCreate folder reef_life_survey...\n";
         //make dir "reef_life_survey"
-        /* works but...
+        // /* works but...
         $command_line = "sudo mkdir " . CONTENT_RESOURCE_LOCAL_PATH . "reef_life_survey";
         $output = shell_exec($command_line);
-        */
-        if (!mkdir(CONTENT_RESOURCE_LOCAL_PATH . "reef_life_survey", 0777)) {
-            die('\nFailed to create folders...\n');
-        }
+        // */
+        
+        // if (!mkdir(CONTENT_RESOURCE_LOCAL_PATH . "reef_life_survey", 0777)) {
+        //     die('\nFailed to create folders...\n');
+        // }
         
         if(!$WRITE = Functions::file_open($this->destination['reef life survey'], "w")) return;
         fwrite($WRITE, implode("\t", $this->fields['reef life survey']) . "\n");
