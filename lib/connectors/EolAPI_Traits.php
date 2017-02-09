539ea120b1f6fa53aad13270826f815260f89483
eliagbayani <eli@eol.org>
2017-02-09 02:10:22 -0500

    in the works - api traits

--- lib/connectors/EolAPI_Traits.php
+++ lib/connectors/EolAPI_Traits.php
@@ -31,62 +31,72 @@ class EolAPI_Traits
         
         $this->do = array();
         $this->agent_ids = array();
+        
+        // others
+        $this->unique_index = array();
+        // %3A :
+        // %2F /
     }
     
     function start()
     {
         /*
-        %3A :
-        %2F /
-        */
-
-
-        // $attribute = "http%3A%2F%2Fpurl.obolibrary.org%2Fobo%2FVT_0001259&q=&sort=desc&taxon_concept_id=1703";
-        // $attribute = "http%3A%2F%2Fpurl.obolibrary.org%2Fobo%2FVT_0001259&q=&sort=desc&taxon_concept_id=1703";
-
-        // $attribute = "http%3A%2F%2Fpurl.obolibrary.org%2Fobo%2FVT_0001259&q=&sort=desc&taxon_concept_id=1642";
-        // $attribute = "http%3A%2F%2Fpurl.obolibrary.org%2Fobo%2FVT_0001259&q=&sort=desc&taxon_concept_id=1642";
-
-        $attribute = "http%3A%2F%2Fpurl.obolibrary.org%2Fobo%2FVT_0001259&q=&sort=desc&taxon_concept_id=695";
-
+        // DATA-1648 derivative files: Cichlidae 
+        $attribute = "http%3A%2F%2Fpurl.obolibrary.org%2Fobo%2FVT_0001259&q=&sort=desc&taxon_concept_id=5344";
+        $attribute = "http%3A%2F%2Fpurl.obolibrary.org%2Fobo%2FPATO_0000050&q=&sort=desc&taxon_concept_id=5344";
         
-        // $attribute = "http%3A%2F%2Fpurl.obolibrary.org%2Fobo%2FTO_0002725&q=&sort=desc";                             //last 7th problematic
-        // $attribute = "http%3A%2F%2Fpurl.obolibrary.org%2Fobo%2FTO_0002725&q=&sort=desc";                             //last 7th problematic
-        // $attribute = "http%3A%2F%2Fpurl.obolibrary.org%2Fobo%2FTO_0002725&q=&sort=desc";                             //last 7th problematic
-        // $attribute = "http%3A%2F%2Fpurl.obolibrary.org%2Fobo%2FTO_0002725&q=&sort=desc";                             //last 7th problematic
-
-        // $attribute = "http%3A%2F%2Fpurl.obolibrary.org%2Fobo%2FOBA_1000036&commit=Search&taxon_name=Halosphaera&q=&taxon_concept_id=90645"; //cell mass; csv sample from Jen
-        
-        
-        
-        /*
-        https://eol-jira.bibalex.org/browse/DATA-1649
-        derivative file: body mass, various groups
+        // DATA-1649 - derivative file: body mass, various groups
         $attribute = "http%3A%2F%2Fpurl.obolibrary.org%2Fobo%2FVT_0001259&q=&sort=desc&taxon_concept_id=38541712";   //done
         $attribute = "http%3A%2F%2Fpurl.obolibrary.org%2Fobo%2FVT_0001259&q=&sort=desc&taxon_concept_id=1552";       //done
-        http%3A%2F%2Fpurl.obolibrary.org%2Fobo%2FVT_0001259&q=&sort=desc&taxon_concept_id=1703
-        http%3A%2F%2Fpurl.obolibrary.org%2Fobo%2FVT_0001259&q=&sort=desc&taxon_concept_id=1642
-        http%3A%2F%2Fpurl.obolibrary.org%2Fobo%2FVT_0001259&q=&sort=desc&taxon_concept_id=695
-        
-        DATA-1650 lifespan of mammalia
+        $attribute = "http%3A%2F%2Fpurl.obolibrary.org%2Fobo%2FVT_0001259&q=&sort=desc&taxon_concept_id=1703";
+        $attribute = "http%3A%2F%2Fpurl.obolibrary.org%2Fobo%2FVT_0001259&q=&sort=desc&taxon_concept_id=1642";       //done
+        $attribute = "http%3A%2F%2Fpurl.obolibrary.org%2Fobo%2FVT_0001259&q=&sort=desc&taxon_concept_id=695";        //done
+
+        // DATA-1650 lifespan of mammalia
         $attribute = "http%3A%2F%2Fpurl.obolibrary.org%2Fobo%2FPATO_0000050&q=&sort=desc&taxon_concept_id=1642";     //done
         
-        DATA-1651 plant propagation method
+        // DATA-1651 plant propagation method
         $attribute = "http%3A%2F%2Feol.org%2Fschema%2Fterms%2FPropagationMethod&q=&sort=desc";                       //done
         
-        DATA-1652 carbon per cell
+        // DATA-1652 carbon per cell
         $attribute = "http%3A%2F%2Feol.org%2Fschema%2Fterms%2Fcarbon_per_cell&q=&sort=desc";                         //done
 
-        DATA-1653 life cycle habit
-        http%3A%2F%2Fpurl.obolibrary.org%2Fobo%2FTO_0002725&q=&sort=desc
+        // DATA-1653 life cycle habit
+        $attribute = "http%3A%2F%2Fpurl.obolibrary.org%2Fobo%2FTO_0002725&q=&sort=desc";
 
-        DATA-1654 growth habit
+        // DATA-1654 growth habit
         $attribute = "http%3A%2F%2Feol.org%2Fschema%2Fterms%2FPlantHabit&q=&sort=desc";                              //done
         */
-        
+
+        $attribute = "http%3A%2F%2Fpurl.obolibrary.org%2Fobo%2FOBA_1000036&commit=Search&taxon_name=Halosphaera&q=&taxon_concept_id=90645"; //cell mass; csv sample from Jen
+        // $attribute = "http%3A%2F%2Fpurl.obolibrary.org%2Fobo%2FOBA_1000036&commit=Search&q="; //cell mass; csv sample from Jen
+
         echo "\n" . urldecode($attribute) . "\n"; //exit;
         
-        self::get_data_search_results($attribute);
+        
+        /* just for stats
+        $attributes = array();
+        $attributes[] = "http%3A%2F%2Fpurl.obolibrary.org%2Fobo%2FVT_0001259&q=&sort=desc&taxon_concept_id=38541712";   //done
+        $attributes[] = "http%3A%2F%2Fpurl.obolibrary.org%2Fobo%2FVT_0001259&q=&sort=desc&taxon_concept_id=1552";       //done
+        $attributes[] = "http%3A%2F%2Fpurl.obolibrary.org%2Fobo%2FVT_0001259&q=&sort=desc&taxon_concept_id=1703";
+        $attributes[] = "http%3A%2F%2Fpurl.obolibrary.org%2Fobo%2FVT_0001259&q=&sort=desc&taxon_concept_id=1642";       //done
+        $attributes[] = "http%3A%2F%2Fpurl.obolibrary.org%2Fobo%2FVT_0001259&q=&sort=desc&taxon_concept_id=695";        //done
+        $attributes[] = "http%3A%2F%2Fpurl.obolibrary.org%2Fobo%2FPATO_0000050&q=&sort=desc&taxon_concept_id=1642";     //done
+        $attributes[] = "http%3A%2F%2Feol.org%2Fschema%2Fterms%2FPropagationMethod&q=&sort=desc";                       //done
+        $attributes[] = "http%3A%2F%2Feol.org%2Fschema%2Fterms%2Fcarbon_per_cell&q=&sort=desc";                         //done
+        $attributes[] = "http%3A%2F%2Fpurl.obolibrary.org%2Fobo%2FTO_0002725&q=&sort=desc";
+        $attributes[] = "http%3A%2F%2Feol.org%2Fschema%2Fterms%2FPlantHabit&q=&sort=desc";                              //done
+        $attributes[] = "http%3A%2F%2Fpurl.obolibrary.org%2Fobo%2FOBA_1000036&commit=Search&q="; //cell mass; csv sample from Jen
+        */
+
+        $attributes = array();
+        // $attributes[] = "http%3A%2F%2Fpurl.obolibrary.org%2Fobo%2FVT_0001259&q=&sort=desc&taxon_concept_id=5344";
+        $attributes[] = "http%3A%2F%2Fpurl.obolibrary.org%2Fobo%2FPATO_0000050&q=&sort=desc&taxon_concept_id=5344";
+        
+        
+        foreach($attributes as $attribute) self::get_data_search_results($attribute);
+
+        print_r($this->unique_index);
         exit("\n-eli stops-\n");
     }
     
@@ -95,6 +105,7 @@ class EolAPI_Traits
         $total = self::get_total_records($attrib);
         echo "\nTotal: $total";
         $pages = ceil($total/100);
+        // $pages = 20;
         echo "\nPages: $pages";
         for($page = 1; $page <= $pages; $page++)
         {
@@ -106,7 +117,7 @@ class EolAPI_Traits
                     foreach($arr[1] as $row)
                     {
                         $i++;
-                        print($row); //exit;
+                        // print($row); //exit;
                         $rec = array();
                         if(preg_match("/\/pages\/(.*?)\/data/ims", $row, $arr2))                    $rec['taxon_id'] = trim($arr2[1]);
                         if(preg_match_all("/\/data\">(.*?)<\/a>/ims", $row, $arr2))
@@ -138,11 +149,31 @@ class EolAPI_Traits
         echo "\n[$msg]";
         if($json = Functions::lookup_with_cache($this->trait_api.$rec['taxon_id'], $this->download_options))
         {
-            $arr = json_decode($json, true);
-            print_r($arr); //exit;
+            $recs = json_decode($json, true);
+            // foreach($recs as $rec)
+            // {
+            //     print_r($rec); exit; 
+            //     
+            // }
+            
+            // print_r($recs);
+            echo "\n".$recs['item']['scientificName']."\n";
+            $common_names = $recs['item']['vernacularNames'];
+            $traits = $recs['item']['traits'];
+            foreach($traits as $trait)
+            {
+                // print_r($trait);
+                $keys = array_keys($trait);
+                $this->unique_index = array_merge($this->unique_index, $keys);
+                $this->unique_index = array_unique($this->unique_index);
+            }
+
+            // exit;
+            
+            
             /*
             EOL page ID	
-            Scientific Name	        dwc:scientificName
+            Scientific Name	                    dwc:scientificName
             Common Name	
             Measurement	                        predicate
             Value	                            value
@@ -169,19 +200,19 @@ class EolAPI_Traits
             cells per counting unit	        eolterms:CellsPerCountingUnit
             scientific name	                dwc:scientificName
             measurement remarks	            dwc:measurementRemarks
-            height	
+            height	                        http://semanticscience.org/resource/SIO_000040
             Reference	                    eol:reference/full_reference
             measurement determined by	    dwc:measurementDeterminedBy
-            occurrence remarks	
-            length	
+            occurrence remarks	            dwc:occurrenceRemarks
+            length	                        http://semanticscience.org/resource/SIO_000041
             diameter 2	
-            width	
-            life stage	
+            width	                        http://semanticscience.org/resource/SIO_000042
+            life stage	                    dwc:lifeStage
             length 2	
             measurement determined date	    dwc:measurementDeterminedDate
             sampling effort	                dwc:samplingEffort
-            standard deviation	
-            number of available reports from the literature
+            standard deviation	            http://semanticscience.org/resource/SIO_000770
+            number of available reports from the literature     eolterms:NLiteratureValues
             */
             
             /*
@@ -195,6 +226,102 @@ class EolAPI_Traits
             "dwc:country": "Belgium",
             "dc:contributor": "Compiler: Anne E Thessen",
             */
+            
+            /*
+            Array
+            (
+                [0] => @id
+                [1] => eol:traitUri
+                [2] => @type
+                [3] => predicate
+                [4] => dwc:measurementType
+                [5] => value
+                [6] => units
+                [7] => eol:dataPointId
+                [8] => eolterms:SizeClass
+                [9] => http://ncicb.nci.nih.gov/xml/owl/EVS/Thesaurus.owl#C25285
+                [10] => eolterms:CellsPerCountingUnit
+                [11] => dc:source
+                [12] => dwc:individualCount
+                [13] => dc:bibliographicCitation
+                [14] => dwc:measurementMethod
+                [15] => eolterms:statisticalMethod
+                [16] => dwc:measurementValue
+                [17] => dwc:measurementUnit
+                [18] => dwc:scientificName
+                [19] => eolterms:CountingUnit
+                [20] => dwc:locality
+                [21] => dwc:eventDate
+                [22] => dwc:samplingProtocol
+                [23] => eolterms:resource
+                [24] => eolterms:VolumeFormula
+                [25] => dwc:measurementRemarks
+                [26] => http://semanticscience.org/resource/SIO_000770
+                [27] => dc:contributor
+                [28] => dwc:measurementDeterminedDate
+                [29] => eolterms:SampleSize
+                [30] => dwc:measurementDeterminedBy
+                [31] => dwc:occurrenceRemarks
+                [32] => source
+                [33] => http://semanticscience.org/resource/SIO_000040
+                [34] => eol:reference/full_reference
+                [35] => eolterms:NLiteratureValues
+                [36] => dwc:samplingEffort
+                [37] => http://semanticscience.org/resource/SIO_000041
+                [38] => eolterms:Salinity
+                [39] => dwc:municipality
+                [40] => dwc:month
+                [41] => dwc:year
+                [42] => dwc:island
+                [43] => dwc:country
+                [44] => eol:associationType
+                [45] => eol:inverseAssociationType
+                [46] => eol:subjectPage
+                [47] => eol:objectPage
+                [48] => eol:targetTaxonID
+                [49] => dwc:lifeStage
+                [50] => http://semanticscience.org/resource/SIO_000042
+                [51] => eolterms:SeawaterTemperature
+                [52] => dwc:verbatimEventDate
+                [53] => dwc:day
+                [54] => dwc:continent
+                [55] => dwc:waterBody
+                [56] => dwc:associatedMedia
+                [57] => dwc:recordNumber
+                [58] => dwc:startDayOfYear
+                [59] => dwc:endDayOfYear
+                [60] => dwc:catalogNumber
+                [61] => dwc:collectionCode
+                [62] => dwc:institutionCode
+                [63] => dwc:collectionID
+                [64] => dwc:typeStatus
+                [65] => dwc:stateProvince
+                [66] => dwc:recordedBy
+                [67] => dwc:higherGeography
+                [68] => eolterms:HasToxin
+                [69] => eolterms:ToxicEffect
+                [70] => dwc:verbatimDepth
+                [71] => dwc:decimalLongitude
+                [72] => dwc:countryCode
+                [73] => eolterms:GenbankAccessionNumber
+                [74] => dwc:identifiedBy
+                [75] => dwc:preparations
+                [76] => dwc:decimalLatitude
+                [77] => dwc:islandGroup
+                [78] => http://ecoinformatics.org/oboe/oboe.1.0/oboe-characteristics.owl#Irradiance
+                [79] => dwc:fieldNotes
+                [80] => dwc:measurementAccuracy
+                [81] => dwc:county
+                [82] => eolterms:Reviewer
+                [83] => eolterms:Assessor
+                [84] => eolterms:Version
+                [85] => eolterms:RedListCriteria
+                [86] => dwc:behavior
+            )
+            
+            */                          
+            
+            
             
         }
         
---
+++ update_resources/connectors/eol_api_traits.php
@@ -0,0 +1,23 @@
+<?php
+namespace php_active_record;
+/* used for /lib/EolAPI.php */
+include_once(dirname(__FILE__) . "/../../config/environment.php");
+$timestart = time_elapsed();
+
+require_library('connectors/EolAPI_Traits');
+
+$resource_id = 'eol';
+// /* normal operation ============================================
+$func = new EolAPI_Traits($resource_id);
+$func->start(); //normal operation
+// ================================================================*/
+
+
+$elapsed_time_sec = time_elapsed() - $timestart;
+echo "\n\n";
+echo "\n elapsed time = " . $elapsed_time_sec/60 . " minutes";
+echo "\n elapsed time = " . $elapsed_time_sec/60/60 . " hours";
+echo "\n Done processing.\n";
+
+
+?>
