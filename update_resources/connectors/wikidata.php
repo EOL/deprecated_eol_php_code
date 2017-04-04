6057a3ec3410c546a55283df98b08e2ac140bdf0
eliagbayani <eli@eol.org>
2017-04-04 09:15:35 -0400

    wikimedia in the works

--- update_resources/connectors/wikidata.php
+++ update_resources/connectors/wikidata.php
@@ -4,26 +4,34 @@ namespace php_active_record;
 include_once(dirname(__FILE__) . "/../../config/environment.php");
 require_library('connectors/WikiDataAPI');
 $timestart = time_elapsed();
-$resource_id = "1-ja";
+$resource_id = "1";
 
 // /* //main operation
-// $func = new WikiDataAPI($resource_id, "es");
-// $func = new WikiDataAPI($resource_id, "fr");
-// $func = new WikiDataAPI("957", "de");
+// $func = new WikiDataAPI($resource_id, "es");     //done final-es
+// $func = new WikiDataAPI($resource_id, "fr");     //done final-fr
+// $func = new WikiDataAPI("957", "de");            //done
+// $func = new WikiDataAPI($resource_id, "ja");     //done
+// $func = new WikiDataAPI($resource_id, "it");     //done
+// $func = new WikiDataAPI($resource_id, "ru");     //done
+// $func = new WikiDataAPI($resource_id, "ko");     //done
+// $func = new WikiDataAPI($resource_id, "cu");     //done
+// $func = new WikiDataAPI($resource_id, "uk");     //done
+// $func = new WikiDataAPI($resource_id, "pl");     //done
 
-// $func = new WikiDataAPI($resource_id, "en", true); //3rd param is boolean taxonomy; true means will generate hierarchy resource.
-// $func = new WikiDataAPI($resource_id, "en");
+// $func = new WikiDataAPI($resource_id, "en", "taxonomy"); //3rd param is boolean taxonomy; true means will generate hierarchy resource. [wikidata-hierarchy]    //done
+$func = new WikiDataAPI($resource_id, "en", "wikimedia");     //done - Used for Commons
+// $func = new WikiDataAPI($resource_id, "en");     //done
 
-// $func = new WikiDataAPI($resource_id, "cu");
-// $func = new WikiDataAPI($resource_id, "sv");
-// $func = new WikiDataAPI($resource_id, "ceb");
-// $func = new WikiDataAPI($resource_id, "nl");
-// $func = new WikiDataAPI($resource_id, "ru");
-// $func = new WikiDataAPI($resource_id, "it");
-// $func = new WikiDataAPI($resource_id, "pl");
-// $func = new WikiDataAPI($resource_id, "vi");
 // $func = new WikiDataAPI($resource_id, "pt");
-$func = new WikiDataAPI($resource_id, "ja");
+// $func = new WikiDataAPI($resource_id, "nl");
+// $func = new WikiDataAPI($resource_id, "sv");    //still being run, many many bot inspired
+// $func = new WikiDataAPI($resource_id, "zh");
+// $func = new WikiDataAPI($resource_id, "vi");
+
+//===================
+
+// $func = new WikiDataAPI($resource_id, "ceb");
+
 
 
 $func->get_all_taxa();
