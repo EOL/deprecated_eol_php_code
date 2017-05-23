193aee48c5f4b904591792179f88f6ee4facaba3
eliagbayani <eli@eol.org>
2017-05-22 22:39:14 -0400

    comments

--- applications/genHigherClass/generate_cmd.php
+++ applications/genHigherClass/generate_cmd.php
@@ -8,6 +8,9 @@ set_time_limit(0);
 ini_set("memory_limit","5000M"); //orig
 ini_set("memory_limit","8000M"); //orig
 
+/*
+Being used by GBIF backbone, but can be used by any DwCA taxon extension. See below how to use:
+*/
 
 /* Important settings
 Apache httpd.conf:
@@ -27,16 +30,12 @@ $func = new DwCA_Utility_cmd();
 
 echo "\ninput file to lib: [$file]\n";
 if($info = $func->tool_generate_higherClassification($file)) {}
-else
-{
-    echo "The file is not ready for processing. The file needs the minimum three fields column header: '<i>taxonID</i>', '<i>scientificName</i>' and '<i>parentNameUsageID</i>'";
-}
+else echo "The file is not ready for processing. The file needs the minimum three fields column header: '<i>taxonID</i>', '<i>scientificName</i>' and '<i>parentNameUsageID</i>'";
 
 $elapsed_time_sec = time_elapsed() - $timestart;
 echo "elapsed time = $elapsed_time_sec seconds";
 echo "\nelapsed time = " . $elapsed_time_sec/60 . " minutes";
 echo "\nDone processing.\n\n";
-
 
 function get_ext_of_orig_file_in_zip($orig)
 {
