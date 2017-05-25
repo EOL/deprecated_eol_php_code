9ff52add028aaa5af9bd5575557d0debaff5339c
eliagbayani <eli@eol.org>
2017-05-25 02:46:31 -0400

    cleaning

--- update_resources/connectors/google_client.php
+++ update_resources/connectors/google_client.php
@@ -12,11 +12,8 @@ $func = new GoogleClientAPI();
 $params['spreadsheetID'] = '1-nTN2i_epQzl-rOaQJjIFbVRUfVirVKZpTEwC8kH7k8';
 $params['range']         = 'Sheet1!A2:C'; //where "A" is the starting column, "C" is the ending column, and "2" is the starting row.
 
-
 $params['spreadsheetID'] = '1gHdrWRaZbEKp3bCI7kXhN95le-jGvQOXXxeVpgmypJ4';
 $params['range']         = 'Sheet1!A1:C'; //where "A" is the starting column, "C" is the ending column, and "2" is the starting row.
-
-
 
 $func->access_google_sheet($params);
 
