2fade0e9a798bcfb8468241c3f4e154fd4e61f17
eliagbayani <eli@eol.org>
2017-06-09 00:13:38 -0400

    Pruning GBIF continued TRAM-552

--- applications/genHigherClass/generate_cmd.php
+++ applications/genHigherClass/generate_cmd.php
@@ -33,18 +33,33 @@ if($info = $func->tool_generate_higherClassification($file)) {}
 else echo "The file is not ready for processing. The file needs the minimum three fields column header: '<i>taxonID</i>', '<i>scientificName</i>' and '<i>parentNameUsageID</i>'";
 */
 
-// /* additional task: pruning GBIF backbone: https://eol-jira.bibalex.org/browse/TRAM-552
+/* additional task: pruning GBIF backbone: https://eol-jira.bibalex.org/browse/TRAM-552
 require_library('connectors/GBIFtaxaAPI');
 $func = new GBIFtaxaAPI();
-$new_file = $func->prune_gbif_backbone_taxa("temp/GBIF_Taxa_accepted.tsv");
-// */
+$new_file = $func->prune_gbif_backbone_taxa("temp/GBIF_Taxa_accepted.tsv"); // pruning alone takes 151 hours (6.3 days)
+*/
 
-/* utility
+/* utility - this takes 75 minutes
+$new_file = "temp/GBIF_Taxa_accepted_pruned.tsv"; //OR this should be the output from above
 require_library('connectors/DWCADiagnoseAPI');
 $func = new DWCADiagnoseAPI();
 $undefined_parents = $func->check_if_all_parents_have_entries(pathinfo($new_file, PATHINFO_FILENAME), true, $new_file); //true means output will write to text file
 echo "\nundefined parents: ".count($undefined_parents)."\n";
+// OUTPUT IS: GBIF_Taxa_accepted_pruned_undefined_parent_ids.txt
 */
+
+
+// /* utility to cache AND/OR generate "GBIF_invalid_descendants.txt"
+// INPUT IS: GBIF_Taxa_accepted_pruned_undefined_parent_ids.txt
+require_library('connectors/GBIFtaxaAPI');
+$func = new GBIFtaxaAPI();
+$new_file = $func->get_GBIF_invalid_descendants();
+// OUTPUT IS: GBIF_invalid_descendants.txt
+// */
+
+
+
+
 
 $elapsed_time_sec = time_elapsed() - $timestart;
 echo "\nelapsed time = " . $elapsed_time_sec/60 . " minutes";
