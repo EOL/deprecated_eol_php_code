2a0221c2c722b6b2d8b8318d2119cb76dfdf2d21
eliagbayani <eli@eol.org>
2017-09-20 14:06:53 -0400

    ontology URLs updated

--- vendor/eol_content_schema_v2/Occurrence.php
+++ vendor/eol_content_schema_v2/Occurrence.php
@@ -3,7 +3,9 @@ namespace eol_schema;
 
 class Occurrence extends DarwinCoreExtensionBase
 {
-    const EXTENSION_URL = "https://dl.dropboxusercontent.com/u/1355101/ontology/occurrence_extension.xml";
+    // const EXTENSION_URL = "https://dl.dropboxusercontent.com/u/1355101/ontology/occurrence_extension.xml";
+    const EXTENSION_URL = "http://editors.eol.org/other_files/ontology/occurrence_extension.xml";
+
     const ROW_TYPE = "http://rs.tdwg.org/dwc/terms/Occurrence";
     const PRIMARY_KEY = "http://rs.tdwg.org/dwc/terms/occurrenceID";
     const GRAPH_NAME = "occurrences";
