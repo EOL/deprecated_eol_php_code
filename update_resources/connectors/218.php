248c41dc271fde615b7481eeaf506a06b1cd2e98
eliagbayani <eli@eol.org>
2017-09-26 09:59:48 -0400

    maintenance

--- update_resources/connectors/218.php
+++ update_resources/connectors/218.php
@@ -7,8 +7,14 @@ return;
 include_once(dirname(__FILE__) . "/../../config/environment.php");
 require_library('connectors/TropicosArchiveAPI');
 
+// $url = "http://localhost/eol_php_code/applications/content_server/resources/1.xml";
+// $xml = Functions::get_remote_file($url);
+// $xml = simplexml_load_string($xml);
+// print_r($xml); exit("\n\n");
+
 $timestart = time_elapsed();
 $resource_id = 218;
+// if(!Functions::can_this_connector_run($resource_id)) return;
 $func = new TropicosArchiveAPI($resource_id);
 
 $func->get_all_taxa($resource_id);
