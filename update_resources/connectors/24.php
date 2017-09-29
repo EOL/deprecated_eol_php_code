b6bdc954b52d62a3857abc6aa0e315fdc29dd3bd
eliagbayani <eli@eol.org>
2017-09-28 20:34:24 -0400

    maintenance

--- update_resources/connectors/24.php
+++ update_resources/connectors/24.php
@@ -5,28 +5,21 @@ include_once(dirname(__FILE__) . "/../../config/environment.php");
 
 $mysqli =& $GLOBALS['mysqli_connection'];
 
-
-
 $file = trim(Functions::get_remote_file("http://antweb.org/getEOL.do"));
 //$file = trim(Functions::get_remote_file("../../temp/ants.xml"));
 //echo "$file";
 $xml = simplexml_load_string($file);
 $taxon_index = 0;
-foreach($xml->taxon as $taxon)
-{
+foreach($xml->taxon as $taxon) {
     $i = 0;
     $label_index = 0;
-    foreach($taxon->dataObject as $dataObject)
-    {
+    foreach($taxon->dataObject as $dataObject) {
         $i++;
         $dataObject_dc = $dataObject->children("http://purl.org/dc/elements/1.1/");
         
-        if($identifier = @$dataObject_dc->identifier)
-        {
-            if(preg_match("/^\/images\//", $identifier, $arr))
-            {
-                if(preg_match("/_l_[0-9]{1,}_high\.jpg/", $identifier))
-                {
+        if($identifier = @$dataObject_dc->identifier) {
+            if(preg_match("/^\/images\//", $identifier, $arr)) {
+                if(preg_match("/_l_[0-9]{1,}_high\.jpg/", $identifier)) {
                     $label_index = $i;
                     continue;
                 }
@@ -36,8 +29,7 @@ foreach($xml->taxon as $taxon)
         }
     }
     
-    if($label_index)
-    {
+    if($label_index) {
         $label_index -= 1;
         //echo "Unsetting taxon[$taxon_index]->dataObject[$label_index]\n";
         unset($xml->taxon[$taxon_index]->dataObject[$label_index]);
@@ -46,10 +38,8 @@ foreach($xml->taxon as $taxon)
     $taxon_index++;
 }
 
-if($taxon_index)
-{
+if($taxon_index) {
     $old_resource_path = CONTENT_RESOURCE_LOCAL_PATH . "24.xml";
-    
     if(!($OUT = fopen($old_resource_path, "w+")))
     {
       debug(__CLASS__ .":". __LINE__ .": Couldn't open file: " .$old_resource_path);
