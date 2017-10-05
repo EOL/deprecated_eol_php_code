22fd5d6055bbecbd549278246873b887623413e6
eliagbayani <eli@eol.org>
2017-10-05 10:39:38 -0400

    test

--- applications/dwc_validator/templates/validator/index.php
+++ applications/dwc_validator/templates/validator/index.php
@@ -14,10 +14,10 @@ namespace php_active_record;
 
 <html>
     <head>
-        <title>Archive and Spreadsheet Validator Thu Oct 5</title>
+        <title>Archive and Spreadsheet Validator</title>
     </head>
     <body>
-        <h2 align="center">Archive and Spreadsheet Validator</h2>
+        <h2 align="center">Archive and Spreadsheet Validator - Thu Oct 5</h2>
         <?php render_template("validator/form", array("file_url" => $file_url)); ?>
         <hr/>
         <?php render_template("validator/result", array("file_url" => $file_url, "file_upload" => $file_upload, "errors" => $errors, "structural_errors" => $structural_errors, "warnings" => $warnings, "stats" => $stats)); ?>
