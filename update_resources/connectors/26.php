2576d72004e74bf5a34e3917cc1793afc3425196
eliagbayani <eli@eol.org>
2017-10-10 01:22:04 -0400

    maintenance

--- update_resources/connectors/26.php
+++ update_resources/connectors/26.php
@@ -4,21 +4,23 @@ namespace php_active_record;
 WORMS archive
 Now partner provides/hosts a DWC-A file. Connector also converts Distribution text into structured data.
 estimated execution time: 30 mins, excluding download time of the archive file from partner's server
+*Started Jenkins execution 2017-Oct-10. Took 22 mins.
                                                                                                         2017
-                24Sep'14    20Nov'14                    1Dec'14     1Apr'15     8Apr    15Jun   8March  6-Oct
-agent:          [922]       948                         948         1015        1033    1044    1245    1352
-measurement:    [1,172,968] 1,484,488   diff 311,520    293,645     383423      383981  384798  411206  425373
-media_resource: [101,513]   102,009     diff 496        102,009     134461      134708  135570  144212  152067
-occurrence:     [279,966]   576,055                     291,683     380880      381457  382283          422847
-reference:      [319987]    322257                      322257      550506      552080  554390  581465  629030
-taxon:          [311866]    313006      diff 1,140      313006      539550      540877  512283  532578  573202
-vernacular:     [42231]     42226                       42226       46654       46657   46661   47809   72552
+                24Sep'14    20Nov'14                    1Dec'14     1Apr'15     8Apr    15Jun   8March  6-Oct   10-Oct
+agent:          [922]       948                         948         1015        1033    1044    1245    1352    1388
+measurement:    [1,172,968] 1,484,488   diff 311,520    293,645     383423      383981  384798  411206  425373  428106
+media_resource: [101,513]   102,009     diff 496        102,009     134461      134708  135570  144212  152067  160716
+occurrence:     [279,966]   576,055                     291,683     380880      381457  382283          422847  425583
+reference:      [319987]    322257                      322257      550506      552080  554390  581465  629030  637206
+taxon:          [311866]    313006      diff 1,140      313006      539550      540877  512283  532578  573202  581849
+vernacular:     [42231]     42226                       42226       46654       46657   46661   47809   72552   73967
                             
                             2017
-with media objects:         6-Oct
-[accepted]  =>      327966  332194
-[synonym]   =>      211583  203983
-[count]     =>      539549  573202
+with media objects:         6-Oct   10-Oct
+[accepted]  =>      327966  332194  337287
+[count]     =>      539549  573202  581849
+[synonym]   =>      211583  203983  207575
+[]          =>                      36987
 
 dynamic hierarchy   27Apr2017   18May2017
 [accepted] =>       303570      300105
@@ -26,8 +28,14 @@ dynamic hierarchy   27Apr2017   18May2017
 [] =>               5329        0
 [count] =>          443031      435129
 
-total no parent:    134134      135026
-/terms/taxon:       443440      435608
+total no parent:    134134      135026          ? not sure about these counts
+/terms/taxon:       443440      435608          ? not sure about these counts
+
+                            10-Oct
+Total undefined parents:    2503
+Total taxa without parents: 207577
+
+
 
 exec time: ~30 minutes
 */
@@ -35,11 +43,17 @@ include_once(dirname(__FILE__) . "/../../config/environment.php");
 require_library('connectors/WormsArchiveAPI');
 $timestart = time_elapsed();
 ini_set('memory_limit','7096M'); //required
+
 $resource_id = 26;
+// $resource_id = "26_taxonomy";
 
 // /* //main operation
 $func = new WormsArchiveAPI($resource_id);
-$func->get_all_taxa("media_objects"); //'taxonomy' not sure when was used |OR| 'media_objects' is for original resource = 26
+
+//only one of these 2 will run at any given time
+$func->get_all_taxa("media_objects"); //'media_objects' is for original resource = 26
+// $func->get_all_taxa("taxonomy");      //'taxonomy' not sure when was used
+
 Functions::finalize_dwca_resource($resource_id);
 // */
 
