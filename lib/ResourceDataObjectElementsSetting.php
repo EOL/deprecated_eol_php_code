ae420a60fa08b7277ef07a44fa4e066a42466957
eliagbayani <eli@eol.org>
2017-10-20 12:00:03 -0400

    mtce

--- lib/ResourceDataObjectElementsSetting.php
+++ lib/ResourceDataObjectElementsSetting.php
@@ -237,7 +237,7 @@ class ResourceDataObjectElementsSetting
         fclose($OUT);
     }
 
-    public function delete_taxon_if_no_dataObject($xml_string)
+    public static function delete_taxon_if_no_dataObject($xml_string)
     {
         if($xml = simplexml_load_string($xml_string))
         {
