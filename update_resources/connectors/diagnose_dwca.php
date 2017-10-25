d2fd568c0d54a4ee29c1ab0c85d7848e3e052bd5
eliagbayani <eli@eol.org>
2017-10-25 17:20:49 -0400

    mtce

--- update_resources/connectors/diagnose_dwca.php
+++ update_resources/connectors/diagnose_dwca.php
@@ -33,13 +33,23 @@ exit("\n\n");
 */
 
 
-// /*
+/*
 $resource_id = "Coral_Skeletons";//355;
 $func = new DWCADiagnoseAPI();
 $resource_id = 26; //435609
 Functions::count_resource_tab_files($resource_id);
 names_breakdown(26);
 exit;
+*/
+
+
+// /*
+$resource_id = "primate-measurements";
+$func = new DWCADiagnoseAPI();
+Functions::count_resource_tab_files($resource_id, ".txt");
+$result['undefined_uris'] = Functions::get_undefined_uris_from_resource($resource_id);
+print_r($result);
+exit("\n-end-\n");
 // */
 
 //=========================================================
