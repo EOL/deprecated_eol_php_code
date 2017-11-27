bc3d516f7180fa18b246670ae0802050110c019a
eliagbayani <eli@eol.org>
2017-11-27 07:16:22 -0500

    wip

--- update_resources/connectors/wikipedia.php
+++ update_resources/connectors/wikipedia.php
@@ -71,18 +71,18 @@ $func = new WikiDataAPI($resource_id, "en", "wikimedia");   //done - Used for Co
 
 // print_r($argv);
 // php5.6 wikipedia.php jenkins en generate_resource 1 300000 1of6
-$cmdline_params['jenkins_or_cron']  = @$argv[1];
-$cmdline_params['language']         = @$argv[2];
+$params['jenkins_or_cron']  = @$argv[1];
+$params['language']         = @$argv[2];
 
-$cmdline_params['task']             = @$argv[3];
-$cmdline_params['range_from']       = @$argv[4];
-$cmdline_params['range_to']         = @$argv[5];
-$cmdline_params['actual']           = @$argv[6];
-print_r($cmdline_params);
+$params['task']             = @$argv[3];
+$params['range_from']       = @$argv[4];
+$params['range_to']         = @$argv[5];
+$params['actual']           = @$argv[6];
+print_r($params);
 
 // /* //----------start main operation
-if($val = $cmdline_params['language']) $language = $val;
-else                                   $language = "zh"; //manually supplied
+if($val = $params['language']) $language = $val;
+else                           $language = "zh"; //manually supplied
 $resource_id = "wikipedia-".$language;
 $func = new WikiDataAPI($resource_id, $language); //generic call
 
