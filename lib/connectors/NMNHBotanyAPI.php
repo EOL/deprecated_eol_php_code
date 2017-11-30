ac093ad6484aa4d7e646f4452803b360f84d2074
eliagbayani <eli@eol.org>
2017-11-30 00:48:28 -0500

    NMNH Botany API wip

---
+++ lib/connectors/NMNHBotanyAPI.php
@@ -0,0 +1,90 @@
+<?php
+namespace php_active_record;
+/*  connector: 
+*/
+class NMNHBotanyAPI
+{
+    function __construct($folder)
+    {
+        $this->path_to_archive_directory = CONTENT_RESOURCE_LOCAL_PATH . '/' . $folder . '_working/';
+        $this->archive_builder = new \eol_schema\ContentArchiveBuilder(array('directory_path' => $this->path_to_archive_directory));
+        // $this->taxon_ids = array();
+        // $this->occurrence_ids = array();
+        // $this->media_ids = array();
+        // $this->agent_ids = array();
+        $this->debug = array();
+        $this->download_options = array("timeout" => 60*60, "expire_seconds" => 60*60*24*25);
+    }
+    
+    function start($params, $xml_file_YN = false, $expire_seconds = 60*60*24*25) //expires in 25 days
+    {
+        if(!$params["xmlYN"]) {
+            require_library('connectors/INBioAPI');
+            $func = new INBioAPI();
+            $paths = $func->extract_archive_file($params["eol_xml_file"], $params["filename"], $this->download_options);
+            print_r($paths);
+            self::convert_xml($paths["temp_dir"].$params["filename"]);
+            $this->archive_builder->finalize(TRUE);
+            recursive_rmdir($paths["temp_dir"]); // remove temp dir
+        }
+        else //is XML file
+        {
+            $params['path'] = DOC_ROOT . "tmp/";
+            $local_xml_file = Functions::save_remote_file_to_local($params['eol_xml_file'], array('file_extension' => "xml", "cache" => 1, "expire_seconds" => $expire_seconds, "timeout" => 7200, "download_attempts" => 2, "delay_in_minutes" => 2)); 
+            /* expire_seconds is irrelevant if there is no cache => 1 in save_remote_file_to_local() */ 
+            $params['filename'] = pathinfo($local_xml_file, PATHINFO_BASENAME);
+            self::convert_xml($local_xml_file);
+            $this->archive_builder->finalize(TRUE);
+            unlink($local_xml_file);
+        }
+        echo "\ntotal rows: $this->count\n";
+    }
+    
+    
+    function convert_xml($local_path)
+    {
+        $reader = new \XMLReader();
+        $reader->open($local_path);
+        $i = 0;
+        while(@$reader->read()) {
+            if($reader->nodeType == \XMLReader::ELEMENT && $reader->name == "taxon") {
+                $page_xml = $reader->readOuterXML();
+                $t = simplexml_load_string($page_xml, null, LIBXML_NOCDATA);
+                
+                $t_dc = $t->children("http://purl.org/dc/elements/1.1/");
+                $t_dcterms = $t->children("http://purl.org/dc/terms/");
+                $t_dwc = $t->children("http://rs.tdwg.org/dwc/dwcore/");
+                
+                if($val = $t_dc) print_r($val);
+                if($val = $t_dcterms) print_r($val);
+                if($val = $t_dwc) print_r($val);
+                print_r($t);
+            }
+        }
+    }
+    
+    /*
+    private function add_taxon($rec)
+    {
+        $taxon = new \eol_schema\Taxon();
+        $taxon->taxonID         = $rec['taxon_id'];
+        $taxon->scientificName  = ucfirst($rec['scientific_name']);
+        if($family = @$rec['family']) $taxon->family = ucfirst($family);
+        if($taxon->family == "Formicidae") {
+            $taxon->phylum  = 'Arthropoda';
+            $taxon->class   = 'Insecta';
+            $taxon->order   = 'Hymenoptera';
+        }
+        $taxon->furtherInformationURL = self::compute_furtherInformationURL($taxon->scientificName);
+        $taxon->kingdom         = '';
+        $taxon->genus           = '';
+        $taxon->furtherInformationURL = $t['dc_source'];
+        if($reference_ids = @$this->taxa_reference_ids[$t['int_id']]) $taxon->referenceID = implode("; ", $reference_ids);
+        $this->taxon_ids[$taxon->taxonID] = '';
+        $this->archive_builder->write_object_to_file($taxon);
+    }
+    private function add_string_types($rec, $value, $measurementType, $measurementOfTaxon = "") {}
+    private function add_occurrence($taxon_id, $occurrence_id, $rec, $unique_id) {}
+    */
+}
+?>
---
+++ update_resources/connectors/botany_nmnh.php
@@ -0,0 +1,39 @@
+<?php
+namespace php_active_record;
+/* 
+*/
+
+include_once(dirname(__FILE__) . "/../../config/environment.php");
+require_library('connectors/NMNHBotanyAPI');
+$GLOBALS['ENV_DEBUG'] = true;
+// ini_set('memory_limit','4096M'); //314,5728,000
+$timestart = time_elapsed();
+$resource_id = 346;
+
+//test resources - works OK
+$params["eol_xml_file"] = 'http://localhost/eol_php_code/applications/content_server/resources/eli.xml';
+$params["filename"] = 'eli.xml';
+$params["xmlYN"] = true;
+
+// $params["eol_xml_file"] = 'http://localhost/eol_php_code/applications/content_server/resources/eli.xml.zip';
+// $params["filename"] = 'eli.xml';
+// $params["xmlYN"] = false;
+
+//actual resource
+/*
+$params["eol_xml_file"] = 'http://collections.mnh.si.edu/services/eol/nmnh-botany-response.xml.gz';
+$params["eol_xml_file"] = 'http://localhost/cp/OpenData/EOLxml_2_DWCA/nmnh-botany-response.xml.gz';
+$params["filename"] = 'nmnh-botany-response.xml';
+$params["xmlYN"] = false;
+*/
+
+$func = new NMNHBotanyAPI($resource_id);
+$func->start($params);
+
+// Functions::finalize_dwca_resource($resource_id);
+$elapsed_time_sec = time_elapsed() - $timestart;
+echo "\n\n";
+echo "elapsed time = " . $elapsed_time_sec/60 . " minutes \n";
+echo "elapsed time = " . $elapsed_time_sec/60/60 . " hours \n";
+echo "\nDone processing.\n";
+?>
