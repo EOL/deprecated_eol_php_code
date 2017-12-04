37e7dedddf49779b193b238ca536c94d3d8b3937
eliagbayani <eli@eol.org>
2017-12-04 05:54:47 -0500

    cleaning

--- update_resources/connectors/wikipedia.php
+++ update_resources/connectors/wikipedia.php
@@ -1,6 +1,6 @@
 <?php
 namespace php_active_record;
-/* */
+/* Wikipedia in different languages */
 include_once(dirname(__FILE__) . "/../../config/environment.php");
 require_library('connectors/WikiDataAPI');
 $timestart = time_elapsed();
@@ -35,22 +35,6 @@ $func->process_wikimedia_txt_dump(); //initial verification of the wikimedia dum
 exit("\n Finished: just exploring... \n");
 */
 
-
-//First batch: English, French, German, Spanish, Japanese
-// $func = new WikiDataAPI($resource_id, "en");     en English
-// $func = new WikiDataAPI($resource_id, "fr");     fr French
-// $func = new WikiDataAPI($resource_id, "de");     de German
-// $func = new WikiDataAPI("957", "de");            de German
-// $func = new WikiDataAPI($resource_id, "es");     es Spanish
-// $func = new WikiDataAPI($resource_id, "ja");     ja Japanese
-
-//2nd batch:
-// $func = new WikiDataAPI($resource_id, "it");     it (it) Italian OK
-// $func = new WikiDataAPI($resource_id, "ru");     ru (ru) Russian OK
-// $func = new WikiDataAPI($resource_id, "pt");     pt (pt) Portuguese OK
-// $func = new WikiDataAPI($resource_id, "ko");     ko (ko) Korean OK
-// $func = new WikiDataAPI($resource_id, "zh");     zh (zh) Chinese OK
-
 // $func = new WikiDataAPI($resource_id, "cu");     cu ? investigate why so few...
 // $func = new WikiDataAPI($resource_id, "uk");     uk (uk) Ukrainian
 // $func = new WikiDataAPI($resource_id, "pl");     pl (pl) Polish
@@ -68,6 +52,7 @@ $func = new WikiDataAPI($resource_id, "en", "wikimedia");   //done - Used for Co
 // $func = new WikiDataAPI($resource_id, "ceb");
 
 // Wikipedia English is EOL resource_id = 80 --> http://www.eol.org/content_partners/129/resources/80
+// Wikipedia German is EOL resource_id = 957
 
 // print_r($argv);
 // php5.6 wikipedia.php jenkins en generate_resource 1 300000 1of6
@@ -105,10 +90,6 @@ else { //orig - just one connector
     $func->generate_resource();
     Functions::finalize_dwca_resource($resource_id);
 }
-
-
-
-
 // ----------end main operation */
 
 /* final-pt
@@ -131,77 +112,58 @@ echo "\n Done processing.\n";
 
 /* http://opendata.eol.org/dataset/wikipedia_5k
 Data and Resources
+
 wikipedia-en.tar.gz
 wikipedia-es.tar.gz
 wikipedia-de.tar.gz
 wikipedia-ja.tar.gz
 wikipedia-fr.tar.gz
-
 wikipedia-zh.tar.gz Chinese
-wikipedia-pl.tar.gz Polish
 wikipedia-ko.tar.gz Korean
 wikipedia-it.tar.gz Italian
 wikipedia-ru.tar.gz Russian
-wikipedia-uk.tar.gz Ukrainian
 wikipedia-pt.tar.gz Portuguese
+
+wikipedia-pl.tar.gz Polish
+wikipedia-uk.tar.gz Ukrainian
 wikipedia-nl.tar.gz Dutch
 wikipedia-vi.tar.gz Vietnamese
 wikipedia-sv.tar.gz Swedish
 wikipedia-cu.tar.gz Indo-European 	Church Slavic, Church Slavonic, Old Church Slavonic, Old Slavonic, Old Bulgarian
 
-*/
 
+wikipedia-en	Friday 2017-12-01 03:59:01 AM	{"media_resource.tab":671062,"taxon.tab":355505}
+80	            Monday 2017-12-04 04:16:56 AM	{"media_resource.tab":672049,"taxon.tab":356043}
+
+wikipedia-de	Saturday 2017-11-11 08:52:35 PM	{"media_resource.tab":87725,"taxon.tab":55915}
+957	            Monday 2017-12-04 05:20:51 AM	{"media_resource.tab":87940,"taxon.tab":56041}
+
+wikipedia-fr	Sunday 2017-12-03 12:04:24 AM	{"media_resource.tab":214962,"taxon.tab":119824}
+
+wikipedia-es	Sunday 2017-12-03 12:21:46 AM	{"media_resource.tab":300492,"taxon.tab":165487}
+
+wikipedia-ko	Thursday 2017-11-30 10:29:47 PM	{"media_resource.tab":24527,"taxon.tab":16954}
+wikipedia-ko	Sunday 2017-12-03 12:48:17 AM	{"media_resource.tab":24701,"taxon.tab":17060}
+
+wikipedia-ja	Friday 2017-11-10 08:35:41 AM	{"media_resource.tab":26208,"taxon.tab":20431}
+wikipedia-ja	Saturday 2017-12-02 10:53:40 PM	{"media_resource.tab":26264,"taxon.tab":20475}
+
+wikipedia-ru	Sunday 2017-11-12 12:03:09 PM	{"media_resource.tab":77531,"taxon.tab":47336}
+wikipedia-ru	Saturday 2017-12-02 11:31:53 PM	{"media_resource.tab":77649,"taxon.tab":47398}
+
+wikipedia-it	Tuesday 2017-11-14 04:48:25 AM	{"media_resource.tab":64723,"taxon.tab":39992}
+wikipedia-it	Monday 2017-12-04 05:16:39 AM	{"media_resource.tab":64861,"taxon.tab":40069}
+
+wikipedia-pt	Sunday 2017-12-03 01:34:21 AM	{"media_resource.tab":192390,"taxon.tab":108840}
+
+wikipedia-zh	Tuesday 2017-11-14 03:41:12 PM	{"media_resource.tab":156137,"taxon.tab":91145}
+wikipedia-zh	Sunday 2017-12-03 12:33:20 AM	{"media_resource.tab":156585,"taxon.tab":91396}
+*/
 /*
 this is a request made by wikimedia harvester (71): this 2 are same, first is a subset of the 2nd. And one is urlencoded() other is not.
 http://commons.wikimedia.org/w/api.php?action=query&format=json&prop=imageinfo|categories&iiprop=url|mime|mediatype&cllimit=500&redirects&titles=File%3AROSMARINUS+OFFICINALIS+-+MORROCURT+-+IB-976+(Roman%C3%AD).JPG
 http://commons.wikimedia.org/w/api.php?action=query&format=json&prop=imageinfo%7Ccategories&iiprop=url%7Cmime%7Cmediatype&cllimit=500&redirects&titles=File%3AROSMARINUS+OFFICINALIS+-+MORROCURT+-+IB-976+%28Roman%C3%AD%29.JPG%7CFile%3ACapra+hircus+aegagrus-cropped.jpg%7CFile%3AEschrichtius+robustus+01-cropped.jpg%7CFile%3AMonachus+monachus+-+Museo+civico+di+storia+naturale+%28Milan%29-cropped.jpg%7CFile%3AMustela+putorius+01-cropped.jpg%7CFile%3ACondylactis+gigantea+%28giant+Caribbean+sea+anemone%29+closeup.jpg%7CFile%3A20140922-cephalopholis+hemistiktos.jpg%7CFile%3APelecanus+occidentalis+in+flight+at+Bodega+Bay.jpg%7CFile%3AUdotea+flabellum+%28mermaid%27s+fan+alga%29+Bahamas.jpg%7CFile%3ARhipocephalus+phoenix+%28pinecone+alga%29+%28San+Salvador+Island%2C+Bahamas%29.jpg%7CFile%3APadina+boergesenii+%28leafy+rolled-blade+algae+Bahamas.jpg%7CFile%3ABird%27s-nest+Fern+Asplenium+nidus+Leaves+2.jpg%7CFile%3ABird%27s-nest+Fern+Asplenium+nidus+Leaves+1.jpg%7CFile%3ABlack+Caterpillar+Fern+Scyphularia+pentaphylla+Leaves.JPG%7CFile%3ABlunt-lobed+Woodsia+obtusa+Winter+Foliage.JPG%7CFile%3AChinese+Elm+Ulmus+parvifolia+%2832-0052-A%29+Bark+1.JPG%7CFile%3AChinese+Elm+Ulmus+parvifolia+%2832-0052-A%29+Bark+2.JPG%7CFile%3AJapanese+Maple+Acer+palmatum+%282012-055-A%29+Trunk+Bark.JPG%7CFile%3AJapanese+Alder+Alnus+japonica+%2881-305-A%29+Base+Bark.JPG%7CFile%3AJapanese+Maple+Acer+palmatum+%282010-012-A%29+Bark+Closeup+1.JPG%7CFile%3AJapanese+Maple+Acer+palmatum+%282010-012-A%29+Bark+Closeup+2.JPG%7CFile%3AJapanese+Zelkova+serrata+%2881-502-L%29+Upper+Trunk+Bark.JPG%7CFile%3AMiyabe+Maple+Acer+miyabei+%2851-292-A%29+Trunk+Bark+Closeup.JPG%7CFile%3AJapanese+Zelkova+serrata+%2881-502-L%29+Lower+Trunk+Bark.JPG%7CFile%3AMiyabe+Maple+Acer+miyabei+%2851-292-A%29+Trunk+Bark.JPG%7CFile%3ARed+Maple+Acer+rubrum+%2832-0877-A%29+Trunk+Bark.JPG%7CFile%3AWhite+Ash+Fraxinus+americana+%2854-0751-A%29+Trunk+Bark.JPG%7CFile%3ANerita+versicolor+%28four-toothed+nerite+snails%29+Bahamas.jpg%7CFile%3ACenchritis+muricatus+%28beaded+periwinkle+snails%29+Bahamas.jpg%7CFile%3AMonodelphis+domestica+skeleton+-+ZooKeys+465-10.png%7CFile%3AMonodelphis+brevicaudata+skull+-+ZooKeys+465-08.png%7CFile%3APeradectes+molars.png%7CFile%3APediomys+molars.png%7CFile%3AScreen+on+the+supermarket+shelf+%282654814813%29.jpg%7CFile%3ASacpha+Hodgson.jpg%7CFile%3AA+Sulphur+Crested+White+Cockatoo+%28Cacatua+galerita%29%2C+Cronulla%2C+NSW+Australia.jpg%7CFile%3AGOCZA%C5%81KOWICE+ZDR%C3%93J%2C+AB.+071.JPG%7CFile%3AVexillum+ebenus+01.JPG%7CFile%3ASCANDIX+PECTEN-VENERIS+-+AGUDA+-+IB-049+%28Agulles+de+pastor%29.JPG%7CFile%3ASCANDIX+PECTEN-VENERIS+-+AGUDA+-+IB-069+%28Agulles+de+pastor%29.JPG%7CFile%3ACRATAEGUS+MONOGYNA+-+AGUDA+-+IB-082+%28Ar%C3%A7%29.JPG%7CFile%3ACRATAEGUS+MONOGYNA+-+AGUDA+-+IB-089+%28Ar%C3%A7%29.JPG%7CFile%3ATagetes+erecta+23122014+%284%29.jpg%7CFile%3ACalendula+officinalis+27122014+%286%29.jpg%7CFile%3AFlowers+of+Judas+tree.jpg%7CFile%3ASyntrichia+montana+%28a%2C+164114-475524%29+9259.JPG%7CFile%3ASyntrichia+montana+%28a%2C+164114-475524%29+9262.JPG%7CFile%3ASyntrichia+montana+%28a%2C+164114-475524%29+9263.JPG%7CFile%3ASyntrichia+montana+%28a%2C+164114-475524%29+9264.JPG%7CFile%3ASyntrichia+montana+%28a%2C+164114-475524%29+9265.JPG&continue=
 
 http://commons.wikimedia.org/w/api.php?action=query&format=json&prop=imageinfo|categories&iiprop=url|mime|mediatype&cllimit=500&redirects&titles=File:Whales are Paraphyletic.png
-*/
-/* wikimedia stats:
-Statistics
-
-    http://rs.tdwg.org/dwc/terms/taxon:
-
-        Total: 73533
-
-    http://eol.org/schema/agent/agent:
-
-        Total: 17793
-
-    http://eol.org/schema/media/document:
-
-        Total by type:
-
-            http://purl.org/dc/dcmitype/StillImage: 806464
-            http://purl.org/dc/dcmitype/MovingImage: 6223
-            http://purl.org/dc/dcmitype/Sound: 1610
-
-        Total by license:
-
-            http://creativecommons.org/licenses/publicdomain/: 192680
-            http://creativecommons.org/licenses/by/3.0/: 495316
-            http://creativecommons.org/licenses/by-sa/3.0/: 124755
-            http://creativecommons.org/licenses/by-nc/3.0/: 731
-            No known copyright restrictions: 804
-            http://creativecommons.org/licenses/by-nc-sa/3.0/: 11
-
-        Total by language:
-
-            en: 814297
-
-        Total by format:
-
-            image/jpeg: 769547
-            image/png: 18712
-            image/svg+xml: 3954
-            video/ogg: 4498
-            image/tiff: 12955
-            audio/ogg: 1581
-            image/gif: 1296
-            video/webm: 1725
-            audio/x-wav: 29
-
-        Total: 814297
 */
 ?>
