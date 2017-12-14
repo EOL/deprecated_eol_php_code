963f7cd771f2cb81a49bce437e84bdb4ab644d65
eliagbayani <eli@eol.org>
2017-12-14 11:23:30 -0500

    Data-1727

--- update_resources/connectors/spreadsheet_2_dwca.php
+++ update_resources/connectors/spreadsheet_2_dwca.php
@@ -33,6 +33,7 @@ if($val = $cmdline_params['resource_id'])
     elseif($resource_id == 1007) $params['spreadsheet'] = "http://opendata.eol.org/dataset/bd2b0922-950a-4e7b-bf21-fbc7f734a4d7/resource/aeb5f0a9-e506-4d9a-bec4-0b7f825902dd/download/arctic-arthropods-traits-1.xlsx"; //"http://localhost/cp/spreadsheets/Arctic spreadsheets/alaskanarthropoda.xls.zip";
     elseif($resource_id == 707) $params['spreadsheet'] = "http://opendata.eol.org/dataset/99da8344-22a4-4d9d-890d-44ac3fc34a33/resource/87a9bb7f-90f1-4cfd-a52c-09bb8f9c91f6/download/birds-animaldiversityweb.xlsx";
     elseif($resource_id == 'plant_forms_habitat_and_distribution') $params['spreadsheet'] = "https://opendata.eol.org/dataset/aab3818f-d538-4d5f-a3b8-b52d233c7ac0/resource/8e46a838-0394-42bd-9272-0a8591834713/download/plant-habit-habitat-and-distribution.xlsx";
+    elseif($resource_id == 'world_wildlife_fund') $params['spreadsheet'] = "http://opendata.eol.org/dataset/8ace52ee-4c87-4231-8c38-7fa04b2efd37/resource/c2b5b9e3-0b87-498f-bf3a-84cbc937a28e/download/wff-regions-version-2.xlsx";
     else exit("\nProgram will terminate. Invalid resource_id [$resource_id].\n\n");
 }
 else //no resource_id
