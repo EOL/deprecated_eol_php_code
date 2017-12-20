d54b074f7b8cfda9ffb987fe992bb0586bac1cd9
eliagbayani <eli@eol.org>
2017-12-20 00:21:02 -0500

    DATA-1718 WIP

--- lib/connectors/EOLv2MetadataAPI.php
+++ lib/connectors/EOLv2MetadataAPI.php
@@ -22,16 +22,18 @@ class EOLv2MetadataAPI
                 WHERE r.id = 42
                 ORDER BY cp.id";
                 
-        $sql = "SELECT cp.id as partner_id, cp.full_name as partner_name, s.label as status, r.id as resource_id, r.title as resource_title, 
+        $sql = "SELECT cp.id as partner_id, cp.full_name as partner_name, s.label as status, r.id as resource_id, r.title as resource_title, s2.label as resource_status,
             cp.description as overview, cp.homepage as url, 
             cpa.mou_url as agreement_url, cpa.signed_on_date as signed_date, cpa.signed_by, cpa.created_at as create_date,
-            cp.description_of_data as desc_of_data
+            cp.description_of_data as desc_of_data, cp.user_id as manager_eol_id
             FROM content_partners cp
             JOIN translated_content_partner_statuses s ON (cp.content_partner_status_id=s.id)
             JOIN resources r ON (cp.id=r.content_partner_id)
+            JOIN translated_resource_statuses s2 ON (r.resource_status_id=s2.id)
+            
             JOIN content_partner_agreements cpa ON (cp.id=cpa.content_partner_id)
-            WHERE s.language_id = 152 
-            ORDER BY cp.id limit 5";
+            WHERE s.language_id = 152 AND s2.language_id = 152
+            ORDER BY cp.id limit 6";
 
             // $result = $mysqli->query("SELECT r.hierarchy_id, max(he.id) as max FROM resources r JOIN harvest_events he ON (r.id=he.resource_id) GROUP BY r.hierarchy_id");
             // $result = $mysqli->query("SELECT r.hierarchy_id, max(he.id) as max FROM resources r JOIN harvest_events he ON (r.id=he.resource_id) GROUP BY r.hierarchy_id");
@@ -42,18 +44,19 @@ class EOLv2MetadataAPI
                 $first_pub = $this->mysqli->select_value("SELECT min(he.published_at) as last_published FROM resources r JOIN harvest_events he ON (r.id=he.resource_id) WHERE r.id = ".$row['resource_id']);
                 $last_pub = $this->mysqli->select_value("SELECT max(he.published_at) as last_published FROM resources r JOIN harvest_events he ON (r.id=he.resource_id) WHERE r.id = ".$row['resource_id']);
 
-                $recs[$row['partner_id']] = array('partner_name' => $row['partner_name'], 'partner_id' => $row['partner_id'], 
-                'overview' => $row['overview'], 'url' => $row['url'], 'agreement_url' => $row['agreement_url'], 'signed_by' => $row['signed_by'], 'signed_date' => $row['signed_date'],
-                'create_date' => $row['create_date'], 'desc_of_data' => $row['desc_of_data']
+                $recs[$row['partner_id']] = array('partner_name' => $row['partner_name'], 'partner_id' => $row['partner_id'], 'status' => $row['status'],
+                'overview' => $row['overview'], 'url' => $row['url'], 'agreement_url_from_db' => $row['agreement_url'], 'agreement_url' => self::fix_agreement_url($row['agreement_url']), 
+                'signed_by' => $row['signed_by'], 'signed_date' => $row['signed_date'], 'create_date' => $row['create_date'], 'desc_of_data' => $row['desc_of_data'],
+                'manager_eol_id' => $row['manager_eol_id']
                 );
-                $recs[$row['partner_id']]['resources'][] = array('resource_title' => $row['resource_title'], 'first_pub' => $first_pub, 'last_pub' => $last_pub);
+                $recs[$row['partner_id']]['resources'][] = array('resource_title' => $row['resource_title'], 'first_pub' => $first_pub, 'last_pub' => $last_pub, 'status' => $row['resource_status']);
                 
-                $sql = "SELECT cpc.given_name, cpc.family_name, cpc.email, cpc.homepage, cpc.telephone, cpc.address, s.label as contact_role
+                $sql = "SELECT cpc.id as eol_contact_id, cpc.given_name, cpc.family_name, cpc.email, cpc.homepage, cpc.telephone, cpc.address, s.label as contact_role
                 FROM content_partner_contacts cpc JOIN translated_contact_roles s ON (cpc.contact_role_id=s.id) 
                 WHERE cpc.content_partner_id = ".$row['partner_id']." AND s.language_id = 152";
                 $contacts = $this->mysqli->query($sql);
                 while($contacts && $row2=$contacts->fetch_assoc()) {
-                    $recs[$row['partner_id']]['contacts'][] = array('given_name' => $row2['given_name'], 'family_name' => $row2['family_name'], 'email' => $row2['email'],
+                    $recs[$row['partner_id']]['contacts'][] = array('eol_contact_id' => $row2['eol_contact_id'], 'given_name' => $row2['given_name'], 'family_name' => $row2['family_name'], 'email' => $row2['email'],
                     'homepage' => $row2['homepage'], 'telephone' => $row2['telephone'], 'address' => $row2['address'], 'contact_role' => $row2['contact_role'],);
                 }
                 
@@ -61,6 +64,16 @@ class EOLv2MetadataAPI
                 // if(!$harvest_event->published_at) $GLOBALS['hierarchy_preview_harvest_event'][$row['hierarchy_id']] = $row['max'];
             }
             print_r($recs);
+    }
+    private function fix_agreement_url($url_from_db)
+    {
+        //                   /files/pdfs/mou/EOL_FishBase-mou.pdf
+        // http://www.eol.org/files/pdfs/mou/EOL_FishBase-mou.pdf
+        if(substr($url_from_db, 0, 16) == "/files/pdfs/mou/") return "http://www.eol.org".$url_from_db;
+        $url_from_db = str_replace("content8.eol.org", "content.eol.org", $url_from_db);
+        $url_from_db = str_replace("content4.eol.org", "content.eol.org", $url_from_db);
+        $url_from_db = str_replace("content1.eol.org", "content.eol.org", $url_from_db);
+        return $url_from_db;
     }
     
     /*
