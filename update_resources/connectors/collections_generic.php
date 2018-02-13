0aa58bb1889975136ee55ce67a727094fe2a214e
eliagbayani <eli@eol.org>
2018-02-13 02:25:02 -0500

    DATA-1735 wip[

--- update_resources/connectors/collections_generic.php
+++ update_resources/connectors/collections_generic.php
@@ -7,8 +7,8 @@ This connector will use COLLECTIONS and dataObject API to process EOL XML:
 3. it will save media objects to /other_files/media/01/ or /23/. The last 2 chars of a data_object_id.
 4. this will also combine 2 DwCA (IF NEEDED).
 e.g.
-- LD_afrotropicalbirds.tar.gz               ---> for text objects
-- LD_afrotropicalbirds_multimedia.tar.gz    ---> for media objects
+- EOL_afrotropicalbirds.tar.gz               ---> for text objects
+- EOL_afrotropicalbirds_multimedia.tar.gz    ---> for media objects
 */
 
 include_once(dirname(__FILE__) . "/../../config/environment.php");
@@ -17,6 +17,8 @@ $timestart = time_elapsed();
 require_library('connectors/LifeDeskToEOLAPI');
 $func1 = new LifeDeskToEOLAPI();
 
+// $s = "12345601"; echo "\n".substr($s, -2)."\n"; exit;
+
 require_library('connectors/ConvertEOLtoDWCaAPI');
 require_library('connectors/CollectionsScrapeAPI');
 require_library('connectors/DwCA_Utility');
@@ -24,11 +26,10 @@ require_library('connectors/DwCA_Utility');
 $final = array();
 $lifedesks = array('AnAge_text'); $final = array_merge($final, $lifedesks);
 
-$info['AnAge_text'] = array('id' => 195, 'LD_domain' => 'http://www.eol.org/content_partners/33/resources/40', 'OpenData_title' => 'AnAge text', 'resource_id' => 40);
-$xml_path['AnAge_text'] = "http://localhost/cp_new/OpenData/EOLxml_2_DWCA/".$ld."/anagetext.xml.gz";
+$info['AnAge_text'] = array('id' => 195, 'domain' => 'http://www.eol.org/content_partners/33/resources/40', 'OpenData_title' => 'AnAge text', 'resource_id' => 40);
+$xml_path['AnAge_text'] = "http://localhost/cp_new/OpenData/EOLxml_2_DWCA/AnAge_text/anagetext.xml.gz";
 $xml_path['AnAge_text'] = "https://github.com/eliagbayani/EOL-connector-data-files/raw/master/OpenData/EOLxml_2_DWCA/AnAge_text/anagetext.xml.gz";
 $xml_path['AnAge_text'] = "https://opendata.eol.org/dataset/cf4c5598-3a7c-464d-be87-d72bc98b066e/resource/b9bdc248-d2db-427a-af38-90313b168f0e/download/anagetext.xml.gz";
-
 
 /* this works OK. but was decided not to add ancestry if original source doesn't have ancestry. Makes sense.
 $ancestry[40] = array('kingdom' => 'Animalia', 'phylum' => 'Chordata', 'class' => 'Aves'); 
@@ -40,47 +41,49 @@ $final = array_merge($final, array_keys($info));
 
 // /* normal operation
 foreach($final as $ld) {
-    $params[$lifedesk]["local"]["lifedesk"] = $xml_path[$ld];
-    $params[$ld]["local"]["name"]           = $ld;
-    $params[$ld]["local"]["ancestry"]       = @$ancestry[$ld];
+    $params[$ld]["local"]["lifedesk"] = $xml_path[$ld];
+    $params[$ld]["local"]["name"]     = $ld;
+    $params[$ld]["local"]["ancestry"] = @$ancestry[$ld];
 }
 $final = array_unique($final);
 print_r($final); echo "\n".count($final)."\n"; //exit;
 $cont_compile = false;
 
 foreach($final as $lifedesk) {
-    $taxa_from_orig_LifeDesk_XML = array(); //https://eol-jira.bibalex.org/browse/DATA-1569?focusedCommentId=62081&page=com.atlassian.jira.plugin.system.issuetabpanels:comment-tabpanel#comment-62081
-    $taxa_from_orig_LifeDesk_XML = $func1->export_lifedesk_to_eol($params[$lifedesk]["local"]);
-    if(Functions::url_exists($params[$lifedesk]["local"]["lifedesk"])) {
-        convert_xml_2_dwca("LD_".$lifedesk); //convert XML to DwCA
+    $infox = $func1->get_taxa_from_EOL_XML($xml_path[$lifedesk]);
+    $taxa_from_orig_LifeDesk_XML = $infox['taxa_from_EOL_XML'];
+    $path                        = $infox['xml_path']; //e.g. '/Library/WebServer/Documents/eol_php_code/tmp/dir_50900/anagetext.xml'
+    // print_r($info); exit;
+    if(Functions::url_exists($xml_path[$lifedesk])) {
+        convert_xml_2_dwca($path, $lifedesk); //convert XML to DwCA
         $cont_compile = true;
     }
 
     // start generate the 2nd DwCA -------------------------------
-    $resource_id = "LD_".$lifedesk."_multimedia";
+    $resource_id = "EOL_".$lifedesk."_multimedia";
     if($collection_id = @$info[$lifedesk]['id']) { //9528;
-        $func2 = new CollectionsScrapeAPI($resource_id, $collection_id);
+        $func2 = new CollectionsScrapeAPI($resource_id, $collection_id, array('text'));
         $func2->start($taxa_from_orig_LifeDesk_XML);
         Functions::finalize_dwca_resource($resource_id, false, false); //3rd param true means resource folder will be deleted
         $cont_compile = true;
     }
-    else echo "\nNo Collection for this LifeDesk.\n";
+    else echo "\nNo Collection for this resource.\n";
     // end generate the 2nd DwCA -------------------------------
     
     //  --------------------------------------------------- start compiling the 2 DwCA files into 1 final DwCA --------------------------------------------------- 
     if($cont_compile) {
         $dwca_file = false;
-        $resource_id = "LD_".$lifedesk."_final";
+        $resource_id = "EOL_".$lifedesk."_final";
         $func2 = new DwCA_Utility($resource_id, $dwca_file); //2nd param is false bec. it'll process multiple archives, see convert_archive_files() in library DwCA_Utility.php
 
         $archives = array();
         /* use this if we're getting taxa info (e.g. ancestry) from Collection
-        if(file_exists(CONTENT_RESOURCE_LOCAL_PATH."LD_".$lifedesk."_multimedia.tar.gz")) $archives[] = "LD_".$lifedesk."_multimedia";
-        if(file_exists(CONTENT_RESOURCE_LOCAL_PATH."LD_".$lifedesk.".tar.gz"))            $archives[] = "LD_".$lifedesk;
+        if(file_exists(CONTENT_RESOURCE_LOCAL_PATH."EOL_".$lifedesk."_multimedia.tar.gz")) $archives[] = "EOL_".$lifedesk."_multimedia";
+        if(file_exists(CONTENT_RESOURCE_LOCAL_PATH."EOL_".$lifedesk.".tar.gz"))            $archives[] = "EOL_".$lifedesk;
         */
         // Otherwise let the taxa from LifeDesk XML be prioritized
-        if(file_exists(CONTENT_RESOURCE_LOCAL_PATH."LD_".$lifedesk.".tar.gz"))            $archives[] = "LD_".$lifedesk;
-        if(file_exists(CONTENT_RESOURCE_LOCAL_PATH."LD_".$lifedesk."_multimedia.tar.gz")) $archives[] = "LD_".$lifedesk."_multimedia";
+        if(file_exists(CONTENT_RESOURCE_LOCAL_PATH."EOL_".$lifedesk.".tar.gz"))            $archives[] = "EOL_".$lifedesk;
+        if(file_exists(CONTENT_RESOURCE_LOCAL_PATH."EOL_".$lifedesk."_multimedia.tar.gz")) $archives[] = "EOL_".$lifedesk."_multimedia";
 
 
         $func2->convert_archive_files($archives); //this is same as convert_archive(), only it processes multiple DwCA files not just one.
@@ -109,11 +112,11 @@ echo "elapsed time = " . $elapsed_time_sec/60 . " minutes \n";
 echo "elapsed time = " . $elapsed_time_sec/60/60 . " hours \n";
 echo "\nDone processing.\n";
 
-function convert_xml_2_dwca($resource_id)
+function convert_xml_2_dwca($path, $resource_id)
 {
-    $params["eol_xml_file"] = "http://localhost/eol_php_code/applications/content_server/resources/".$resource_id.".xml"; //e.g. LD_afrotropicalbirds
+    $params["eol_xml_file"] = $path;
     $params["filename"]     = "no need to mention here.xml";
-    $params["dataset"]      = "LifeDesk XML files";
+    $params["dataset"]      = "EOL XML files";
     $params["resource_id"]  = $resource_id;
     $func = new ConvertEOLtoDWCaAPI($resource_id);
     
