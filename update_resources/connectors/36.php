399f6e1f92cca59cfac29b27f6f6213b33c2bf69
eliagbayani <eli@eol.org>
2018-02-15 00:35:35 -0500

    resource 36

---
+++ update_resources/connectors/36.php
@@ -0,0 +1,30 @@
+<?php
+namespace php_active_record;
+/* 
+http://www.eol.org/content_partners/47/resources/36
+https://eol-jira.bibalex.org/browse/TRAM-703
+
+This is a generic script that will convert EOL XML to EOL DWC-A
+*/
+
+include_once(dirname(__FILE__) . "/../../config/environment.php");
+require_library('connectors/ConvertEOLtoDWCaAPI');
+$timestart = time_elapsed();
+
+$resource_id = 36;
+$params["eol_xml_file"] = "http://services.eol.org/resources/36.xml";
+$params["filename"]     = "no need to mention here.xml"; //no need to mention if eol_xml_file is already .xml and not .xml.gz
+$params["dataset"]      = "EOL XML files";
+$params["resource_id"]  = $resource_id;
+
+$func = new ConvertEOLtoDWCaAPI($resource_id);
+$func->export_xml_to_archive($params, true, 60*60*24*25); // true => means it is an XML file, not an archive file nor a zip file. Expires in 25 days.
+Functions::finalize_dwca_resource($resource_id);
+$elapsed_time_sec = time_elapsed() - $timestart;
+echo "\n\n";
+echo "elapsed time = " . $elapsed_time_sec/60 . " minutes \n";
+echo "elapsed time = " . $elapsed_time_sec/60/60 . " hours \n";
+echo "\nDone processing.\n";
+?>
+
+
