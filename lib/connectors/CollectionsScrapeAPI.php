83f67fce5cd9cc16c9cda8d7949483f91dffd682
eliagbayani <eli@eol.org>
2018-02-15 05:14:54 -0500

    mtce

--- lib/connectors/CollectionsScrapeAPI.php
+++ lib/connectors/CollectionsScrapeAPI.php
@@ -60,10 +60,10 @@ class CollectionsScrapeAPI
             foreach($do_ids as $do_id) 
             {
                 // /* breakdown when caching:
-                $k++;
+                $k++; echo " $k ";
                 $cont = false;
-                // if($k >=  1    && $k < $m) $cont = true;
-                if($k >=  $m   && $k < $m*2) $cont = true;
+                if($k >=  1    && $k < $m) $cont = true;
+                // if($k >=  $m   && $k < $m*2) $cont = true;
                 // if($k >=  $m*2 && $k < $m*3) $cont = true;
                 // if($k >=  $m*3 && $k < $m*4) $cont = true;
                 // if($k >=  $m*4 && $k < $m*5) $cont = true;
