32e10b9d09b3af795486ef46ab21672e60638c74
eliagbayani <eli@eol.org>
2018-02-15 05:27:14 -0500

    mtce

--- lib/connectors/CollectionsScrapeAPI.php
+++ lib/connectors/CollectionsScrapeAPI.php
@@ -64,8 +64,8 @@ class CollectionsScrapeAPI
                 $cont = false;
                 // if($k >=  1    && $k < $m) $cont = true;
                 // if($k >=  $m   && $k < $m*2) $cont = true;
-                if($k >=  $m*2 && $k < $m*3) $cont = true;
-                // if($k >=  $m*3 && $k < $m*4) $cont = true;
+                // if($k >=  $m*2 && $k < $m*3) $cont = true;
+                if($k >=  $m*3 && $k < $m*4) $cont = true;
                 // if($k >=  $m*4 && $k < $m*5) $cont = true;
                 if(!$cont) continue;
                 // */
