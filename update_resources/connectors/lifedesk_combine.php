8b72da484cc1c50e7b2b44f0b57deecddbe9e637
eliagbayani <eli@eol.org>
2018-02-16 02:25:37 -0500

    mtce

--- update_resources/connectors/lifedesk_combine.php
+++ update_resources/connectors/lifedesk_combine.php
@@ -21,8 +21,8 @@ require_library('connectors/DwCA_Utility');
 /* MicroScope, FieldScope, Biscayne_BioBlitz -> have EOL XML, with media objects that are offline. Has Collections for source of media objects. Media objects from XML will be removed like that of LifeDesks */
 
 $final = array();
-$lifedesks = array('288', '297', '281', '279'); $final = array_merge($final, $lifedesks);    //testing...MicroScope   FieldScope   Biscayne_BioBlitz
-// $lifedesks = array('277', '276', '275', '271'); $final = array_merge($final, $lifedesks);    //testing...MicroScope   FieldScope   Biscayne_BioBlitz
+// $lifedesks = array('288', '297', '281', '279'); $final = array_merge($final, $lifedesks);    //testing...MicroScope   FieldScope   Biscayne_BioBlitz
+$lifedesks = array('277', '276', '275', '271'); $final = array_merge($final, $lifedesks);    //testing...MicroScope   FieldScope   Biscayne_BioBlitz
 
 /* template
 $info['res_id'] = array('id' => col_id, 'domain' => 'http', 'OpenData_title' => 'xxx', 'resource_id' => res_id, 'prefix' => "EOL_");
