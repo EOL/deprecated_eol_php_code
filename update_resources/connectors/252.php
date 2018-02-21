a58c678c17bdc5d3b52592110de81b646f2433fa
eliagbayani <eli@eol.org>
2018-02-21 11:14:55 -0500

    mtce

--- update_resources/connectors/252.php
+++ update_resources/connectors/252.php
@@ -8,11 +8,14 @@ estimated execution time: 4.27 hours
 include_once(dirname(__FILE__) . "/../../config/environment.php");
 $timestart = time_elapsed();
 
+// Connector broken, will use http://services.eol.org/resources/252.xml for the meantime. Will fix after March 31.
+return;
+
 require_library('connectors/DiscoverLife_KeysAPI');
-
 $resource_id = 252;
-DiscoverLife_KeysAPI::get_all_taxa_keys($resource_id);
-
+// DiscoverLife_KeysAPI::get_all_taxa_keys($resource_id);
+$func = new DiscoverLife_KeysAPI();
+$func->get_all_taxa_keys($resource_id);
 Functions::set_resource_status_to_harvest_requested($resource_id);
 
 $elapsed_time_sec = time_elapsed() - $timestart;
