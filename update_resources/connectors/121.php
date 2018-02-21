b19b760af09d4d83fff8ea88299f419ad6692e87
eliagbayani <eli@eol.org>
2018-02-21 00:54:39 -0500

    121: Photographic Identification Guide to Larvae at Hydrothermal Vents E.Pacific Resource

--- update_resources/connectors/121.php
+++ update_resources/connectors/121.php
@@ -11,15 +11,26 @@ $taxa = HydrothermalVentLarvaeAPI::get_all_taxa();
 $xml = \SchemaDocument::get_taxon_xml($taxa);
 $resource_id = 121;
 $resource_path = CONTENT_RESOURCE_LOCAL_PATH . $resource_id . ".xml";
-if(!($OUT = fopen($resource_path, "w+")))
-{
-  debug(__CLASS__ .":". __LINE__ .": Couldn't open file: " .$resource_path);
-  return;
-}
+if(!($OUT = Functions::file_open($resource_path, "w+"))) return;
 fwrite($OUT, $xml);
 fclose($OUT);
-Functions::set_resource_status_to_harvest_requested($resource_id);
+// Functions::set_resource_status_to_harvest_requested($resource_id);
 Functions::gzip_resource_xml($resource_id);
+unlink($resource_path);
+
+
+//start converting to DwCA
+require_library('connectors/ConvertEOLtoDWCaAPI');
+$params["eol_xml_file"] = CONTENT_RESOURCE_LOCAL_PATH . $resource_id . ".xml";
+$params["filename"]     = "no need to mention here.xml"; //no need to mention if eol_xml_file is already .xml and not .xml.gz
+$params["dataset"]      = "EOL XML files";
+$params["resource_id"]  = $resource_id;
+
+$func = new ConvertEOLtoDWCaAPI($resource_id);
+$func->export_xml_to_archive($params, true, 60*60*24*25); // true => means it is an XML file, not an archive file nor a zip file. Expires in 25 days.
+Functions::finalize_dwca_resource($resource_id, false, true); //3rd param true means to delete the dwca folder.
+//end conversion
+
 $elapsed_time_sec = time_elapsed()-$timestart;
 echo "\n";
 echo "elapsed time = $elapsed_time_sec seconds             \n";
