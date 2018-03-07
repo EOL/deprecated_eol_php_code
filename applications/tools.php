5dab138c17fe905d6d53cd4d49b0d6abeeefc81f
eliagbayani <eli@eol.org>
2018-03-07 09:33:08 -0500

    mtce

--- applications/tools.php
+++ applications/tools.php
@@ -1,7 +1,7 @@
 <?php
 namespace php_active_record;
 include_once(dirname(__FILE__) . "/../config/environment.php");
-$GLOBALS['ENV_DEBUG'] = false;
+$GLOBALS['ENV_DEBUG'] = true;
 echo '<a href="' . WEB_ROOT .'/applications/dwc_validator/">Archive and Spreadsheet Validator</a> | ';
 echo '<a href="' . WEB_ROOT .'/applications/validator/">XML File Validator</a> | ';
 echo '<a href="' . WEB_ROOT .'/applications/xls2dwca/">Excel to EOL Archive Converter</a> | ';
