a506e142ac9c8950c276f3f37d2bab5aeeef4bad
eliagbayani <eli@eol.org>
2018-03-08 07:54:33 -0500

    cleaning

--- applications/xls2dwca/form_result.php
+++ applications/xls2dwca/form_result.php
@@ -1,9 +1,6 @@
 <?php
 namespace php_active_record;
 include_once(dirname(__FILE__) . "/../../config/environment.php");
-echo "<hr>";
-print_r(@$GLOBALS);
-echo "<hr>";
 
 $url = $_REQUEST['url'];
 $upload = $_FILES["file_upload"];
