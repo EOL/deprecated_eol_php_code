23528229e8f686396f635bd0cb0384bfe0c9272b
eliagbayani <eli@eol.org>
2018-03-11 22:33:45 -0400

    stats

--- update_resources/connectors/692.php
+++ update_resources/connectors/692.php
@@ -3,8 +3,9 @@ namespace php_active_record;
 /* OBIS Environmental Information
 execution time: 1.11 hours
 
-692	Saturday 2018-03-10 08:24:23 PM	{"measurement_or_fact.tab":1924574,"occurrence.tab":486564,"taxon.tab":162188} - without measurementID
-692	Sunday 2018-03-11 06:13:34 AM	{"measurement_or_fact.tab":1924574,"occurrence.tab":486564,"taxon.tab":162188} - with measurementID
+692	Saturday 2018-03-10 08:24:23 PM	{"measurement_or_fact.tab":1924574,"occurrence.tab":486564,"taxon.tab":162188} - without measurementID - MacMini
+692	Sunday 2018-03-11 06:13:34 AM	{"measurement_or_fact.tab":1924574,"occurrence.tab":486564,"taxon.tab":162188} - with measurementID - MacMini
+692	Sunday 2018-03-11 08:26:06 AM	{"measurement_or_fact.tab":1924554,"occurrence.tab":486561,"taxon.tab":162187} - eol-archive, start with measurementID
 */
 include_once(dirname(__FILE__) . "/../../config/environment.php");
 require_library('connectors/ObisDataConnector');
