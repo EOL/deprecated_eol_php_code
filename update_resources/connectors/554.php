e0e8bef24140ebb1a6d704b0418dc6dd289fc9e2
eliagbayani <eli@eol.org>
2018-03-20 00:18:08 -0400

    mtce

--- update_resources/connectors/554.php
+++ update_resources/connectors/554.php
@@ -5,6 +5,10 @@ This is a generic script that will convert EOL XML to EOL DWC-A
 */
 
 include_once(dirname(__FILE__) . "/../../config/environment.php");
+
+//used collections_generic.php instead.
+return;
+
 require_library('connectors/ConvertEOLtoDWCaAPI');
 $timestart = time_elapsed();
 
