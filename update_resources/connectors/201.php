2a29db4bbeb39f67c08d9110cf0db08b907926c2
eliagbayani <eli@eol.org>
2018-03-21 03:54:24 -0400

    mtce

--- update_resources/connectors/201.php
+++ update_resources/connectors/201.php
@@ -1,6 +1,7 @@
 <?php
 namespace php_active_record;
-/*
+/* https://eol-jira.bibalex.org/browse/DATA-1551
+
 Before partner provides a TSV file:
 estimated execution time: 14 minutes: 40k images | 50 minutes: 72k images
 
@@ -14,7 +15,7 @@ estimated execution time: 55 minutes for:
 
 201	Tuesday 2018-02-27 09:28:41 AM	{"measurement_or_fact.tab":129220,"media_resource.tab":180170,"occurrence.tab":31424,"taxon.tab":25467}
 201	Wednesday 2018-03-21 12:24:43 AM{"measurement_or_fact.tab":130476,"media_resource.tab":181033,"occurrence.tab":31733,"taxon.tab":25494} no measurementID yet
-
+201	Wednesday 2018-03-21 12:48:40 AM{"measurement_or_fact.tab":128349,"media_resource.tab":181033,"occurrence.tab":31733,"taxon.tab":25494} with measurementID and unique at that.
 */
 
 include_once(dirname(__FILE__) . "/../../config/environment.php");
