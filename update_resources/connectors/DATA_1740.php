5f65bc8bf685362eaa79f005f0ff0c27751b7411
eliagbayani <eli@eol.org>
2018-03-22 23:32:31 -0400

    DATA-1740

---
+++ update_resources/connectors/DATA_1740.php
@@ -0,0 +1,24 @@
+<?php
+namespace php_active_record;
+/* 
+This will use the image_sizes table and the dataObjects API to generate a DwCA file.
+https://eol-jira.bibalex.org/browse/DATA-1740
+estimated execution time:
+*/
+include_once(dirname(__FILE__) . "/../../config/environment.php");
+require_library('connectors/CollectionsScrapeAPI');
+$timestart = time_elapsed();
+
+$resource_id = "image_sizes";
+$collection_id = false;
+$func = new CollectionsScrapeAPI($resource_id, $collection_id);
+$func->start_image_sizes();
+Functions::finalize_dwca_resource($resource_id, false, false); //3rd param true means resource folder will be deleted
+
+$elapsed_time_sec = time_elapsed() - $timestart;
+echo "\n\n";
+echo "\n elapsed time = " . $elapsed_time_sec/60 . " minutes";
+echo "\n elapsed time = " . $elapsed_time_sec/60/60 . " hours";
+echo "\n Done processing.\n";
+
+?>
