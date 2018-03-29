8a8abc0419bdc741d13e5acabebad6642593944b
eliagbayani <eli@eol.org>
2018-03-29 06:35:19 -0400

    new BOLDS connector

---
+++ update_resources/connectors/bolds.php
@@ -0,0 +1,22 @@
+<?php
+namespace php_active_record;
+/* https://eol-jira.bibalex.org/browse/TRAM-737
+BOLDS connector for new API service
+estimated execution time:
+*/
+
+include_once(dirname(__FILE__) . "/../../config/environment.php");
+require_library('connectors/BOLDSNewAPI');
+$timestart = time_elapsed();
+$resource_id = 1;
+$func = new BOLDSNewAPI($resource_id);
+
+$func->get_all_taxa();
+// Functions::finalize_dwca_resource($resource_id);
+
+$elapsed_time_sec = time_elapsed() - $timestart;
+echo "\n\n";
+echo "\n elapsed time = " . $elapsed_time_sec/60 . " minutes";
+echo "\n elapsed time = " . $elapsed_time_sec/60/60 . " hours";
+echo "\n Done processing.\n";
+?>
