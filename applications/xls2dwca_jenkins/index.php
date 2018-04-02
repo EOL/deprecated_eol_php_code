1f6f01fe09450bc7380aac18e1b8a1c818f82a36
eliagbayani <eli@eol.org>
2018-04-02 10:55:13 -0400

    mtce

--- applications/xls2dwca_jenkins/index.php
+++ applications/xls2dwca_jenkins/index.php
@@ -1,14 +1,24 @@
 <form action="form_result.php" method="post" enctype="multipart/form-data">
-    <table border="1" cellpadding="15" cellspacing="1" align="center">
+    <table border="1" cellpadding="15" cellspacing="1" align="center" width="40%">
         <tr align="center">
             <td>Excel to EOL Archive Converter</td>
         </tr>
         <tr align="center">
+            <!---
             <td>
-                <font size="2">XLS File URL: </font><input type="text" name="url" size="100">
+                <font size="3">XLS File URL: </font><input type="text" name="url" size="100">
                 <br>
-                <font size="2">
+                <font size="3">
                     <i>e.g. http://mydomain.org/my_spreadsheet.xlsx</i><br>
+                    <a href="https://github.com/eliagbayani/EOL-connector-data-files/raw/master/schema/eol_import_spreadsheet.xlsx">Download</a> the EOL spreadsheet template here.
+                    <br>
+                </font>
+            </td>
+            --->
+            <td>
+                <input type="hidden" name="url" size="100">
+                <font size="3">
+                    <i>Sample spreadsheet template</i><br>
                     <a href="https://github.com/eliagbayani/EOL-connector-data-files/raw/master/schema/eol_import_spreadsheet.xlsx">Download</a> the EOL spreadsheet template here.
                     <br>
                 </font>
@@ -16,7 +26,10 @@
         </tr>
         <tr>
             <td>
-                <font size="2">XLS File Upload: </font><input type="file" name="file_upload" id="file_upload" size="100">
+                <font size="3">XLS File Upload: </font><input type="file" name="file_upload" id="file_upload" size="100">
+                <small><br><br>(.xlsx, .xls) OR<br> Recommended (.xls.zip, .xlsx.zip)
+                <br><br>
+                *It is recommended that you zip your file before uploading.</small>                
             </td>
         </tr>
         <tr align="center">
