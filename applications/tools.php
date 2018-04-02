2db3fe4081e56b894ed0e4885ba6c3f503dbc740
eliagbayani <eli@eol.org>
2018-04-02 04:32:37 -0400

    mtce

--- applications/tools.php
+++ applications/tools.php
@@ -4,7 +4,11 @@ include_once(dirname(__FILE__) . "/../config/environment.php");
 $GLOBALS['ENV_DEBUG'] = false;
 echo '<a href="' . WEB_ROOT .'/applications/dwc_validator/">Archive and Spreadsheet Validator</a> | ';
 echo '<a href="' . WEB_ROOT .'/applications/validator/">XML File Validator</a> | ';
-echo '<a href="' . WEB_ROOT .'/applications/xls2dwca/">Excel to EOL Archive Converter</a> | ';
+
+
+if($GLOBALS['ENV_NAME'] == 'development') echo '<a href="' . WEB_ROOT .'/applications/xls2dwca/">Excel to EOL Archive Converter</a> | ';
+echo '<a href="' . WEB_ROOT .'/applications/xls2dwca_jenkins/">Excel to EOL Archive Converter (Jenkins)</a> | ';
+
 
 if($GLOBALS['ENV_NAME'] == 'development') echo '<a href="' . WEB_ROOT .'/applications/genHigherClass/">Generate highClassification Tool</a> | ';
 
