30d654ef84635be0c8bdb2b45a86d5ad0aa869f4
eliagbayani <eli@eol.org>
2018-04-05 02:07:48 -0400

    mtce

--- update_resources/connectors/31.php
+++ update_resources/connectors/31.php
@@ -3,21 +3,8 @@ namespace php_active_record;
 include_once(dirname(__FILE__) . "/../../config/environment.php");
 require_library('connectors/BiopixAPI');
 
-
 $biopix_connector = new BiopixAPI;
 $biopix_connector->get_all_taxa();
 
-if(filesize(CONTENT_RESOURCE_LOCAL_PATH . "31_working/taxon.tab") > 1000 &&
-    filesize(CONTENT_RESOURCE_LOCAL_PATH . "31_working/media_resource.tab") > 1000)
-{
-    if(is_dir(CONTENT_RESOURCE_LOCAL_PATH . "31"))
-    {
-        recursive_rmdir(CONTENT_RESOURCE_LOCAL_PATH . "31_previous");
-        Functions::file_rename(CONTENT_RESOURCE_LOCAL_PATH . "31", CONTENT_RESOURCE_LOCAL_PATH . "31_previous");
-    }
-    Functions::file_rename(CONTENT_RESOURCE_LOCAL_PATH . "31_working", CONTENT_RESOURCE_LOCAL_PATH . "31");
-    $GLOBALS['db_connection']->update("UPDATE resources SET resource_status_id=".ResourceStatus::find_or_create_by_translated_label('Harvest Requested')->id." WHERE id=31");
-}
-
-
+Functions::finalize_dwca_resource(31, false, true); //2nd param NOT a big file | 3rd param YES will delete working folders /31/
 ?>
