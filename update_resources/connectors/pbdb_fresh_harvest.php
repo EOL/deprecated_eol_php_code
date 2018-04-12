9555bf59ec7900b1a2ff4bc8eac4e64107d5c6ff
eliagbayani <eli@eol.org>
2018-04-12 06:07:26 -0400

    mtce

--- update_resources/connectors/pbdb_fresh_harvest.php
+++ update_resources/connectors/pbdb_fresh_harvest.php
@@ -6,6 +6,7 @@ estimated execution time:
 
 368	Wednesday 2018-04-11 09:41:49 AM	{"measurement_or_fact.tab":2450420,"occurrence.tab":528288,"taxon.tab":359968,"vernacular_name.tab":4273} - local
 368	Wednesday 2018-04-11 11:17:47 AM	{"measurement_or_fact.tab":2451203,"occurrence.tab":528502,"taxon.tab":360156,"vernacular_name.tab":4366} - eol-archive (without 'ignore' and multiple values yet)
+368	Wednesday 2018-04-11 01:05:31 PM	{"measurement_or_fact.tab":2698963,"occurrence.tab":553064,"taxon.tab":360156,"vernacular_name.tab":4366} - eol-archive (with ignore and multiple values)
 
 */
 
