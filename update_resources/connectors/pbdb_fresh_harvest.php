4239f59b244d41e90d85447cee69285402a10811
eliagbayani <eli@eol.org>
2018-04-12 11:14:20 -0400

    stats

--- update_resources/connectors/pbdb_fresh_harvest.php
+++ update_resources/connectors/pbdb_fresh_harvest.php
@@ -5,8 +5,11 @@ we use their data service to assemble their data and ingest it as structured dat
 estimated execution time: 
 
 368	Wednesday 2018-04-11 09:41:49 AM	{"measurement_or_fact.tab":2450420,"occurrence.tab":528288,"taxon.tab":359968,"vernacular_name.tab":4273} - local
+368	Thursday 2018-04-12 06:34:45 AM	    {"measurement_or_fact.tab":2696122,"occurrence.tab":552850,"taxon.tab":359968,"vernacular_name.tab":4273}
+
 368	Wednesday 2018-04-11 11:17:47 AM	{"measurement_or_fact.tab":2451203,"occurrence.tab":528502,"taxon.tab":360156,"vernacular_name.tab":4366} - eol-archive (without 'ignore' and multiple values yet)
 368	Wednesday 2018-04-11 01:05:31 PM	{"measurement_or_fact.tab":2698963,"occurrence.tab":553064,"taxon.tab":360156,"vernacular_name.tab":4366} - eol-archive (with ignore and multiple values)
+368	Thursday 2018-04-12 06:24:10 AM	    {"measurement_or_fact.tab":2697018,"occurrence.tab":553064,"taxon.tab":360156,"vernacular_name.tab":4366}
 
 */
 
