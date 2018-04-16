239cfa1feecd85cd1a8ee3d26a78137fc0044f7f
eliagbayani <eli@eol.org>
2018-04-16 02:19:37 -0400

    Bioimages_Vanderbilt

---
+++ update_resources/connectors/200.php
@@ -0,0 +1,26 @@
+<?php
+namespace php_active_record;
+/* http://www.eol.org/content_partners/285/resources/200 - DATA-
+This is a generic script that will convert EOL XML to EOL DWC-A
+*/
+
+include_once(dirname(__FILE__) . "/../../config/environment.php");
+require_library('connectors/ConvertEOLtoDWCaAPI');
+$timestart = time_elapsed();
+
+$resource_id = 200;
+$params["eol_xml_file"] = "http://localhost/cp/Bioimages_Vanderbilt/eol-harvest.xml";
+// $params["eol_xml_file"] = "http://bioimages.vanderbilt.edu/eol-harvest.xml";
+$params["filename"]     = "no need to mention here.xml";
+$params["dataset"]      = "";
+$params["resource_id"]  = $resource_id;
+
+$func = new ConvertEOLtoDWCaAPI($resource_id);
+$func->export_xml_to_archive($params, true, 60*60*24*5); // true => means it is an XML file, not an archive file nor a zip file. Expires in 5 days.
+Functions::finalize_dwca_resource($resource_id, false, true);
+$elapsed_time_sec = time_elapsed() - $timestart;
+echo "\n\n";
+echo "elapsed time = " . $elapsed_time_sec/60 . " minutes \n";
+echo "elapsed time = " . $elapsed_time_sec/60/60 . " hours \n";
+echo "\nDone processing.\n";
+?>
