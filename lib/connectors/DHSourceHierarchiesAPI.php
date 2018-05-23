13acf3056985ccb2caa235639820c44b017f244b
eliagbayani <eli@eol.org>
2018-05-23 10:20:39 -0400

    mtce

--- lib/connectors/DHSourceHierarchiesAPI.php
+++ lib/connectors/DHSourceHierarchiesAPI.php
@@ -43,9 +43,10 @@ class DHSourceHierarchiesAPI
         print_r(json_decode($json, true));
         exit;
         */
-        
+        /*
         self::save_2local_gnparsed_file($what);
         exit;
+        */
         
         $meta_xml_path = $this->sh[$what]['source']."meta.xml";
         $meta = self::analyze_meta_xml($meta_xml_path);
@@ -56,13 +57,15 @@ class DHSourceHierarchiesAPI
         $with_authorship = false;
         if(self::need_2run_gnparser_YN($meta)) {
             $with_authorship = true;
-            self::run_file_with_gnparser($what);
+            self::run_file_with_gnparser($meta);
             self::save_2local_gnparsed_file($what);
         }
         self::process_taxon_file($meta, $with_authorship);
+        self::parent_id_check($what);
     }
     private function need_2run_gnparser_YN($meta)
     {
+        $what = $meta['what']; $i = 0;
         foreach(new FileIterator($this->sh[$what]['source'].$meta['taxon_file']) as $line => $row) {
             $i++;
             if($meta['ignoreHeaderLines'] && $i == 1) continue;
@@ -75,7 +78,7 @@ class DHSourceHierarchiesAPI
                 $k++;
             }
             // print_r($rec); //exit; //use to test if field - value is OK
-            if(self::gnsparse_canonical($rec['scientificName']) != $rec['scientificName']) return true;
+            if(self::gnsparse_canonical($rec['scientificName'], "api") != $rec['scientificName']) return true;
         }
         return false;
     }
@@ -86,7 +89,7 @@ class DHSourceHierarchiesAPI
         $fn_syn = fopen($this->sh[$what]['source']."synonym.tsv", "w"); //will overwrite existing
         fwrite($fn_tax, implode("\t|\t", $this->taxonomy_header)."\t|\t"."\n");
         fwrite($fn_syn, implode("\t|\t", $this->synonym_header)."\t|\t"."\n");
-        $i = 0; $run_gnparser = false;
+        $i = 0;
         $m = 3765285/10; //for CoL
         foreach(new FileIterator($this->sh[$what]['source'].$meta['taxon_file']) as $line => $row) {
             $i++;
@@ -133,7 +136,7 @@ class DHSourceHierarchiesAPI
                 */
                 $t = array();
                 $t['parent_id']     = $rec['parentNameUsageID'];    //row[4]
-                if($with_authorship) $t['name'] = self::get_json_with_cache($rec['scientificName']); //row[8]
+                if($with_authorship) $t['name'] = self::gnsparse_canonical($rec['scientificName'], 'cache'); //row[8]
                 else                 $t['name'] = $rec['scientificName'];
                 $t['taxon_id']      = $rec['taxonID'];              //row[9]
                 $t['accepted_id']   = $rec['acceptedNameUsageID'];  //row[7]
@@ -168,7 +171,7 @@ class DHSourceHierarchiesAPI
                 */
                 $t = array();
                 $t['parent_id'] = $rec['parentNameUsageID'];
-                if($with_authorship) $t['name'] = self::get_json_with_cache($rec['scientificName']); //row[8]
+                if($with_authorship) $t['name'] = self::gnsparse_canonical($rec['scientificName'], 'cache'); //row[8]
                 else                 $t['name'] = $rec['scientificName'];
                 $t['taxon_id']  = $rec['taxonID'];
                 $t['rank']      = ($val = @$rec['taxonRank']) ? $val: "no rank";
@@ -187,7 +190,7 @@ class DHSourceHierarchiesAPI
                 // if($i >=  $m*3 && $i < $m*4) $cont = true;
                 // if($i >=  $m*4 && $i < $m*5) $cont = true;
                 // if($i >=  $m*5 && $i < $m*6) $cont = true;
-                if($i >=  $m*6 && $i < $m*7) $cont = true;
+                // if($i >=  $m*6 && $i < $m*7) $cont = true;
                 // if($i >=  $m*7 && $i < $m*8) $cont = true;
                 // if($i >=  $m*8 && $i < $m*9) $cont = true;
                 // if($i >=  $m*9 && $i < $m*10) $cont = true;
@@ -198,31 +201,21 @@ class DHSourceHierarchiesAPI
             //--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
             //--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
             //--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-            /* no longer used
-            if(!$run_gnparser) {
-                if(self::gnsparse_canonical($t['name']) != $t['name']) $run_gnparser = true;
-            }
-            */
         }
         fclose($fn_tax);
         fclose($fn_syn);
         if(!$has_synonym) unlink($this->sh[$what]['source']."synonym.tsv");
-
-        /* no longer used
-        if($run_gnparser) {
-            self::run_file_with_gnparser($what);
-            self::save_2local_gnparsed_file($what);
-        }
-        else              echo "\nNo need to run gnparser()\n";
-        */
-        
-        self::parent_id_check($what);
     }
     private function parent_id_check($what)
     {
     }
-    private function run_file_with_gnparser($what) //working OK but not used
+    private function run_file_with_gnparser($meta) //working OK but not used
     {
+        $what = $meta['what']; $i = 0;
+        if(file_exists($this->sh[$what]['source'].'name_only_gnparsed_DONE.txt')) {
+            echo "\nAll names for [$what] has already been cached.\n";
+            return;
+        }
         echo "\nRunning gnparser...\n";
         $WRITE = fopen($this->sh[$what]['source']."name_only.txt", "w"); //will overwrite existing
         foreach(new FileIterator($this->sh[$what]['source'].$meta['taxon_file']) as $line => $row) {
@@ -247,6 +240,10 @@ class DHSourceHierarchiesAPI
     }
     private function save_2local_gnparsed_file($what)
     {
+        if(file_exists($this->sh[$what]['source'].'name_only_gnparsed_DONE.txt')) {
+            echo "\nAll names for [$what] has already been cached.\n";
+            return;
+        }
         $i = 0;
         foreach(new FileIterator($this->sh[$what]['source'].'name_only_gnparsed.txt') as $line => $json) {
             $i++; if($i == 1) continue;
@@ -254,8 +251,7 @@ class DHSourceHierarchiesAPI
             $arr = json_decode($json, true);
             // print_r($arr); exit;
             $name = $arr['verbatim'];
-            echo "\n$i. $name";
-            
+            if(($i % 1000) == 0) echo "\n".number_format($i)." $name - ";
             //now check if json already cached. Ignore if it does and save/cache it if it doesn't
             $options['cache_path'] = $this->smasher_download_options['cache_path'];
             $md5 = md5($name);
@@ -265,32 +261,17 @@ class DHSourceHierarchiesAPI
             if(!file_exists($options['cache_path'] . "$cache1/$cache2")) mkdir($options['cache_path'] . "$cache1/$cache2");
             $cache_path = $options['cache_path'] . "$cache1/$cache2/$md5.json";
             if(!file_exists($cache_path)) {
-                echo " - saving...";
+                if(($i % 1000) == 0) echo " - saving...";
                 if($FILE = Functions::file_open($cache_path, 'w')) {
                     fwrite($FILE, $json);
                     fclose($FILE);
                 }
             }
-            else echo " - already saved/cached";
+            else if(($i % 1000) == 0) echo " - already saved/cached";
         }
+        Functions::file_rename($this->sh[$what]['source'].'name_only_gnparsed.txt', $this->sh[$what]['source'].'name_only_gnparsed_DONE.txt');
     }
-    /*
-    private function replace_sciname_with_gnparser_canonical($what)
-    {
-        Functions::file_rename($this->sh[$what]['source'].'taxonomy.tsv', $this->sh[$what]['source'].'taxonomy_tmp.tsv');
-        $WRITE = fopen($this->sh[$what]['source']."taxonomy.tsv", "w"); $i = 0;
-        foreach(new FileIterator($this->sh[$what]['source'].'taxonomy_tmp.tsv') as $line => $row) {
-            $i++; if($i == 1) continue;
-            $tmp = explode("\t|\t", $row);
-            // print_r($tmp); exit;
-            if($name = @$tmp[2]) {
-                $json = self::get_json_with_cache($name);
-                echo "\n$i. [$json]\n";
-            }
-        }
-    }
-    */
-    private function get_json_with_cache($name, $options = array()) //json generated by gnparser
+    private function get_json_from_cache($name, $options = array()) //json generated by gnparser
     {
         // download_wait_time
         if(!isset($options['expire_seconds'])) $options['expire_seconds'] = false;
@@ -302,7 +283,7 @@ class DHSourceHierarchiesAPI
         if(!file_exists($options['cache_path'] . "$cache1/$cache2")) mkdir($options['cache_path'] . "$cache1/$cache2");
         $cache_path = $options['cache_path'] . "$cache1/$cache2/$md5.json";
         if(file_exists($cache_path)) {
-            echo "\nRetrieving cache ($name)...\n";
+            // echo "\nRetrieving cache ($name)...\n"; //good debug
             $file_contents = file_get_contents($cache_path);
             $cache_is_valid = true;
             if(($file_contents && $cache_is_valid) || (strval($file_contents) == "0" && $cache_is_valid)) {
@@ -328,10 +309,12 @@ class DHSourceHierarchiesAPI
         if($ext == "syn")     fwrite($fn, $t['accepted_id'] . "\t|\t" . $t['name'] . "\t|\t" . 'synonym' . "\t|\t" . "\t|\t" . "\n");
         elseif($ext == "tax") fwrite($fn, $t['taxon_id'] . "\t|\t" . $t['parent_id'] . "\t|\t" . $t['name'] . "\t|\t" . $t['rank'] . "\t|\t" . $t['source'] . "\t|\t" . "\n");
     }
-    private function gnsparse_canonical($sciname)
+    private function gnsparse_canonical($sciname, $method)
     {
-        $json = Functions::lookup_with_cache($this->gnparser.urlencode($sciname), $this->smasher_download_options);
+        if($method == "api")       $json = Functions::lookup_with_cache($this->gnparser.urlencode($sciname), $this->smasher_download_options);
+        elseif($method == "cache") $json = self::get_json_from_cache($sciname);
         if($obj = json_decode($json)) return @$obj->namesJson[0]->canonical_name->value;
+        exit("\nInvestigate cannot identify name [$sciname][$method]\n");
     }
     private function analyze_eol_meta_xml($meta_xml_path)
     {
