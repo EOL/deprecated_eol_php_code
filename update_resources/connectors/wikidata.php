0e9712570feb591bfb6a544c261103241a2c4645
eliagbayani <eli@eol.org>
2018-06-07 00:59:12 -0400

    Wikimedia stats

--- update_resources/connectors/wikidata.php
+++ update_resources/connectors/wikidata.php
@@ -5,8 +5,9 @@ commons Sunday 2017-11-26 01:34:01 PM   {"agent.tab":19055,"media_resource.tab":
 71  Saturday 2017-12-02 04:39:38 AM {"agent.tab":19252,"media_resource.tab":912111,"taxon.tab":89748}
 71  Tuesday 2017-12-19 02:05:02 AM  {"agent.tab":19813,"media_resource.tab":932989,"taxon.tab":89753,"vernacular_name.tab":291572}
 71  Wednesday 2018-01-03 08:40:51 PM{"agent.tab":19369,"media_resource.tab":935068,"taxon.tab":89806,"vernacular_name.tab":291603}
-71  Saturday 2018-04-21 06:58:29 AM	{"agent.tab":20658,"media_resource.tab":946750,"taxon.tab":89694,"vernacular_name.tab":291271}
+71  Saturday 2018-04-21 06:58:29 AM {"agent.tab":20658,"media_resource.tab":946750,"taxon.tab":89694,"vernacular_name.tab":291271}
 71	Wednesday 2018-05-02 04:59:07 AM{"agent.tab":20658,"media_resource.tab":946750,"taxon.tab":89694,"vernacular_name.tab":291271}
+71	Wednesday 2018-06-06 06:59:52 AM{"agent.tab":21060,"media_resource.tab":943420,"taxon.tab":89739,"vernacular_name.tab":291235}
 */
 
 include_once(dirname(__FILE__) . "/../../config/environment.php");
