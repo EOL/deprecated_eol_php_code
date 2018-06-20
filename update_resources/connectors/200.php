205dca92e810a5d93c99ae98713fc8ff58d97ee6
eliagbayani <eli@eol.org>
2018-06-20 09:59:14 -0400

    BioImages Vanderbilt 200

--- update_resources/connectors/200.php
+++ update_resources/connectors/200.php
@@ -9,14 +9,14 @@ require_library('connectors/ConvertEOLtoDWCaAPI');
 $timestart = time_elapsed();
 
 $resource_id = 200;
-$params["eol_xml_file"] = "http://localhost/cp/Bioimages_Vanderbilt/eol-harvest.xml";
-// $params["eol_xml_file"] = "http://bioimages.vanderbilt.edu/eol-harvest.xml";
+// $params["eol_xml_file"] = "http://localhost/cp/Bioimages_Vanderbilt/eol-harvest.xml";
+$params["eol_xml_file"] = "http://bioimages.vanderbilt.edu/eol-harvest.xml";
 $params["filename"]     = "no need to mention here.xml";
 $params["dataset"]      = "";
 $params["resource_id"]  = $resource_id;
 
 $func = new ConvertEOLtoDWCaAPI($resource_id);
-$func->export_xml_to_archive($params, true, 60*60*24*5); // true => means it is an XML file, not an archive file nor a zip file. Expires in 5 days.
+$func->export_xml_to_archive($params, true, 60*60*24*25); // true => means it is an XML file, not an archive file nor a zip file. Expires in 25 days.
 Functions::finalize_dwca_resource($resource_id, false, true);
 $elapsed_time_sec = time_elapsed() - $timestart;
 echo "\n\n";
