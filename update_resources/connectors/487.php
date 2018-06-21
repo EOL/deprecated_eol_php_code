b0c5c2f63c822a248b93f65fc29cd57279f75704
eliagbayani <eli@eol.org>
2018-06-21 02:14:15 -0400

    mtce

--- update_resources/connectors/487.php
+++ update_resources/connectors/487.php
@@ -1,6 +1,9 @@
 <?php
 namespace php_active_record;
-/* estimated execution time:   */
+/* estimated execution time:
+487	Thursday 2018-06-21 02:07:15 AM	{"media_resource.tab":257,"reference.tab":69,"taxon.tab":162,"vernacular_name.tab":119} -> eol-archive
+*Un-published resource
+*/
 
 include_once(dirname(__FILE__) . "/../../config/environment.php");
 require_library('connectors/SciELOAPI');
@@ -11,9 +14,7 @@ $resource_id = "487";
 $scielo_connector = new SciELOAPI($resource_id);
 $scielo_connector->get_all_taxa();
 
-Functions::finalize_dwca_resource($resource_id);
-
-
+Functions::finalize_dwca_resource($resource_id, false, true);
 
 $elapsed_time_sec = time_elapsed() - $timestart;
 echo "\n\n elapsed time = " . $elapsed_time_sec/60 . " minutes";
