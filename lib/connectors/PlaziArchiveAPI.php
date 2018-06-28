35891dd3b7eeb5aa2d85d045087033c170818e6c
eliagbayani <eli@eol.org>
2018-06-28 10:16:39 -0400

    mtce

--- lib/connectors/PlaziArchiveAPI.php
+++ lib/connectors/PlaziArchiveAPI.php
@@ -8,21 +8,15 @@ class PlaziArchiveAPI
     {
         require_library('connectors/INBioAPI');
         $func = new INBioAPI();
-        if($paths = $func->extract_archive_file($dwca_file, "meta.xml"))
-        {
+        if($paths = $func->extract_archive_file($dwca_file, "meta.xml")) {
             print_r($paths);
-            if($contents = Functions::get_remote_file($paths['archive_path'] . "media.txt", array('timeout' => 172800)))
-            {
+            if($contents = Functions::get_remote_file($paths['archive_path'] . "media.txt", array('timeout' => 172800))) {
                 $contents = str_ireplace('<a title=""', '<a title="', $contents);
                 $contents = str_ireplace('"" href=""', '" href="', $contents);
                 $contents = str_ireplace('"">', '">', $contents);
                 
                 //saving new media.txt
-                if(!($WRITE = fopen($paths['archive_path'] . "media.txt", "w")))
-                {
-                  debug(__CLASS__ .":". __LINE__ .": Couldn't open file: " .$paths['archive_path'] . "media.txt");
-                  return;
-                }
+                if(!($WRITE = Functions::file_open($paths['archive_path'] . "media.txt", "w"))) return;
                 fwrite($WRITE, $contents);
                 fclose($WRITE);
                 
@@ -40,8 +34,7 @@ class PlaziArchiveAPI
                 $src = $paths['archive_path'];
                 $dst = CONTENT_RESOURCE_LOCAL_PATH . $resource_id . "/";
                 $files = glob($paths['archive_path'] . "*.*");
-                foreach($files as $file)
-                {
+                foreach($files as $file) {
                     $file_to_go = str_replace($src, $dst, $file);
                     copy($file, $file_to_go);
                 }
