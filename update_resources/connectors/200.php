8d357042f0ed1cd271e6e2e3aed11e803a13f16c
eliagbayani <eli@eol.org>
2018-07-09 06:05:33 -0400

    stats

--- update_resources/connectors/200.php
+++ update_resources/connectors/200.php
@@ -2,6 +2,9 @@
 namespace php_active_record;
 /* http://www.eol.org/content_partners/285/resources/200 - DATA-
 This is a generic script that will convert EOL XML to EOL DWC-A
+
+200	Monday 2018-07-09 05:49:39 AM	{"agent.tab":35,"media_resource.tab":13774,"reference.tab":1,"taxon.tab":1158} - eol-archive
+
 */
 
 include_once(dirname(__FILE__) . "/../../config/environment.php");
