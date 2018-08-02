f17d1231847e4f5def57879bd8c59bf5b89dd407
eliagbayani <eli@eol.org>
2018-08-02 11:37:25 -0400

    stats

--- update_resources/connectors/509.php
+++ update_resources/connectors/509.php
@@ -2,6 +2,8 @@
 namespace php_active_record;
 /* execution time: 21 minutes 
 http://www.eol.org/content_partners/557/resources
+
+509	Thursday 2018-08-02 11:16:03 AM	{"media_resource.tab":2559,"reference.tab":9003,"taxon.tab":128,"vernacular_name.tab":128}  eol-archive
 */
 include_once(dirname(__FILE__) . "/../../config/environment.php");
 require_library('connectors/USDAfsfeisAPI');
