f995a96c4ac2e9688ab51fc605eea2c9864c140c
eliagbayani <eli@eol.org>
2018-08-02 18:51:07 -0400

    stats

--- update_resources/connectors/505.php
+++ update_resources/connectors/505.php
@@ -7,7 +7,7 @@ http://www.eol.org/content_partners/557/resources
 505	Saturday 2018-04-14 11:54:15 PM	{"media_resource.tab":26654,"reference.tab":78635,"taxon.tab":1015}
 505	Sunday 2018-04-15 03:05:41 AM	{"media_resource.tab":26654,"reference.tab":78635,"taxon.tab":1015} - MacMini
 505	Monday 2018-07-02 12:30:14 PM	{"media_resource.tab":26654,"reference.tab":78635,"taxon.tab":1015}
-
+505	Thursday 2018-08-02 11:41:50 AM	{"media_resource.tab":23726,"reference.tab":69514,"taxon.tab":955,"vernacular_name.tab":955}    - eol-archive
 */
 
 include_once(dirname(__FILE__) . "/../../config/environment.php");
