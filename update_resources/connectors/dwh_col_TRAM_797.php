2fe4995956a7d2a6eb6ccdac7dc42f3f4faa96d9
eliagbayani <eli@eol.org>
2018-08-13 10:20:52 -0400

    mtce

--- update_resources/connectors/dwh_col_TRAM_797.php
+++ update_resources/connectors/dwh_col_TRAM_797.php
@@ -5,15 +5,16 @@ estimated execution time:
 */
 include_once(dirname(__FILE__) . "/../../config/environment.php");
 require_library('connectors/DWH_COL_API');
-// ini_set('memory_limit','5096M');
-// $GLOBALS['ENV_DEBUG'] = true;
+// ini_set('memory_limit','15096M');
+ini_set('memory_limit','6096M');
 
+// $GLOBALS['ENV_DEBUG'] = true;
 $timestart = time_elapsed();
 $resource_id = "Catalogue_of_Life_DH"; //orig
 $func = new DWH_COL_API($resource_id);
-
 // /* un-comment in normal operation
 $func->start_tram_797();
+$func = null;
 Functions::finalize_dwca_resource($resource_id);
 // */
 
@@ -29,9 +30,6 @@ $undefined = $func->check_if_all_parents_have_entries($resource_id, true, false,
 if($undefined) echo "\nERROR: There is undefined acceptedNameUsageID(s): ".count($undefined)."\n";
 else           echo "\nOK: All acceptedNameUsageID have entries.\n";
 
-$undefined = $func->check_if_all_vernaculars_have_entries($resource_id, true); //true means output will write to text file
-if($undefined) echo "\nERROR: There is undefined taxonID(s) in vernacular_name.tab: ".count($undefined)."\n";
-else           echo "\nOK: All taxonID(s) in vernacular_name.tab have entries.\n";
 // */
 
 /* this will delete the working dir --- never used here...
