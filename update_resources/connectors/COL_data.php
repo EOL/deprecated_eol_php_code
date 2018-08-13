eb07b09cb9d13733b38e6bbd9376254b3d587a7f
eliagbayani <eli@eol.org>
2018-08-13 11:34:58 -0400

    CoL DATA-1744 run again

--- update_resources/connectors/COL_data.php
+++ update_resources/connectors/COL_data.php
@@ -2,8 +2,9 @@
 namespace php_active_record;
 /* https://eol-jira.bibalex.org/browse/DATA-1744
 Connector for Catalogue of Life hierarchy, data, descriptions
-estimated execution time:
+estimated execution time: 5 days 16 hr (eol-archive)
 
+log in eol-archive:
 col	Thursday 2018-05-10 09:34:05 AM	{"measurement_or_fact.tab":3961387,"media_resource.tab":1150989,"occurrence.tab":2982057,"reference.tab":579322,"taxon.tab":3765285,"vernacular_name.tab":429242}
 
 */
@@ -16,24 +17,24 @@ print_r($uris); exit;
 */
 
 // $GLOBALS['ENV_DEBUG'] = false;
-ini_set('memory_limit','15096M');
+ini_set('memory_limit','15096M'); //15096M
 $timestart = time_elapsed();
 
-$resource_id = 'col';
+$resource_id = 'col_v2';
 require_library('connectors/COLDataAPI');
 $func = new COLDataAPI($resource_id);
 $func->convert_archive();
 unset($func);
-Functions::finalize_dwca_resource($resource_id, true, true); //2nd param true means a big big file
+Functions::finalize_dwca_resource($resource_id, true, false); //2nd param true means a big big file
 
-/*
+// /*
 $func = new DWCADiagnoseAPI();
 if($undefined = $func->check_if_all_parents_have_entries($resource_id, true)) { //2nd param True means write to text file
-    $arr['parents without entries'] = $undefined;
+    $arr['parents without entries total count'] = count($undefined);
     print_r($arr);
 }
 else echo "\nAll parents have entries OK\n";
-*/
+// */
 
 $elapsed_time_sec = time_elapsed() - $timestart;
 echo "\n\n";
