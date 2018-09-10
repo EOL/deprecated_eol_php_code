b68b1017365011e75fb4ae444065261968405494
eliagbayani <eli@eol.org>
2018-09-10 03:14:51 -0400

    mtce

--- update_resources/connectors/SDR.php
+++ update_resources/connectors/SDR.php
@@ -5,6 +5,16 @@ namespace php_active_record;
 include_once(dirname(__FILE__) . "/../../config/environment.php");
 require_library('connectors/SummaryDataResourcesAPI');
 
+/*
+$str = "http://purl.obolibrary.org/obo/ENVO_01000206, http://purl.obolibrary.org/obo/ENVO_00000463, http://purl.obolibrary.org/obo/ENVO_00002009, http://purl.obolibrary.org/obo/ENVO_00000446, 
+        http://purl.obolibrary.org/obo/ENVO_00000144, http://purl.obolibrary.org/obo/ENVO_00002033, http://purl.obolibrary.org/obo/ENVO_01000204, http://purl.obolibrary.org/obo/ENVO_00000856, 
+        http://purl.obolibrary.org/obo/ENVO_00002030, http://purl.obolibrary.org/obo/ENVO_01001305";
+$arr = explode(",", $str);
+$arr = array_map('trim', $arr);
+asort($arr);
+print_r($arr); exit;
+*/
+
 /* //tests
 $parents = array(1,2,3);
 $preferred_terms = array(4,5);
