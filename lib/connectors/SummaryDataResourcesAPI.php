e909bff98c3751777b9a35ccdfdc56c60eaf9f61
eliagbayani <eli@eol.org>
2018-09-26 22:57:36 -0400

    mtce

--- lib/connectors/SummaryDataResourcesAPI.php
+++ lib/connectors/SummaryDataResourcesAPI.php
@@ -138,8 +138,8 @@ class SummaryDataResourcesAPI
         // $input[] = array('page_id' => 46559197, 'predicate' => "http://eol.org/schema/terms/Present");
         // $input[] = array('page_id' => 46559217, 'predicate' => "http://eol.org/schema/terms/Present");
         
-        $input[] = array('page_id' => 7662, 'predicate' => "http://eol.org/schema/terms/Habitat"); //first test case
-        // $input[] = array('page_id' => 328607, 'predicate' => "http://eol.org/schema/terms/Habitat");
+        // $input[] = array('page_id' => 7662, 'predicate' => "http://eol.org/schema/terms/Habitat"); //first test case
+        $input[] = array('page_id' => 328607, 'predicate' => "http://eol.org/schema/terms/Habitat");
         // $input[] = array('page_id' => 328682, 'predicate' => "http://eol.org/schema/terms/Habitat");
         // $input[] = array('page_id' => 328609, 'predicate' => "http://eol.org/schema/terms/Habitat");
         // $input[] = array('page_id' => 328598, 'predicate' => "http://eol.org/schema/terms/Habitat");
@@ -1370,11 +1370,12 @@ class SummaryDataResourcesAPI
         $delete_list_1 = array('http://purl.obolibrary.org/obo/ENVO_00000094', 'http://purl.obolibrary.org/obo/ENVO_01000155', 'http://purl.obolibrary.org/obo/ENVO_00000002', 'http://purl.obolibrary.org/obo/ENVO_00000077');
         echo "\nList: "; print_r($delete_list_1);
         if($roots_inside_the_list = self::get_roots_inside_the_list($roots, $delete_list_1)) {
+            exit("\ntest sample here...\n");
             echo "\nThere are root(s) in the 1st list: ".count($roots_inside_the_list)." "; print_r($roots_inside_the_list);
             echo "\norig 'shared values ancestry tree': ".count($ISVAT)."\n";
             foreach($ISVAT as $a) {
-                if(              in_array($a[0], $delete_list_1)) {}
-                elseif(!$a[0] && in_array($a[1], $delete_list_1)) {}
+                if(              in_array($a[0], $roots_inside_the_list)) {}
+                elseif(!$a[0] && in_array($a[1], $roots_inside_the_list)) {}
                 else $new_isvat[] = $a;
             }
             echo "\ntrimmed shared ancestry tree: ".count($new_isvat); foreach($new_isvat as $a) echo "\n".$a[0]."\t".$a[1];
