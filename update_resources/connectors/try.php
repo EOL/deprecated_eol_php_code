acdd4f69546d25c43dee68865fc2b49516a8b166
eliagbayani <eli@eol.org>
2018-10-03 08:27:20 -0400

    stats

--- update_resources/connectors/try.php
+++ update_resources/connectors/try.php
@@ -1,6 +1,10 @@
 <?php
 namespace php_active_record;
-/* This can be a generic connector for CSV DwCA resources - without meta.xml. (Another similar resource is 430.php) */
+/* This can be a generic connector for CSV DwCA resources - without meta.xml. (Another similar resource is 430.php) 
+
+from eol-archive:
+try_dbase	Wednesday 2018-10-03 05:22:45 AM	{"measurement_or_fact_specific.tab":548631,"occurrence.tab":164442,"reference.tab":182,"taxon.tab":103373}
+*/
 
 include_once(dirname(__FILE__) . "/../../config/environment.php");
 require_library('connectors/TryDatabaseAPI');
