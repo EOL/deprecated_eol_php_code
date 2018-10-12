4aa2b8a7cefecc0874be716af741d5144fa8de06
eliagbayani <eli@eol.org>
2018-10-12 12:20:26 -0400

    mtce

--- update_resources/connectors/SDR.php
+++ update_resources/connectors/SDR.php
@@ -77,8 +77,10 @@ $func = new SummaryDataResourcesAPI($resource_id);
 // $func->generate_page_id_txt_files();        return; //important initial step
 // $func->generate_children_of_taxa_usingDH(); return; //the big long program
 
-$func->test_basal_values_parent();       return;
-// $func->print_parent_basal_values();      return;
+// $func->test_basal_values_parent();       return;
+$func->print_basal_values();      return;
+
+$func->print_parent_basal_values();      return;
 // $func->print_lifeStage_statMeth();       return;
 
 
