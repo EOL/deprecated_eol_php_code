e63fcc39f9f6fe2dcd1d6e0995857d24df9a6f4b
eliagbayani <eli@eol.org>
2018-10-18 12:12:42 -0400

    TRAM-746 Done so far.

--- update_resources/connectors/pbdb_fresh_harvest.php
+++ update_resources/connectors/pbdb_fresh_harvest.php
@@ -17,47 +17,35 @@ include_once(dirname(__FILE__) . "/../../config/environment.php");
 ini_set('memory_limit','8096M');
 $timestart = time_elapsed();
 $resource_id = 368;
-
-// /*
 require_library('connectors/PaleoDBAPI_v2');
-$func = new PaleoDBAPI_v2($resource_id);
-$func->get_all_taxa();
-unset($func);
-Functions::finalize_dwca_resource($resource_id);
-// */
 
-// /* utility - but also now became part of the entire process since we're going to remove taxa that are descendants of 'parents without entries'.
-require_library('connectors/DWCADiagnoseAPI');
+// first loop ------------------------------------------------------------- start
+$func = new PaleoDBAPI_v2($resource_id); main_proc($func, $resource_id);
+$descendant_taxon_ids = utility($resource_id, false); // utility - to remove taxa that are descendants of 'parents without entries'.
+$func = new PaleoDBAPI_v2($resource_id); main_proc($func, $resource_id, $descendant_taxon_ids); // 2nd round
 $func = new DWCADiagnoseAPI();
 if($parents_without_entries = $func->check_if_all_parents_have_entries($resource_id, true)) { //2nd param True means write to text file
-    print_r($parents_without_entries);
-}
-else echo "\nAll parents have entries OK (1st try)\n";
-// */
-
-if($parents_without_entries)
-{
-    /* Given a list of parent_ids, get all descendants of these taxa. Get all the taxon_ids of descendants. */
-    $func = new PaleoDBAPI_v2($resource_id);
-    $dwca_file = CONTENT_RESOURCE_LOCAL_PATH . "$resource_id".".tar.gz";
-    $descendant_taxon_ids = $func->get_descendants_given_parent_ids($dwca_file, $parents_without_entries);
-    // print_r($descendant_taxon_ids);
-}
-else $descendant_taxon_ids = array();
-
-// /* 2nd round
-$func = new PaleoDBAPI_v2($resource_id);
-$func->get_all_taxa($descendant_taxon_ids);
-unset($func);
-Functions::finalize_dwca_resource($resource_id);
-
-$func = new DWCADiagnoseAPI();
-if($undefined = $func->check_if_all_parents_have_entries($resource_id, true)) { //2nd param True means write to text file
-    $arr['parents without entries'] = $undefined;
-    print_r($arr);
+    echo "\nparents without entries: "; print_r($parents_without_entries);
 }
 else echo "\nAll parents have entries OK (2nd try)\n";
-// */
+// first loop ------------------------------------------------------------- end
+
+if($parents_without_entries) {
+    $parents_without_entries_last_round = $parents_without_entries;
+    // 2nd loop
+    echo "\nStarting a 2nd loop...\n";
+    // first loop ------------------------------------------------------------- start
+    $func = new PaleoDBAPI_v2($resource_id); main_proc($func, $resource_id);
+    $descendant_taxon_ids = utility($resource_id, $parents_without_entries_last_round); // utility - to remove taxa that are descendants of 'parents without entries'.
+    $func = new PaleoDBAPI_v2($resource_id); main_proc($func, $resource_id, $descendant_taxon_ids); // 2nd round
+    $func = new DWCADiagnoseAPI();
+    if($parents_without_entries = $func->check_if_all_parents_have_entries($resource_id, true)) { //2nd param True means write to text file
+        echo "\nparents without entries: "; print_r($parents_without_entries);
+    }
+    else echo "\nAll parents have entries OK (2nd try)\n";
+    // first loop ------------------------------------------------------------- end
+    if($parents_without_entries) echo "\nA 3rd loop is needed.\n";
+}
 
 recursive_rmdir(CONTENT_RESOURCE_LOCAL_PATH . $resource_id); //to remove temp folder /368/
 
@@ -66,4 +54,31 @@ echo "\n\n";
 echo "elapsed time = " . $elapsed_time_sec/60 . " minutes \n";
 echo "elapsed time = " . $elapsed_time_sec/60/60 . " hours \n";
 echo "\nDone processing.\n";
+
+function main_proc($func, $resource_id, $descendant_taxon_ids = false)
+{
+    $func->get_all_taxa($descendant_taxon_ids);
+    unset($func);
+    Functions::finalize_dwca_resource($resource_id);
+}
+function utility($resource_id, $prev_parents_without_entries = false)
+{
+    require_library('connectors/DWCADiagnoseAPI');
+    $func = new DWCADiagnoseAPI();
+    if($parents_without_entries = $func->check_if_all_parents_have_entries($resource_id, true)) { //2nd param True means write to text file
+        /* these 2 entries I got after the first run. This will initialize to zero records on every harvest. This is done manually */
+        if($prev_parents_without_entries) $parents_without_entries = array_merge($parents_without_entries, $prev_parents_without_entries);
+        echo "\nparents without entries: "; print_r($parents_without_entries);
+    }
+    else echo "\nAll parents have entries OK (1st try)\n";
+
+    if($parents_without_entries) {
+        /* Given a list of parent_ids, get all descendants of these taxa. Get all the taxon_ids of descendants. */
+        $func = new PaleoDBAPI_v2($resource_id);
+        $dwca_file = CONTENT_RESOURCE_LOCAL_PATH . "$resource_id".".tar.gz";
+        $descendant_taxon_ids = $func->get_descendants_given_parent_ids($dwca_file, $parents_without_entries);
+    }
+    else $descendant_taxon_ids = array();
+    return $descendant_taxon_ids;
+}
 ?>
