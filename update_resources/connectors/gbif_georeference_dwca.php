d9a4842201c67a463cb5c17d9410276f4f389d74
eliagbayani <eli@eol.org>
2018-10-25 10:36:28 -0400

    mtce

--- update_resources/connectors/gbif_georeference_dwca.php
+++ update_resources/connectors/gbif_georeference_dwca.php
@@ -1,16 +1,17 @@
 <?php
 namespace php_active_record;
-/* */
+/* DATA-1748: GBIF map data harvest
+This will generate the map data (.json files) for the EOL maps.
+*/
 include_once(dirname(__FILE__) . "/../../config/environment.php");
 require_library('connectors/GBIFoccurrenceAPI_DwCA');
 $timestart = time_elapsed();
-$resource_id = 1;
 $GLOBALS['ENV_DEBUG'] = true;
 
-$func = new GBIFoccurrenceAPI_DwCA($resource_id);
+$func = new GBIFoccurrenceAPI_DwCA();
 $func->start(); //normal operation
 
-// $func->save_ids_to_text_from_many_folders(); //utility, important as last step
+// $func->save_ids_to_text_from_many_folders(); //utility, important as last step. This is now added to main program $func->start(); 
 
 $elapsed_time_sec = time_elapsed() - $timestart;
 echo "\n\n";
