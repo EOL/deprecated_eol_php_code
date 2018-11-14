ad8dfd7a6a1e38912174d5ccadca539597f1163f
eliagbayani <eli@eol.org>
2018-11-14 01:35:10 -0500

    Revert "reverting back"
    This reverts commit 8a392808ff46c2abcff5d02b8df365961ca14e6d.

--- update_resources/connectors/wikipedia.php
+++ update_resources/connectors/wikipedia.php
@@ -4,7 +4,7 @@ namespace php_active_record;
 include_once(dirname(__FILE__) . "/../../config/environment.php");
 require_library('connectors/WikiDataAPI');
 $timestart = time_elapsed();
-$GLOBALS['ENV_DEBUG'] = false;
+$GLOBALS['ENV_DEBUG'] = true;
 /*
 $str = "abcd|e|||";
 echo "\n[$str]\n";
