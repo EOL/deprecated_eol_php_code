126349c7383bda1903731a5adedc4e2728f785a3
eliagbayani <eli@eol.org>
2018-11-23 10:30:21 -0500

    mtce

--- lib/connectors/WikimediaRangeMapsAPI.php
+++ lib/connectors/WikimediaRangeMapsAPI.php
@@ -10,7 +10,18 @@ class WikimediaRangeMapsAPI
     }
     public function store_wikimedia_range_map_images()
     {
+        using json dump:
+        [[Category:Canidae distribution maps]]
+        [[Category:Animal distribution maps of Africa]]
+        [[Category:Pleuronectiformes distribution maps]]
         
+        using api:
+        [Categories] => Array
+        (
+            [value] => Hippoglossus stenolepis|NASA World Wind|PD-self|Pleuronectiformes distribution maps|Retouched pictures|Self-published work|Uploaded with derivativeFX
+            [source] => commons-categories
+            [hidden] => 
+        )
     }
 }
 ?>
