753ec2cf051e30d7ab46b47082623e181fa5cff4
eliagbayani <eli@eol.org>
2018-12-19 12:29:29 -0500

    mtce

--- update_resources/connectors/dwh.php
+++ update_resources/connectors/dwh.php
@@ -18,6 +18,7 @@ $func->start($cmdline_params['what']);
 
 // $func->syn_integrity_check();        //to check record integrity of synoyms spreadsheet: 1XreJW9AMKTmK13B32AhiCVc7ZTerNOH6Ck_BJ2d4Qng
 // $func->generate_python_file();       //to generate script entry to build_dwh.py
+// $func->clean_up_destination_folder();
 
 // $func->test($cmdline_params['what']);                    //for testing only
 // $func->start($cmdline_params['what'], "CLP_adjustment"); //from CLP #3 from: https://eol-jira.bibalex.org/browse/TRAM-800?focusedCommentId=63045&page=com.atlassian.jira.plugin.system.issuetabpanels:comment-tabpanel#comment-63045
