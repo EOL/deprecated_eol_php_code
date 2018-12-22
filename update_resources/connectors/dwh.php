4ddbdab0ef27a00da0da5729351a16076844c394
eliagbayani <eli@eol.org>
2018-12-22 11:39:23 -0500

    mtce

--- update_resources/connectors/dwh.php
+++ update_resources/connectors/dwh.php
@@ -28,33 +28,38 @@ exit("\n");
 // /* //main operation ------------------------------------------------------------
 $resource_id = "2018_12_19";
 $func = new DHSourceHierarchiesAPI($resource_id);
-// $func->start($cmdline_params['what']);
+$func->start($cmdline_params['what']);
 
-// $func->syn_integrity_check();        //to check record integrity of synoyms spreadsheet: 1XreJW9AMKTmK13B32AhiCVc7ZTerNOH6Ck_BJ2d4Qng
-// $func->generate_python_file();       //to generate script entry to build_dwh.py
-// $func->clean_up_destination_folder();
-
-// /*
-// $func->save_all_ids_from_all_hierarchies_2MySQL(); exit("\n-end txt 2MySQL-\n"); //one-time only. DONE.
-$func->generate_dwca($resource_id);
-Functions::finalize_dwca_resource($resource_id, false, false);
-// */
+// $func->syn_integrity_check();            //to check record integrity of synoyms spreadsheet: 1XreJW9AMKTmK13B32AhiCVc7ZTerNOH6Ck_BJ2d4Qng
+// $func->generate_python_file();           //to generate script entry to build_dwh.py
+// $func->clean_up_destination_folder();    //to do before uploading hierarchies to eol-smasher server
 
 // $func->test($cmdline_params['what']);                    //for testing only
 // $func->start($cmdline_params['what'], "CLP_adjustment"); //from CLP #3 from: https://eol-jira.bibalex.org/browse/TRAM-800?focusedCommentId=63045&page=com.atlassian.jira.plugin.system.issuetabpanels:comment-tabpanel#comment-63045
 // $func->compare_results();                                //a utility to compare results. During initial stages
 // -------------------------------------------------------------------------------- */
 
-/* utility ==========================
+/* =========== generate DwCA --- OK
+// $func->save_all_ids_from_all_hierarchies_2MySQL(); exit("\n-end txt 2MySQL-\n"); //one-time only. DONE.
+$func->generate_dwca($resource_id);
+Functions::finalize_dwca_resource($resource_id, false, false);
+=========== */
+
+/* utility ========================== a good utility after generating DwCA --- OK
 require_library('connectors/DWCADiagnoseAPI');
 $func = new DWCADiagnoseAPI();
 
 $undefined_parents = $func->check_if_all_parents_have_entries($resource_id, true); //true means output will write to text file
 echo "\nTotal undefined parents:" . count($undefined_parents)."\n"; unset($undefined_parents);
 
+$undefined_accepted = $func->check_if_all_parents_have_entries($resource_id, true, false, false, "acceptedNameUsageID");
+echo "\nTotal undefined_accepted:" . count($undefined_accepted)."\n"; unset($undefined_accepted);
+=====================================*/
+
+/* another utility but was never used actually:
 $without = $func->get_all_taxa_without_parent($resource_id, true); //true means output will write to text file
 echo "\nTotal taxa without parents:" . count($without)."\n";
-=====================================*/
+*/
 
 $elapsed_time_sec = time_elapsed() - $timestart;
 echo "\n\n";
