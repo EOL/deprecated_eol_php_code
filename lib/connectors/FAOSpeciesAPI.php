28388f130c9b23434400f42e79ab30aadf843f7f
eliagbayani <eli@eol.org>
2018-12-26 00:55:55 -0500

    WIP: Fao species

--- lib/connectors/FAOSpeciesAPI.php
+++ lib/connectors/FAOSpeciesAPI.php
@@ -11,28 +11,38 @@ class FAOSpeciesAPI
         $this->download_options = array(
             'resource_id'        => 'FAO',                              //resource_id here is just a folder name in cache
             'expire_seconds'     => false,
-            'download_wait_time' => 5000000, 'timeout' => 60*5, 'download_attempts' => 1, 'delay_in_minutes' => 1, 'cache' => 1);
+            'download_wait_time' => 1000000, 'timeout' => 60*5, 'download_attempts' => 1, 'delay_in_minutes' => 1, 'cache' => 1);
         $this->debug = array();
         $this->species_list = "http://www.fao.org/figis/ws/factsheets/domain/species/";
-        $this->factsheet_page = "http://www.fao.org/fishery/species/the_id/en";
+        // $this->factsheet_page = "http://www.fao.org/fishery/species/the_id/en";
+        $this->local_species_page = "http://localhost/cp_new/FAO_species_catalog/www.fao.org/fishery/species/the_id/en.html";
     }
     function start()
     {
-        $ids = self::get_ids();
-        echo "\n".count($ids)."\n";
-        /* just one-time - save the output to: http://localhost/fao.html. Then this url will be inputed to SiteSucker. Saved in Desktop/FAO/FAO_Species.suck
-        $html = '';
-        foreach($ids as $id) $html .= '<br><a href="http://www.fao.org/fishery/species/'.$id.'/en">'.$id.'</a>';
-        echo $html; exit;
-        */
-        exit("\n-end-\n");
+        $ids = self::get_ids(); echo "\n".count($ids)."\n";
+        foreach($ids as $id) {
+            $rec = self::assemble_record($id);
+            exit("\n-stopx-\n");
+        }
     }
     private function assemble_record($id)
     {
-        $url = str_replace("the_id", $id, $this->factsheet_page);
+        $url = str_replace("the_id", $id, $this->local_species_page);
         echo "\n$url\n";
-        if($html = Functions::get_remote_file_fake_browser($url, $this->download_options)) {
-            echo $html;
+        if($html = Functions::lookup_with_cache($url, $this->download_options)) {
+            $html = str_replace(array("\n"), "<p>", $html);
+            $html = str_replace(array("\t"), "<br>", $html);
+            // echo $html; exit;
+            $html = Functions::remove_whitespace($html);
+            $sections = array("FAO Names", "Diagnostic Features", "Geographical Distribution", "Habitat and Biology", "Size", "Interest to Fisheries", "Local Names", "Source of Information");
+            foreach($sections as $section) {
+                if(preg_match("/>$section<(.*?)bgcolor=\"#6699ff\" align=\"left\"/ims", $html, $arr)) {
+                    $str = "<".str_replace("<br>", " ", $arr[1]);
+                    $str = Functions::remove_whitespace($str);
+                    $str = strip_tags($str, "<p><i>");
+                    echo "\n[$section]---------------------\n$str\n---------------------\n";
+                }
+            }
         }
     }
     private function get_ids()
@@ -42,6 +52,17 @@ class FAOSpeciesAPI
             $ids = array_unique($arr[1]);
             return $ids;
         }
+    }
+    function utility_SiteSucker()
+    {
+        $ids = self::get_ids();
+        echo "\n".count($ids)."\n";
+        /* just one-time - save the output to: http://localhost/fao.html. Then this url will be inputed to SiteSucker. Saved in Desktop/FAO/FAO_Species.suck
+        $html = '';
+        foreach($ids as $id) $html .= '<br><a href="http://www.fao.org/fishery/species/'.$id.'/en">'.$id.'</a>';
+        echo $html; exit;
+        */
+        exit("\n-end-\n");
     }
 
 }
