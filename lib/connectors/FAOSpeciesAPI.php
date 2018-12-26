2e0089d5918692f7fedddf0dae5ffcc94f3a4326
eliagbayani <eli@eol.org>
2018-12-26 12:18:09 -0500

    mtce

--- lib/connectors/FAOSpeciesAPI.php
+++ lib/connectors/FAOSpeciesAPI.php
@@ -22,11 +22,14 @@ class FAOSpeciesAPI
         $ids = self::get_ids(); echo "\n".count($ids)."\n";
         foreach($ids as $id) {
             $rec = self::assemble_record($id);
-            exit("\n-stopx-\n");
+            break;
+            // exit("\n-stopx-\n");
         }
+        if($this->debug) print_r($this->debug);
     }
     private function assemble_record($id)
     {
+        // $id = 2004;
         $url = str_replace("the_id", $id, $this->local_species_page);
         echo "\n$url\n";
         if($html = Functions::lookup_with_cache($url, $this->download_options)) {
@@ -42,14 +45,15 @@ class FAOSpeciesAPI
                     $str = strip_tags($str, "<p><i>");
                     echo "\n[$section]---------------------\n$str\n---------------------\n";
                     if($section == "FAO Names") $rec[$section] = self::parse_FAO_Names($str);
-                    elseif($section == "Geographical Distribution") $rec[$section] = self::parse_Geographical_Distribution($str);
+                    elseif($section == "Local Names") $rec[$section] = self::parse_Local_Names($str);
+                    elseif($section == "Geographical Distribution") $rec[$section] = self::parse_Geographical_Distribution($str, $id);
                     else {
                         $rec[$section] = self::other_str_format($str);
                     }
                 }
             }
         }
-        print_r($rec);
+        // print_r($rec);
         return $rec;
     }
     private function other_str_format($str)
@@ -59,9 +63,70 @@ class FAOSpeciesAPI
         if(substr($str, -3) == "<p>") $str = substr($str,0,strlen($str)-3);
         return $str;
     }
-    private function parse_Geographical_Distribution($str)
+    private function parse_Geographical_Distribution($str, $id)
     {
+        echo "\n[$str]\n";
+        $str = str_replace(". </i>","</i> .", $str);
+        $str = str_replace("e. g.", "e.g.", $str);
+        
+        $arr = explode("<p>", $str);
+        // print_r($arr);
+        if($str = @$arr[2]) {}
+        else {
+            return;
+            exit("\nInvestigate id [$id]. No geographical dist.\n");
+        }
+        
+        $letters = array("N.W","e.g","i.e","fig19","St","fig","D","S","P","L","A","E","h","p","N","R","M","O","T","I","C");
+        foreach($letters as $letter) $str = str_replace($letter.". ", $letter."xxx ", $str);
+        // $str = str_replace("S. ", "Sxxx ", $str);
+        // $str = str_replace("P. ", "Pxxx ", $str);
+        // $str = str_replace("L. ", "Lxxx ", $str);
+        
+        $arr = explode(". ", $str." ");
+        $arr = array_map('trim', $arr);
+        $arr = array_filter($arr);
+        // print_r($arr);
+        foreach($arr as $distrib) {
+            foreach($letters as $letter) $distrib = str_replace($letter."xxx ", $letter.". ", $distrib);
+            $this->debug['Geographical Distribution'][$distrib] = '';
+        }
         return "";
+    }
+    private function parse_Local_Names($str)
+    {
+        $comnames = array();
+        /* 
+        Japan : <p> <p> Higezame . 
+        Mexico : <p> <p> Sand shark , <p> Gata .West Indies : <p> <p> Sand shark , <p> Gata .Brazil : <p> <p> Gata atlantica , <p> Cacao lixa .
+        */
+        $str = strip_tags($str);
+        $str = Functions::remove_whitespace($str);
+        
+        $letters = array("etc","U.S.A","S");
+        foreach($letters as $letter) $str = str_replace($letter.". ", $letter."xxx ", $str);
+        
+        $arr = explode('. ', $str." ");
+        $arr = array_map('trim', $arr);
+        $arr = array_filter($arr);
+        // print_r($arr);
+        foreach($arr as $val) {
+            foreach($letters as $letter) $val = str_replace($letter."xxx ", $letter.". ", $val);
+            
+            $arr2 = explode(":", $val);
+            $arr2 = array_map('trim', $arr2);
+            
+            $lang_ctry = trim($arr2[0]);
+            $arr3 = explode(",", $arr2[1]);
+            $arr3 = array_map('trim', $arr3);
+            // print_r($arr3);
+            foreach($arr3 as $comname) {
+                $this->debug['Country Local Names'][$lang_ctry] = '';
+                $comnames[] = array("lang" => $lang_ctry, "comname" => $comname);
+            }
+        }
+        // exit("\n-end Local Names-\n");
+        return $comnames;
     }
     private function parse_FAO_Names($str)
     {
