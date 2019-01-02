5e8a39a27a87d932b1f942b57b3bce1b5eae588c
eliagbayani <eli@eol.org>
2019-01-02 03:06:03 -0500

    brought back synonyms

--- lib/connectors/CITESspeciesAPI.php
+++ lib/connectors/CITESspeciesAPI.php
@@ -131,7 +131,7 @@ class CITESspeciesAPI
                 $this->archive_builder->write_object_to_file($taxon);
                 $this->taxon_ids[$taxon->taxonID] = '';
             }
-            // if($val = @$obj->synonyms)     self::write_synonyms($val, $taxon->taxonID);
+            if($val = @$obj->synonyms)     self::write_synonyms($val, $taxon->taxonID);
             if($val = @$obj->common_names) self::write_comnames($val, $taxon->taxonID);
             self::get_distribution_per_id($taxon->taxonID);
         }
