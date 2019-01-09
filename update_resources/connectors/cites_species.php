ea80cf7077ad7150a6d3c1f5f35070914ae8a0f0
eliagbayani <eli@eol.org>
2019-01-09 10:07:53 -0500

    mtce

--- update_resources/connectors/cites_species.php
+++ update_resources/connectors/cites_species.php
@@ -2,6 +2,8 @@
 namespace php_active_record;
 /* DATA-1790: CITES species records
 cites_taxa	Wednesday 2019-01-02 04:09:07 AM	{"measurement_or_fact.tab":18128,"occurrence.tab":18128,"taxon.tab":75623,"vernacular_name.tab":30384}
+cites_taxa	Wednesday 2019-01-09 09:42:31 AM	{"measurement_or_fact.tab":57748,"occurrence.tab":57748,"taxon.tab":75623,"vernacular_name.tab":30384}
+
 */
 include_once(dirname(__FILE__) . "/../../config/environment.php");
 require_library('connectors/CITESspeciesAPI');
