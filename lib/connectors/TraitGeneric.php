639d2832966d7e1528d6d549454612610b94438a
eliagbayani <eli@eol.org>
2019-01-22 07:54:21 -0500

    complete the Occurrence properties assignment

--- lib/connectors/TraitGeneric.php
+++ lib/connectors/TraitGeneric.php
@@ -52,10 +52,42 @@ class TraitGeneric
         $o = new \eol_schema\Occurrence();
         $o->occurrenceID = $occurrence_id;
         $o->taxonID = $taxon_id;
-        $o->catalogNumber = @$rec['catalogNumber'];
-        $o->dateIdentified = @$rec['dateIdentified'];
-        $o->eventDate = @$rec['dateCollected'];
-        // $o->locality = '';
+
+        //below are non-standard assignments, used by partner source files
+        if($val = @$rec['dateCollected'])   $o->eventDate = $val;
+        
+        //below are standard properties from https://editors.eol.org/other_files/ontology/occurrence_extension.xml
+        /* normally I assign these two: see above
+        if($val = @$rec['occurrenceID'])    $o->occurrenceID = $val; 
+        if($val = @$rec['taxonID'])         $o->taxonID = $val;
+        */
+        if($val = @$rec['eventID'])             $o->eventID = $val;
+        if($val = @$rec['institutionCode'])     $o->institutionCode = $val;
+        if($val = @$rec['collectionCode'])      $o->collectionCode = $val;
+        if($val = @$rec['catalogNumber'])       $o->catalogNumber = $val;
+        if($val = @$rec['sex'])                 $o->sex = $val;
+        if($val = @$rec['lifeStage'])           $o->lifeStage = $val;
+        if($val = @$rec['reproductiveCondition']) $o->reproductiveCondition = $val;
+        if($val = @$rec['behavior'])            $o->behavior = $val;
+        if($val = @$rec['establishmentMeans'])  $o->establishmentMeans = $val;
+        if($val = @$rec['occurrenceRemarks'])   $o->occurrenceRemarks = $val;
+        if($val = @$rec['individualCount'])     $o->individualCount = $val;
+        if($val = @$rec['preparations'])        $o->preparations = $val;
+        if($val = @$rec['fieldNotes'])          $o->fieldNotes = $val;
+        if($val = @$rec['samplingProtocol'])    $o->samplingProtocol = $val;
+        if($val = @$rec['samplingEffort'])      $o->samplingEffort = $val;
+        if($val = @$rec['recordedBy'])          $o->recordedBy = $val;
+        if($val = @$rec['identifiedBy'])        $o->identifiedBy = $val;
+        if($val = @$rec['dateIdentified'])      $o->dateIdentified = $val;
+        if($val = @$rec['eventDate'])           $o->eventDate = $val;
+        if($val = @$rec['modified'])            $o->modified = $val;
+        if($val = @$rec['locality'])            $o->locality = $val;
+        if($val = @$rec['decimalLatitude'])     $o->decimalLatitude = $val;
+        if($val = @$rec['decimalLongitude'])    $o->decimalLongitude = $val;
+        if($val = @$rec['verbatimLatitude'])    $o->verbatimLatitude = $val;
+        if($val = @$rec['verbatimLongitude'])   $o->verbatimLongitude = $val;
+        if($val = @$rec['verbatimElevation'])   $o->verbatimElevation = $val;
+        
         $o->occurrenceID = Functions::generate_measurementID($o, $this->resource_id, 'occurrence');
         if(isset($this->occurrence_ids[$o->occurrenceID])) return $o->occurrenceID;
         $this->archive_builder->write_object_to_file($o);
