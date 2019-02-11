0a21c7180f1ac53fb6e79a975b070b040d3bf68e
eliagbayani <eli@eol.org>
2019-02-11 05:33:56 -0500

    stats

--- update_resources/connectors/wikimedia_subsets.php
+++ update_resources/connectors/wikimedia_subsets.php
@@ -15,6 +15,7 @@ wikimedia_comnames	Thursday 2018-11-29 03:57:36 AM	{"taxon.tab":114046,"vernacul
 wikimedia_comnames	Tuesday 2018-12-11 09:40:20 AM	{"taxon.tab":123249,"vernacular_name.tab":327414}
 wikimedia_comnames	Tuesday 2019-01-08 09:44:54 PM	{"taxon.tab":123849,"vernacular_name.tab":327898}
 wikimedia_comnames	Thursday 2019-01-17 07:49:35 PM	{"taxon.tab":123849,"vernacular_name.tab":327898}
+wikimedia_comnames	Saturday 2019-02-09 03:21:30 PM	{"taxon.tab":128099,"vernacular_name.tab":330621}
 */
 
 // ini_set('error_reporting', E_ALL);
