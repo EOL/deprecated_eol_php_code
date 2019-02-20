43cf04f25edc11ed84b24eef1b90726238059f31
eliagbayani <eli@eol.org>
2019-02-20 00:20:32 -0500

    stats

--- update_resources/connectors/coral_traits.php
+++ update_resources/connectors/coral_traits.php
@@ -2,11 +2,12 @@
 namespace php_active_record;
 /* DATA-1793 
 coraltraits	Tuesday 2019-02-05 11:37:23 AM	{"measurement_or_fact_specific.tab":56713,"occurrence_specific.tab":33518,"reference.tab":555,"taxon.tab":1547} -- first run
+coraltraits	Tuesday 2019-02-19 10:57:19 PM	{"measurement_or_fact_specific.tab":56713,"occurrence_specific.tab":33518,"reference.tab":555,"taxon.tab":1547}
 */
 include_once(dirname(__FILE__) . "/../../config/environment.php");
 require_library('connectors/CoralTraitsAPI');
 $timestart = time_elapsed();
-$resource_id = "coraltraits";
+$resource_id = "cotr"; //coraltraits
 $func = new CoralTraitsAPI($resource_id);
 $func->start(); //main operation
 Functions::finalize_dwca_resource($resource_id, false);
