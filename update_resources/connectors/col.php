7f82e960dc07c2d10f1941791b7405022dae6d72
eliagbayani <eli@eol.org>
2019-03-10 23:19:39 -0400

    mtce

--- update_resources/connectors/col.php
+++ update_resources/connectors/col.php
@@ -4,7 +4,7 @@ include_once(dirname(__FILE__) . "/../../config/environment.php");
 require_library('connectors/NCBIConnector');
 
 $resource_id = 999;
-$ncbi = new CatalogueOfLifeConnector($resource_id);
+$ncbi = new CatalogueOfLifeConnector($resource_id); //non-existent library. This may not be used anymore...
 $ncbi->build_archive();
 Functions::set_resource_status_to_harvest_requested($resource_id);
 
