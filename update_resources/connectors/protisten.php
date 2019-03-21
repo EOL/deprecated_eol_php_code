d303f4fb8e3b85376c8a3c3c8906ef1f05b98642
eliagbayani <eli@eol.org>
2019-03-21 05:19:07 -0400

    mtce

--- update_resources/connectors/protisten.php
+++ update_resources/connectors/protisten.php
@@ -8,7 +8,7 @@ $timestart = time_elapsed();
 $resource_id = "protisten";
 $func = new Protisten_deAPI($resource_id);
 $func->start();
-Functions::finalize_dwca_resource($resource_id, false, false); //3rd param true means to delete working resource folder
+Functions::finalize_dwca_resource($resource_id, false, true); //3rd param true means to delete working resource folder
 $elapsed_time_sec = time_elapsed() - $timestart;
 echo "\n\n elapsed time = " . $elapsed_time_sec . " seconds";
 echo "\n elapsed time = " . $elapsed_time_sec/60 . " minutes";
