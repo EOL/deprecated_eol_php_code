1630311a40b54da1047aa7147aa536feec570327
eliagbayani <eli@eol.org>
2019-03-25 03:44:33 -0400

    mtce

--- update_resources/connectors/dwh_itis.php
+++ update_resources/connectors/dwh_itis.php
@@ -5,7 +5,11 @@ include_once(dirname(__FILE__) . "/../../config/environment.php");
 require_library('connectors/DWH_ITIS_API');
 $timestart = time_elapsed();
 
-$dwca_file = "http://localhost/cp/ITIS_DWH/itisMySQLTables.tar.gz";
+/*
+Note: Database download files are currently from the 25-Feb-2019 data load.
+*/
+
+$dwca_file = "http://localhost/cp/ITIS_DWH/2019-02-25/itisMySQLTables.tar.gz";
 // $dwca_file = "https://www.itis.gov/downloads/itisMySQLTables.tar.gz";
 $resource_id = "itis"; //
 $func = new DWH_ITIS_API($resource_id, $dwca_file);
