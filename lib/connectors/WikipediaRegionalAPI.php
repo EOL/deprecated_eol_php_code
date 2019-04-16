9f65caa8698ab1357f6c3336d7a3dcd82e61a122
eliagbayani <eli@eol.org>
2019-04-16 01:30:49 -0400

    cleaning

--- lib/connectors/WikipediaRegionalAPI.php
+++ lib/connectors/WikipediaRegionalAPI.php
@@ -53,20 +53,17 @@ class WikipediaRegionalAPI
         self::get_taxa_with_taxobox();
         $this->archive_builder->finalize(TRUE);
     }
-
     private function get_taxa_with_taxobox()
     {
         $eilimit = 500; //orig 500 debug
         $continue = false;
         $i = 0;
         $k = 0; //just used when caching, running multiple connectors
-        while(true)
-        {
+        while(true) {
             $url = $this->wikipedia_api . "?action=query&list=embeddedin&eititle=Template:taxobox&eilimit=$eilimit&format=json&continue=";
             if($continue) $url .= "&eicontinue=" . $continue;
             // echo "\n [$url] \n";
-            if($json = Functions::lookup_with_cache($url, $this->download_options))
-            {
+            if($json = Functions::lookup_with_cache($url, $this->download_options)) {
                 $j = json_decode($json);
                 if($val = @$j->continue->eicontinue) $continue = $val;
                 else $continue = false;
@@ -81,8 +78,7 @@ class WikipediaRegionalAPI
                 if(!$cont) continue;
                 */
                 
-                if($recs = $j->query->embeddedin)
-                {
+                if($recs = $j->query->embeddedin) {
                     $i = $i + count($recs);
                     echo "\n" . count($recs) . " -- " . number_format($i) . "\n";
                     self::process_pages_with_taxobox($recs);
@@ -94,18 +90,15 @@ class WikipediaRegionalAPI
             // break; //debug
         }
     }
-    
     private function process_pages_with_taxobox($recs)
     {
         $title = array();
-        foreach($recs as $rec)
-        {
+        foreach($recs as $rec) {
             if($val = $rec->title) $titles[] = $val;
             if(count($titles) == 10) //10 is the manageable no. for the wikipedia api to respond correctly. 50 is max but saw unreliable results.
             {
                 $url = $this->wikipedia_api . "?action=query&prop=langlinks&format=json&llprop=url&lllang=" . $this->language_code . "&continue=&titles=" . implode("|", $titles);
-                if($json = Functions::lookup_with_cache($url, $this->download_options))
-                {
+                if($json = Functions::lookup_with_cache($url, $this->download_options)) {
                     $j = json_decode($json);
                     // echo "\n$url\n";
                     self::process_language_specific_pages($j, $url);
@@ -115,22 +108,18 @@ class WikipediaRegionalAPI
             }
         }
     }
-    
     private function process_language_specific_pages($recs, $url)
     {
-        if(!@$recs->query)
-        {
+        if(!@$recs->query) {
             echo "\n Number of records:"; print_r($recs);
             $options = $this->download_options;
             $options['expire_seconds'] = 0; // 0 -> expires now, orig value; false -> does not expire
             Functions::lookup_with_cache($url, $options);
             return; //this means you have to run the connector again
         }
-        foreach($recs->query->pages as $page_id => $rec)
-        {
+        foreach($recs->query->pages as $page_id => $rec) {
             $rekord = array();
-            if($url = @$rec->langlinks[0]->url)
-            {
+            if($url = @$rec->langlinks[0]->url) {
                 // echo "\n[$url]\n";
                 $this->exclude_url['de'] = array("http://de.wikipedia.org/wiki/Fuchsf%C3%A4cherschwanz", "https://de.wikipedia.org/wiki/Fuchsf%C3%A4cherschwanz", 
                                                  "http://de.wikipedia.org/wiki/Bali_cattle", "https://de.wikipedia.org/wiki/Bali_cattle", "http://de.wikipedia.org/wiki/Br%C3%A4unling_(Vogel)", 
@@ -169,15 +158,12 @@ class WikipediaRegionalAPI
                     
                     // print_r($rekord); //exit;
                     
-                    if(is_numeric(stripos($html, 'summary="Taxobox">')))
-                    {
-                        if(!@$rekord['sciname']) //needs investigation
-                        {
+                    if(is_numeric(stripos($html, 'summary="Taxobox">'))) {
+                        if(!@$rekord['sciname']) { //needs investigation
                             echo "\n no sciname:[$html]";
                             print_r($rekord); //exit;
                         }
-                        else 
-                        {
+                        else {
                             // if($url == "http://de.wikipedia.org/wiki/Lactobacillus_paracasei") exit; //debug
                             self::create_archive($rekord);
                         }
@@ -187,7 +173,6 @@ class WikipediaRegionalAPI
             }// with valid url
         }
     }
-    
     function prepare_wiki_for_parsing($html, $domain_name)
     {
         $html = str_ireplace('href="//', "href=xxxxxx", $html);
@@ -200,7 +185,6 @@ class WikipediaRegionalAPI
         
         return $html;
     }
-    
     function get_comprehensive_desc($html)
     {
         $lang = $this->language_code;
@@ -213,33 +197,26 @@ class WikipediaRegionalAPI
         }
         exit("\nInvestigate WikipediaRegionalAPI [$lang]...\n");
     }
-    
     function get_domain_name($url)
     {
         if(preg_match("/http:\/\/(.*?)\//ims", pathinfo($url, PATHINFO_DIRNAME), $arr)) return $arr[1];
         if(preg_match("/https:\/\/(.*?)\//ims", pathinfo($url, PATHINFO_DIRNAME), $arr)) return $arr[1];
     }
-    
     private function get_sciname($html) //for 'de' only
     {
-        if(preg_match_all("/<td class=\"taxo-name\">(.*?)<\/td>/ims", $html, $arr))
-        {
+        if(preg_match_all("/<td class=\"taxo-name\">(.*?)<\/td>/ims", $html, $arr)) {
             $items = array_reverse($arr[1]);
-            foreach($items as $item)
-            {
+            foreach($items as $item) {
                 if($val = trim(strip_tags($item))) return $val;
             }
         }
 
         // second option e.g. http://de.wikipedia.org/wiki/Piranhas
-        if(preg_match("/<td class=\"taxo-bild\" style=\"font-size:smaller;\">(.*?)<\/td>/ims", $html, $arr))
-        {
+        if(preg_match("/<td class=\"taxo-bild\" style=\"font-size:smaller;\">(.*?)<\/td>/ims", $html, $arr)) {
             $sciname = strip_tags($arr[1], "<i>");
             if(preg_match("/<i>(.*?)<\/i>/ims", $sciname, $arr)) return $arr[1];
-            elseif(preg_match("/\((.*?)\)/ims", $sciname, $arr))
-            {
-                if(!is_numeric(stripos($arr[1], "."))) //e.g. (E. a. americanus)
-                {
+            elseif(preg_match("/\((.*?)\)/ims", $sciname, $arr)) {
+                if(!is_numeric(stripos($arr[1], "."))) { //e.g. (E. a. americanus)
                     if(!(substr_count($arr[1], " ") > 2)) return $arr[1]; //e.g. (Die Länge des weißen Striches entspricht 1 Mikrometer) in http://de.wikipedia.org/wiki/Haptophyta    
                 }
             }
@@ -247,21 +224,15 @@ class WikipediaRegionalAPI
         }
         return false;
     }
-
     private function get_ancestry($html) //for 'de' only
     {
         $ancestry = array();
-        if(preg_match("/<table class=\"toptextcells\"(.*?)<\/table>/ims", $html, $arr))
-        {
-            if(preg_match_all("/<tr>(.*?)<\/tr>/ims", $arr[1], $arr2))
-            {
-                foreach($arr2[1] as $item)
-                {
-                    if(preg_match_all("/<td>(.*?)<\/td>/ims", $item, $arr3))
-                    {
+        if(preg_match("/<table class=\"toptextcells\"(.*?)<\/table>/ims", $html, $arr)) {
+            if(preg_match_all("/<tr>(.*?)<\/tr>/ims", $arr[1], $arr2)) {
+                foreach($arr2[1] as $item) {
+                    if(preg_match_all("/<td>(.*?)<\/td>/ims", $item, $arr3)) {
                         $td = $arr3[1];
-                        foreach($this->ranks[$this->language_code] as $rank)
-                        {
+                        foreach($this->ranks[$this->language_code] as $rank) {
                             if(is_numeric(stripos($td[0], ">$rank</a>"))) //e.g. >Ordnung</a>
                             {
                                 if(preg_match("/\((.*?)\)/ims", strip_tags($td[1], "<i>"), $arr4)) $ancestry[$this->ranks_en[$rank]] = strip_tags($arr4[1]);
@@ -274,44 +245,35 @@ class WikipediaRegionalAPI
         }
         return $ancestry;
     }
-    
     function get_permalink($html)
     {
         // <li id="t-permalink"><a href="/w/index.php?title=Piranhas&amp;oldid=140727080" title="Dauerhafter Link zu dieser Seitenversion">Permanenter Link</a></li>
         if(preg_match("/<li id=\"t-permalink\"><a href=\"(.*?)\"/ims", $html, $arr)) return html_entity_decode($arr[1]);
     }
-    
     private function get_brief_description($html) //for 'de' only
     {
-        if(preg_match("/summary=\"Taxobox\">(.*?)<p><\/p>/ims", $html, $arr)) 
-        {
+        if(preg_match("/summary=\"Taxobox\">(.*?)<p><\/p>/ims", $html, $arr)) {
             if(preg_match_all("/<p>(.*?)<\/p>/ims", $arr[1], $arr2)) return self::format_wiki_substr(array_pop($arr2[1])); //get the last <p>xxx</p> 
         }
-        elseif(preg_match("/summary=\"Taxobox\">(.*?)<h2>/ims", $html, $arr)) 
-        {
+        elseif(preg_match("/summary=\"Taxobox\">(.*?)<h2>/ims", $html, $arr)) {
             if(preg_match_all("/<p>(.*?)<\/p>/ims", $arr[1], $arr2)) return self::format_wiki_substr(array_pop($arr2[1])); //get the last <p>xxx</p> 
         }
     }
-    
     function get_last_modified($html)
     {
-        if($this->language_code == 'de')
-        {
+        if($this->language_code == 'de') {
             // <li id="footer-info-lastmod"> Diese Seite wurde zuletzt am 12. Dezember 2013 um 20:57 Uhr geändert.</li>
             if(preg_match("/<li id=\"footer-info-lastmod\"> Diese Seite wurde zuletzt am(.*?)Uhr geändert\./ims", $html, $arr)) return trim(str_replace(" um ", ", ", $arr[1]));
         }
-        if($this->language_code == 'es')
-        {
+        if($this->language_code == 'es') {
             //<li id="footer-info-lastmod"> Esta página fue modificada por última vez el 18 mar 2017 a las 10:54.</li>
             if(preg_match("/<li id=\"footer-info-lastmod\"> Esta página fue modificada por última vez el(.*?)\./ims", $html, $arr)) return trim(str_replace(" a las ", ", ", $arr[1]));
         }
-        if($this->language_code == 'fr')
-        {
+        if($this->language_code == 'fr') {
             //<li id="footer-info-lastmod"> Dernière modification de cette page le 22 juillet 2016, à 08:05.</li>
             if(preg_match("/<li id=\"footer-info-lastmod\"> Dernière modification de cette page le(.*?)\./ims", $html, $arr)) return trim(str_replace(", à ", ", ", $arr[1]));
         }
-        if($this->language_code == 'en')
-        {
+        if($this->language_code == 'en') {
             //<li id="footer-info-lastmod"> This page was last modified on 18 March 2017, at 20:43.</li>
             if(preg_match("/<li id=\"footer-info-lastmod\"> This page was last modified on(.*?)\./ims", $html, $arr)) return trim(str_replace(", at ", ", ", $arr[1]));
         }
@@ -319,14 +281,11 @@ class WikipediaRegionalAPI
         // <li id="footer-info-lastmod"> Deze pagina is het laatst bewerkt op 10 mrt 2017 om 13:22.</li>
         return self::get_start_of_numerical_part($html);
     }
-    
     private function get_start_of_numerical_part($html)
     {
-        if(preg_match("/<li id=\"footer-info-lastmod\">(.*?)<\/li>/ims", $html, $arr))
-        {
+        if(preg_match("/<li id=\"footer-info-lastmod\">(.*?)<\/li>/ims", $html, $arr)) {
             $str = $arr[1]; //echo "\n$str\n";
-            for ($x = 0; $x <= strlen($str); $x++) 
-            {
+            for ($x = 0; $x <= strlen($str); $x++) {
                 if(is_numeric(substr($str,$x,1))) break;
             }
             $final = trim(substr($str,$x,strlen($str)));
@@ -336,7 +295,6 @@ class WikipediaRegionalAPI
             return $final;
         }
     }
-    
     function translate_source_target_lang($source_text, $source_lang, $target_lang)
     {
         // based from: https://ctrlq.org/code/19909-google-translate-api
@@ -350,29 +308,24 @@ class WikipediaRegionalAPI
             if(preg_match("/\"(.*?)\"/ims", $json, $arr)) return self::format_wiki_substr(ucfirst($arr[1]));
         }
     }
-    
     private function translate($source_text)
     {
         if($val = @$this->trans[$source_text][$this->language_code]) return $val;
         else return self::translate_source_target_lang($source_text, "en", $this->language_code);
     }
-    
     function get_wikipedia_phrase($html)
     {
         //<div id="siteSub">De Wikipedia, la enciclopedia libre</div>
         if(preg_match("/<div id=\"siteSub\">(.*?)<\/div>/ims", $html, $arr)) return ucfirst(trim($arr[1]));
     }
-    
     function get_citation($title, $permalink, $last_modified, $phrase)
     {
         /* orig
         if($this->language_code == 'de')
         {return "Seite '" . $title . "'. In: Wikipedia, Die freie Enzyklopädie. Bearbeitungsstand: " . $last_modified . ". URL: " . $permalink . " (Abgerufen: " . date("d. F Y, h:i T") . ")";}
         */
-        
         return self::translate("Page") . " '" . $title . "'. $phrase. " . self::translate("Modified") . ": " . $last_modified . ". URL: " . $permalink . " (" . self::translate("Retrieved") . ": " . date("d. F Y, h:i T") . ")";
     }
-    
     /* not used since it will call another extra webpage
     private function get_citation_v1($html)
     {
@@ -390,7 +343,6 @@ class WikipediaRegionalAPI
         }
     }
     */
-    
     private function create_archive($rec)
     {
         $t = new \eol_schema\Taxon();
@@ -402,13 +354,11 @@ class WikipediaRegionalAPI
         $t->furtherInformationURL   = $rec['permalink'];
 
         $ranks = array("order", "family", "genus");
-        foreach($ranks as $rank)
-        {
+        foreach($ranks as $rank) {
             if($t->$rank == $t->scientificName) $t->$rank = ''; //any of the ancestry names cannot be same as the scientificname
         }
 
-        if(!isset($this->taxon_ids[$t->taxonID]))
-        {
+        if(!isset($this->taxon_ids[$t->taxonID])) {
             $this->taxon_ids[$t->taxonID] = '';
             $this->archive_builder->write_object_to_file($t);
         }
@@ -438,7 +388,6 @@ class WikipediaRegionalAPI
         $media['CVterm']                 = 'http://rs.tdwg.org/ontology/voc/SPMInfoItems#TaxonBiology';
         if($media['description']) self::create_media_object($media);
     }
-    
     private function create_media_object($media)
     {
         $mr = new \eol_schema\MediaResource();
@@ -459,7 +408,6 @@ class WikipediaRegionalAPI
             $this->archive_builder->write_object_to_file($mr);
         }
     }
-    
     private function format_wiki_substr($substr) //https://en.wikipedia.org/wiki/Control_character
     {   /*
         0 (null, NUL, \0, ^@), originally intended to be an ignored character, but now used by many programming languages to mark the end of a string.
@@ -503,7 +451,6 @@ class WikipediaRegionalAPI
         echo "\n333 - ".$detected." -- ". strlen($substr) ."\n";
         */
     }
-    
     private function remove_utf8_bom($text)
     {
         $bom = pack('H*','EFBBBF');
@@ -513,7 +460,6 @@ class WikipediaRegionalAPI
         */
         return $text;
     }
-
     /* will be replaced by WikiData
     $this->ranks['es'] = array("reino", "filo", "clase", "orden", "familia", "género");
     $this->ranks_en['reino']    = "kingdom";
@@ -533,6 +479,5 @@ class WikipediaRegionalAPI
     $this->ranks_en['género']   = "genre";
     $this->word_User_for_this_region = "Utilisateur";
     */
-
 }
 ?>
