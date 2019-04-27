8330815a2d483a2cd8410c0ec8a29216405beec7
eliagbayani <eli@eol.org>
2019-04-27 12:03:09 -0400

    mtce

--- lib/connectors/GBIFoccurrenceAPI_DwCA.php
+++ lib/connectors/GBIFoccurrenceAPI_DwCA.php
@@ -86,10 +86,10 @@ class GBIFoccurrenceAPI_DwCA //this makes use of the GBIF DwCA occurrence downlo
         require_once(DOC_ROOT."../LiteratureEditor/Custom/lib/Functions.php");
         require_once(DOC_ROOT."../FreshData/controllers/other.php");
         require_once(DOC_ROOT."../FreshData/controllers/freshdata.php");
-        ini_set('memory_limit','15096M'); //15096M
         echo "\nCACHE_PATH 02 is ".CACHE_PATH."\n";
 
         $ctrler = new \freshdata_controller(array());
+        ini_set('memory_limit','15096M'); //15096M
         $postfix = "_map_data";
 
         /* was never used here
