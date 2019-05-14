aea8e7a0f389cb69938af005417866d54b0c8c8b
eliagbayani <eli@eol.org>
2019-05-14 03:05:40 -0400

    mtce

--- lib/connectors/DH_v1_1_Mapping_EOL_IDs.php
+++ lib/connectors/DH_v1_1_Mapping_EOL_IDs.php
@@ -451,10 +451,7 @@ class DH_v1_1_mapping_EOL_IDs
         }
         fclose($WRITE);
         Functions::start_print_debug($this->debug, $this->resource_id."_after_step2");
-        
-        
-        // retire xxx
-        
+        self::retire_old_DH_with_these_taxonIDs("old_DH_after_step2"); //not yet implemented... may not be implemented anymore 
         
         /*
         ------------sent email
@@ -636,9 +633,7 @@ class DH_v1_1_mapping_EOL_IDs
         }
         fclose($WRITE);
         Functions::start_print_debug($this->debug, $this->resource_id."_after_step1");
-        // /*
         self::retire_old_DH_with_these_taxonIDs("old_DH_after_step1"); //not yet implemented... may not be implemented anymore 
-        // */
     }
     // /*
     private function retire_old_DH_with_these_taxonIDs($table)
