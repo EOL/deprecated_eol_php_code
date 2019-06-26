8f9d65b8da9887c0ebcdd34c9f36fb744cc4809b
eliagbayani <eli@eol.org>
2019-06-26 01:28:35 -0400

    stats

--- update_resources/connectors/usa_endangered.php
+++ update_resources/connectors/usa_endangered.php
@@ -6,6 +6,7 @@ ues	Tuesday 2019-02-12 10:11:12 AM	{"measurement_or_fact_specific.tab":2351,"occ
 below is the start when we removed the 2 un-mapped convervation_status. We're only getting 'Threatened and Endangered'.
 ues	Wednesday 2019-02-20 10:18:04 AM{"measurement_or_fact_specific.tab":2293,"occurrence_specific.tab":2293,"reference.tab":3796,"taxon.tab":2273,"vernacular_name.tab":2077}
 ues	Thursday 2019-02-21 02:46:29 AM	{"measurement_or_fact_specific.tab":2293,"occurrence_specific.tab":2293,"reference.tab":3796,"taxon.tab":2273,"vernacular_name.tab":2077}
+ues	Wednesday 2019-06-26 12:33:47 AM{"measurement_or_fact_specific.tab":2295,"occurrence_specific.tab":2295,"reference.tab":3745,"taxon.tab":2275,"vernacular_name.tab":2079}
 */
 
 include_once(dirname(__FILE__) . "/../../config/environment.php");
