ae6d31160c9c67a27f1b104196a5f7de5fc47c97
eliagbayani <eli@eol.org>
2019-07-08 11:19:19 -0400

    stats

--- update_resources/connectors/coral_traits.php
+++ update_resources/connectors/coral_traits.php
@@ -4,6 +4,7 @@ namespace php_active_record;
 coraltraits	Tuesday 2019-02-05 11:37:23 AM	{"measurement_or_fact_specific.tab":56713,"occurrence_specific.tab":33518,"reference.tab":555,"taxon.tab":1547} -- first run
 coraltraits	Tuesday 2019-02-19 10:57:19 PM	{"measurement_or_fact_specific.tab":56713,"occurrence_specific.tab":33518,"reference.tab":555,"taxon.tab":1547}
 cotr	    Wednesday 2019-02-20 12:23:55 AM{"measurement_or_fact_specific.tab":56713,"occurrence_specific.tab":33518,"reference.tab":555,"taxon.tab":1547}
+cotr	Monday 2019-07-08 10:58:43 AM	    {"measurement_or_fact_specific.tab":56713,"occurrence_specific.tab":33518,"reference.tab":555,"taxon.tab":1547}
 */
 include_once(dirname(__FILE__) . "/../../config/environment.php");
 require_library('connectors/CoralTraitsAPI');
