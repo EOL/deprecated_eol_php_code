4b9db54ac37856537368f5588f0d99bbde5be218
eliagbayani <eli@eol.org>
2019-07-09 06:34:59 -0400

    mtce

--- update_resources/connectors/wikidata.php
+++ update_resources/connectors/wikidata.php
@@ -171,6 +171,7 @@ $elapsed_time_sec = time_elapsed() - $timestart;
 echo "\n\n";
 echo "\n elapsed time = " . $elapsed_time_sec/60 . " minutes";
 echo "\n elapsed time = " . $elapsed_time_sec/60/60 . " hours";
+echo "\n elapsed time = " . $elapsed_time_sec/60/60/24 . " days";
 echo "\n Done processing.\n";
 
 /*
