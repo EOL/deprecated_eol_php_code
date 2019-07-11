3b4a36e8d5ae2b8b97a68a1c3451817ed3a68490
eliagbayani <eli@eol.org>
2019-07-11 07:43:09 -0400

    stats

--- update_resources/connectors/770.php
+++ update_resources/connectors/770.php
@@ -2,6 +2,9 @@
 namespace php_active_record;
 /* americaninsects.net - length: structured data
 estimated execution time:
+
+770	Thursday 2019-07-11 06:58:32 AM	{"measurement_or_fact_specific.tab":977,"occurrence_specific.tab":976,"reference.tab":1,"taxon.tab":919}
+
 */
 include_once(dirname(__FILE__) . "/../../config/environment.php");
 require_library('connectors/AmericanInsectsAPI');
