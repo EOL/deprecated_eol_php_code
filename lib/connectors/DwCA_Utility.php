211ef6d92208367d664035931c5689001f89d7c5
eliagbayani <eli@eol.org>
2019-08-01 04:19:36 -0400

    mtce

--- lib/connectors/DwCA_Utility.php
+++ lib/connectors/DwCA_Utility.php
@@ -92,8 +92,11 @@ class DwCA_Utility
     function convert_archive($preferred_rowtypes = false) //same as convert_archive_by_adding_higherClassification(); just doesn't generate higherClassification
     {   /* param $preferred_rowtypes is the option to include-only those row_types you want on your final DwCA. 1st client was DATA-1770 */
         echo "\nConverting archive to EOL DwCA...\n";
-        if($this->resource_id == 'test_eli') $info = self::start(false, array('timeout' => 172800, 'expire_seconds' => 60*60*24*30)); //placeholder for customized resources with respective download_options
-        else                                 $info = self::start(); //default
+
+        // if($this->resource_id == 'test_eli') $info = self::start(false, array('timeout' => 172800, 'expire_seconds' => 60*60*24*30)); //placeholder for customized resources with respective download_options
+        // else                                 $info = self::start(); //default
+        
+        $info = self::start(false, array('timeout' => 172800, 'expire_seconds' => 60));
         
         $temp_dir = $info['temp_dir'];
         $harvester = $info['harvester'];
