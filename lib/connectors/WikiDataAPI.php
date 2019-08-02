3a443b745c4acb86b1240dce82ea7b842ffe205c
eliagbayani <eli@eol.org>
2019-08-02 01:14:54 -0400

    mtce

--- lib/connectors/WikiDataAPI.php
+++ lib/connectors/WikiDataAPI.php
@@ -100,6 +100,7 @@ class WikiDataAPI
     }
     function save_all_media_filenames($task, $range_from, $range_to, $actual_task = false) //one of pre-requisite steps | only for wikimedia
     {   
+        // define('MAX_FILE_SIZE', 600000);
         $GLOBALS['ENV_DEBUG'] = true; //debug only --- Aug 2 troubleshooting a problem.
         //initialize:
         $txtfile = CONTENT_RESOURCE_LOCAL_PATH . "wikimedia_filenames_" . date("Y_m") . ".txt";
@@ -421,7 +422,8 @@ class WikiDataAPI
         echo "\n\nStart add parent entries...\n\n";
         foreach(new FileIterator($this->TEMP_FILE_PATH) as $line_number => $row) {
             $arr = json_decode($row, true);
-            while(@$arr['parent']) {
+            $i = 0; //eli debug troubleshoot
+            while(@$arr['parent']) { $i++; echo " [x $i y]";
                 //first record
                 $rec = array();
                 $rec['id']          = $arr['id'];
