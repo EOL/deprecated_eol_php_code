d62a35c9bdf4cd1d79fa79a8dbb106ec097180bf
eliagbayani <eli@eol.org>
2019-08-09 02:40:08 -0400

    mtce

--- update_resources/connectors/SDR_all.php
+++ update_resources/connectors/SDR_all.php
@@ -164,9 +164,9 @@ $func->generate_page_id_txt_files(); return; //important initial step -- OBSOLET
 // $func->generate_children_of_taxa_usingDH(); return; //the big long program                  _ids/56/97/10594877 - check this later  _ids/85/70/2634372_c.t
 */
 
-// /*
-$func->build_up_children_cache(); exit("\n-end build_up_children_cache()-\n"); //can run multiple connectors. auto-breakdown installed. IMPORTANT STEP
-// */
+/* IMPORTANT STEP - for parent BV and parent TS
+$func->build_up_children_cache(); exit("\n-end build_up_children_cache()-\n"); //can run multiple connectors. auto-breakdown installed.
+*/
 
 /* replaced by: generate_refs_per_eol_pk_MySQL()
 $func->generate_refs_per_eol_pk(); return; //important step for counting refs per eol_pk
@@ -177,15 +177,15 @@ $func->generate_refs_per_eol_pk(); return; //important step for counting refs pe
 // $func->test_basal_values('BV');          return;
 // $func->print_basal_values('BV');         //return;   //3.91 hours
 // $func->test_parent_basal_values('BV', false);   return; //2nd parm is debugModeYN
-// $func->print_parent_basal_values('BV');  //return; //main orig report //92.75 minutes
+$func->print_parent_basal_values('BV');  //return; //main orig report //92.75 minutes
 // $func->print_parent_basal_values('BV', false, false, true);  return; //4th param true means it is debugMode true
 
 // $func->gen_SampleSize_4parent_BV('BV', array('7662')); //generate SampleSize lookup table
 
 // /* for multiple page_ids: BV
 // $page_ids = array(7662, 4528789, 7675, 7669, 7672, 10647853, 7673, 7674, 4529519, 39311345, 7663, 4524096, 7665, 7677, 7676, 7664, 7670, 7671, 7666, 7667, 7668);
-$page_ids = array(7662);
-$func->print_parent_basal_values('BV', $page_ids, 'Carnivora'); return; //used also for test for SampleSize task
+// $page_ids = array(7662);
+// $func->print_parent_basal_values('BV', $page_ids, 'Carnivora'); return; //used also for test for SampleSize task
 // $page_ids = array(1); $func->print_parent_basal_values('BV', $page_ids, 'Metazoa'); //return;
 // foreach($page_ids as $page_id) $final[$page_id] = array('taxonRank' => 'not species', 'Landmark' => 1); //good but not used eventually
 // */
