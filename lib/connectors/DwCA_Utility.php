c3bb48642d4aba0d9bbb59c96ed22ce38ae54685
eliagbayani <eli@eol.org>
2019-08-13 10:59:25 -0400

    mtce

--- lib/connectors/DwCA_Utility.php
+++ lib/connectors/DwCA_Utility.php
@@ -94,7 +94,7 @@ class DwCA_Utility
         echo "\nConverting archive to EOL DwCA...\n";
         
         if($this->resource_id == 'test_eli') $info = self::start(false, array('timeout' => 172800, 'expire_seconds' => 60*60*24*30)); //placeholder for customized resources with respective download_options
-        elseif(in_array($this->resource_id, array('wikimedia_comnames', '71_new'))) $info = self::start(false, array('timeout' => 172800, 'expire_seconds' => 0));
+        elseif(in_array($this->resource_id, array('wikimedia_comnames', '71_new', '368'))) $info = self::start(false, array('timeout' => 172800, 'expire_seconds' => 0));
         else $info = self::start(); //default
 
         $temp_dir = $info['temp_dir'];
