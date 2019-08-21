47397473ef2c85fd18651c5e701d41cccd48255a
eliagbayani <eli@eol.org>
2019-08-21 09:58:06 -0400

    cleaning

--- lib/connectors/WormsArchiveAPI.php
+++ lib/connectors/WormsArchiveAPI.php
@@ -573,8 +573,7 @@ class WormsArchiveAPI
         return false;
     }
     private function get_measurements($meta)
-    {   //print_r($meta);
-        echo "\nprocess_measurementorfact...\n"; $i = 0;
+    {   echo "\nprocess_measurementorfact...\n"; $i = 0;
         foreach(new FileIterator($meta->file_uri) as $line => $row) {
             $i++; if(($i % 100000) == 0) echo "\n".number_format($i);
             if($meta->ignore_header_lines && $i == 1) continue;
@@ -586,8 +585,7 @@ class WormsArchiveAPI
                 if(!$field['term']) continue;
                 $rec[$field['term']] = $tmp[$k];
                 $k++;
-            }
-            // print_r($rec); exit;
+            } // print_r($rec); exit;
             //========================================================================================================first task
             if($rec['http://rs.tdwg.org/dwc/terms/measurementType'] == 'Feedingtype > Host') { // print_r($rec); exit;
                 /*Array(
@@ -606,9 +604,8 @@ class WormsArchiveAPI
                 occurrenceID , associationType , targetOccurrenceID
                 292968_RO_0002454 , http://purl.obolibrary.org/obo/RO_0002454 , 217662_292968_RO_0002454
                 */
-                $param = array('source_taxon_id' => $rec['http://rs.tdwg.org/dwc/terms/MeasurementOrFact'], 'predicate' => 'http://purl.obolibrary.org/obo/RO_0002454', 
-                               'target_taxon_id' => $rec['http://rs.tdwg.org/dwc/terms/measurementValueID'], 
-                               'target_taxon_name' => $rec['http://rs.tdwg.org/dwc/terms/measurementValue']);
+                $param = array('source_taxon_id' => $rec['http://rs.tdwg.org/dwc/terms/MeasurementOrFact'],     'predicate' => 'http://purl.obolibrary.org/obo/RO_0002454', 
+                               'target_taxon_id' => $rec['http://rs.tdwg.org/dwc/terms/measurementValueID'],    'target_taxon_name' => $rec['http://rs.tdwg.org/dwc/terms/measurementValue']);
                 self::add_association($param);
                 /*Now do the reverse*/
                 $sciname = 'will look up or create';
