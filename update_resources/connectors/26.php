cead304bc03cef918dd5ac87a725c8ee9df6fae9
eliagbayani <eli@eol.org>
2019-08-23 09:44:32 -0400

    stats

--- update_resources/connectors/26.php
+++ update_resources/connectors/26.php
@@ -50,7 +50,9 @@ exec time: ~30 minutes
 as of Aug 23, 2019
 26	Friday 2019-08-23 05:38:00 AM	{"agent.tab":1592,"association.tab":69630,"measurement_or_fact_specific.tab":2853465,"media_resource.tab":85088,"occurrence_specific.tab":2138761,"reference.tab":604956,"taxon.tab":579821,"vernacular_name.tab":79059} Mac Mini
 26	Friday 2019-08-23 05:09:26 AM	{"agent.tab":1592,"association.tab":69630,"measurement_or_fact_specific.tab":2853465,"media_resource.tab":85088,"occurrence_specific.tab":2138761,"reference.tab":604956,"taxon.tab":579821,"vernacular_name.tab":79059} eol-archive
-exec time: ?
+26	Friday 2019-08-23 08:53:32 AM	{"agent.tab":1592,"association.tab":69629,"measurement_or_fact_specific.tab":2853465,"media_resource.tab":85088,"occurrence_specific.tab":2138760,"reference.tab":604956,"taxon.tab":579820,"vernacular_name.tab":79059} eol-archive
+
+exec time: 45.29 minutes
 */
 include_once(dirname(__FILE__) . "/../../config/environment.php");
 
