da824491f6a824be7729cad59691e57e0b275aed
eliagbayani <eli@eol.org>
2019-09-04 10:45:54 -0400

    mtce

--- applications/opendata/resource.php
+++ applications/opendata/resource.php
@@ -2,7 +2,7 @@
 namespace php_active_record;
 
 include_once(dirname(__FILE__) . "/../../config/environment.php");
-$GLOBALS['ENV_DEBUG'] = true;
+$GLOBALS['ENV_DEBUG'] = false;
 
 // $resource_id = @$_GET["resource_id"];
 // if(!$resource_id) $function = @$_POST["resource_id"]; //not needed yet
@@ -10,7 +10,7 @@ $GLOBALS['ENV_DEBUG'] = true;
 // exit("\n".$_SERVER."\n");
 
 $ret = $_SERVER; // echo "<pre>"; print_r($ret); echo "</pre>";
-// header('Content-Type: application/json');
+header('Content-Type: application/json');
 require_library('OpenData');
 $func = new OpenData();
 $resource_id = $func->get_id_from_REQUEST_URI($ret['REQUEST_URI']);
