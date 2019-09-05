af452205e9884f2a1861ec1cda7ca988d5830ede
eliagbayani <eli@eol.org>
2019-09-05 11:13:33 -0400

    mtce

--- applications/opendata/organization.php
+++ applications/opendata/organization.php
@@ -7,7 +7,9 @@ $ret = $_SERVER; // echo "<pre>"; print_r($ret); echo "</pre>";
 header('Content-Type: application/json');
 require_library('OpenData');
 $func = new OpenData();
-$org_id = $func->get_id_from_REQUEST_URI($ret['REQUEST_URI']);
+// $org_id = $func->get_id_from_REQUEST_URI($ret['REQUEST_URI']);
+$info   = $func->get_id_from_REQUEST_URI($ret['REQUEST_URI']);
+
 debug("<hr>$org_id<hr>");
-$func->get_organization_by_id($org_id);
+$func->get_organization_by_id($info['id']);
 ?>
--- applications/opendata/resource.php
+++ applications/opendata/resource.php
@@ -3,8 +3,6 @@ namespace php_active_record;
 include_once(dirname(__FILE__) . "/../../config/environment.php");
 $GLOBALS['ENV_DEBUG'] = false;
 
-
-
 // $resource_id = @$_GET["resource_id"];
 // if(!$resource_id) $function = @$_POST["resource_id"]; //not needed yet
 // print_r(@$_GET);
@@ -14,5 +12,7 @@ header('Content-Type: application/json');
 require_library('OpenData');
 $func = new OpenData();
 $resource_id = $func->get_id_from_REQUEST_URI($ret['REQUEST_URI']);
-$func->get_resource_by_id($resource_id);
+
+$info = $func->get_id_from_REQUEST_URI($ret['REQUEST_URI']);
+$func->get_resource_by_id($info['id']);
 ?>
