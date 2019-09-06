6daf3a4ed3341a6dee333a6736427859c16353b6
eliagbayani <eli@eol.org>
2019-09-06 19:06:31 -0400

    mtce

--- lib/OpenData_utility.php
+++ lib/OpenData_utility.php
@@ -11,7 +11,7 @@ class OpenData_utility
     }
     function copy_uploaded_files_to_a_telling_name()
     {
-        $i = 0;
+        $i = 0; $debug = array();
         foreach(new FileIterator(CONTENT_RESOURCE_LOCAL_PATH."/CKAN_file_system.txt") as $line_number => $line) {
             $line = explode("\t", $line); $i++; if(($i % 200000) == 0) echo "\n".number_format($i);
             if($i == 1) $fields = $line;
@@ -21,7 +21,7 @@ class OpenData_utility
                 foreach($fields as $fld) {
                     $rec[$fld] = $line[$k]; $k++;
                 }
-                print_r($rec); //exit;
+                // print_r($rec); //exit;
                 /*Array(
                     [resource_id] => 0045454e-1ae4-4ea3-a087-59f8250e53f0
                     [url] => https://opendata.eol.org/dataset/a52ef387-d650-4f26-bcd5-fca5114462ef/resource/0045454e-1ae4-4ea3-a087-59f8250e53f0/download/wikipedia-fr.tar.gz
@@ -30,18 +30,26 @@ class OpenData_utility
                     [file_path] => /extra/ckan_resources/004/545/
                 )*/
                 $basename = pathinfo($rec['url'], PATHINFO_BASENAME);
-                echo "\n$rec[url]\n[$basename]\n";
+                // echo "\n$rec[url]\n[$basename]\n";
                 $source = $rec['file_path'].$rec['file_id'];
                 $destination = $rec['file_path'].$basename;
                 if(copy($source, $destination)) {
                     echo "\nCopied OK";
                     if(chmod($destination, 0755)) echo " - mode changed OK";
-                    else                          echo " - ERROR: mode not changed";
+                    else {
+                        echo " - ERROR: mode not changed";
+                        $debug['error mode chanage'][$rec['resource_id']] = '';
+                    }
                 }
-                else                            echo "\nERROR: not copied";
-                exit("\nend for now\n");
+                else {
+                    echo "\nERROR: not copied";
+                    $debug['error copy'][$rec['resource_id']] = '';
+                }
+                // exit("\nend for now\n");
             }
+            sleep(1);
         }
+        print_r($debug); exit("\n-end copy_uploaded_files_to_a_telling_name-\n");
     }
     /* Ran already. Run once only. Can be commented now.
     function connect_old_file_system_with_new()
