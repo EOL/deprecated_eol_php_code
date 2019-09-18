0f8c0121a07cb35edefccab3b452db6a12a44b1f
eliagbayani <eli@eol.org>
2019-09-18 08:59:45 -0400

    stats

--- update_resources/connectors/726.php
+++ update_resources/connectors/726.php
@@ -8,6 +8,8 @@ measurementorfact:  20307   21993   21520   21485
 taxon:              988     989     969     968
 occurrence:                 2839    2839    2838
 
+726	Wednesday 2019-09-18 08:58:15 AM	{"measurement_or_fact.tab":21485,"occurrence.tab":2838,"taxon.tab":968} //used parent-child in MoF
+
 */
 
 include_once(dirname(__FILE__) . "/../../config/environment.php");
