36a5ad1f9d2b4fe12f18ddff3db5398eaa66201e
eliagbayani <eli@eol.org>
2019-09-26 07:54:30 -0400

    mtce

--- update_resources/connectors/170.php
+++ update_resources/connectors/170.php
@@ -7,7 +7,7 @@ Connector screen scrapes the partner website.
 170	Monday 2018-04-16 11:34:23 PM	{"media_resource.tab":1880,"taxon.tab":513} eol-archive
 170	Monday 2019-09-23 10:26:56 AM	{"media_resource.tab":1892,"taxon.tab":514} eol-archive
 
-find . -name "waic_seisrnger_cent35_21.mov"
+IMPORTANT: after this connector, run 170_final.php to convert .mov to .mp4.
 
 */
 
