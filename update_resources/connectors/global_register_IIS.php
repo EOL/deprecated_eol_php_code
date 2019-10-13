f865533678abbe9386564b433b7262becef410b0
eliagbayani <eli@eol.org>
2019-10-12 21:30:12 -0400

    mtce

--- update_resources/connectors/global_register_IIS.php
+++ update_resources/connectors/global_register_IIS.php
@@ -1,16 +1,22 @@
 <?php
 namespace php_active_record;
 /* Global Register of Introduced and Invasive Species : DATA-1838
-
 e.g. Belgium
 https://www.gbif.org/dataset/6d9e952f-948c-4483-9807-575348147c7e
 https://api.gbif.org/v1/dataset/6d9e952f-948c-4483-9807-575348147c7e/document
-
 */
 include_once(dirname(__FILE__) . "/../../config/environment.php");
 require_library('connectors/GlobalRegister_IntroducedInvasiveSpecies');
 $timestart = time_elapsed();
 $cmdline_params['jenkins_or_cron'] = @$argv[1]; //irrelevant here
+
+/* test
+$south = array('a','b','c');
+$belgium = array('a','b','c','d','e');
+$diff = array_diff($belgium, $south); //proper
+// $diff = array_diff($south, $belgium); //can be used to check what is in others but South Africa doesn't have
+print_r($diff); exit;
+*/
 
 // remote
 $params["dwca_file"]     = "https://editors.eol.org/other_files/GBIF_DwCA/xxx.zip";
