ce679f7ac50e7872c3d87c3a258bab3c7e3b9a8b
eliagbayani <eli@eol.org>
2019-11-05 01:52:14 -0500

    notes

--- lib/connectors/GBIF_classificationAPI.php
+++ lib/connectors/GBIF_classificationAPI.php
@@ -375,7 +375,7 @@ class GBIF_classificationAPI
             // if($i >=  3000000 && $i < $m*4)  $cont = true;          //conn2 max 3 347 542
             // if($i >=  4000000 && $i < $m*5)  $cont = true;          //conn3 max 4 184 428
             // if($i >=  4900000 && $i < $m*6)  $cont = true;          //conn8 max 5 021 314
-            if($i >=  5700000 && $i < $m*7)  $cont = true;          //7th run max 5 858 200
+            if($i >=  5700000 && $i < $m*7)  $cont = true;          //conn9 max 5 858 200
             if(!$cont) continue;
             // */
 
