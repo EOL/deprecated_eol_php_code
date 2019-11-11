afd492216f0c9196715159de7b2c45862433d333
eliagbayani <eli@eol.org>
2019-11-11 03:54:56 -0500

    notes

--- update_resources/connectors/taxon_image_bundles.php
+++ update_resources/connectors/taxon_image_bundles.php
@@ -4,10 +4,13 @@ namespace php_active_record;
 
 wget -q https://content.eol.org/data/media/91/b9/c7/740.027116-1.jpg -O /extra/other_files/image_bundles/xxx/740.027116-1.jpg
 
+wget -nc https://content.eol.org/data/media/91/b9/c7/740.027116-1.jpg -O /Volumes/AKiTiO4/other_files/bundle_images/xxx/740.027116-1.jpg
+
 php5.6 taxon_image_bundles.php jenkins '{"eol_page_id":919224, "sci":"wormy guy"}'
 php5.6 taxon_image_bundles.php jenkins '{"eol_page_id":695, "sci":"Aves"}'
 php5.6 taxon_image_bundles.php jenkins '{"eol_page_id":164, "sci":"Arthropoda"}'
 php5.6 taxon_image_bundles.php jenkins '{"eol_page_id":328672, "sci":"Panthera leo"}'
+php5.6 taxon_image_bundles.php jenkins '{"eol_page_id":1642, "sci":"Mammalia"}'
 */
 include_once(dirname(__FILE__) . "/../../config/environment.php");
 $timestart = time_elapsed();
@@ -24,7 +27,7 @@ $param = json_decode($params['json'], true);
 
 $destination = CONTENT_RESOURCE_LOCAL_PATH.'images_for_'.str_replace(" ", "_", $param['sci']).".txt"; //false;
 $func->get_images_per_eol_page_id($param, array(), $destination); //normal operation
-$func->bundle_images_4download_per_eol_page_id($param, $destination); //normal operation
+// $func->bundle_images_4download_per_eol_page_id($param, $destination); //normal operation
 
 $elapsed_time_sec = time_elapsed() - $timestart;
 echo "\n\n";
