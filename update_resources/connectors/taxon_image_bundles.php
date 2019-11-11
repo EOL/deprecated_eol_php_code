b54a7ff692c8e53df51a8829158132fb80847ffd
eliagbayani <eli@eol.org>
2019-11-11 03:21:29 -0500

    mtce

--- update_resources/connectors/taxon_image_bundles.php
+++ update_resources/connectors/taxon_image_bundles.php
@@ -4,6 +4,10 @@ namespace php_active_record;
 
 wget -q https://content.eol.org/data/media/91/b9/c7/740.027116-1.jpg -O /extra/other_files/image_bundles/xxx/740.027116-1.jpg
 
+php5.6 taxon_image_bundles.php jenkins '{"eol_page_id":919224, "sci":"wormy guy"}'
+php5.6 taxon_image_bundles.php jenkins '{"eol_page_id":695, "sci":"Aves"}'
+php5.6 taxon_image_bundles.php jenkins '{"eol_page_id":164, "sci":"Arthropoda"}'
+php5.6 taxon_image_bundles.php jenkins '{"eol_page_id":328672, "sci":"Panthera leo"}'
 */
 include_once(dirname(__FILE__) . "/../../config/environment.php");
 $timestart = time_elapsed();
@@ -13,21 +17,13 @@ require_library('connectors/Eol_v3_API');
 $resource_id = '';
 $func = new Eol_v3_API($resource_id);
 
-/* not used here...
 // print_r($argv);
 $params['jenkins_or_cron']   = @$argv[1]; //irrelevant here
 $params['json']              = @$argv[2]; //useful here
-$arr = json_decode($params['json'], true);
-*/
+$param = json_decode($params['json'], true);
 
-// $eol_page_id = 164; $sci = 'Arthropoda';
-// $eol_page_id = 695; $sci = 'Aves';
-$eol_page_id = 328672; $sci = 'Panthera leo';
-// $eol_page_id = 919224; $sci = 'wormy guy';
-
-$param = array('eol_page_id' => $eol_page_id, 'sci' => $sci);
-$destination = CONTENT_RESOURCE_LOCAL_PATH.'images_for_'.str_replace(" ", "_", $sci).".txt"; //false;
-// $func->get_images_per_eol_page_id($param, array(), $destination); //normal operation
+$destination = CONTENT_RESOURCE_LOCAL_PATH.'images_for_'.str_replace(" ", "_", $param['sci']).".txt"; //false;
+$func->get_images_per_eol_page_id($param, array(), $destination); //normal operation
 $func->bundle_images_4download_per_eol_page_id($param, $destination); //normal operation
 
 $elapsed_time_sec = time_elapsed() - $timestart;
