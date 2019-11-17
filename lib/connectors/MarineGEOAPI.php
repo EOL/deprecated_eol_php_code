3cc88af14e4715b886fa9e30ab3b1c6afc979409
eliagbayani <eli@eol.org>
2019-11-17 02:34:55 -0500

    mtce

--- lib/connectors/MarineGEOAPI.php
+++ lib/connectors/MarineGEOAPI.php
@@ -14,11 +14,38 @@ class MarineGEOAPI
         
         $this->api['coll_num'] = 'http://www.boldsystems.org/index.php/API_Public/specimen?ids=COLL_NUM&format=json';
         
+        $this->input['path'] = '/Volumes/AKiTiO4/other_files/MarineGeo/'; //input.xlsx
+        $this->input['worksheets'] = array('Voucher Data', 'Specimen Details', 'Taxonomy Data', 'Collection Data');
     }
     function start()
-    {
+    {   /*
         $coll_num = 'KB17-277';
         self::search_collector_no($coll_num);
+        */
+        $input_file = $this->input['path'].'input.xlsx';
+        self::read_input_file($input_file);
+        
+    }
+    private function read_input_file($input_file)
+    {
+        /*
+        if($local_xls = Functions::save_remote_file_to_local($this->ant_habitat_mapping_file, array('cache' => 1, 'download_wait_time' => 1000000, 'timeout' => 600, 'download_attempts' => 1, 'file_extension' => 'xlsx', 'expire_seconds' => false)))
+        {}
+        unlink($local_xls);
+        */
+        $final = array();
+        require_library('XLSParser'); $parser = new XLSParser();
+        debug("\n reading: " . $input_file . "\n");
+        // $temp = $parser->convert_sheet_to_array($input_file, '2');
+        $temp = $parser->convert_sheet_to_array($input_file, NULL, NULL, false, 'Collection Data');
+        
+        $headers = array_keys($temp);
+        print_r($temp);
+        print_r($headers);
+        
+        exit;
+        return $final;
+        
     }
     private function search_collector_no($coll_num)
     {
