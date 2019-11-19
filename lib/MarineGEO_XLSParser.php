e9724f4a71625858e0cb5d73ef49aebc97a3addb
eliagbayani <eli@eol.org>
2019-11-19 07:49:28 -0500

    cleaning

--- lib/MarineGEO_XLSParser.php
+++ lib/MarineGEO_XLSParser.php
@@ -60,7 +60,6 @@ class MarineGEO_XLSParser
                 $objPHPExcel->getActiveSheet()->mergeCells($alpha[$col]."1:".$alpha[$col+$no_of_cols-1]."1");
                 $objPHPExcel->getActiveSheet()->setCellValue($alpha[$col]."1", $main_head);
                 $objPHPExcel->getActiveSheet()->getStyle($alpha[$col]."1")->getAlignment()->setHorizontal(\PHPExcel_Style_Alignment::HORIZONTAL_CENTER);
-                
                 $col = $col+$no_of_cols;
             }
 
@@ -85,6 +84,7 @@ class MarineGEO_XLSParser
         return;
         // */
         
+        /* working when testing examples
         $objPHPExcel->setActiveSheetIndex(0);
         $objPHPExcel->getActiveSheet()->setTitle('Sheet ONE');
         
@@ -103,7 +103,6 @@ class MarineGEO_XLSParser
 
         $objPHPExcel->setActiveSheetIndex(2);
         $objPHPExcel->getActiveSheet()->setTitle('Sheet 333');
-
         $objPHPExcel->createSheet();
 
         //save Excel file
@@ -111,74 +110,7 @@ class MarineGEO_XLSParser
         // require_once '/Library/WebServer/Documents/eol_php_code/vendor/PHPExcel/Classes/PHPExcel/IOFactory.php'; //by Eli
         $objWriter = \PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel5');
         $objWriter->save($output_file);
-    }
-    public function convert_sheet_to_array($spreadsheet, $sheet_index_number = NULL, $startRow = NULL, $save_params = false, $sheet_index_name = NULL)
-    {
-        require_once DOC_ROOT . '/vendor/PHPExcel/Classes/PHPExcel.php';
-        
-        if(!isset($this->open_spreadsheets)) $this->open_spreadsheets = array();
-        $temp = explode('.', $spreadsheet); //to avoid E_STRICT warning - only variables can be passed by reference
-        $ext = strtolower(end($temp));
-        if(isset($this->open_spreadsheets['spreadsheet']))
-        {
-            $objPHPExcel = $this->open_spreadsheets['spreadsheet'];
-        }else
-        {
-            if    ($ext == "xls") $objReader = \PHPExcel_IOFactory::createReader('Excel5');
-            elseif($ext == "xlsx")$objReader = \PHPExcel_IOFactory::createReader('Excel2007'); //memory intensive, slow response
-            elseif($ext == "zip") $objReader = \PHPExcel_IOFactory::createReader('Excel2007'); //memory intensive, slow response
-            elseif($ext == "csv") $objReader = new \PHPExcel_Reader_CSV();
-            if($ext != "csv") $objReader->setReadDataOnly(true);
-            $objPHPExcel = $objReader->load($spreadsheet);
-            $this->open_spreadsheets['spreadsheet'] = $objPHPExcel;
-        }
-        if(is_null($sheet_index_number)) {
-            if(is_null($sheet_index_name)) $objWorksheet = $objPHPExcel->getActiveSheet();
-            else                           $objWorksheet = $objPHPExcel->setActiveSheetIndexByName($sheet_index_name);
-        }
-        else
-        {
-            if($sheet_index_number+1 > $objPHPExcel->getSheetCount()) return false;
-            $objWorksheet = $objPHPExcel->setActiveSheetIndex($sheet_index_number);
-        }
-        $highestRow         = $objWorksheet->getHighestRow(); // e.g. 10
-        $highestColumn      = $objWorksheet->getHighestColumn(); // e.g 'F'
-        $highestColumnIndex = \PHPExcel_Cell::columnIndexFromString($highestColumn); // e.g. 5
-        $sheet_label = array();
-        $sheet_value = array();
-        if(is_null($startRow)) $startRow = 1;
-        if($save_params) $FILE = Functions::file_open($save_params['path']."/".$save_params['worksheet_title'].".txt", 'w');
-        for ($row = $startRow; $row <= $highestRow; ++$row)
-        {
-            if($save_params) $saved_row = array();
-            for ($col = 0; $col <= $highestColumnIndex; ++$col)
-            {
-                $cell = self::cell_value($objWorksheet, $col, $row, $ext);
-                if($row == $startRow)
-                {
-                    $sheet_label[] = $cell;
-                    if($save_params) $saved_row[] = $cell;
-                }
-                else
-                {
-                    // /* special
-                    if($sheet_label[0] == $sheet_label[1]) {
-                        $sheet_label[1] .= "_2";
-                    }
-                    // print_r($sheet_label); exit;
-                    // */
-                    $index = trim($sheet_label[$col]);
-                    if($index)
-                    {
-                        if($save_params) $saved_row[] = $cell;
-                        else $sheet_value[$index][] = $cell;
-                    }
-                }
-            }
-            if($save_params) fwrite($FILE, implode("\t", $saved_row)."\n");
-        }
-        if($save_params) fclose($FILE);
-        return $sheet_value;
+        */
     }
     function cell_value($obj, $col, $row, $ext)
     {
