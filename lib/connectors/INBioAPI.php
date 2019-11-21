349873a456bf7213cb34a2fef77785404a0243a1
eliagbayani <eli@eol.org>
2019-11-21 10:13:54 -0500

    cleaning

--- lib/connectors/INBioAPI.php
+++ lib/connectors/INBioAPI.php
@@ -22,8 +22,7 @@ class INBioAPI
         $temp_dir = $paths['temp_dir'];
         $harvester = new ContentArchiveReader(NULL, $archive_path);
         $tables = $harvester->tables;
-        if(!($GLOBALS['fields'] = $tables["http://www.pliniancore.org/plic/pcfcore/pliniancore2.3"][0]->fields))
-        {
+        if(!($GLOBALS['fields'] = $tables["http://www.pliniancore.org/plic/pcfcore/pliniancore2.3"][0]->fields)) {
             debug("Invalid archive file. Program will terminate.");
             return false;
         }
@@ -36,8 +35,7 @@ class INBioAPI
         $taxa = $harvester->process_row_type('http://rs.tdwg.org/dwc/terms/Taxon');
         $i = 0;
         $total = sizeof($taxa);
-        foreach($taxa as $taxon)
-        {
+        foreach($taxa as $taxon) {
             $i++;
             debug("$i of $total");
             $taxon_id = @$taxon['http://rs.tdwg.org/dwc/terms/taxonID'];
@@ -55,7 +53,6 @@ class INBioAPI
         if($temp_dir) shell_exec("rm -fr $temp_dir");
         return $all_taxa;
     }
-
     function extract_archive_file($dwca_file, $check_file_or_folder_name, $download_options = array('timeout' => 172800, 'expire_seconds' => 0), $force_extension = false) //e.g. with force_extension is NMNHTypeRecordAPI_v2.php
     {
         debug("Please wait, downloading resource document...");
@@ -64,8 +61,7 @@ class INBioAPI
         if($force_extension) $filename = "elix.".$force_extension; //you can just make-up a filename (elix) here and add the forced extension.
         $temp_dir = create_temp_dir() . "/";
         debug($temp_dir);
-        if($file_contents = Functions::lookup_with_cache($dwca_file, $download_options))
-        {
+        if($file_contents = Functions::lookup_with_cache($dwca_file, $download_options)) {
             $temp_file_path = $temp_dir . "" . $filename;
             debug("temp_dir: $temp_dir");
             debug("Extracting... $temp_file_path");
@@ -74,13 +70,11 @@ class INBioAPI
             fclose($TMP);
             sleep(5);
 
-            if($force_extension == 'zip')
-            {
+            if($force_extension == 'zip') {
                 shell_exec("unzip -ad $temp_dir $temp_file_path");
                 $archive_path = str_ireplace(".zip", "", $temp_file_path);
             }
-            else
-            {
+            else {
                 if(preg_match("/^(.*)\.(tar.gz|tgz)$/", $dwca_file, $arr)) {
                     $cur_dir = getcwd();
                     chdir($temp_dir);
@@ -105,8 +99,7 @@ class INBioAPI
 
             debug("archive path: [" . $archive_path . "]");
         }
-        else
-        {
+        else {
             debug("Connector terminated. Remote files are not ready.");
             return;
         }
@@ -118,8 +111,7 @@ class INBioAPI
         elseif(file_exists($temp_dir ."itisMySQL022519/". $check_file_or_folder_name)) return array('archive_path' => $temp_dir."itisMySQL022519/", 'temp_dir' => $temp_dir); //from ITIS downloads - TRAM-804
         elseif(file_exists($temp_dir ."itisMySQL033119/". $check_file_or_folder_name)) return array('archive_path' => $temp_dir."itisMySQL033119/", 'temp_dir' => $temp_dir); //from ITIS downloads - TRAM-806
         elseif(file_exists($temp_dir ."itisMySQL082819/". $check_file_or_folder_name)) return array('archive_path' => $temp_dir."itisMySQL082819/", 'temp_dir' => $temp_dir); //from ITIS downloads
-        else
-        {
+        else {
             echo "\n1. ".$temp_dir . $check_file_or_folder_name."\n";
             echo "\n2. ".$archive_path . "/" . $check_file_or_folder_name."\n";
             echo "\n3. ".$temp_dir ."dwca/". $check_file_or_folder_name."\n";
@@ -129,23 +121,18 @@ class INBioAPI
             // return array('archive_path' => $temp_dir, 'temp_dir' => $temp_dir);
         }
     }
-
     public static function assign_eol_subjects($xml_string)
     {
         if(!stripos($xml_string, "http://www.eol.org/voc/table_of_contents#")) return $xml_string;
         debug("this resource has http://www.eol.org/voc/table_of_contents# ");
         $xml = simplexml_load_string($xml_string);
-        foreach($xml->taxon as $taxon)
-        {
-            foreach($taxon->dataObject as $dataObject)
-            {
+        foreach($xml->taxon as $taxon) {
+            foreach($taxon->dataObject as $dataObject) {
                 $eol_subjects[] = self::EOL . "SystematicsOrPhylogenetics";
                 $eol_subjects[] = self::EOL . "TypeInformation";
                 $eol_subjects[] = self::EOL . "Notes";
-                if(@$dataObject->subject)
-                {
-                    if(in_array($dataObject->subject, $eol_subjects))
-                    {
+                if(@$dataObject->subject) {
+                    if(in_array($dataObject->subject, $eol_subjects)) {
                         $dataObject->addChild("additionalInformation", "");
                         $dataObject->additionalInformation->addChild("subject", $dataObject->subject);
                         if    ($dataObject->subject == self::EOL . "SystematicsOrPhylogenetics") $dataObject->subject = self::SPM . "Evolution";
@@ -157,14 +144,11 @@ class INBioAPI
         }
         return $xml->asXML();
     }
-
     private function get_images($imagex)
     {
         $images = array();
-        foreach($imagex as $image)
-        {
-            if($image['http://purl.org/dc/terms/identifier'])
-            {
+        foreach($imagex as $image) {
+            if($image['http://purl.org/dc/terms/identifier']) {
                 $taxon_id = $image['http://rs.tdwg.org/dwc/terms/taxonID'];
                 $images[$taxon_id]['url'][]           = $image['http://purl.org/dc/terms/identifier'];
                 $images[$taxon_id]['caption'][]       = $image['http://purl.org/dc/terms/description'];
@@ -177,38 +161,31 @@ class INBioAPI
         }
         return $images;
     }
-
     private function get_references($refs)
     {
         $references = array();
-        foreach($refs as $ref)
-        {
+        foreach($refs as $ref) {
             $taxon_id = $ref['http://rs.tdwg.org/dwc/terms/taxonID'];
             if($ref['http://purl.org/dc/terms/bibliographicCitation']) $references[$taxon_id] = self::parse_references($ref['http://purl.org/dc/terms/bibliographicCitation']);
         }
         return $references;
     }
-
     private function get_vernacular_names($names)
     {
         $vernacular_names = array();
-        foreach($names as $name)
-        {
+        foreach($names as $name) {
             $taxon_id = $name['http://rs.tdwg.org/dwc/terms/taxonID'];
-            if($name['http://rs.tdwg.org/dwc/terms/vernacularName'])
-            {
+            if($name['http://rs.tdwg.org/dwc/terms/vernacularName']) {
                 $vernacular_names[$taxon_id][] = array("name" => $name['http://rs.tdwg.org/dwc/terms/vernacularName'], "language" => self::get_language(@$name['http://purl.org/dc/terms/language']));
             }
         }
         return $vernacular_names;
     }
-
     public static function get_inbio_taxa($taxon, $used_collection_ids)
     {
         $response = self::parse_xml($taxon);
         $page_taxa = array();
-        foreach($response as $rec)
-        {
+        foreach($response as $rec) {
             if(@$used_collection_ids[$rec["identifier"]]) continue;
             $taxon = Functions::prepare_taxon_params($rec);
             if($taxon) $page_taxa[] = $taxon;
@@ -216,16 +193,13 @@ class INBioAPI
         }
         return array($page_taxa, $used_collection_ids);
     }
-
     private function parse_xml($taxon)
     {
         $taxon_id = $taxon["id"];
         $arr_data = array();
         $arr_objects = array();
-        if($taxon["media"])
-        {
-            foreach($GLOBALS['fields'] as $field)
-            {
+        if($taxon["media"]) {
+            foreach($GLOBALS['fields'] as $field) {
                 $term = $field["term"];
                 $mappings = self::$MAPPINGS;
                 if(@$mappings[$term] && @$taxon["media"][$term]) $arr_objects[] = self::prepare_text_objects($taxon, $term);
@@ -233,8 +207,7 @@ class INBioAPI
             $arr_objects = self::prepare_image_objects($taxon, $arr_objects);
             $refs = array();
             if($taxon["reference"]) $refs = $taxon["reference"];
-            if(sizeof($arr_objects))
-            {
+            if(sizeof($arr_objects)) {
                 $sciname = @$taxon["http://rs.tdwg.org/dwc/terms/scientificName"];
                 if(@$taxon["http://rs.tdwg.org/dwc/terms/scientificNameAuthorship"]) $sciname .= " " . $taxon["http://rs.tdwg.org/dwc/terms/scientificNameAuthorship"];
                 $arr_data[]=array(  "identifier"   => $taxon_id,
@@ -255,7 +228,6 @@ class INBioAPI
         }
         return $arr_data;
     }
-
     private function parse_references($refs)
     {
         if    (is_numeric(stripos($refs, "<p>")))  $refs = explode("<p>", $refs);
@@ -265,17 +237,13 @@ class INBioAPI
         foreach($refs as $ref) $references[] = array("fullReference" => $ref);
         return $references;
     }
-
     private function prepare_image_objects($taxon, $arr_objects)
     {
         $image_urls = @$taxon["image"]['url'];
         $i = 0;
-        if($image_urls)
-        {
-          foreach($image_urls as $image_url)
-          {
-            if($image_url)
-            {
+        if($image_urls) {
+          foreach($image_urls as $image_url) {
+            if($image_url) {
                 $identifier     = @$taxon["image"]['url'][$i];
                 $description    = @$taxon["image"]['caption'][$i];
                 $mimeType       = "image/jpeg";
@@ -304,14 +272,12 @@ class INBioAPI
         }
         return $arr_objects;
     }
-
     private function get_language($lang)
     {
         if($lang == "Ingles") return "en";
         elseif($lang == "Español") return "es";
         else return "es";
     }
-
     private function prepare_text_objects($taxon, $term)
     {
         $temp = parse_url($term);
@@ -335,27 +301,22 @@ class INBioAPI
         $language      = self::get_language($taxon["http://purl.org/dc/terms/language"]);
         return self::add_objects($identifier, $dataType, $mimeType, $title, $source, $description, $mediaURL, $agent, $license, $location, $rightsHolder, $refs, $subject, $modified, $created, $language);
     }
-
     private function get_agents($taxon)
     {
         $agent = array();
-        if($taxon["media"]["http://purl.org/dc/terms/creator"])
-        {
+        if($taxon["media"]["http://purl.org/dc/terms/creator"]) {
             $creators = explode(",", $taxon["media"]["http://purl.org/dc/terms/creator"]);
             foreach($creators as $creator) $agent[] = array("role" => "author", "homepage" => "", "fullName" => trim(strip_tags($creator)));
         }
-        if($taxon["media"]["http://purl.org/dc/elements/1.1/contributor"])
-        {
+        if($taxon["media"]["http://purl.org/dc/elements/1.1/contributor"]) {
             $contributors = explode(",", $taxon["media"]["http://purl.org/dc/elements/1.1/contributor"]);
-            foreach($contributors as $contributor)
-            {
+            foreach($contributors as $contributor) {
                 $contributor = trim(strip_tags(str_replace("\\", "", $contributor)));
                 if($contributor) $agent[] = array("role" => "editor", "homepage" => "", "fullName" => $contributor);
             }
         }
         return $agent;
     }
-
     private function add_objects($identifier, $dataType, $mimeType, $title, $source, $description, $mediaURL, $agent, $license, $location, $rightsHolder, $refs, $subject, $modified, $created, $language)
     {
         return array( "identifier"   => $identifier,
@@ -376,7 +337,6 @@ class INBioAPI
                       "language"     => $language
                     );
     }
-
     private function assign_mappings()
     {
         return array(  "http://www.pliniancore.org/plic/pcfcore/scientificDescription"        => self::SPM . "DiagnosticDescription",
@@ -415,6 +375,5 @@ class INBioAPI
                        "http://www.pliniancore.org/plic/pcfcore/unstructuredDocumentation"    => self::EOL . "Notes"
                    );
     }
-
 }
 ?>
