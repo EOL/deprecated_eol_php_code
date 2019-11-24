da7a86d6d2fa3021bd504a75368214662cfa5934
eliagbayani <eli@eol.org>
2019-11-24 01:13:51 -0500

    stats

--- update_resources/connectors/cites_species.php
+++ update_resources/connectors/cites_species.php
@@ -5,7 +5,8 @@ cites_taxa	Wednesday 2019-01-02 04:09:07 AM	{"measurement_or_fact.tab":18128,"oc
 cites_taxa	Wednesday 2019-01-09 09:42:31 AM	{"measurement_or_fact.tab":57748,"occurrence.tab":57748,"taxon.tab":75623,"vernacular_name.tab":30384}
 This run seems erroneous:
 cites_taxa	Friday 2019-11-22 08:58:59 PM	    {"measurement_or_fact_specific.tab":91042,"occurrence_specific.tab":91042,"taxon.tab":38862,"vernacular_name.tab":29532,"time_elapsed":{"sec":37821.53,"min":630.36,"hr":10.51}}
-
+This run seems OK:
+cites_taxa	Saturday 2019-11-23 06:59:43 PM	    {"measurement_or_fact_specific.tab":154576,"occurrence_specific.tab":154576,"taxon.tab":80558,"vernacular_name.tab":30405,"time_elapsed":{"sec":63636.93,"min":1060.62,"hr":17.68}}
 */
 include_once(dirname(__FILE__) . "/../../config/environment.php");
 require_library('connectors/CITESspeciesAPI');
