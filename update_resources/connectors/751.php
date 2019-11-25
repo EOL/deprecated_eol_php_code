dd5b7943de4260879a55d0be7963f24e9d83d4ff
eliagbayani <eli@eol.org>
2019-11-25 11:02:05 -0500

    stats

--- update_resources/connectors/751.php
+++ update_resources/connectors/751.php
@@ -2,6 +2,7 @@
 namespace php_active_record;
 /* execution time:  14 mins eol-archive 
 751	Thursday 2018-08-02 09:18:48 AM	{"measurement_or_fact.tab":20770,"occurrence.tab":20770,"taxon.tab":868}    eol-archive
+751	Monday 2019-11-25 10:51:43 AM	{"measurement_or_fact.tab":20768,"occurrence.tab":20768,"taxon.tab":868,"time_elapsed":{"sec":1814.62,"min":30.24,"hr":0.5}} Consistent OK
 */
 include_once(dirname(__FILE__) . "/../../config/environment.php");
 require_library('connectors/InvasiveSpeciesDataConnector');
