4a7eb1fa805ebce6e1090e01c68ec3bc23eb4c0c
eliagbayani <eli@eol.org>
2019-12-26 22:14:05 -0500

    mtce

--- update_resources/connectors/xml_2_dwca.php
+++ update_resources/connectors/xml_2_dwca.php
@@ -5,6 +5,7 @@ This is a generic script that will convert EOL XML to EOL DWC-A
 
 20	Thursday 2019-12-26 10:52:17 AM	{"agent.tab":2031,"media_resource.tab":28979,"reference.tab":1420,"taxon.tab":8830,"time_elapsed":{"sec":32.97,"min":0.55,"hr":0.01}}
 327	Thursday 2019-12-26 11:20:32 AM	{"agent.tab":120,"media_resource.tab":31927,"reference.tab":5025,"taxon.tab":17539,"vernacular_name.tab":4678,"time_elapsed":{"sec":29.3,"min":0.49,"hr":0.01}}
+TaiEOL	Thursday 2019-12-26 09:57:10 PM	{"agent.tab":15,"media_resource.tab":3823,"taxon.tab":1292,"vernacular_name.tab":807,"time_elapsed":{"sec":4.74,"min":0.08,"hr":0}}
 */
 
 include_once(dirname(__FILE__) . "/../../config/environment.php");
@@ -20,17 +21,20 @@ if($val = @$cmdline_params['resource_id']) $resource_id = $val;
 else exit("\nNo resource_id passed. Will terminate.\n");
 
 // $resource_id = 20; //debug
-$xml[20]['url'] = 'https://opendata.eol.org/dataset/e09787e8-1428-401a-a10d-c28872f2dc93/resource/f2c6d809-abd9-4b98-9b00-39546dcb4eac/download/20.xml.zip';
-$xml[327]['url'] = 'https://opendata.eol.org/dataset/1220f735-a568-47e2-adee-f1bbf65c4ffe/resource/fd17f8dd-74f7-43eb-a547-b3f65deec976/download/327.xml.zip';
-$xml['TaiEOL']['url'] = 'http://eoldata.taibif.tw/files/eoldata/eol/taieol_export_taxonpage_44903.xml';
+$xml[20]['url'] = 'https://opendata.eol.org/dataset/e09787e8-1428-401a-a10d-c28872f2dc93/resource/f2c6d809-abd9-4b98-9b00-39546dcb4eac/download/20.xml.zip'; //ZooKeys
+$xml[327]['url'] = 'https://opendata.eol.org/dataset/1220f735-a568-47e2-adee-f1bbf65c4ffe/resource/fd17f8dd-74f7-43eb-a547-b3f65deec976/download/327.xml.zip'; //Flora of Zimbabwe
+$xml['TaiEOL']['url'] = 'http://eoldata.taibif.tw/files/eoldata/eol/taieol_export_taxonpage_44903.xml'; //Taiwan Encyclopedia of Life
+$xml['802']['url'] = 'http://eoldata.taibif.tw/files/eoldata/eol/taieol_export_taxonpage_69268.xml'; //Taiwan Insecta
 
 $xml[20]['xmlYN'] = false;
 $xml[327]['xmlYN'] = false;
 $xml['TaiEOL']['xmlYN'] = true;
+$xml['802']['xmlYN'] = true;
 
 $xml[20]['expire_seconds'] = false; //no expire
 $xml[327]['expire_seconds'] = false;
 $xml['TaiEOL']['expire_seconds'] = 60*60*24*30; //expires in a month
+$xml['802']['expire_seconds'] = 60*60*24*30; //expires in a month
 
 if($val = @$cmdline_params['expire_seconds']) $xml[$resource_id]['expire_seconds'] = $val;
 
