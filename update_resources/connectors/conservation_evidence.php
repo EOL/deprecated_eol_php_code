8165fdbefdad66ae30d2522e43c06db8ced7f991
eliagbayani <eli@eol.org>
2020-01-13 07:13:51 -0500

    stats

--- update_resources/connectors/conservation_evidence.php
+++ update_resources/connectors/conservation_evidence.php
@@ -2,6 +2,7 @@
 namespace php_active_record;
 /* DATA-1844 
 Tuesday 2019-12-17 12:36:49 PM	{"measurement_or_fact_specific.tab":13119,"occurrence_specific.tab":13119,"taxon.tab":5250,"time_elapsed":{"sec":12058.37,"min":200.97,"hr":3.35}}
+Monday 2020-01-13 07:11:12 AM	{"measurement_or_fact_specific.tab":13119,"occurrence_specific.tab":13119,"taxon.tab":5250,"time_elapsed":{"sec":64.99,"min":1.08,"hr":0.02}}
 */
 include_once(dirname(__FILE__) . "/../../config/environment.php");
 require_library('connectors/ConservationEvidenceDataAPI');
