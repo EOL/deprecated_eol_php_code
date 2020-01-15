815cf30deabd11985cc22ad46cabc2944677072a
eliagbayani <eli@eol.org>
2020-01-15 10:39:28 -0500

    stats

--- update_resources/connectors/mad_natdb.php
+++ update_resources/connectors/mad_natdb.php
@@ -13,6 +13,7 @@ natdb	Monday 2019-10-14 05:42:34 AM	{"measurement_or_fact_specific.tab":132656,"
 natdb	Monday 2019-10-14 06:12:34 AM	{"measurement_or_fact_specific.tab":132656,"occurrence_specific.tab":97870,"reference.tab":11,"taxon.tab":2778,"time_elapsed":{"sec":242.49,"min":4.04,"hr":0.07}} - eol-archive
 Start DATA-1481 terms remapping
 natdb	Monday 2019-12-02 08:12:33 AM	{"measurement_or_fact_specific.tab":132656,"occurrence_specific.tab":97870,"reference.tab":11,"taxon.tab":2778,"time_elapsed":{"sec":260.54,"min":4.34,"hr":0.07000000000000001}}
+natdb	Wednesday 2020-01-15 09:24:06 AM{"measurement_or_fact_specific.tab":130356,"occurrence_specific.tab":97870,"reference.tab":11,"taxon.tab":2778,"time_elapsed":{"sec":250.58,"min":4.18,"hr":0.07000000000000001}}
 */
 include_once(dirname(__FILE__) . "/../../config/environment.php");
 require_library('connectors/MADtoolNatDBAPI');
