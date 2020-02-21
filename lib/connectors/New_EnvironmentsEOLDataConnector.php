4c94176654778ab3c04596f0333b5865b4c17564
eliagbayani <eli@eol.org>
2020-02-20 23:01:51 -0500

    mtce

--- lib/connectors/New_EnvironmentsEOLDataConnector.php
+++ lib/connectors/New_EnvironmentsEOLDataConnector.php
@@ -23,7 +23,7 @@ class New_EnvironmentsEOLDataConnector
         /* START DATA-1841 terms remapping */
         require_library('connectors/TraitGeneric');
         $func = new TraitGeneric(false, false); //params are false and false bec. we just need to access 1 function.
-        $this->remapped_terms = $func->initialize_terms_remapping();
+        $this->remapped_terms = $func->initialize_terms_remapping(60*60*24);
         echo "\nremapped_terms: ".count($this->remapped_terms)."\n";
         /* END DATA-1841 terms remapping */
         
