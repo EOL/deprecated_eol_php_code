a25321ed29a5d0f648bfda5d9f30c8fd9de551e1
eliagbayani <eli@eol.org>
2020-03-04 04:51:56 -0500

    descendants of Ostracoda remove records mtype is RO_0002303

--- lib/connectors/RemoveAvesChildrenAPI.php
+++ lib/connectors/RemoveAvesChildrenAPI.php
@@ -11,28 +11,75 @@ class RemoveAvesChildrenAPI
 
     }
     /*================================================================= STARTS HERE ======================================================================*/
-    private function get_children_of_Aves()
+    private function get_children_of_taxa_group($taxon_ids)
     {
         require_library('connectors/PaleoDBAPI_v2');
         $func = new PaleoDBAPI_v2("");
         $dwca_file = CONTENT_RESOURCE_LOCAL_PATH . "368".".tar.gz";
-        $descendant_taxon_ids = $func->get_descendants_given_parent_ids($dwca_file, array(36616, 7022)); //Aves taxon_id is 36616 | Polychaeta = 7022
+        $descendant_taxon_ids = $func->get_descendants_given_parent_ids($dwca_file, $taxon_ids);
         return $descendant_taxon_ids;
     }
     function start($info)
     {
+        //----------------------------------------------------------------------------------------------
         $this->children_of_Aves = array();
-        $children = self::get_children_of_Aves();
+        $children = self::get_children_of_taxa_group(array(36616, 7022)); //Aves taxon_id is 36616 | Polychaeta = 7022
         foreach($children as $child) $this->children_of_Aves[$child] = '';
         unset($children);
-        echo "\nChildren of Aves: ".count($this->children_of_Aves)."\n";
+        echo "\nChildren of Aves and Polychaeta: ".count($this->children_of_Aves)."\n";
+        //----------------------------------------------------------------------------------------------
+        $this->children_of_Ostracoda = array();
+        $children = self::get_children_of_taxa_group(array(22826)); //Ostracoda taxon_id is 22826
+        foreach($children as $child) $this->children_of_Ostracoda[$child] = '';
+        unset($children);
+        echo "\nChildren of Ostracoda: ".count($this->children_of_Ostracoda)."\n";
+        //----------------------------------------------------------------------------------------------
         
         $tables = $info['harvester']->tables;
+        
+        self::process_generic_table_for_Ostracoda($tables['http://rs.tdwg.org/dwc/terms/occurrence'][0], 'occurrence');
+        self::process_generic_table_for_Ostracoda($tables['http://rs.tdwg.org/dwc/terms/measurementorfact'][0], 'MoF');
+        
         self::process_generic_table($tables['http://rs.tdwg.org/dwc/terms/taxon'][0], 'taxon');
         self::process_generic_table($tables['http://rs.gbif.org/terms/1.0/vernacularname'][0], 'vernacular');
         self::process_generic_table($tables['http://rs.tdwg.org/dwc/terms/occurrence'][0], 'occurrence');
         self::process_generic_table($tables['http://rs.tdwg.org/dwc/terms/measurementorfact'][0], 'MoF');
     }
+    private function process_generic_table_for_Ostracoda($meta, $what)
+    {   //print_r($meta);
+        echo "\nprocess $what...Ostracoda\n"; $i = 0;
+        foreach(new FileIterator($meta->file_uri) as $line => $row) {
+            $i++; if(($i % 100000) == 0) echo "\n".number_format($i);
+            if($meta->ignore_header_lines && $i == 1) continue;
+            if(!$row) continue;
+            // $row = Functions::conv_to_utf8($row); //possibly to fix special chars. but from copied template
+            $tmp = explode("\t", $row);
+            $rec = array(); $k = 0;
+            foreach($meta->fields as $field) {
+                if(!$field['term']) continue;
+                $rec[$field['term']] = $tmp[$k];
+                $k++;
+            }
+            // print_r($rec); exit;
+            if($what == 'occurrence') {
+                if(isset($this->children_of_Ostracoda[$rec['http://rs.tdwg.org/dwc/terms/taxonID']])) {
+                    $this->occurrence_id_Ostracoda[$rec['http://rs.tdwg.org/dwc/terms/occurrenceID']] = '';
+                }
+            }
+            elseif($what == 'MoF') {
+                if(isset($this->occurrence_id_Ostracoda[$rec['http://rs.tdwg.org/dwc/terms/occurrenceID']])) {
+                    /* per: https://eol-jira.bibalex.org/browse/DATA-1831?focusedCommentId=64595&page=com.atlassian.jira.plugin.system.issuetabpanels:comment-tabpanel#comment-64595
+                    For all descendants of Ostracoda, (FurtherInformationURL=https://paleobiodb.org/classic/checkTaxonInfo?is_real_user=1&taxon_no=22826)
+                    please remove all records with measurementType= http://purl.obolibrary.org/obo/RO_0002303
+                    */
+                    if($rec['http://rs.tdwg.org/dwc/terms/measurementType'] == 'http://purl.obolibrary.org/obo/RO_0002303') {
+                        $this->Ostracoda_remove_occurrence_id[$rec['http://rs.tdwg.org/dwc/terms/occurrenceID']] = '';
+                    }
+                }
+            }
+        }
+    }
+
     private function process_generic_table($meta, $what)
     {   //print_r($meta);
         echo "\nprocess $what...\n"; $i = 0;
@@ -66,10 +113,12 @@ class RemoveAvesChildrenAPI
                     $this->remove_occurrence_id[$rec['http://rs.tdwg.org/dwc/terms/occurrenceID']] = '';
                     continue;
                 }
+                if(isset($this->Ostracoda_remove_occurrence_id[$rec['http://rs.tdwg.org/dwc/terms/occurrenceID']])) continue;
                 $o = new \eol_schema\Occurrence();
             }
             elseif($what == 'MoF') {
                 if(isset($this->remove_occurrence_id[$rec['http://rs.tdwg.org/dwc/terms/occurrenceID']])) continue;
+                if(isset($this->Ostracoda_remove_occurrence_id[$rec['http://rs.tdwg.org/dwc/terms/occurrenceID']])) continue;
                 $o = new \eol_schema\MeasurementOrFact_specific();
             }
             else exit("\nInvestigate [$what]\n");
