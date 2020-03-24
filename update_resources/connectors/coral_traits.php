b370169a6da2b82d98ac1d9649377996e047fb29
eliagbayani <eli@eol.org>
2020-03-24 11:20:13 -0400

    stats

--- update_resources/connectors/coral_traits.php
+++ update_resources/connectors/coral_traits.php
@@ -8,6 +8,7 @@ cotr	Tuesday 2019-07-09 12:43:12 AM	    {"measurement_or_fact_specific.tab":5671
 cotr	Friday 2019-11-22 11:04:16 AM	    {"measurement_or_fact_specific.tab":56713,"occurrence_specific.tab":33518,"reference.tab":555,"taxon.tab":1547,"time_elapsed":{"sec":56.13,"min":0.9399999999999999,"hr":0.02}}
 cotr	Monday 2020-02-10 06:42:54 AM	    {"measurement_or_fact_specific.tab":56713, "occurrence_specific.tab":33518, "reference.tab":555, "taxon.tab":1547, "time_elapsed":{"sec":52.81,"min":0.88,"hr":0.01}}
 cotr	Tuesday 2020-03-03 04:30:02 AM	    {"measurement_or_fact_specific.tab":56670, "occurrence_specific.tab":33475, "reference.tab":555, "taxon.tab":1547, "time_elapsed":{"sec":64.98999999999999, "min":1.08, "hr":0.02}}
+cotr	Tuesday 2020-03-24 11:01:13 AM	    {"measurement_or_fact_specific.tab":56648, "occurrence_specific.tab":33475, "reference.tab":555, "taxon.tab":1547, "time_elapsed":{"sec":78.41, "min":1.31, "hr":0.02}}
 */
 include_once(dirname(__FILE__) . "/../../config/environment.php");
 require_library('connectors/CoralTraitsAPI');
