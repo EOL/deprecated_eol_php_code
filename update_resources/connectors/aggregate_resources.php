903a4f5d11ebcf454117757b3c26b6d423e7749a
eliagbayani <eli@eol.org>
2020-03-26 19:07:50 -0400

    mtce

--- update_resources/connectors/aggregate_resources.php
+++ update_resources/connectors/aggregate_resources.php
@@ -8,7 +8,8 @@ $timestart = time_elapsed();
 $resource_id = 'wikipedia_combined_languages';
 require_library('connectors/DwCA_Aggregator');
 $func = new DwCA_Aggregator($resource_id);
-$langs = array('ta', 'ceb', 'el', 'mk', 'ky', 'sco', 'hi', 'fy', 'tl', 'jv', 'ia', 'be-x-old', 'oc', 'qu', 'ne', 'koi', 'frr', 'udm', 'ba', 'an', 'zh-min-nan', 'sw');
+$langs = array('ta', 'ceb', 'el', 'mk', 'ky', 'sco', 'hi', 'fy', 'tl', 'jv', 'ia', 'be-x-old', 'oc', 'qu', 'ne', 'koi', 'frr', 'udm', 'ba', 'an', 'zh-min-nan', 'sw', 
+               'ku', 'uz', 'te', 'bs', 'io', 'my', 'mn', 'kv', 'lb', 'su', 'kn', 'tt', 'co', 'sq', 'csb', 'mr', 'fo', 'os', 'cv');
 // $langs = array('mk'); //for testing
 $func->combine_wikipedia_DwCAs($langs);
 Functions::finalize_dwca_resource($resource_id, false, true, $timestart);
