11f9bddd8f446beb6ea7eb46da7b2e1bf14d98af
eliagbayani <eli@eol.org>
2020-04-03 09:03:15 -0400

    mtce

--- update_resources/connectors/aggregate_resources.php
+++ update_resources/connectors/aggregate_resources.php
@@ -8,8 +8,10 @@ $timestart = time_elapsed();
 $resource_id = 'wikipedia_combined_languages';
 require_library('connectors/DwCA_Aggregator');
 $func = new DwCA_Aggregator($resource_id);
-$langs = array('ta', 'ceb', 'el', 'mk', 'ky', 'sco', 'hi', 'fy', 'tl', 'jv', 'ia', 'be-x-old', 'oc', 'qu', 'ne', 'koi', 'frr', 'udm', 'ba', 'an', 'zh-min-nan', 'sw', 
-               'ku', 'uz', 'te', 'bs', 'io', 'my', 'mn', 'kv', 'lb', 'su', 'kn', 'tt', 'co', 'sq', 'csb', 'mr', 'fo', 'os', 'cv');
+$langs = array('ta', 'ceb', 'el', 'mk', 'ky', 'sco', 'hi', 'fy', 'tl', 'jv', 'ia', 'be-x-old', 'oc', 'qu', 'ne', 'koi', 'frr', 'udm', 'ba', 'an', 'zh-min-nan', 'sw', 'ku', 'uz', 'te', 
+               'bs', 'io', 'my', 'mn', 'kv', 'lb', 'su', 'kn', 'tt', 'co', 'sq', 'csb', 'mr', 'fo', 'os', 'cv', 'kab', 'sah', 'nds', 'lmo', 'pa', 'wa', 'vls', 'gv', 'wuu', 'mi', 'nah', 
+               'dsb', 'kbd', 'to', 'mdf', 'li', 'as', 'bat-smg', 'olo', 'mhr', 'tg', 'pcd', 'ps', 'sd', 'vep', 'se', 'am', 'si', 'ht', 'gn', 'rue', 'mt', 'gu', 'ckb', 'als');
+
 // $langs = array('mk'); //for testing
 $func->combine_wikipedia_DwCAs($langs);
 Functions::finalize_dwca_resource($resource_id, false, true, $timestart);
