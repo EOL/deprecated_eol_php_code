d57d51f87bbb122c08e40d57373b059c1ecd6d8e
eliagbayani <eli@eol.org>
2020-05-08 09:17:21 -0400

    mtce

--- update_resources/connectors/aggregate_resources.php
+++ update_resources/connectors/aggregate_resources.php
@@ -1,6 +1,14 @@
 <?php
 namespace php_active_record;
-/* This can be a generic connector that combines DwCA's. */
+/* This can be a generic connector that combines DwCA's. 
+
+wikipedia_combined_languages	    Friday 2020-05-08 06:21:06 AM	{"media_resource.tab":99277, "taxon.tab":25775, "time_elapsed":{"sec":576.91, "min":9.62, "hr":0.16}}
+wikipedia_combined_languages_batch2	Friday 2020-05-08 06:22:23 AM	{"media_resource.tab":3033, "taxon.tab":3366, "time_elapsed":{"sec":653.4, "min":10.89, "hr":0.18}}
+
+wget https://editors.eol.org/eol_php_code/applications/content_server/resources/wikipedia_combined_languages.tar.gz
+wget https://editors.eol.org/eol_php_code/applications/content_server/resources/wikipedia_combined_languages_batch2.tar.gz
+
+*/
 
 include_once(dirname(__FILE__) . "/../../config/environment.php");
 $timestart = time_elapsed();
@@ -12,7 +20,7 @@ $langs[1] = array('ta', 'ceb', 'el', 'mk', 'ky', 'sco', 'hi', 'fy', 'tl', 'jv', 
                'dsb', 'kbd', 'to', 'mdf', 'li', 'as', 'bat-smg', 'olo', 'mhr', 'tg', 'pcd', 'ps', 'sd', 'vep', 'se', 'am', 'si', 'ht', 'gn', 'rue', 'mt', 'gu', 'ckb', 'als', 
                'or', 'bh', 'myv', 'scn', 'gd', 'dv', 'pam', 'xmf', 'cdo', 'bar', 'nap', 'lfn', 'nds-nl', 'bo', 'stq', 'inh', 'lbe', 'ha');
 
-$langs[2] = array('lij', 'lez', 'sa', 'yi', 'ace', 'diq', 'ce', 'yo', 'rw', 'vec', 'sc', 'ln', 'hak', 'kw', 'bcl', 'za', 'ang', 'eml', 'av', 'chy', 'fj', 'ik', 'ug', 'zea', 'bxr', 'zh-classical', 'bjn', 'so', 'arz', 'mwl', 'sn', 'chr', 
+$langs[2] = array('lij', 'lez', 'sa', 'yi', 'ace', 'diq', 'ce', 'yo', 'rw', 'vec', 'sc', 'ln', 'hak_eli', 'kw', 'bcl', 'za', 'ang', 'eml', 'av', 'chy', 'fj', 'ik', 'ug', 'zea', 'bxr', 'zh-classical', 'bjn', 'so', 'arz', 'mwl', 'sn', 'chr', 
                'mai', 'tk', 'tcy', 'szy', 'mzn', 'wo', 'ab', 'ban', 'ay', 'tyv', 'atj', 'new', 'fiu-vro', 'mg', 'rm', 'ltg', 'ext', 'kl', 'roa-rup', 'nrm', 'rn', 'dty', 'hyw', 'lo', 'kg', 
                'km', 'gom', 'frp', 'sat', 'gan', 'haw', 'hif', 'nso', 'xal', 'mnw', 'zu', 'bi', 'lad', 'map-bms', 'roa-tara', 'pdc', 'kbp', 'jbo', 'kaa', 'srn', 'vo', 'gag', 'ty', 'fur', 
                'ie', 'lg', 'ts', 'bpy', 'iu', 'arc', 'gor', 'nov', 'crh', 'tum', 'glk', 'krc', 'ksh', 'na', 'ny', 'pfl', 'xh', 'tpi', 'cr', 'gcr', 'jam', 'ak', 'bm', 'cu', 'ks', 'pap', 
