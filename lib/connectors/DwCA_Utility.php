14943779632b461a7f4cf8d423b3a072dda6b8df
eliagbayani <eli@eol.org>
2020-05-30 09:06:13 -0400

    mtce

--- lib/connectors/DwCA_Utility.php
+++ lib/connectors/DwCA_Utility.php
@@ -104,8 +104,9 @@ class DwCA_Utility
         echo "\nConverting archive to EOL DwCA...\n";
         
         //placeholder for customized resources with respective download_options
-        if(in_array($this->resource_id, array('globi_associations', '170_final', 'BF'))) $info = self::start(false, array('timeout' => 172800, 'expire_seconds' => 60*60*24*30)); //1 month expire
+        if(in_array($this->resource_id, array('170_final', 'BF'))) $info = self::start(false, array('timeout' => 172800, 'expire_seconds' => 60*60*24*30)); //1 month expire
         elseif(in_array($this->resource_id, array('wikimedia_comnames', '71_new', '368_removed_aves', 'itis_2019-08-28', '368_final'))) $info = self::start(false, array('timeout' => 172800, 'expire_seconds' => 0)); //expires now
+        elseif(in_array($this->resource_id, array('globi_associations'))) $info = self::start(false, array('timeout' => 172800, 'expire_seconds' => 60*60*24)); //expires in a day
         elseif(in_array($this->resource_id, array('gbif_classification', 'gbif_classification_without_ancestry', '26'))) {
             if(Functions::is_production()) $info = self::start(false, array('timeout' => 172800, 'expire_seconds' => 0)); //expires now
             else                           $info = self::start(false, array('timeout' => 172800, 'expire_seconds' => 60*60*1)); //1 hour expire
