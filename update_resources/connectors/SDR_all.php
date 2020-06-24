2d732ff832b44fe7f678cf9d0c4602bed50dcd09
eliagbayani <eli@eol.org>
2020-06-24 01:10:50 -0400

    mtce

--- update_resources/connectors/SDR_all.php
+++ update_resources/connectors/SDR_all.php
@@ -5,33 +5,6 @@ namespace php_active_record;
 */
 include_once(dirname(__FILE__) . "/../../config/environment.php");
 require_library('connectors/SummaryDataResourcesAllAPI');
-
-/*
-$a[3] = 'three';
-$a[1] = 'one';
-$a[5] = 'five';
-print_r($a);
-ksort($a);
-print_r($a);
-exit("\n");
-*/
-
-/*
-$children_of[111]['01'] = 'species';
-$children_of[111]['02'] = 'species';
-$children_of[111]['03'] = 'species';
-print_r($children_of);
-exit;
-*/
-/*
-$arr = array(2,4,6,8,10);
-$arr = array(1,2,3,4);
-$arr = array(1,2);
-$middle = get_middle_record($arr);
-echo "\n$arr[$middle]\n";
-exit("\n");
-*/
-
 /*
 $a = array(5319, 1905, 2774383, 8814528, 1, 2910700, 2908256, 2913056);     
 $a = array_reverse($a); print_r($a);
@@ -42,91 +15,24 @@ foreach($a as $id) {
     else                         $children_of[$id] = $temp;
     $children_of[$id] = array_unique($children_of[$id]);
 }
-
-// $a = Array(5110, 5083, 1905, 2774383, 8814528, 1, 2910700, 2908256, 2913056);
-// $a = array_reverse($a);                                                     print_r($a);
-// $temp = $a;
-// foreach($a as $id) {
-//     array_shift($temp);
-//     if(isset($children_of[$id])) $children_of[$id] = array_merge($children_of[$id], $temp);
-//     else                         $children_of[$id] = $temp;
-//     $children_of[$id] = array_unique($children_of[$id]);
-// }
-
 print_r($children_of);
 exit("\n");
 */
-
 /*
-$str = "http://purl.obolibrary.org/obo/ENVO_00000020, http://purl.obolibrary.org/obo/ENVO_00000043, http://purl.obolibrary.org/obo/ENVO_00000065, http://purl.obolibrary.org/obo/ENVO_00000067, 
-http://purl.obolibrary.org/obo/ENVO_00000081, http://purl.obolibrary.org/obo/ENVO_00000086, http://purl.obolibrary.org/obo/ENVO_00000220, http://purl.obolibrary.org/obo/ENVO_00000264, 
-http://purl.obolibrary.org/obo/ENVO_00000360, http://purl.obolibrary.org/obo/ENVO_00000446, http://purl.obolibrary.org/obo/ENVO_00001995, http://purl.obolibrary.org/obo/ENVO_00002000, 
-http://purl.obolibrary.org/obo/ENVO_00002033, http://purl.obolibrary.org/obo/ENVO_01000206, http://purl.obolibrary.org/obo/ENVO_01001305, http://purl.obolibrary.org/obo/ENVO_00000078, 
-http://purl.obolibrary.org/obo/ENVO_00000113, http://purl.obolibrary.org/obo/ENVO_00000144, http://purl.obolibrary.org/obo/ENVO_00000261, http://purl.obolibrary.org/obo/ENVO_00000316, 
-http://purl.obolibrary.org/obo/ENVO_00000320, http://purl.obolibrary.org/obo/ENVO_00000358, http://purl.obolibrary.org/obo/ENVO_00000486, http://purl.obolibrary.org/obo/ENVO_00000572, 
-http://purl.obolibrary.org/obo/ENVO_00000856, http://purl.obolibrary.org/obo/ENVO_00002030, http://purl.obolibrary.org/obo/ENVO_00002040, http://purl.obolibrary.org/obo/ENVO_01000204, 
-http://purl.obolibrary.org/obo/ENVO_00000002, http://purl.obolibrary.org/obo/ENVO_00000016, http://eol.org/schema/terms/temperate_grasslands_savannas_and_shrublands, 
-http://purl.obolibrary.org/obo/ENVO_01001125";
-
+$str = "http://purl.obolibrary.org/obo/ENVO_00000020, http://purl.obolibrary.org/obo/ENVO_00000043, http://purl.obolibrary.org/obo/ENVO_00000065";
 $arr = explode(",", $str);
 $arr = array_map('trim', $arr);
 asort($arr); print_r($arr); 
-
 echo "\n rows: ".count($arr);
 foreach($arr as $tip) echo "\n$tip";
 exit("\ntotal: ".count($arr)."\n");
 */
-
-/* //tests
-$parents = array(1,2,3);
-$preferred_terms = array(4,5);
-$inclusive = array_merge($parents, $preferred_terms);
-print_r($inclusive);
-exit("\n-end tests'\n");
-*/
-
 /*
-$arr = json_decode('["717136"]');
-if(!is_array($arr) && is_null($arr)) {
-    $arr = array();
-    echo "\nwent here 01\n";
-}
-else {
-    echo "\nwent here 02\n";
-    print_r($arr);
-}
-exit("\n");
+$file = "/Volumes/AKiTiO4/web/cp/summary data resources/page_ids/99/cd/R96-PK42697173.txt";
+$file = "/Volumes/AKiTiO4/web/cp/summary data resources/page_ids/38/49/R344-PK19315117.txt";
+$json = file_get_contents($file);
+print_r(json_decode($json, true)); exit;
 */
-
-/*
-$a1 = array('45511473' => Array(46557930));
-$a2 = array('308533' => Array(1642, 46557930));
-$a3 = $a1 + $a2; print_r($a3);
-exit("\n");
-*/
-
-// $json = "[]";
-// $arr = json_decode($json, true);
-// if(is_array($arr)) echo "\nis array\n";
-// else               echo "\nnot array\n";
-// if(is_null($arr)) echo "\nis null\n";
-// else               echo "\nnot null\n";
-// print_r($arr);
-// // if(!is_array($arr) && is_null($arr)) $arr = array();
-// exit("\n");
-
-// $file = "/Volumes/AKiTiO4/web/cp/summary data resources/page_ids/99/cd/R96-PK42697173.txt";
-// $file = "/Volumes/AKiTiO4/web/cp/summary data resources/page_ids/38/49/R344-PK19315117.txt";
-// $json = file_get_contents($file);
-// print_r(json_decode($json, true)); exit;
-
-// $terms = array("Braunbär", " 繡球菌", "Eli");
-// foreach($terms as $t){
-//     echo "\n".$t."\n";
-//     // $t = utf8_encode($t); echo "\n".$t."\n";
-//     $t = Functions::conv_to_utf8($t); echo "\n".$t."\n";
-// }
-// exit("\nexit muna\n");
 
 ini_set('memory_limit','7096M'); //required
 $timestart = time_elapsed();
