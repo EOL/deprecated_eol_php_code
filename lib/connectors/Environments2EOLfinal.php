6d25221ebdd03d4289980828a59a473c19e9287f
eliagbayani <eli@eol.org>
2020-07-27 09:54:07 -0400

    reconstructing the Environments-EOL - WIP

---
+++ lib/connectors/Environments2EOLfinal.php
@@ -0,0 +1,44 @@
+<?php
+namespace php_active_record;
+/* connector: [called from DwCA_Utility.php, which is called from Environments2EOLAPI.php; from environments_2_eol.php for DATA-1851] */
+class Environments2EOLfinal
+{
+    function __construct($archive_builder, $resource_id)
+    {
+        $this->resource_id = $resource_id;
+        $this->archive_builder = $archive_builder;
+        $this->download_options = array('cache' => 1, 'resource_id' => $resource_id, 'expire_seconds' => 60*60*24*30*4, 'download_wait_time' => 1000000, 'timeout' => 10800, 'download_attempts' => 1, 'delay_in_minutes' => 1);
+    }
+    /*================================================================= STARTS HERE ======================================================================*/
+    function start($info)
+    {   
+        require_library('connectors/TraitGeneric'); 
+        $this->func = new TraitGeneric($this->resource_id, $this->archive_builder);
+        /* START DATA-1841 terms remapping */
+        $this->func->initialize_terms_remapping(60*60*24); //param is $expire_seconds. 0 means expire now.
+        /* END DATA-1841 terms remapping */
+        self::initialize_mapping(); //for location string mappings
+        self::add_environmental_traits();
+        /* Below will be used if there are adjustments to existing MoF and Occurrences
+        $tables = $info['harvester']->tables;
+        self::process_measurementorfact($tables['http://rs.tdwg.org/dwc/terms/measurementorfact'][0]);
+        self::process_occurrence($tables['http://rs.tdwg.org/dwc/terms/occurrence'][0]);
+        */
+    }
+    private function initialize_mapping()
+    {   $mappings = Functions::get_eol_defined_uris(false, true);     //1st param: false means will use 1day cache | 2nd param: opposite direction is true
+        echo "\n".count($mappings). " - default URIs from EOL registry.";
+        $this->uris = Functions::additional_mappings($mappings); //add more mappings used in the past
+    }
+    private function add_environmental_traits()
+    {
+        
+    }
+    /* Below will be used if there are adjustments to existing MoF and Occurrences
+    private function process_measurementorfact($meta)
+    {}
+    private function process_occurrence($meta)
+    {}
+    */
+}
+?>
