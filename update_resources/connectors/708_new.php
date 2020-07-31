ba5a1012a17afa563f42dee0eca12cd8e3701014
eliagbayani <eli@eol.org>
2020-07-31 09:44:54 -0400

    mtce

--- update_resources/connectors/708_new.php
+++ update_resources/connectors/708_new.php
@@ -18,6 +18,7 @@ Expected for MoF to be reduced - so consistent OK
 708	Thursday 2020-03-19 03:00:34 AM	{"measurement_or_fact.tab":727108, "occurrence.tab":631535, "reference.tab":198537, "taxon.tab":196191, "time_elapsed":{"sec":580.76, "min":9.68, "hr":0.16}}
 708	Wednesday 2020-05-13 05:42:17 AM{"measurement_or_fact.tab":720242, "occurrence.tab":625163, "reference.tab":198537, "taxon.tab":194207, "time_elapsed":{"sec":933.52, "min":15.56, "hr":0.26}} Mac Mini
 708	Wednesday 2020-05-13 09:35:56 AM{"measurement_or_fact.tab":720242, "occurrence.tab":625163, "reference.tab":195531, "taxon.tab":194207, "time_elapsed":{"sec":571.25, "min":9.52, "hr":0.16}} back to eol-archive
+saved at one point: 708_31Jul2020.tar.gz
 */
 
 include_once(dirname(__FILE__) . "/../../config/environment.php");
@@ -25,7 +26,8 @@ $timestart = time_elapsed();
 // $GLOBALS['ENV_DEBUG'] = true;
 
 $resource_id = 708;
-if(Functions::is_production())  $dwca_file = 'https://editors.eol.org/eol_php_code/applications/content_server/resources/708_25Nov2018.tar.gz';
+// if(Functions::is_production())  $dwca_file = 'https://editors.eol.org/eol_php_code/applications/content_server/resources/708_25Nov2018.tar.gz';
+if(Functions::is_production())  $dwca_file = 'https://editors.eol.org/eol_php_code/applications/content_server/resources/708_26Nov2018.tar.gz';
 else                            $dwca_file = 'http://localhost/cp/Environments/legacy/708_25Nov2018.tar.gz';
 process_resource_url($dwca_file, $resource_id, $timestart);
 
