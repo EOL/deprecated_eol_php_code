e71d3ce54492af569576e8eb61bd085b7b1ec61f
eliagbayani <eli@eol.org>
2020-08-09 11:36:05 -0400

    stats

--- update_resources/connectors/ioc.php
+++ update_resources/connectors/ioc.php
@@ -11,11 +11,12 @@ measurements    31172   43214   42992       42992
 ioc-birdlist	Sunday 2018-05-20 12:51:02 PM	                        {"measurement_or_fact.tab":42989,"occurrence.tab":42989,"taxon.tab":33520,"vernacular_name.tab":11104}
 ioc-birdlist	Sunday 2018-05-20 12:52:41 PM	                        {"measurement_or_fact.tab":42989,"occurrence.tab":42989,"taxon.tab":33520,"vernacular_name.tab":11104}
 ioc	            Sunday 2018-05-20 01:19:14 PM	                        {"measurement_or_fact.tab":42989,"occurrence.tab":42989,"taxon.tab":33520,"vernacular_name.tab":11104}
+
 ioc-birdlist	Sunday 2018-10-21 11:37:12 PM	                        {"measurement_or_fact.tab":43007,"occurrence.tab":43007,"taxon.tab":33526,"vernacular_name.tab":11114}
 ioc-birdlist-with-higherClassification	Sunday 2018-10-21 11:37:56 PM	{"measurement_or_fact.tab":43007,"occurrence.tab":43007,"taxon.tab":33526,"vernacular_name.tab":11114}
 
-ioc-birdlist	Tuesday 2020-07-21 05:29:13 AM	                        {"measurement_or_fact.tab":43070, "occurrence.tab":43070, "taxon.tab":33549, "vernacular_name.tab":11178, "time_elapsed":false}
-ioc-birdlist-with-higherClassification	Tuesday 2020-07-21 05:30:09 AM	{"measurement_or_fact.tab":43076, "occurrence.tab":43076, "taxon.tab":33564, "vernacular_name.tab":11166, "time_elapsed":false}
+ioc-birdlist	Sunday 2020-08-09 11:19:09 AM	                        {"measurement_or_fact.tab":43070, "occurrence.tab":43070, "taxon.tab":33549, "vernacular_name.tab":11178, "time_elapsed":false}
+ioc-birdlist-with-higherClassification	Sunday 2020-08-09 11:20:08 AM	{"measurement_or_fact.tab":43070, "occurrence.tab":43070, "taxon.tab":33549, "vernacular_name.tab":11178, "time_elapsed":false}
 */
 
 include_once(dirname(__FILE__) . "/../../config/environment.php");
