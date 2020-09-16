9bb1ea8bd39d23a2057b14a2266cdbb3fecab884
eliagbayani <eli@eol.org>
2020-09-16 12:59:22 -0400

    notes

--- update_resources/connectors/dwh_worms_TRAM_798.php
+++ update_resources/connectors/dwh_worms_TRAM_798.php
@@ -7,7 +7,52 @@ WoRMS_DH	Thursday 2018-08-23 08:27:28 AM	    {"taxon.tab":53272} - eol-archive
 WoRMS_DH	Wednesday 2019-03-27 08:01:50 AM	{"taxon.tab":57539} - eol-archive
 
 WoRMS_DH	Tuesday 2019-03-26 11:00:40 AM	    {"taxon.tab":57539} - MacMini
+
+php update_resources/connectors/dwh_worms_TRAM_798.php
 */
+
+// $a1 = array('a','b', 'c');
+// $a2 = array('b','a','c');
+// $a3 = array_diff($a1, $a2);
+// print_r($a3);
+// $a3 = array_values($a3); //reindex key
+// print_r($a3);
+// exit("\n".count($a3)."\n");
+
+// $str = 'In 3My C3art : 11 22 items';
+// $str = 'Eualus suckleyi (Stimpson, 1864)';
+// $str = 'Halofolliculina annulata (Andrews, 1944) Hadzi, 1951';
+// $str = 'Cushmanidea3 grosjeani 2 (Keij, 1957) Ducasse & Vigneaux, 1961';
+// $str = "eli boy";
+// $int = (int) filter_var($str, FILTER_SANITIZE_NUMBER_INT);
+// echo("\n[$int]\n");
+// 
+// preg_match_all('!\d+!', $str, $matches);
+// // preg_match('!\d+!', $str, $matches);
+// print_r($matches);
+// exit;
+
+// include_once(dirname(__FILE__) . "/../../config/environment.php");
+// require_library('connectors/DWH_WoRMS_API');
+// $func = new DWH_WoRMS_API(1);
+// $sci = "Plagioecia (Gadus) dorsalis (Waters, 1879)";
+// $sci = "Paradoxostoma acuminatum Müller, 1894";
+// $sci = "Paradoxostoma acuminatum Weller, 1894";
+// $arr = $func->call_gnparser($sci);
+// print_r($arr);
+// echo "\n".@$arr[0]['details'][0]['infragenericEpithet']['value']."\n";
+// exit;
+
+// $int[0] = 1928;
+// $int[1] = 1909;
+// $int[2] = 1905;
+// print_r($int); asort($int); print_r($int);
+// $index = array_keys($int);
+// print_r($index);
+// exit;
+
+
+
 
 include_once(dirname(__FILE__) . "/../../config/environment.php");
 require_library('connectors/DWH_WoRMS_API');
