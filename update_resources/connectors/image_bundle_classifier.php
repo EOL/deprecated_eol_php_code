a316a004c665f5d3ee750567ccf323a0fbc592e8
eliagbayani <eli@eol.org>
2020-09-24 03:09:43 -0400

    DATA-1865 Image bundles for the next classifier

--- update_resources/connectors/image_bundle_classifier.php
+++ update_resources/connectors/image_bundle_classifier.php
@@ -2,14 +2,23 @@
 namespace php_active_record;
 /* Image bundles for the next classifier - DATA-1865
 estimated execution time:
+
+php5.6 image_bundle_classifier.php jenkins '{"task": "task_1"}'
+php5.6 image_bundle_classifier.php jenkins '{"task": "task_3a"}'
+
 */
 include_once(dirname(__FILE__) . "/../../config/environment.php");
 require_library('connectors/ImageBundleClassifierAPI');
 $timestart = time_elapsed();
 
+// print_r($argv);
+$params['jenkins_or_cron']   = @$argv[1]; //irrelevant here
+$params['json']              = @$argv[2]; //useful here
+$param = json_decode($params['json'], true);
+print_r($param);
+$task = $param['task'];
+
 $func = new ImageBundleClassifierAPI();
-
-$func->task_1_Herbarium_Sheets();
-$func->task_3a_Zoological_illustrations();
-
+if($task == 'task_1')       $func->task_1_Herbarium_Sheets();
+elseif($task == 'task_3a')  $func->task_3a_Zoological_illustrations();
 ?>
