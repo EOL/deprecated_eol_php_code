9ced0a7392ba7bdd336fc1fc4e751108877d61a1
eliagbayani <eli@eol.org>
2020-12-10 08:54:17 -0500

    DATA-1870 filter_term_group_by_taxa - WIP

--- lib/connectors/FilterTermGroupByTaxa.php
+++ lib/connectors/FilterTermGroupByTaxa.php
@@ -13,16 +13,9 @@ class FilterTermGroupByTaxa
         // $this->download_options = array('resource_id' => $resource_id, 'expire_seconds' => 60*60*24*30*3, 'download_wait_time' => 1000000, 'timeout' => 10800, 'download_attempts' => 1, 'delay_in_minutes' => 1);
     }
     /*================================================================= STARTS HERE ======================================================================*/
-    private function get_children_of_taxa_group($taxon_ids)
-    {
-        require_library('connectors/PaleoDBAPI_v2');
-        $func = new PaleoDBAPI_v2("");
-        $dwca_file = CONTENT_RESOURCE_LOCAL_PATH . $this->params['source'] . ".tar.gz"; //617_ENV.tar.gz
-        $descendant_taxon_ids = $func->get_descendants_given_parent_ids($dwca_file, $taxon_ids);
-        return $descendant_taxon_ids;
-    }
     function start($info)
     {
+        self::initialize();
         // print_r($this->params); exit("\n");
         /*Array(
             [source] => 617_ENV
@@ -32,28 +25,51 @@ class FilterTermGroupByTaxa
         e.g. taxonIDs is insects, spiders, amphibians
         */
         //----------------------------------------------------------------------------------------------
-        $this->children_of_IDs = array();
+        $this->children_of_TaxaGroup = array();
         $taxonIDs = explode(',', $this->params['taxonIDs']);
-        $children = self::get_children_of_taxa_group($taxonIDs); //e.g. $taxonIDs is insects = Q1390 | spiders = Q1357 | amphibians = Q10908
-        foreach($children as $child) $this->children_of_IDs[$child] = '';
+        $children = self::get_children_of_TaxaGroup($taxonIDs); //e.g. $taxonIDs is insects = Q1390 | spiders = Q1357 | amphibians = Q10908
+        foreach($children as $child) $this->children_of_TaxaGroup[$child] = '';
         unset($children);
-        // print_r($this->children_of_IDs);
-        echo "\nChildren of IDs: ".count($this->children_of_IDs)."\n"; exit;
+        // print_r($this->children_of_TaxaGroup);
+        // echo "\nChildren of IDs: ".count($this->children_of_TaxaGroup)."\n"; exit;
         //----------------------------------------------------------------------------------------------
         
         $tables = $info['harvester']->tables;
         
-        self::process_generic_table_for_Ostracoda($tables['http://rs.tdwg.org/dwc/terms/occurrence'][0], 'occurrence');
-        self::process_generic_table_for_Ostracoda($tables['http://rs.tdwg.org/dwc/terms/measurementorfact'][0], 'MoF');
+        self::process_generic_table_for_TaxaGroup($tables['http://rs.tdwg.org/dwc/terms/occurrence'][0], 'occurrence');
+        self::process_generic_table_for_TaxaGroup($tables['http://rs.tdwg.org/dwc/terms/measurementorfact'][0], 'MoF');
         
-        self::process_generic_table($tables['http://rs.tdwg.org/dwc/terms/taxon'][0], 'taxon');
-        self::process_generic_table($tables['http://rs.gbif.org/terms/1.0/vernacularname'][0], 'vernacular');
-        self::process_generic_table($tables['http://rs.tdwg.org/dwc/terms/occurrence'][0], 'occurrence');
-        self::process_generic_table($tables['http://rs.tdwg.org/dwc/terms/measurementorfact'][0], 'MoF');
+        // self::process_generic_table($tables['http://rs.tdwg.org/dwc/terms/taxon'][0], 'taxon');
+        // self::process_generic_table($tables['http://rs.gbif.org/terms/1.0/vernacularname'][0], 'vernacular');
+        // self::process_generic_table($tables['http://rs.tdwg.org/dwc/terms/occurrence'][0], 'occurrence');
+        // self::process_generic_table($tables['http://rs.tdwg.org/dwc/terms/measurementorfact'][0], 'MoF');
     }
-    private function process_generic_table_for_Ostracoda($meta, $what)
+    private function initialize()
+    {
+        require_library('connectors/Pensoft2EOLAPI');
+        $this->pensoft = new Pensoft2EOLAPI($param);
+        $this->descendants_of_saline_water = $this->pensoft->get_descendants_of_habitat_group('saline water');
+        /* e.g. Q1390, Q1357, and Q10908. i.e. no saltwater insects, spiders, or amphibians. */
+        // print_r($this->descendants_of_saline_water); exit;("\n");
+        /*Array(
+            [t.uri] => 
+            [http://purl.obolibrary.org/obo/ENVO_00002010] => 
+            [http://purl.obolibrary.org/obo/ENVO_00002227] => 
+            [http://purl.obolibrary.org/obo/ENVO_01000307] => 
+            [http://purl.obolibrary.org/obo/ENVO_00002019] => 
+        */
+    }
+    private function get_children_of_TaxaGroup($taxon_ids)
+    {
+        require_library('connectors/PaleoDBAPI_v2');
+        $func = new PaleoDBAPI_v2("");
+        $dwca_file = CONTENT_RESOURCE_LOCAL_PATH . $this->params['source'] . ".tar.gz"; //617_ENV.tar.gz
+        $descendant_taxon_ids = $func->get_descendants_given_parent_ids($dwca_file, $taxon_ids);
+        return $descendant_taxon_ids;
+    }
+    private function process_generic_table_for_TaxaGroup($meta, $what)
     {   //print_r($meta);
-        echo "\nprocess $what...Ostracoda\n"; $i = 0;
+        echo "\nprocess $what...TaxaGroup\n"; $i = 0;
         foreach(new FileIterator($meta->file_uri) as $line => $row) {
             $i++; if(($i % 100000) == 0) echo "\n".number_format($i);
             if($meta->ignore_header_lines && $i == 1) continue;
@@ -68,24 +84,37 @@ class FilterTermGroupByTaxa
             }
             // print_r($rec); exit;
             if($what == 'occurrence') {
-                if(isset($this->children_of_Ostracoda[$rec['http://rs.tdwg.org/dwc/terms/taxonID']])) {
-                    $this->occurrence_id_Ostracoda[$rec['http://rs.tdwg.org/dwc/terms/occurrenceID']] = '';
+                if(isset($this->children_of_TaxaGroup[$rec['http://rs.tdwg.org/dwc/terms/taxonID']])) {
+                    $this->occurrence_id_TaxaGroup[$rec['http://rs.tdwg.org/dwc/terms/occurrenceID']] = '';
                 }
             }
             elseif($what == 'MoF') {
-                if(isset($this->occurrence_id_Ostracoda[$rec['http://rs.tdwg.org/dwc/terms/occurrenceID']])) {
-                    /* per: https://eol-jira.bibalex.org/browse/DATA-1831?focusedCommentId=64595&page=com.atlassian.jira.plugin.system.issuetabpanels:comment-tabpanel#comment-64595
-                    For all descendants of Ostracoda, (FurtherInformationURL=https://paleobiodb.org/classic/checkTaxonInfo?is_real_user=1&taxon_no=22826)
+                if(isset($this->occurrence_id_TaxaGroup[$rec['http://rs.tdwg.org/dwc/terms/occurrenceID']])) {
+                    /* copied template from RemoveAvesChildrenAPI.php
+                    per: https://eol-jira.bibalex.org/browse/DATA-1831?focusedCommentId=64595&page=com.atlassian.jira.plugin.system.issuetabpanels:comment-tabpanel#comment-64595
+                    For all descendants of TaxaGroup, (FurtherInformationURL=https://paleobiodb.org/classic/checkTaxonInfo?is_real_user=1&taxon_no=22826)
                     please remove all records with measurementType= http://purl.obolibrary.org/obo/RO_0002303
-                    */
                     if($rec['http://rs.tdwg.org/dwc/terms/measurementType'] == 'http://purl.obolibrary.org/obo/RO_0002303') {
-                        $this->Ostracoda_remove_occurrence_id[$rec['http://rs.tdwg.org/dwc/terms/occurrenceID']] = '';
+                        $this->TaxaGroup_remove_occurrence_id[$rec['http://rs.tdwg.org/dwc/terms/occurrenceID']] = '';
+                    }
+                    */
+                    /* per: https://eol-jira.bibalex.org/browse/DATA-1870?focusedCommentId=65425&page=com.atlassian.jira.plugin.system.issuetabpanels:comment-tabpanel#comment-65425
+                    I'd like to filter out the "descendants_of_salt_water" terms for taxa that are descendants of these taxon IDs in the wikipedia traits resource:
+                    (I may think of more; I'll add them here)
+                    Q1390, Q1357, and Q10908
+                    i.e. no saltwater insects, spiders, or amphibians.
+                    */
+                    $mValue = $rec['http://rs.tdwg.org/dwc/terms/measurementValue'];
+                    $mType = $rec['http://rs.tdwg.org/dwc/terms/measurementType'];
+                    if($mType == 'http://purl.obolibrary.org/obo/RO_0002303') { //habitat
+                        if(isset($this->descendants_of_saline_water[$mValue])) {
+                            $this->TaxaGroup_remove_occurrence_id[$rec['http://rs.tdwg.org/dwc/terms/occurrenceID']] = '';
+                        }
                     }
                 }
             }
         }
     }
-
     private function process_generic_table($meta, $what)
     {   //print_r($meta);
         echo "\nprocess $what...\n"; $i = 0;
@@ -104,9 +133,9 @@ class FilterTermGroupByTaxa
             // print_r($rec); exit;
             /**/
             
+            /* copied template
             if($what == 'taxon') {
                 if(isset($this->children_of_Aves[$rec['http://rs.tdwg.org/dwc/terms/taxonID']])) continue;
-                // if(isset($this->children_of_Aves[$rec['http://rs.tdwg.org/dwc/terms/acceptedNameUsageID']])) continue; --- COMMMENT THIS - VERY WRONG.
                 if(isset($this->children_of_Aves[$rec['http://rs.tdwg.org/dwc/terms/parentNameUsageID']])) continue;
                 $o = new \eol_schema\Taxon();
             }
@@ -119,15 +148,28 @@ class FilterTermGroupByTaxa
                     $this->remove_occurrence_id[$rec['http://rs.tdwg.org/dwc/terms/occurrenceID']] = '';
                     continue;
                 }
-                if(isset($this->Ostracoda_remove_occurrence_id[$rec['http://rs.tdwg.org/dwc/terms/occurrenceID']])) continue;
+                if(isset($this->TaxaGroup_remove_occurrence_id[$rec['http://rs.tdwg.org/dwc/terms/occurrenceID']])) continue;
                 $o = new \eol_schema\Occurrence();
             }
             elseif($what == 'MoF') {
                 if(isset($this->remove_occurrence_id[$rec['http://rs.tdwg.org/dwc/terms/occurrenceID']])) continue;
-                if(isset($this->Ostracoda_remove_occurrence_id[$rec['http://rs.tdwg.org/dwc/terms/occurrenceID']])) continue;
+                if(isset($this->TaxaGroup_remove_occurrence_id[$rec['http://rs.tdwg.org/dwc/terms/occurrenceID']])) continue;
                 $o = new \eol_schema\MeasurementOrFact_specific();
             }
             else exit("\nInvestigate [$what]\n");
+            */
+            
+            if($this->params['target'] == 'wikipedia_en_traits_FTG') {
+                if($what == 'occurrence') {
+                    if(isset($this->TaxaGroup_remove_occurrence_id[$rec['http://rs.tdwg.org/dwc/terms/occurrenceID']])) continue;
+                    $o = new \eol_schema\Occurrence();
+                }
+                elseif($what == 'MoF') {
+                    if(isset($this->TaxaGroup_remove_occurrence_id[$rec['http://rs.tdwg.org/dwc/terms/occurrenceID']])) continue;
+                    $o = new \eol_schema\MeasurementOrFact_specific();
+                }
+                else exit("\nInvestigate [$what]\n");
+            }
             
             $uris = array_keys($rec);
             foreach($uris as $uri) {
