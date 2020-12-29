ba655de017a90d7faa98426f01807cefdc2951a0
eliagbayani <eli@eol.org>
2020-12-28 20:16:42 -0500

    stats

--- update_resources/connectors/nmnh_images.php
+++ update_resources/connectors/nmnh_images.php
@@ -5,6 +5,8 @@ nmnh_images	Thu 2020-12-24 05:28:07 AM	{"agent.tab":1001, "media_resource.tab":3
 nmnh_images	Thu 2020-12-24 06:05:58 AM	{"agent.tab":1001, "media_resource.tab":367537, "taxon.tab":63374, "time_elapsed":{"sec":509.55, "min":8.49, "hr":0.14}}
 nmnh_images	Thu 2020-12-24 06:14:34 AM	{"agent.tab":1001, "media_resource.tab":347040, "taxon.tab":60121, "time_elapsed":{"sec":492.24, "min":8.2, "hr":0.14}}
 nmnh_images	Sat 2020-12-26 07:00:04 AM	{"agent.tab":1001, "media_resource.tab":346792, "taxon.tab":60111, "time_elapsed":{"sec":509.26, "min":8.49, "hr":0.14}}
+Jenkins with API request and newly downloaded DwCA
+nmnh_images	Mon 2020-12-28 10:24:51 AM	{"agent.tab":1001, "media_resource.tab":346792, "taxon.tab":60111, "time_elapsed":{"sec":475.05, "min":7.92, "hr":0.13}}
 */
 include_once(dirname(__FILE__) . "/../../config/environment.php");
 require_library('connectors/NMNHimagesAPI');
