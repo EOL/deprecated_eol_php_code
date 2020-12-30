f8462cd909fd35b303949905a0a3fc970c16a5fa
eliagbayani <eli@eol.org>
2020-12-29 22:54:46 -0500

    ADW resource - WIP

--- lib/connectors/QuaardvarkAPI.php
+++ lib/connectors/QuaardvarkAPI.php
@@ -15,7 +15,15 @@ class QuaardvarkAPI
         $this->download_options = array('resource_id' => $folder, 'download_wait_time' => 1000000, 'timeout' => 172800, 'download_attempts' => 2, 'delay_in_minutes' => 1);
         $this->download_options["expire_seconds"] = false; //60*60*24*25;
         $this->debug = array();
-        $this->url['all species with habitat keywords'] = 'https://animaldiversity.ummz.umich.edu/quaardvark/search/1E268FDE-F3B2-0001-913C-B28812191D82/?start=';
+        $this->url['Habitat'] = 'https://animaldiversity.ummz.umich.edu/quaardvark/search/1E268FDE-F3B2-0001-913C-B28812191D82/?start=';
+        $this->url['Geographic Range'] = 'https://animaldiversity.ummz.umich.edu/quaardvark/search/1E269055-F07D-0001-79AC-D4E055D018F4/?start=';
+        $this->url['Physical Description'] = 'https://animaldiversity.ummz.umich.edu/quaardvark/search/1E26905C-DEFA-0001-C0BD-C94B291C77C0/?start=';
+
+        $this->field_count['Habitat'] = 9;
+        $this->field_count['Geographic Range'] = 0;
+        $this->field_count['Physical Description'] = 18;
+
+
         /*
         https://animaldiversity.ummz.umich.edu/quaardvark/search/1E268FDE-F3B2-0001-913C-B28812191D82/?start=1
         https://animaldiversity.ummz.umich.edu/quaardvark/search/1E268FF7-B855-0001-DDF1-12C01181A670/?start=201
@@ -34,17 +42,24 @@ class QuaardvarkAPI
             $sum = 1;
             for ($i = 1; $i <= $loops; $i++) {
                 echo "\n$i. $sum";
-                $url = $this->url['all species with habitat keywords'].$sum;
+                
+                $url = $this->url['Habitat'].$sum;
+                $url = $this->url['Geographic Range'].$sum;
+                $data = 'Geographic Range';
+                $data = 'Physical Description';
+                $url = $this->url[$data].$sum;
+                
+                
                 if($html = Functions::lookup_with_cache($url, $this->download_options)) { //hits="6369"
-                    $recs = self::parse_page($html);
+                    $recs = self::parse_page($html, $data);
                 }
                 $sum = $sum + 200;
-                // if($i >= 2) break; //debug only
+                if($i >= 2) break; //debug only
             }
             exit("\n-end-\n");
         }
     }
-    private function parse_page($html)
+    private function parse_page($html, $data)
     {
         $left = '<table xmlns:media="urn:animaldiversity.org:templates:media"';
         if(preg_match("/".preg_quote($left, '/')."(.*?)<\/table>/ims", $html, $a)) {
@@ -52,11 +67,11 @@ class QuaardvarkAPI
 
             $fields = array();
             if(preg_match_all("/<th>(.*?)<\/th>/ims", $main_block, $a1)) $fields = $a1[1];
-            if(count($fields) != 9) exit("\nInvestigate fields <th> tags\n");
+            print_r($fields);
+            if(count($fields) != $this->field_count[$data]) exit("\nInvestigate fields <th> tags: ".count($fields)."\n");
             
             $f = Functions::file_open($this->report, "w");
             fwrite($f, implode("\t", $fields)."\n");
-            
 
             if(preg_match_all("/<tr>(.*?)<\/tr>/ims", $main_block, $a2)) {
                 $rows201 = $a2[1];
@@ -64,21 +79,35 @@ class QuaardvarkAPI
                     $row = str_replace('<td type="sequence"/>', '<td type="sequence"></td>', $row);
                     $row = str_replace('<td type="text"/>', '<td type="text"></td>', $row);
                     
+                    $row = str_replace("<span/>", "<span></span>", $row);
+                    $row = str_replace("<td/>", "<td></td>", $row);
+                    
+                    
                     if(preg_match_all("/<td(.*?)<\/td>/ims", $row, $a3)) {
                         $cols = $a3[1];
                         // print_r($cols); exit; //good debug
                         $ret = array();
                         foreach($cols as $col) {
+                            
+                            // $col = str_replace("<span/>", "<span></span>", $col);
+                            // $col = str_replace("<td/>", "<td></td>", $col);
+                            
                             $tmp = strip_tags("<td".$col, "<span>");
                             if(preg_match_all("/<span>(.*?)<\/span>/ims", $tmp, $a4)) {
                                 $tmp = $a4[1];
-                                $tmp = implode(" | ", $tmp);
+                                
+                                $tmp = array_filter($tmp); //remove null arrays
+                                $tmp = array_unique($tmp); //make unique
+                                $tmp = array_values($tmp); //reindex key
+                                
+                                if($tmp) $tmp = implode(" | ", $tmp);
+                                else     $tmp = '';
                             }
                             $ret[] = $tmp;
                         }
                         $ret = array_map('trim', $ret);
-                        // print_r($ret); //exit; //good debug
-                        /*Array(
+                        // print_r($ret); exit; //good debug
+                        /*Array( Habitat
                             [0] => Alpheus heterochaelis
                             [1] => Malacostraca
                             [2] => Decapoda
@@ -97,8 +126,8 @@ class QuaardvarkAPI
                             }
                             $i++;
                         }
-                        // print_r($rek); //good debug
-                        /*Array(
+                        print_r($rek); //exit("\nsample record\n"); //good debug
+                        /*Array( Habitat
                             [Species] => Alpheus heterochaelis
                             [Class] => Malacostraca
                             [Order] => Decapoda
@@ -119,11 +148,9 @@ class QuaardvarkAPI
     }
     private function get_total_number_of_pages()
     {
-        $url = $this->url['all species with habitat keywords']."1";
+        $url = $this->url['Habitat']."1";
         if($html = Functions::lookup_with_cache($url, $this->download_options)) { //hits="6369"
-            if(preg_match("/hits=\"(.*?)\"/ims", $html, $a)) {
-                return $a[1];
-            }
+            if(preg_match("/hits=\"(.*?)\"/ims", $html, $a)) return $a[1];
             else exit("\nNo hits, investigate URL: [$url]\n");
         }
     }
