48813a0e43c25c8503888699e589ab75d71ae8fb
eliagbayani <eli@eol.org>
2021-01-11 10:25:06 -0500

    DATA-1875: recoding unrecognized fields - WIP

--- lib/connectors/RecodeUnrecognizedFieldsAPI.php
+++ lib/connectors/RecodeUnrecognizedFieldsAPI.php
@@ -17,28 +17,63 @@ class RecodeUnrecognizedFieldsAPI
     {
         if(!$dwca_file) $dwca_file = $this->dwca_file; //used if called elsewhere
         $paths = self::extract_dwca($dwca_file);
-        if(is_file($paths.'meta.xml')) self::parse_meta_xml($paths.'meta.xml');
+        if(is_file($paths['temp_dir'].'meta.xml')) self::parse_meta_xml($paths['temp_dir'].'meta.xml');
         else echo "\n- No meta.xml [$dwca_file]\n";
         
         // remove temp dir
+        /*
         recursive_rmdir($paths['temp_dir']);
         echo ("\n temporary directory removed: " . $paths['temp_dir']);
+        */
+    }
+    private function parse_meta_xml($meta_xml)
+    {
+        echo "\n$meta_xml\n";
+        $xml = simplexml_load_file($meta_xml);
+        $final = array();
+        foreach($xml->table as $tab) {
+            // print_r($tab);
+            /*SimpleXMLElement Object(
+                [@attributes] => Array(
+                        [encoding] => UTF-8
+                        [fieldsTerminatedBy] => \t
+                        [linesTerminatedBy] => \n
+                        [ignoreHeaderLines] => 1
+                        [rowType] => http://rs.tdwg.org/dwc/terms/Taxon
+                    )
+                [files] => SimpleXMLElement Object(
+                        [location] => taxon.tab
+                    )
+                [field] => Array(
+                        [0] => SimpleXMLElement Object(
+                                [@attributes] => Array(
+                                        [index] => 0
+                                        [term] => http://rs.tdwg.org/dwc/terms/taxonID
+                                    )
+                            )
+            */
+            $rowType = (string) $tab{'rowType'}; //echo "\n$rowType";
+            foreach($tab->field as $fld) { //echo "\n".$fld{'term'}."\n";
+                $final[$rowType][] = (string) $fld{'term'};
+            }
+        }
+        print_r($final);
     }
     private function extract_dwca($dwca_file)
     {
-        // /* un-comment in real operation
+        /* un-comment in real operation
         require_library('connectors/INBioAPI');
         $func = new INBioAPI();
         $paths = $func->extract_archive_file($dwca_file, "meta.xml", $this->download_options); //true 'expire_seconds' means it will re-download, will NOT use cache. Set TRUE when developing
-        print_r($paths); //exit("\n-exit muna-\n");
-        // */
-
-        /* development only
-        $paths = Array(
-            'archive_path' => '/Volumes/AKiTiO4/eol_php_code_tmp/dir_99613/',
-            'temp_dir' => '/Volumes/AKiTiO4/eol_php_code_tmp/dir_99613/'
-        );
+        print_r($paths); exit("\n-exit muna-\n");
         */
+
+        // /* development only
+        $paths = Array(
+            'archive_path' => '/Volumes/AKiTiO4/eol_php_code_tmp/dir_81560/',
+            'temp_dir' => '/Volumes/AKiTiO4/eol_php_code_tmp/dir_81560/'
+        );
+        // */
         return $paths;
     }
 }
