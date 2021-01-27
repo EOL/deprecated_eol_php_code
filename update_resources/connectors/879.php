9de2939b5621f6d5e48790b7b64b8585addadd2f
eliagbayani <eli@eol.org>
2021-01-27 10:05:51 -0500

    wikipedia for fungi - notes, stats, for mapping

--- update_resources/connectors/879.php
+++ update_resources/connectors/879.php
@@ -3,8 +3,7 @@ namespace php_active_record;
 /* DATA-1544 Structured data from wikipedia for fungi
 
 undefined needs mapping: as of Mar 14, 2018. Might need to ask Jen to update spreadsheet mapping used.
-Array
-(
+Array(
     [edibility: edible or can cause allergic reactions] => 
     [edibility: edible, but unpalatable or unknown] => 
     [edibility: edible or psychoactive] => 
@@ -14,8 +13,26 @@ Array
     [edibility: can cause allergic reactions or deadly] => 
 )
 
+undefined needs mapping: as of Jan 27, 2021. Might need to ask Jen to update spreadsheet mapping used.
+Array(
+    [edibility: psychoactive or '] => 
+    [edibility: inedible or psychoactive] => 
+    [edibility: edible or psychoactive] => 
+    [spore print is purple-black to olive] => 
+    [edibility: edible or can cause allergic reactions] => 
+    [cap is flat or offset] => 
+    [edibility: deadly or choice] => 
+    [edibility: inedible or poisonous] => 
+    [gills on hymenium or flat] => 
+    [cap is convex or offset] => 
+    [spore print is yellow to olive] => 
+    [hymenium is subdecurrent or adnate] => 
+)
+
 879	Wednesday 2018-03-14 12:30:56 AM	{"measurement_or_fact.tab":9875,"occurrence.tab":6250,"taxon.tab":882} - with measurementID
 879	Wednesday 2018-03-14 01:11:54 AM	{"measurement_or_fact.tab":6628,"occurrence.tab":6250,"taxon.tab":882} - with unique measurementID
+879	Tue 2021-01-26 08:27:03 PM	        {"measurement_or_fact.tab":5155, "occurrence.tab":4881, "taxon.tab":901, "time_elapsed":false}
+879	Tue 2021-01-26 09:02:19 PM	        {"measurement_or_fact.tab":6707, "occurrence.tab":6359, "taxon.tab":901, "time_elapsed":false}
 */
 include_once(dirname(__FILE__) . "/../../config/environment.php");
 require_library('connectors/WikipediaMycologicalAPI');
