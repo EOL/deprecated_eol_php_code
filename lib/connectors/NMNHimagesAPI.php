f8fe855527c09d86967b5a96c975f7e449ad7cdc
eliagbayani <eli@eol.org>
2021-01-30 20:45:46 -0500

    NMNH add filter 'label' to title field

--- lib/connectors/NMNHimagesAPI.php
+++ lib/connectors/NMNHimagesAPI.php
@@ -350,7 +350,7 @@ class NMNHimagesAPI
     }
     private function valid_record($title, $description, $source)
     {
-        $terms = array('Ledger', 'card', 'Barcode', 'documentation', 'Book', 'note', 'scanned paper', 'sheet');
+        $terms = array('Ledger', 'card', 'Barcode', 'documentation', 'Book', 'note', 'scanned paper', 'sheet', 'Label');
         foreach($terms as $term) {
             if(stripos($description, $term) !== false) return false; //string is found
             if(stripos($title, $term) !== false) return false; //string is found
