aa6a5e681ac91d91ee73e54e02755b31b6097db0
eliagbayani <eli@eol.org>
2021-01-31 06:27:40 -0500

    stats

--- update_resources/connectors/nmnh_images.php
+++ update_resources/connectors/nmnh_images.php
@@ -7,6 +7,10 @@ nmnh_images	Thu 2020-12-24 06:14:34 AM	{"agent.tab":1001, "media_resource.tab":3
 nmnh_images	Sat 2020-12-26 07:00:04 AM	{"agent.tab":1001, "media_resource.tab":346792, "taxon.tab":60111, "time_elapsed":{"sec":509.26, "min":8.49, "hr":0.14}}
 Jenkins with API request and newly downloaded DwCA
 nmnh_images	Mon 2020-12-28 10:24:51 AM	{"agent.tab":1001, "media_resource.tab":346792, "taxon.tab":60111, "time_elapsed":{"sec":475.05, "min":7.92, "hr":0.13}}
+nmnh_images	Fri 2021-01-15 02:09:47 PM	{"agent.tab":1265, "media_resource.tab":468818, "taxon.tab":77802, "time_elapsed":{"sec":1094.13, "min":18.24, "hr":0.3}}
+after adding a new filter for title field: 'Label'
+nmnh_images	Sat 2021-01-30 09:04:23 PM	{"agent.tab":1240, "media_resource.tab":379156, "taxon.tab":73469, "time_elapsed":{"sec":909.76, "min":15.16, "hr":0.25}}
+
 */
 include_once(dirname(__FILE__) . "/../../config/environment.php");
 require_library('connectors/NMNHimagesAPI');
