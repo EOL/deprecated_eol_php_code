f3105bc53459fbfe34384bcb90a48c1f3cd09171
eliagbayani <eli@eol.org>
2021-02-09 02:47:08 -0500

    TRAM-991 smasher DH2 - WIP

--- update_resources/connectors/smasher_process.php
+++ update_resources/connectors/smasher_process.php
@@ -12,9 +12,9 @@ $timestart = time_elapsed();
 
 // This is built using this google sheet: https://docs.google.com/spreadsheets/d/1mBgsVZi1hqcwz37ZZI_5mEHdlXS_RiXpP_CY3RnLzXA/edit#gid=0
 // and this Dropbox folder: https://www.dropbox.com/scl/fo/nn34xhcjcoxnuro65ryve/AACaBly_BUjkJDHMdHBr-hoka?dl=0&oref=e&r=AAdw397jN5KF6_UtynJjAYlSPhAGW7V4VHBVxs7tSv4Rrj0brXhqgzCKAOvkTbx0j-Tv6NI8eLsU1QHfg3o8VTy8r6GqE0jbHGXkn0S411EhTgS3hd1dQaIIILlsVFUJhpqGuD3Y1PYN_lApCOIQL5JiLCSQfQwvFe7KhsqHx3311A&sm=1
+/* TRAM-800
 $p["smasher"] = array("desc" => "Smasher Output file", "url" => "http://localhost/cp/dynamic_hierarchy/smasher/EOLDynamicHierarchyDraftAug2017/dwh_taxa.txt");
 $p["EHE"] = array("desc" => "EOL Hierarchy Entries (EHE)", "url" => "http://localhost/cp/dynamic_hierarchy/smasher/richness_and_names.tsv");
-
 $p["AMP"] = array("desc" => "Amphibia Genera & Species", "url" => "http://localhost/cp/dynamic_hierarchy/smasher/amphibia/amphibia.txt");
 $p["APH"] = array("desc" => "Aphid Species File", "url" => "http://localhost/cp/dynamic_hierarchy/smasher/dwca-aphid-v8.6/taxon.txt");
 $p["BLA"] = array("desc" => "Cockroach Species File", "url" => "http://localhost/cp/dynamic_hierarchy/smasher/dwca-blattodea-v8.8/taxon.txt");
@@ -25,10 +25,7 @@ $p["EET"] = array("desc" => "Earthworms", "url" => "http://localhost/cp/dynamic_
 $p["EMB"] = array("desc" => "Embioptera Species File", "url" => "http://localhost/cp/dynamic_hierarchy/smasher/dwca-embioptera-v8.6/taxon.txt");
 $p["gbif"] = array("desc" => "GBIF Backbone Taxonomy (original version)", "url" => "http://localhost/cp/dynamic_hierarchy/smasher/backbone-current/Taxon.tsv");
 $p["GRY"] = array("desc" => "Grylloblattodea Species File", "url" => "http://localhost/cp/dynamic_hierarchy/smasher/dwca-grylloblattodea-v1.4/taxon.txt");
-
-// $p["ictv"] = array("desc" => "ICTV Virus Taxonomy", "url" => "http://localhost/cp/dynamic_hierarchy/smasher/dwh_taxa_accepted.txt");
 $p["ictv"] = array("desc" => "ICTV Virus Taxonomy", "url" => "http://localhost/cp/dynamic_hierarchy/smasher/ictv-virustaxonomy-with-higherclassification/taxon.tab");
-
 $p["IOC"] = array("desc" => "IOC World Bird List with higherClassification", "url" => "http://localhost/cp/dynamic_hierarchy/smasher/ioc-birdlist-with-higherclassification/taxon.tab");
 $p["lhw"] = array("desc" => "World Checklist of Hornworts and Liverworts", "url" => "http://localhost/cp/dynamic_hierarchy/smasher/liverhornworts/liverhornworts.txt");
 $p["LYG"] = array("desc" => "Lygaeoidea Species File", "url" => "http://localhost/cp/dynamic_hierarchy/smasher/dwca-lygaeoidea-v1.0/taxon.txt");
@@ -47,6 +44,50 @@ $p["TPL"] = array("desc" => "The Plant List with literature", "url" => "http://l
 $p["trunk"] = array("desc" => "Dynamic Hierarchy Trunk 14 June 2017", "url" => "http://localhost/cp/dynamic_hierarchy/smasher/dynamichierarchytrunk14jun201720170615085118/taxon.txt");
 $p["WOR"] = array("desc" => "WoRMS: use original WoRMS2EOL file for this", "url" => "http://localhost/cp/WORMS/WoRMS2EoL/taxon.txt"); //will use downloaded Aug 16, 2017
 $p["ZOR"] = array("desc" => "Zoraptera Species File", "url" => "http://localhost/cp/dynamic_hierarchy/smasher/dwca-zoraptera-v1.4/taxon.txt");
+*/
+// /* TRAM-991
+$p["trunk"] = array("desc" => "EOL Dynamic Hierarchy Trunk", "url" => "http://localhost/cp/dynamic_hierarchy/smasher_TRAM991/dhtrunk27jan2021/taxon.txt");
+$p["ictv"] = array("desc" => "ICTV Virus Taxonomy", "url" => "http://localhost/cp/dynamic_hierarchy/smasher_TRAM991/ICTV-virus_taxonomy-with-higherClassification/taxon.tab");
+$p["dino"] = array("desc" => "EOL Dynamic Hierarchy Dinosauria Patch", "url" => "http://localhost/cp/dynamic_hierarchy/smasher_TRAM991/eoldinosauriapatch/taxa.txt");
+$p["IOC"] = array("desc" => "IOC World Bird List", "url" => "http://localhost/cp/dynamic_hierarchy/smasher_TRAM991/ioc-birdlist/taxon.tab");
+$p["MAM"] = array("desc" => "EOL Dynamic Hierarchy Mammals Patch", "url" => "http://localhost/cp/dynamic_hierarchy/smasher_TRAM991/eolmammalpatch/taxa.txt");
+$p["LIZ"] = array("desc" => "EOL Dynamic Hierarchy Lizards Patch", "url" => "http://localhost/cp/dynamic_hierarchy/smasher_TRAM991/eollizardspatch/EOLlizardPatch.txt");
+$p["ODO"] = array("desc" => "World Odonata List", "url" => "http://localhost/cp/dynamic_hierarchy/smasher_TRAM991/worldodonatalist/taxa.txt");
+$p["BOM"] = array("desc" => "A global checklist of the Bombycoidea", "url" => "http://localhost/cp/dynamic_hierarchy/smasher_TRAM991/kitchingetal2018/taxa.txt");
+$p["ERE"] = array("desc" => "EOL Dynamic Hierarchy Erebidae Patch", "url" => "http://localhost/cp/dynamic_hierarchy/smasher_TRAM991/eoldynamichierarchyerebidaepatch/taxon.txt");
+$p["COC"] = array("desc" => "EOL Dynamic Hierarchy Coccinelloidea Patch", "url" => "http://localhost/cp/dynamic_hierarchy/smasher_TRAM991/eolcoccinelloideapatch/taxa.txt");
+$p["VSP"] = array("desc" => "EOL Dynamic Hierarchy Vespoidea Patch", "url" => "http://localhost/cp/dynamic_hierarchy/smasher_TRAM991/eolvespoideapatch/taxa.txt");
+$p["ONY"] = array("desc" => "Oliveira et al. 2012 Onychophora Classification", "url" => "http://localhost/cp/dynamic_hierarchy/smasher_TRAM991/onychophora/taxa.txt");
+
+$p["ANN"] = array("desc" => "EOL Annelida Patch", "url" => "http://localhost/cp/dynamic_hierarchy/smasher_TRAM991/xxx/xxx.xxx");
+
+$p["TRI"] = array("desc" => "EOL Trilobites Patch", "url" => "http://localhost/cp/dynamic_hierarchy/smasher_TRAM991/xxx/xxx.xxx");
+
+Catalogue of Life Collembola	SPR	open data
+$p["xxx"] = array("desc" => "xxx", "url" => "http://localhost/cp/dynamic_hierarchy/smasher_TRAM991/xxx/xxx.xxx");
+
+ITIS extract	ITIS	open data
+$p["xxx"] = array("desc" => "xxx", "url" => "http://localhost/cp/dynamic_hierarchy/smasher_TRAM991/xxx/xxx.xxx");
+
+EOL Microbes Patch	MIP	open data
+$p["xxx"] = array("desc" => "xxx", "url" => "http://localhost/cp/dynamic_hierarchy/smasher_TRAM991/xxx/xxx.xxx");
+
+NCBI extract	NCBI	open data
+$p["xxx"] = array("desc" => "xxx", "url" => "http://localhost/cp/dynamic_hierarchy/smasher_TRAM991/xxx/xxx.xxx");
+
+WoRMS extract	WOR	open data
+$p["xxx"] = array("desc" => "xxx", "url" => "http://localhost/cp/dynamic_hierarchy/smasher_TRAM991/xxx/xxx.xxx");
+
+EOL Crustacea Patch	CRU	open data
+$p["xxx"] = array("desc" => "xxx", "url" => "http://localhost/cp/dynamic_hierarchy/smasher_TRAM991/xxx/xxx.xxx");
+
+EOL Mollusca Patch	MOL	open data
+$p["xxx"] = array("desc" => "xxx", "url" => "http://localhost/cp/dynamic_hierarchy/smasher_TRAM991/xxx/xxx.xxx");
+
+Catalogue of Life extract	COL	open data
+$p["xxx"] = array("desc" => "xxx", "url" => "http://localhost/cp/dynamic_hierarchy/smasher_TRAM991/xxx/xxx.xxx");
+// */
+
 
 
 // /* sample IOC process.txt  -> should get 18990
