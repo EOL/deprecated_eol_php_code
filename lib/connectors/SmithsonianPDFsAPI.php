d03766e372908bd5db4946a90206aae813001e2a
eliagbayani <eli@eol.org>
2021-03-30 20:41:15 -0400

    DATA-1877: file organization - WIP

--- lib/connectors/SmithsonianPDFsAPI.php
+++ lib/connectors/SmithsonianPDFsAPI.php
@@ -14,6 +14,10 @@ class SmithsonianPDFsAPI
         // https://repository.si.edu/handle/10088/5097/browse?rpp=20&sort_by=2&type=dateissued&offset=0&etal=-1&order=ASC
         // https://repository.si.edu/handle/10088/5097/browse?rpp=20&sort_by=2&type=dateissued&offset=20&etal=-1&order=ASC
         $this->web['PDFs per page'] = "https://repository.si.edu/handle/10088/5097/browse?rpp=20&sort_by=2&type=dateissued&offset=NUM_OFFSET&etal=-1&order=ASC";
+        $this->web['domain'] = 'https://repository.si.edu';
+
+        if(Functions::is_production()) $this->path['working_dir'] = '/extra/other_files/Smithsonian/epub/';
+        else                           $this->path['working_dir'] = '/Volumes/AKiTiO4/other_files/Smithsonian/epub/';
     }
     function start()
     {
@@ -27,18 +31,55 @@ class SmithsonianPDFsAPI
                     [url] => https://repository.si.edu//handle/10088/5349
                     [title] => Deep-sea Cerviniidae (Copepoda: Harpacticoida) from the Western Indian Ocean, collected with RV Anton Bruun in 1964)
         */
-        $i = -1
+        $i = -1;
         foreach($pdfs_info as $info) { $i++;
-            
+            self::process_a_pdf($info);
         }
     }
-    private function
+    private function process_a_pdf($info)
     {
-        /*
+        // print_r($info); exit;
+        /*Array(
+            [url] => https://repository.si.edu//handle/10088/5292
+            [title] => Recent ostracodes of the family Pontocyprididae chiefly from the Indian Ocean
+        )*/
+        $epub_info = self::get_epub_info($info['url']);
+        /*Array(
+            [filename] => SCtZ-0007.epub
+            [url] => https://repository.si.edu/bitstream/handle/10088/5292/SCtZ-0007.epub
+        )*/
+        $destination = $this->path['working_dir'].$epub_info['filename'];
+        $cmd = "wget -nc ".$epub_info['url']." -O $destination";
+        $cmd .= " 2>&1";
+        $json = shell_exec($cmd);
+        exit;
+    }
+    private function get_epub_info($url)
+    {   /*
         <div style="height: 80px;" class="file-link">
         <a href="/bitstream/handle/10088/5551/SCtZ-0081.epub?sequence=2&amp;isAllowed=y">View/<wbr xmlns:i18n="http://apache.org/cocoon/i18n/2.1" />Open</a>
         </div>
         */
+        if($html = Functions::lookup_with_cache($url, $this->download_options)) {
+            if(preg_match_all("/".preg_quote('class="file-link">', '/')."(.*?)<\/div>/ims", $html, $a)) {
+                // print_r($a[1]); exit;
+                foreach($a[1] as $line) {
+                    if(stripos($line, ".epub") !== false) { //string is found
+                        // exit("\n$line\n");
+                        /* <a href="/bitstream/handle/10088/5292/SCtZ-0007.epub?sequence=3&amp;isAllowed=y">View/<wbr xmlns:i18n="http://apache.org/cocoon/i18n/2.1" />Open</a> */
+                        if(preg_match("/href=\"(.*?)\"/ims", $line, $a)) {
+                            $tmp = $this->web['domain'].$a[1];
+                            $ret = array();
+                            $ret['filename'] = pathinfo($tmp, PATHINFO_FILENAME).".epub";
+                            $ret['url'] = pathinfo($tmp, PATHINFO_DIRNAME)."/".$ret['filename'];
+                            // print_r($ret); exit;
+                            return $ret;
+                        }
+                    }
+                }
+            }
+            
+        }
     }
     private function get_all_pdfs()
     {
@@ -64,7 +105,7 @@ class SmithsonianPDFsAPI
                     */
                     foreach($temp as $t) {
                         $rec = array();
-                        if(preg_match("/href=\"(.*?)\"/ims", $t, $a)) $rec['url'] = "https://repository.si.edu/".$a[1];
+                        if(preg_match("/href=\"(.*?)\"/ims", $t, $a)) $rec['url'] = $this->web['domain']."/".$a[1];
                         if(preg_match("/\">(.*?)<\/a>/ims", $t, $a)) $rec['title'] = $a[1];
                         $final[] = $rec;
                     }
