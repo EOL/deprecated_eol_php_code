b03965f4213821e9da63729d7429c8678bd10729
eliagbayani <eli@eol.org>
2021-05-26 10:39:09 -0400

    DATA-1881: recoding contributors with URIs - FishBase

--- lib/connectors/ContributorsMapAPI.php
+++ lib/connectors/ContributorsMapAPI.php
@@ -1,12 +1,16 @@
 <?php
 namespace php_active_record;
-/**/
+/* This is called from many places:
+- contributor_map.php --> mostly utility
+- Environments2EOLfinal.php --> class Environments2EOLfinal extends ContributorsMapAPI
+*/
 class ContributorsMapAPI
 {
     function __construct($resource_id = false)
     {
         $this->resource_id = $resource_id;
         $this->download_options = array('cache' => 1, 'download_wait_time' => 500000, 'timeout' => 10800, 'expire_seconds' => 60*60*1);
+        $this->FishBase_collaborators = 'https://www.fishbase.de/Collaborators/CollaboratorsTopicList.php';
     }
     private function initialize()
     {
@@ -42,6 +46,39 @@ class ContributorsMapAPI
         unlink($local);
         // print_r($final);
         return $final;
+    }
+    function get_collab_name_and_ID_from_FishBase()
+    {
+        $options = $this->download_options;
+        $options['expire_seconds'] = 60*60*24*30; //1 month
+        $html = Functions::lookup_with_cache($this->FishBase_collaborators, $options);
+        // echo "\n$html\n";
+        /* <a href="/collaborators/CollaboratorSummary.php?id=1713">Aarud, Thomas</a></td> */
+        
+        if(Functions::is_production()) $path = "/extra/other_files/contributor_mappings/";
+        else                           $path = "/Volumes/AKiTiO4/other_files/contributor_mappings/";
+        if(!is_dir($path)) mkdir($path);
+        $file = $path.'FishBase_contributors.tsv';
+        
+        $handle = fopen($file, "w");
+        fwrite($handle, implode("\t", array('Label','URI')) . "\n");
+        if(preg_match_all("/CollaboratorSummary.php\?id\=(.*?)<\/a>/ims", $html, $a)) {
+            // print_r($a[1]);
+            foreach($a[1] as $tmp) { //2290">Chae, Byung-Soo
+                $arr = explode('">', $tmp);
+                $id = $arr[0];
+                $label = $arr[1];
+                $tmp = explode(",", $label);
+                $tmp = array_map('trim', $tmp);
+                $label = trim(@$tmp[1]." ".$tmp[0]); //to make it "Eli E. Agbayani"
+                $uri = "https://www.fishbase.de/collaborators/CollaboratorSummary.php?id=$id";
+                if(!isset($final[$label])) {
+                    fwrite($handle, implode("\t", array($label,$uri)) . "\n");
+                    $final[$label] = '';
+                }
+            }
+        }
+        fclose($handle);
     }
 }
 ?>
