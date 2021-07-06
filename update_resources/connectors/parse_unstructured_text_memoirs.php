2e7175452ea735f54a52bb540f70cd2b298aee8d
eliagbayani <eli@eol.org>
2021-07-06 12:02:23 -0400

    DATA-1890: doc 1: 15423

--- update_resources/connectors/parse_unstructured_text_memoirs.php
+++ update_resources/connectors/parse_unstructured_text_memoirs.php
@@ -14,6 +14,12 @@ namespace php_active_record;
 removed 'Chin' in eol-geonames
 118935	Thu 2021-06-24 12:33:22 AM	    {                "media_resource.tab":1309,                   "taxon.tab":1308, "time_elapsed":{"sec":1.09, "min":0.02, "hr":0}}
 118935_ENV	Thu 2021-06-24 12:35:46 AM	{"MoF.tab":1447,                            "occur.tab":1447, "taxon.tab":1308, "time_elapsed":{"sec":83.93, "min":1.4, "hr":0.02}}
+must have a big script change:
+118935	Tue 2021-07-06 07:05:18 AM	    {                "media_resource.tab":782,                    "taxon.tab":781, "time_elapsed":{"sec":1.16, "min":0.02, "hr":0}}
+118935_ENV	Tue 2021-07-06 07:08:38 AM	{"MoF.tab":983,                             "occur.tab":983,  "taxon.tab":781, "time_elapsed":{"sec":140.04, "min":2.33, "hr":0.04}}
+brought back script change: correctly
+118935	Tue 2021-07-06 10:05:29 AM	    {               "media_resource.tab":1309,                    "taxon.tab":1308, "time_elapsed":{"sec":1.08, "min":0.02, "hr":0}}
+118935_ENV	Tue 2021-07-06 10:07:10 AM	{"MoF.tab":1479,                            "occur.tab":1479, "taxon.tab":1308, "time_elapsed":{"sec":41.18, "min":0.69, "hr":0.01}}
 ------------------------------------------------------------
 120081	Tue 2021-06-22 12:43:28 PM	    {               "media_resource.tab":95,                  "taxon.tab":95, "time_elapsed":{"sec":0.92, "min":0.02, "hr":0}}
 120081_ENV	Tue 2021-06-22 12:48:10 PM	{"MoF.tab":519, "media_resource.tab":95, "occur.tab":519, "taxon.tab":95, "time_elapsed":{"sec":280.95, "min":4.68, "hr":0.08}}
@@ -27,6 +33,8 @@ removed 'Chin' in eol-geonames
 remove traits in eol-geonames if inside literature reference
 120081	Fri 2021-06-25 08:13:25 AM	    {               "media_resource.tab":95,                  "taxon.tab":95, "time_elapsed":{"sec":2.23, "min":0.04, "hr":0}}
 120081_ENV	Fri 2021-06-25 08:15:48 AM	{"MoF.tab":523, "media_resource.tab":95, "occur.tab":523, "taxon.tab":95, "time_elapsed":{"sec":81.53, "min":1.36, "hr":0.02}}
+120081	Tue 2021-07-06 07:09:13 AM	    {               "media_resource.tab":95,                  "taxon.tab":95, "time_elapsed":{"sec":0.42, "min":0.01, "hr":0}}
+120081_ENV	Tue 2021-07-06 07:10:50 AM	{"MoF.tab":523, "media_resource.tab":95, "occur.tab":523, "taxon.tab":95, "time_elapsed":{"sec":37.72, "min":0.63, "hr":0.01}}
 ------------------------------------------------------------
 120082	Thu 2021-06-24 11:27:32 AM	    {                                       "media_resource.tab":25,                               "taxon.tab":25, "time_elapsed":{"sec":0.37, "min":0.01, "hr":0}}
 120082_ENV	Thu 2021-06-24 11:31:27 AM	{"measurement_or_fact_specific.tab":92, "media_resource.tab":25, "occurrence_specific.tab":92, "taxon.tab":25, "time_elapsed":{"sec":175.03, "min":2.92, "hr":0.05}}
@@ -38,6 +46,8 @@ remove traits in eol-geonames if inside literature reference
 120082_ENV	Fri 2021-06-25 08:00:49 AM	{"measurement_or_fact_specific.tab":61, "media_resource.tab":25, "occurrence_specific.tab":61, "taxon.tab":25, "time_elapsed":{"sec":28.45, "min":0.47, "hr":0.01}}
 120082	Mon 2021-06-28 07:59:54 AM	    {                                       "media_resource.tab":25,                               "taxon.tab":25, "time_elapsed":{"sec":0.52, "min":0.01, "hr":0}}
 120082_ENV	Mon 2021-06-28 08:01:23 AM	{"measurement_or_fact_specific.tab":61, "media_resource.tab":25, "occurrence_specific.tab":61, "taxon.tab":25, "time_elapsed":{"sec":27.93, "min":0.47, "hr":0.01}}
+120082	Tue 2021-07-06 07:11:15 AM	    {                                       "media_resource.tab":25,                               "taxon.tab":25, "time_elapsed":{"sec":0.38, "min":0.01, "hr":0}}
+120082_ENV	Tue 2021-07-06 07:12:49 AM	{"measurement_or_fact_specific.tab":61, "media_resource.tab":25, "occurrence_specific.tab":61, "taxon.tab":25, "time_elapsed":{"sec":33.79, "min":0.56, "hr":0.01}}
 ------------------------------------------------------------
 118986	Tue 2021-06-29 11:36:55 PM	    {                                        "media_resource.tab":41,                                "taxon.tab":41, "time_elapsed":{"sec":0.4, "min":0.01, "hr":0}}
 118986_ENV	Tue 2021-06-29 11:48:52 PM	{"measurement_or_fact_specific.tab":512, "media_resource.tab":41, "occurrence_specific.tab":512, "taxon.tab":41, "time_elapsed":{"sec":657.64, "min":10.96, "hr":0.18}}
@@ -45,14 +55,29 @@ remove traits in eol-geonames if inside literature reference
 118986_ENV	Wed 2021-06-30 01:36:21 AM	{"measurement_or_fact_specific.tab":512, "media_resource.tab":41, "occurrence_specific.tab":512, "taxon.tab":41, "time_elapsed":{"sec":148.16, "min":2.47, "hr":0.04}}
 118986	Thu 2021-07-01 01:25:48 AM	    {                                        "media_resource.tab":41,                                "taxon.tab":41, "time_elapsed":{"sec":0.58, "min":0.01, "hr":0}}
 118986_ENV	Thu 2021-07-01 01:27:42 AM	{"measurement_or_fact_specific.tab":512, "media_resource.tab":41, "occurrence_specific.tab":512, "taxon.tab":41, "time_elapsed":{"sec":54.05, "min":0.9, "hr":0.02}}
+118986	Tue 2021-07-06 07:13:09 AM	    {                                        "media_resource.tab":41,                                "taxon.tab":41, "time_elapsed":{"sec":0.42, "min":0.01, "hr":0}}
+118986_ENV	Tue 2021-07-06 07:24:29 AM	{"measurement_or_fact_specific.tab":511, "media_resource.tab":41, "occurrence_specific.tab":511, "taxon.tab":41, "time_elapsed":{"sec":619.78, "min":10.33, "hr":0.17}}
 ------------------------------------------------------------
 118920	Wed 2021-06-30 07:48:29 AM	    {                                       "media_resource.tab":27,                               "taxon.tab":27, "time_elapsed":{"sec":0.36, "min":0.01, "hr":0}}
 118920_ENV	Wed 2021-06-30 07:52:50 AM	{"measurement_or_fact_specific.tab":74, "media_resource.tab":27, "occurrence_specific.tab":74, "taxon.tab":27, "time_elapsed":{"sec":200.19, "min":3.34, "hr":0.06}}
+118920	Tue 2021-07-06 07:24:48 AM	    {                                       "media_resource.tab":27,                               "taxon.tab":27, "time_elapsed":{"sec":0.36, "min":0.01, "hr":0}}
+118920_ENV	Tue 2021-07-06 07:26:23 AM	{"measurement_or_fact_specific.tab":74, "media_resource.tab":27, "occurrence_specific.tab":74, "taxon.tab":27, "time_elapsed":{"sec":35.55, "min":0.59, "hr":0.01}}
 ------------------------------------------------------------
 120083	Mon 2021-07-05 06:48:23 AM	    {                                        "media_resource.tab":383,                                "taxon.tab":294, "time_elapsed":{"sec":4.32, "min":0.07, "hr":0}}
 120083_ENV	Mon 2021-07-05 07:02:36 AM	{"measurement_or_fact_specific.tab":752, "media_resource.tab":190, "occurrence_specific.tab":752, "taxon.tab":294, "time_elapsed":{"sec":793.29, "min":13.22, "hr":0.22}}
+120083	Tue 2021-07-06 02:50:01 AM	    {                                        "media_resource.tab":379,                                "taxon.tab":294, "time_elapsed":{"sec":5.62, "min":0.09, "hr":0}}
+120083_ENV	Tue 2021-07-06 02:56:09 AM	{"measurement_or_fact_specific.tab":752, "media_resource.tab":186, "occurrence_specific.tab":752, "taxon.tab":294, "time_elapsed":{"sec":305.1, "min":5.09, "hr":0.08}}
+120083	Tue 2021-07-06 07:27:16 AM	    {                                        "media_resource.tab":379,                                "taxon.tab":294, "time_elapsed":{"sec":0.57, "min":0.01, "hr":0}}
+120083_ENV	Tue 2021-07-06 07:28:48 AM	{"measurement_or_fact_specific.tab":752, "media_resource.tab":186, "occurrence_specific.tab":752, "taxon.tab":294, "time_elapsed":{"sec":32.02, "min":0.53, "hr":0.01}}
 ------------------------------------------------------------
+MoftheAES	Tue 2021-07-06 03:12:33 AM	{                                        "media_resource.tab":1349,                                "taxon.tab":1263, "time_elapsed":{"sec":26.05, "min":0.43, "hr":0.01}}
+MoftheAES_ENV	Tue 2021-07-06 03:57:26 {"measurement_or_fact_specific.tab":2915,"media_resource.tab":374, "occurrence_specific.tab":2915, "taxon.tab":1263, "time_elapsed":{"sec":2631.55, "min":43.86, "hr":0.73}}
+MoftheAES	Tue 2021-07-06 07:28:58 AM	{                                        "media_resource.tab":1349,                                "taxon.tab":1263, "time_elapsed":{"sec":1.29, "min":0.02, "hr":0}}
+MoftheAES_ENV	Tue 2021-07-06 07:31:46 {"measurement_or_fact_specific.tab":2915,"media_resource.tab":374, "occurrence_specific.tab":2915, "taxon.tab":1263, "time_elapsed":{"sec":108.13, "min":1.8, "hr":0.03}}
 
+OLD mof 3369    media 374   taxon 1790
+NEW mof 2904    media 374   taxon 1263
+------------------------------------------------------------
 php5.6 parse_unstructured_text_memoirs.php jenkins '{"resource_id": "118935", "resource_name":"1st doc"}'
 php5.6 parse_unstructured_text_memoirs.php jenkins '{"resource_id": "120081", "resource_name":"2nd doc"}'
 
@@ -63,6 +88,9 @@ parse_unstructured_text_memoirs.php _ '{"resource_id": "118986", "resource_name"
 parse_unstructured_text_memoirs.php _ '{"resource_id": "118920", "resource_name":"6th doc"}'
 parse_unstructured_text_memoirs.php _ '{"resource_id": "120083", "resource_name":"7th doc"}'
 parse_unstructured_text_memoirs.php _ '{"resource_id": "118237", "resource_name":"8th doc"}' skipped
+=== START BHL RESOURCES ===
+parse_unstructured_text_memoirs.php _ '{"resource_id": "15423", "resource_name":"1st doc"}'
+
 */
 include_once(dirname(__FILE__) . "/../../config/environment.php");
 // $GLOBALS["ENV_DEBUG"] = true;
@@ -74,6 +102,16 @@ $params['jenkins_or_cron'] = @$argv[1]; //not needed here
 $param                     = json_decode(@$argv[2], true);
 $pdf_id = $param['resource_id'];
 $func = new ParseUnstructuredTextAPI_Memoirs();
+/*
+$str = "1. Geothallus tuberosus Campb. Bot. Gaz. 21: 13. 1896.";
+$words = explode(" ", $str); print_r($words);
+if(is_numeric($words[0])) {
+    array_shift($words); //remove first element
+    print_r($words); //exit;
+    $str = implode(" ", $words);
+    exit("\n$str\n");
+}
+*/
 /*
 $row = "EZRA TOWNSEND CRESSON 2J";
 $tmp = str_replace(array(" ",".",","), "", $row);
@@ -102,6 +140,8 @@ $rec[118237] = array('filename' => '118237.txt', 'lines_before_and_after_sciname
 /* TO DO: 
 doc 5: didn't get a valid binomial: "Laccophilus spergatus Sharp (Figs. 98-105, 297)"
 */
+// === START BHL RESOURCES ===
+$rec[15423] = array('filename' => '15423.txt', 'lines_before_and_after_sciname' => 2, 'doc' => 'BHL'); /*2 blocks: xx    Raw scinames: xx */
 
 /*--------------------------------------------------------------------------------------------------------------*/
 if($val = @$rec[$pdf_id]) $input = $val;
@@ -109,10 +149,11 @@ else exit("\nUndefined PDF ID\n");
 /* ---------------------------------- List-type here:
 // variable lines_before_and_after_sciname is important. It is the lines before and after the "list header".
 ---------------------------------- */
-
 $pdf_id = pathinfo($input['filename'], PATHINFO_FILENAME);
-if(Functions::is_production()) $input['epub_output_txts_dir'] = '/extra/other_files/Smithsonian/MoftheAES/'.$pdf_id.'/';
-else                           $input['epub_output_txts_dir'] = '/Volumes/AKiTiO4/other_files/Smithsonian/MoftheAES/'.$pdf_id.'/';
+if($val = @$input['doc']) $doc = $val;
+else                      $doc = "MoftheAES";
+if(Functions::is_production()) $input['epub_output_txts_dir'] = '/extra/other_files/Smithsonian/'.$doc.'/'.$pdf_id.'/';
+else                           $input['epub_output_txts_dir'] = '/Volumes/AKiTiO4/other_files/Smithsonian/'.$doc.'/'.$pdf_id.'/';
 // /*
 $folder = $input['epub_output_txts_dir'];
 if(!is_dir($folder)) mkdir($folder);
