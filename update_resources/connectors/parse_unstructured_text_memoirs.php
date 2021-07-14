e5a31ad20aa10ab3f65e94e40f60c6d00e974eb0
eliagbayani <eli@eol.org>
2021-07-14 11:22:36 -0400

    stats

--- update_resources/connectors/parse_unstructured_text_memoirs.php
+++ update_resources/connectors/parse_unstructured_text_memoirs.php
@@ -206,11 +206,12 @@ doc 5: didn't get a valid binomial: "Laccophilus spergatus Sharp (Figs. 98-105, 
 if($resource_name == 'MotAES') {
     $rec[$pdf_id] = array('filename' => $pdf_id.'.txt', 'lines_before_and_after_sciname' => 1); //1st client here is 27822
     /*
-    27822 --- blocks: 107   Raw scinames: 175 
+    27822 --- blocks: 124|107   Raw scinames: 159|175 
     30353 --- blocks: 2   Raw scinames: 26 (skipped)
+    30354 --- blocks: 81   Raw scinames: 165
     */
 }
-$rec['30355'] = array('filename' => '30355.txt', 'lines_before_and_after_sciname' => 1); /* blocks: 2610   Raw scinames: 2641 */
+$rec['30355'] = array('filename' => '30355.txt', 'lines_before_and_after_sciname' => 1); /* blocks: 2611   Raw scinames: 2641 */
 
 // === START BHL RESOURCES ===
 $rec[15423] = array('filename' => '15423.txt', 'lines_before_and_after_sciname' => 1, 'doc' => 'BHL'); /*1 blocks: 73|75    Raw scinames: 96|96 */
