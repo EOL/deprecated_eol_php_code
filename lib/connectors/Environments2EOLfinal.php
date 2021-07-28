453ec94216c90d18a700ed852f22b8ee5bbc1b0b
eliagbayani <eli@eol.org>
2021-07-28 07:46:39 -0400

    DATA-1887 119520 119188 and for general docs

--- lib/connectors/Environments2EOLfinal.php
+++ lib/connectors/Environments2EOLfinal.php
@@ -94,7 +94,7 @@ class Environments2EOLfinal extends ContributorsMapAPI
 
         if(in_array($this->resource_id, array('10088_5097_ENV', '10088_6943_ENV', '118935_ENV', '120081_ENV', '120082_ENV', '118986_ENV', 
             '118920_ENV', '120083_ENV', '118237_ENV', 'MoftheAES_ENV', '30355_ENV', "27822_ENV", "30354_ENV", "119035_ENV", "118946_ENV", "118936_ENV", "118950_ENV", "120602_ENV", 
-            "119187_ENV", "118978_ENV", "118941_ENV",
+            "119187_ENV", "118978_ENV", "118941_ENV", "119520_ENV", "119188_ENV",
             '15423_ENV', '91155_ENV'))
                 || stripos($this->resource_id, "SCtZ-") !== false
                 || stripos($this->resource_id, "scb-") !== false
@@ -447,7 +447,7 @@ class Environments2EOLfinal extends ContributorsMapAPI
                 // /*
                 if(in_array($this->resource_id, array('10088_5097_ENV', '10088_6943_ENV', '118935_ENV', '120081_ENV', '120082_ENV', '118986_ENV', '118920_ENV', '120083_ENV', 
                     '118237_ENV', 'MoftheAES_ENV', '30355_ENV', "27822_ENV", "30354_ENV", "119035_ENV", "118946_ENV", "118936_ENV", "118950_ENV", "120602_ENV", 
-                    "119187_ENV", "118978_ENV", "118941_ENV",
+                    "119187_ENV", "118978_ENV", "118941_ENV", "119520_ENV", "119188_ENV",
                     '15423_ENV', '91155_ENV'))
                         || stripos($this->resource_id, "SCtZ-") !== false
                         || stripos($this->resource_id, "scb-") !== false
--- lib/connectors/Pensoft2EOLAPI.php
+++ lib/connectors/Pensoft2EOLAPI.php
@@ -191,7 +191,7 @@ class Pensoft2EOLAPI
         elseif($this->param['resource_id'] == '21_ENV') $excluded_rowtypes = array(); //AmphibiaWeb text -> doesn't create a new DwCA
         if(in_array($this->param['resource_id'], array("10088_5097_ENV", "10088_6943_ENV", "118935_ENV", "120081_ENV", "120082_ENV", "118986_ENV", "118920_ENV", "120083_ENV", 
             "118237_ENV", "MoftheAES_ENV", "30355_ENV", "27822_ENV", "30354_ENV", "119035_ENV", "118946_ENV", "118936_ENV", "118950_ENV",
-            "120602_ENV", "119187_ENV", "118978_ENV", "118941_ENV",
+            "120602_ENV", "119187_ENV", "118978_ENV", "118941_ENV", "119520_ENV", "119188_ENV",
             "15423_ENV", "91155_ENV"))) $excluded_rowtypes = array('http://eol.org/schema/media/document');
         if(stripos($this->param['resource_id'], "SCtZ-") !== false) $excluded_rowtypes = array('http://eol.org/schema/media/document'); //string is found
         elseif(stripos($this->param['resource_id'], "scb-") !== false)  $excluded_rowtypes = array('http://eol.org/schema/media/document'); //string is found
@@ -386,7 +386,7 @@ class Pensoft2EOLAPI
                 elseif(in_array($this->param['resource_id'], array("118935_ENV")))     $this->ontologies = "eol-geonames";
                 if(in_array($this->param['resource_id'], array("120081_ENV", "120082_ENV", "118986_ENV", "118920_ENV", "120083_ENV", 
                     "118237_ENV", "MoftheAES_ENV", "30355_ENV", "27822_ENV", "30354_ENV", "119035_ENV", "118946_ENV", "118936_ENV", "118950_ENV",
-                    "120602_ENV", "119187_ENV", "118978_ENV", "118941_ENV",
+                    "120602_ENV", "119187_ENV", "118978_ENV", "118941_ENV", "119520_ENV", "119188_ENV",
                     "15423_ENV", "91155_ENV"))) $this->ontologies = "envo,eol-geonames";
                 // ---------------------- end customize ----------------------*/
                 
@@ -691,7 +691,7 @@ class Pensoft2EOLAPI
             // for all resources of: Memoirs of the American Entomological Society
             if(in_array($this->param['resource_id'], array("118935_ENV", "120081_ENV", "120082_ENV", "118986_ENV", "118920_ENV", 
                     "120083_ENV", "118237_ENV", "MoftheAES_ENV", "30355_ENV", "27822_ENV", "30354_ENV", "119035_ENV", "118946_ENV", "118936_ENV", "118950_ENV",
-                    "120602_ENV", "119187_ENV", "118978_ENV", "118941_ENV"))) {
+                    "120602_ENV", "119187_ENV", "118978_ENV", "118941_ENV", "119520_ENV", "119188_ENV"))) {
                 if($rek['id'] == 'http://purl.obolibrary.org/obo/ENVO_00000447') continue; //remove 'ocean' Per Jen: https://eol-jira.bibalex.org/browse/DATA-1887?focusedCommentId=66228&page=com.atlassian.jira.plugin.system.issuetabpanels:comment-tabpanel#comment-66228
             }
             // */
