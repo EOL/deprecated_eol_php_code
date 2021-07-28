eb2082b5ae5f00fdb774b081f5a55dee34b460d8
eliagbayani <eli@eol.org>
2021-07-28 09:35:14 -0400

    DATA-1887 GNRD sees multiple taxa

--- lib/connectors/ParseListTypeAPI_Memoirs.php
+++ lib/connectors/ParseListTypeAPI_Memoirs.php
@@ -597,6 +597,10 @@ class ParseListTypeAPI_Memoirs
         if($this->pdf_id == '119520') {
             if($sciname_line == 'Hypolimnas (Hypolimnas) salmicis (Drury)') return 'Hypolimnas salmicis'; //GNRD incorrectly outputs wrong name
         }
+        if($this->pdf_id == '119188') {
+            if($sciname_line == 'Anoplius (Anoplius) toluco (Cameron)') return 'Anoplius toluco'; //GNRD incorrectly outputs wrong name
+            $sciname_line = str_ireplace("{Hesperopompilus)", "(Hesperopompilus)", $sciname_line);
+        }
         // */
         
         // /* Tiphia (Tiphia) intermedia Malloch
