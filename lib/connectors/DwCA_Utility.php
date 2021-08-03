79309b7f1995b69378e46b002f826873167f7f53
eliagbayani <eli@eol.org>
2021-08-03 09:02:49 -0400

    Pensoft mtce

--- lib/connectors/DwCA_Utility.php
+++ lib/connectors/DwCA_Utility.php
@@ -125,7 +125,7 @@ class DwCA_Utility
                                                   "118935_ENV", "120081_ENV", "120082_ENV", "118986_ENV", "118920_ENV", "120083_ENV", "118237_ENV",
                                     "MoftheAES_ENV", "30355_ENV", "27822_ENV", "30354_ENV", "119035_ENV", "118946_ENV", "118936_ENV", "118950_ENV", "120602_ENV", 
                                     "119187_ENV", "118978_ENV", "118941_ENV", "119520_ENV", "119188_ENV",
-                                    "15423_ENV", "91155_ENV")) || $this->params['resource'] == 'all_BHL') {
+                                    "15423_ENV", "91155_ENV")) || @$this->params['resource'] == 'all_BHL') {
             if(Functions::is_production()) $info = self::start(false, array("timeout" => 172800, 'expire_seconds' => 0)); //expires now
             else                           $info = self::start(false, array("timeout" => 172800, 'expire_seconds' => 60*60*1)); //1 hour expire
         }
@@ -183,8 +183,8 @@ class DwCA_Utility
                 if(in_array($this->resource_id, array("10088_5097_ENV", "10088_6943_ENV", "118935_ENV", "120081_ENV", "120082_ENV", "118986_ENV", "118920_ENV", "120083_ENV", 
                     "118237_ENV", "MoftheAES_ENV", "30355_ENV", "27822_ENV", "30354_ENV", "119035_ENV", "118946_ENV", "118936_ENV", "118950_ENV", "120602_ENV", 
                     "119187_ENV", "118978_ENV", "118941_ENV", "119520_ENV", "119188_ENV",
-                    "15423_ENV", "91155_ENV") || $this->params['resource'] == 'all_BHL') && $row_type == "http://rs.tdwg.org/dwc/terms/occurrence") {
-                    self::process_fields($harvester->process_row_type($row_type), 'occurrence_specific');
+                    "15423_ENV", "91155_ENV")) || @$this->params['resource'] == 'all_BHL') {
+                    if($row_type == "http://rs.tdwg.org/dwc/terms/occurrence") self::process_fields($harvester->process_row_type($row_type), 'occurrence_specific');
                 }
                 elseif($annotateYes && $row_type == "http://rs.tdwg.org/dwc/terms/occurrence") {
                     self::process_fields($harvester->process_row_type($row_type), 'occurrence_specific');
@@ -316,7 +316,7 @@ class DwCA_Utility
         if(in_array($this->resource_id, array("21_ENV", "617_ENV", "26_ENV", "10088_5097_ENV", "10088_6943_ENV", "118935_ENV", "120081_ENV", "120082_ENV", "118986_ENV", "118920_ENV", "120083_ENV", 
             "118237_ENV", "MoftheAES_ENV", "30355_ENV", "27822_ENV", "30354_ENV", "119035_ENV", "118946_ENV", "118936_ENV", "118950_ENV", "120602_ENV", 
             "119187_ENV", "118978_ENV", "118941_ENV", "119520_ENV", "119188_ENV",
-            "15423_ENV", "91155_ENV")) || $annotateYes || $this->params['resource'] == 'all_BHL') { //first 2 clients: Amphibiaweb, Wikipedia EN
+            "15423_ENV", "91155_ENV")) || $annotateYes || @$this->params['resource'] == 'all_BHL') { //first 2 clients: Amphibiaweb, Wikipedia EN
             echo "\nGoes here really: [$this->resource_id]\n";
             require_library('connectors/ContributorsMapAPI');
             require_library('connectors/Environments2EOLfinal');
