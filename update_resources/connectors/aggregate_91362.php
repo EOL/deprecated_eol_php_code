6e262899ddcba0a5713671476d5e3fa391f76c21
eliagbayani <eli@eol.org>
2021-08-09 05:17:48 -0400

    mtce

--- update_resources/connectors/aggregate_91362.php
+++ update_resources/connectors/aggregate_91362.php
@@ -5,7 +5,7 @@ namespace php_active_record;
 include_once(dirname(__FILE__) . "/../../config/environment.php");
 $timestart = time_elapsed();
 require_library('connectors/DwCA_Aggregator');
-$resource_id = 'MoftheAES_resources';
+$resource_id = '91362_resource';
 $func = new DwCA_Aggregator($resource_id, false, 'regular');
 $resource_ids = array("91362", "91362_species");
 $func->combine_MoftheAES_DwCAs($resource_ids);
