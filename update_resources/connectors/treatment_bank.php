096307073c3e3a7598f6e3b2179bac352acf9c19
eliagbayani <eli@eol.org>
2021-09-07 02:44:07 -0400

    notes

--- update_resources/connectors/treatment_bank.php
+++ update_resources/connectors/treatment_bank.php
@@ -2,24 +2,31 @@
 namespace php_active_record;
 /* DATA-1896: TreatmentBank
 as of Sep 1, 2021 = 611,618 items
-
+STEP 1:
 php update_resources/connectors/treatment_bank.php _ '{"range_from": "1", "range_to":"100000"}'
 php update_resources/connectors/treatment_bank.php _ '{"range_from": "100000", "range_to":"200000"}'
-php update_resources/connectors/treatment_bank.php _ '{"task": "build_up_dwca_list"}'
--> generates /resources/reports/Plazi_DwCA_list.txt
+{total of 6 ranges, see jenkins}
 
-php update_resources/connectors/treatment_bank.php _ '{"task": "generate_single_dwca"}'
--> generates TreatmentBank.tar.gz
-
-php update_resources/connectors/environments_2_eol.php _ '{"task": "generate_eol_tags_pensoft", "resource":"all_BHL", "resource_id":"TreatmentBank", "subjects":"Uses"}'
--> generates TreatmentBank_ENV.tar.gz
-
+6 ranges in jenkins:
 php5.6 treatment_bank.php jenkins '{"range_from": "1", "range_to":"100000"}'
 php5.6 treatment_bank.php jenkins '{"range_from": "100000", "range_to":"200000"}'
 php5.6 treatment_bank.php jenkins '{"range_from": "200000", "range_to":"300000"}'
 php5.6 treatment_bank.php jenkins '{"range_from": "300000", "range_to":"400000"}'
 php5.6 treatment_bank.php jenkins '{"range_from": "400000", "range_to":"500000"}'
 php5.6 treatment_bank.php jenkins '{"range_from": "500000", "range_to":"612000"}'
+
+STEP 2:
+php update_resources/connectors/treatment_bank.php _ '{"task": "build_up_dwca_list"}'
+-> generates /resources/reports/Plazi_DwCA_list.txt
+
+STEP 3:
+php update_resources/connectors/treatment_bank.php _ '{"task": "generate_single_dwca"}'
+-> generates TreatmentBank.tar.gz
+
+STEP 4:
+php update_resources/connectors/environments_2_eol.php _ '{"task": "generate_eol_tags_pensoft", "resource":"all_BHL", "resource_id":"TreatmentBank", "subjects":"Uses"}'
+-> generates TreatmentBank_ENV.tar.gz
+-> last step
 */
 include_once(dirname(__FILE__) . "/../../config/environment.php");
 require_library('connectors/TreatmentBankAPI');
