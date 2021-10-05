16239ee8a8dbe29facd923cf3c0993c211dd5316
eliagbayani <eli@eol.org>
2021-10-05 08:45:02 -0400

    stats

--- update_resources/connectors/globi_data.php
+++ update_resources/connectors/globi_data.php
@@ -54,6 +54,7 @@ globi_associations	Mon 2021-06-14 10:12:55 AM	{"association.tab":2372284, "occur
 stable run: without the 'null' enties. Association integrity OK:
 globi_associations_final	Mon 2021-06-14 08:04:13 AM	{"association.tab":2316688, "occurrence_specific.tab":3863894, "reference.tab":1746832, "taxon.tab":320638, "time_elapsed":{"sec":4964.02, "min":82.73, "hr":1.38}}
 globi_associations_final	Mon 2021-06-14 11:34:55 AM	{"association.tab":2316688, "occurrence_specific.tab":3863894, "reference.tab":1746832, "taxon.tab":320638, "time_elapsed":{"sec":10824.99, "min":180.42, "hr":3.01}}
+globi_associations_final	Tue 2021-10-05 04:59:53 AM	{"association.tab":2760813, "occurrence_specific.tab":4731979, "reference.tab":2475313, "taxon.tab":324636, "time_elapsed":{"sec":15704.97, "min":261.75, "hr":4.36}}
 ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 Stats:
 As of May 27, 2020
