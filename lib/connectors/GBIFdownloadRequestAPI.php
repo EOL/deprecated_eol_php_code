e9774e97960fe665ff85ed66d5fce55f58c11064
eliagbayani <eli@eol.org>
2021-10-17 11:04:20 -0400

    removed -C in curl when downloading from GBIF

--- lib/connectors/GBIFdownloadRequestAPI.php
+++ lib/connectors/GBIFdownloadRequestAPI.php
@@ -265,8 +265,13 @@ class GBIFdownloadRequestAPI
     private function create_bash_file($taxon_group, $downloadLink)
     {
         $row1 = "#!/bin/sh";
+        /* worked for the longest time. But started having this error msg: 
+        "curl: (33) HTTP server doesn't seem to support byte ranges. Cannot resume."
+        So I now removed the "-C"
         $row2 = "curl -L -o '".$taxon_group."_DwCA.zip' -C - $downloadLink";
-        /*
+        */
+        $row2 = "curl -L -o '".$taxon_group."_DwCA.zip' $downloadLink";   //this worked OK as of Oct 17, 2021
+        /* for some reason this -sS is causing error. BETTER TO NOT USE IT.
         -s is silent
         -S is show errors
         */
