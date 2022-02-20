a24fbccb81e3d6277cc90d58a369e6a316c72e6a
eliagbayani <eli@eol.org>
2022-02-20 22:03:09 +0800

    stats

--- update_resources/connectors/fill_up_undefined_parents.php
+++ update_resources/connectors/fill_up_undefined_parents.php
@@ -9,6 +9,8 @@ wikidata-hierarchy-final	Fri 2022-02-11 08:22:30 AM	{"taxon.tab":3365754, "time_
 More stable:
 wikidata-hierarchy	        Mon 2022-02-14 07:03:04 AM	{"taxon.tab":3350652, "time_elapsed":{"sec":10372.9, "min":172.88, "hr":2.88}}
 wikidata-hierarchy-final	Mon 2022-02-14 08:48:27 AM	{"taxon.tab":3365214, "time_elapsed":{"sec":5338.23, "min":88.97, "hr":1.48}}
+Below here is after back and forth exchanges and adjustments: should be a good stable resource.
+wikidata-hierarchy-final	Fri 2022-02-18 11:59:15 PM	{"taxon.tab":3359815, "time_elapsed":{"sec":12240.02, "min":204, "hr":3.4}}
 
 in command-line in eol-archive:
 IMPORTANT TO CD to: cd /var/www/html/eol_php_code/update_resources/connectors/
