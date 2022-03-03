90828d71eedeca27e06478dcdda073e84035748a
eliagbayani <eli@eol.org>
2022-03-03 23:16:45 +0800

    notes

--- update_resources/connectors/inat_images_select.php
+++ update_resources/connectors/inat_images_select.php
@@ -17,6 +17,30 @@ $timestart = time_elapsed();
 $params['jenkins_or_cron'] = @$argv[1]; //not needed here
 $param                     = json_decode(@$argv[2], true);
 */
+
+/* ------------------------------------ in command-line in eol-archive:
+IMPORTANT TO CD to: cd /var/www/html/eol_php_code/update_resources/connectors/
+# this will be run in command-line since python can't be accessed in Jenkins
+php inat_images_select.php _
+# generates inat_images_100cap.tar.gz
+
+$ nohup php inat_images_select.php _ > terminal_inat_images_select.out
+-> use 'nohup' so it continues even after logging out of the terminal
+
+For diagnostics:
+    ps --help simple
+    ps -r 
+    -> very helpful, if u want to delete current running process
+    cat terminal_inat_images_select.txt
+    -> to see progress, very convenient
+    ps -p $PID
+    ps -p 517928
+    -> to investigate a running PID
+    $ cat /var/www/html/eol_php_code/update_resources/connectors/terminal_fill_up_undefined_parents.out
+    -> to monitor runtime
+*/
+
+
 $resource_id = 'inat_images'; //resource generated from inat_images.php
 $dwca_file = 'https://editors.eol.org/eol_php_code/applications/content_server/resources/'.$resource_id.'.tar.gz';
 // $dwca_file = 'http://localhost/eol_php_code/applications/content_server/resources/'.$resource_id.'.tar.gz'; //during dev only
