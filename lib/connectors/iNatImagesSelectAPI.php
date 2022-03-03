262ec271dd34b17a009ca1a7f9a462c111e7b997
eliagbayani <eli@eol.org>
2022-03-03 22:40:33 +0800

    mtce test

--- lib/connectors/iNatImagesSelectAPI.php
+++ lib/connectors/iNatImagesSelectAPI.php
@@ -23,12 +23,12 @@ class iNatImagesSelectAPI
                                   "http://rs.gbif.org/terms/1.0/multimedia"         => "document",
                                   "http://eol.org/schema/reference/reference"       => "reference"
                                   );
-        $this->image_limit = 100; //100 orig
+        $this->image_limit = 5; //100 orig
         if(Functions::is_production()) {
             $this->cache_path = '/extra/other_files/iNat_image_DwCA/cache/';
         }
         else {
-            $this->cache_path = '/Volumes/AKiTiO4/web/cp/iNat_image_DwCA/cache/';
+            $this->cache_path = '/Volumes/AKiTiO4/web/cp/iNat_image_DwCA/cache_image_score/';
             $this->temp_image_repo = "/Library/WebServer/Documents/eol_php_code/applications/blur_detection_opencv_eol/eol_images/";
                                 // = "/Volumes/AKiTiO4/python_apps/blur_detection_opencv/eol_images/";
         }
