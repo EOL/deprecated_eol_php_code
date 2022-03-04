e901da50a4858bbfb7c507baa1cf5a991faf6187
eliagbayani <eli@eol.org>
2022-03-04 12:12:19 +0800

    mtce

--- lib/connectors/iNatImagesSelectAPI.php
+++ lib/connectors/iNatImagesSelectAPI.php
@@ -56,7 +56,7 @@ class iNatImagesSelectAPI
         $this->unique_ids = array();
         $tbl = "http://eol.org/schema/media/document";
         self::process_table($tables[$tbl][0], 'get_total_images_count_per_taxon', $this->extensions[$tbl]);
-        print_r($this->total_images_per_taxon); //exit;
+        print_r($this->total_images_per_taxon); exit;
         
         //step 2:
         $this->unique_ids = array();
@@ -106,7 +106,7 @@ class iNatImagesSelectAPI
                 $accessURI = $rec['http://rs.tdwg.org/ac/terms/accessURI'];
                 @$this->running_taxon_images_count[$taxonID]++;
                 
-                if($this->total_images_per_taxon[$taxonID] > 100) {
+                if($this->total_images_per_taxon[$taxonID] > 100) { //many many images per taxon
                     if($ret = self::get_blurriness_score($accessURI)) {
                         // print_r($ret);
                         /*Array(
