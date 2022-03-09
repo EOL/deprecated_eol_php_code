40b9d3fbd531102e1b9d4bcab3d917f1f128e2dd
eliagbayani <eli@eol.org>
2022-03-09 22:29:09 +0800

    stats

--- update_resources/connectors/inat_images.php
+++ update_resources/connectors/inat_images.php
@@ -31,6 +31,7 @@ inat_images_100limit	Sun 2022-03-06 0{"agent.tab":194651, "media_resource.tab":8
 -> with 100 limit per taxon
 inat_images_75limit	Tue 2022-03-08 07:22{"agent.tab":172958, "media_resource.tab":7452488, "taxon.tab":290388, "time_elapsed":{"sec":41403.1, "min":690.05, "hr":11.5}}
 inat_images_40limit	Tue 2022-03-08 03:02{"agent.tab":133245, "media_resource.tab":5144786, "taxon.tab":290388, "time_elapsed":{"sec":26667.94, "min":444.47, "hr":7.41}}
+inat_images_20limit	Wed 2022-03-09 05:21{"agent.tab":100022, "media_resource.tab":3292778, "taxon.tab":290388, "time_elapsed":{"sec":20674.17, "min":344.57, "hr":5.74}}
 */
 include_once(dirname(__FILE__) . "/../../config/environment.php");
 require_library('connectors/iNatImagesAPI');
