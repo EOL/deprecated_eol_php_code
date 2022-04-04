5b73ed896fd0e590772ee296578a360337bcf083
eliagbayani <eli@eol.org>
2022-04-04 20:34:32 +0800

    mtce

--- lib/connectors/iNatImagesSelectAPI.php
+++ lib/connectors/iNatImagesSelectAPI.php
@@ -208,9 +208,11 @@ class iNatImagesSelectAPI
                 */
                 
                 @$this->running_taxon_images_count[$taxonID]++;
+                // /*
                 @$this->media_count++;
-                if($this->media_count >= 1001) return; //1001 --- just playing around with limits
-
+                if($this->media_count >= 3000000) return; //1001 --- just playing around with limits, during dev - for a report asked by Katja
+                // */
+                
                 // /* start saving
                 $o = new \eol_schema\MediaResource();
                 $uris = array_keys($rec); // print_r($uris); //exit;
