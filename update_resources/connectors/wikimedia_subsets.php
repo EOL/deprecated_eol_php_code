f59b394514df5657b3bb563a507ffdafc142d91e
eliagbayani <eli@eol.org>
2022-04-21 23:53:28 +0800

    notes

--- update_resources/connectors/wikimedia_subsets.php
+++ update_resources/connectors/wikimedia_subsets.php
@@ -32,6 +32,7 @@ wikimedia_comnames	Sat 2021-04-10 05:33:14 AM	    {"taxon.tab":147435, "vernacul
 wikimedia_comnames	Mon 2021-06-07 10:48:22 PM	    {"taxon.tab":148596, "vernacular_name.tab":899297, "time_elapsed":false}
 wikimedia_comnames	Sun 2021-09-05 01:47:50 PM	    {"taxon.tab":150220, "vernacular_name.tab":918796, "time_elapsed":false} big increase since fixing ranges
 wikimedia_comnames	Mon 2022-01-24 05:55:59 AM	    {"taxon.tab":162683, "vernacular_name.tab":991723, "time_elapsed":false}
+wikimedia_comnames	Wed 2022-04-20 01:02:22 PM	    {"taxon.tab":162683, "time_elapsed":false} --- wrong timing, will run again soon
 */
 
 // ini_set('error_reporting', E_ALL);
