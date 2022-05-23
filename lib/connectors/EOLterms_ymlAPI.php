dde4e68e41bf2afa6f7f2efdb2031ceed440ca36
eliagbayani <eli@eol.org>
2022-05-23 12:50:36 +0800

    EOL_terms_yml project

---
+++ lib/connectors/EOLterms_ymlAPI.php
@@ -0,0 +1,64 @@
+<?php
+namespace php_active_record;
+/* This lib is all about accesing EOL terms in .yml:
+https://raw.githubusercontent.com/EOL/eol_terms/main/resources/terms.yml
+-> actual yml file
+https://github.com/EOL/eol_terms/blob/main/resources/terms.yml
+-> Github entry
+
+1st client: [USDAPlants2019.php] -> called by [727.php] -> [USDAPlants.tmproj]
+2nd client: [Trait spreadsheet to DwC-A Tool] -> [http://localhost/eol_php_code/applications/trait_data_import/]
+*/
+class EOLterms_ymlAPI
+{
+    function __construct($archive_builder, $resource_id)
+    {
+        $this->download_options = array('cache' => 1, 'expire_seconds' => 60*60*24*1, 'download_wait_time' => 1000000, 'timeout' => 10800, 'download_attempts' => 1, 'delay_in_minutes' => 1);
+        // $this->download_options['expire_seconds'] = false; //comment after first harvest
+        $this->EOL_terms_yml_url = "https://raw.githubusercontent.com/EOL/eol_terms/main/resources/terms.yml";
+    }
+    function get_terms_yml($sought_type = 'ALL') //possible values: 'measurement', 'value', 'ALL'
+    {                                            //output structure: $final[label] = URI;
+        $final = array();
+        if($yml = Functions::lookup_with_cache($this->EOL_terms_yml_url, $this->download_options)) { //orig 1 day cache
+            $yml .= "alias: ";
+            if(preg_match_all("/name\:(.*?)alias\:/ims", $yml, $a)) {
+                $arr = array_map('trim', $a[1]);
+                foreach($arr as $block) { // echo "\n$block\n"; exit;
+                    /*
+                     [10713] => verbatim coordinates
+                    type: measurement
+                    uri: http://rs.tdwg.org/dwc/terms/verbatimCoordinates
+                    parent_uris:
+                    synonym_of_uri: []
+                    units_term_uri:
+                    */
+                    $rek = array();
+                    if(preg_match("/elicha(.*?)\n/ims", "elicha".$block, $a)) $rek['name'] = trim($a[1]);
+                    if(preg_match("/type\: (.*?)\n/ims", $block, $a)) $rek['type'] = trim($a[1]);
+                    if(preg_match("/uri\: (.*?)\n/ims", $block, $a)) $rek['uri'] = trim($a[1]); //https://eol.org/schema/terms/thallus_length
+                    $rek = array_map('trim', $rek);
+                    // print_r($rek);
+                    /*Array(
+                        [name] => compound fruit
+                        [type] => value
+                        [uri] => https://www.wikidata.org/entity/Q747463
+                    )*/
+                    if($sought_type == 'ALL') $final[$rek['name']] = $rek['uri'];
+                    elseif(@$rek['type'] == $sought_type) $final[$rek['name']] = $rek['uri'];
+                    else {
+                        echo "\n-----------------------\n";
+                        echo "\n[block]\n";
+                        print_r($rek)
+                        exit("\nUndefined sought type: [$sought_type]\n");
+                        echo "\n-----------------------\n";
+                    }
+                }
+            }
+            else exit("\nInvestigate: EOL terms file structure had changed.\n");
+        }
+        else exit("Remote EOL terms (.yml) file not accessible.");
+    }
+    return $final;
+}
+?>
