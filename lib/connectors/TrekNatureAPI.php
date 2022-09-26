c8767bbf5760a0adf2c472b6c2b34bdbbc0b0d52
eliagbayani <eli@eol.org>
2022-09-26 18:30:49 +0800

    mice

--- lib/connectors/TrekNatureAPI.php
+++ lib/connectors/TrekNatureAPI.php
@@ -10,7 +10,7 @@ class TrekNatureAPI
         $this->path_to_archive_directory = CONTENT_RESOURCE_LOCAL_PATH . '/' . $resource_id . '_working/';
         $this->archive_builder = new \eol_schema\ContentArchiveBuilder(array('directory_path' => $this->path_to_archive_directory));
         $this->resource_agent_ids = array();
-        $this->download_options = array('download_wait_time' => 1000000, 'timeout' => 3000, 'download_attempts' => 1, 'expire_seconds' => 60*60*24*30); //expires in a month
+        $this->download_options = array('download_wait_time' => 2000000, 'timeout' => 3000, 'download_attempts' => 1, 'expire_seconds' => 60*60*24*30); //expires in a month
         
         // $this->download_options["expire_seconds"] = false; // "expire_seconds" -- false => won't expire; 0 => expires now
         $this->image_list_page = "http://www.treknature.com/members/fragman/photos/";
