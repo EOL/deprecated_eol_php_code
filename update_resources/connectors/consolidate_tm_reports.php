29a0d2ef134095b511ec517239c893afdd876093
eliagbayani <eli@eol.org>
2022-11-14 17:50:49 +0800

    mtce

--- update_resources/connectors/consolidate_tm_reports.php
+++ update_resources/connectors/consolidate_tm_reports.php
@@ -1,7 +1,6 @@
 <?php
 namespace php_active_record;
-/*
-*/
+/* */
 include_once(dirname(__FILE__) . "/../../config/environment.php");
 require_library('connectors/BHL_Download_API');
 require_library('connectors/ConsolidateTMReportsAPI');
@@ -11,11 +10,10 @@ $timestart = time_elapsed();
 $cmdline_params['jenkins_or_cron']  = @$argv[1]; //irrelevant here
 $SearchTerm                         = @$argv[2]; //useful here
 
-$resource_id = "".$SearchTerm;
+$resource_id = $SearchTerm;
 $func = new ConsolidateTMReportsAPI($resource_id, $SearchTerm);
 $func->get_all_taxa();
-Functions::finalize_dwca_resource($resource_id, false, false, $timestart); //3rd param false means not delete folder
-
+Functions::finalize_dwca_resource($resource_id, false, true, $timestart); //3rd param false means not delete folder
 $elapsed_time_sec = time_elapsed() - $timestart;
 echo "\n\n";
 echo "\n elapsed time = " . $elapsed_time_sec/60 . " minutes";
