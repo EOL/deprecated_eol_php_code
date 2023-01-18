54f1db1985fce52d56d90f56b33b7c74fc054adc
eliagbayani <eagbayani@eol.org>
2023-01-19 00:25:30 +0800

    mtce

--- applications/genHigherClass_jenkins/generate_jenkins.php
+++ applications/genHigherClass_jenkins/generate_jenkins.php
@@ -117,11 +117,11 @@ if($info = $func->tool_generate_higherClassification($file)) {
         echo "Report <a href='../content_server/resources/" . pathinfo($filename, PATHINFO_FILENAME) . "_undefined_parent_ids.txt'>here</a><hr>";
     }
 
-    echo "<a href='index.php'>&lt;&lt; Back to main</a><br><p>";
+    echo "<a href='main.php'>&lt;&lt; Back to main</a><br><p>";
 }
 else {
     echo "The file is not ready for processing. The file needs the minimum three fields column header: '<i>taxonID</i>', '<i>scientificName</i>' and '<i>parentNameUsageID</i>'.
-    <br><a href='index.php'>&lt;&lt; Go back</a>
+    <br><a href='main.php'>&lt;&lt; Go back</a>
     <br><p>";
 }
 
