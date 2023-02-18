ce754685353755c9fd3bad9be35885198a3dd96d
eliagbayani <eagbayani@eol.org>
2023-02-18 14:10:54 +0800

    Update CypherQueryAPI.php

--- lib/connectors/CypherQueryAPI.php
+++ lib/connectors/CypherQueryAPI.php
@@ -363,7 +363,8 @@ class CypherQueryAPI
                 // if(!in_array($real_row, array(21,22,23,24,25,26,27,28,29,30))) continue; //dev only -- ready for review, with ancestry
                 // if(!in_array($real_row, array(31))) continue; // 7 connectors 403648
 
-                if(!in_array($real_row, array(13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,3,31))) continue; //dev only --  caching...
+                // if(!in_array($real_row, array(13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,3,31))) continue; //dev only --  caching...
+                if(!in_array($real_row, array(3,31))) continue; // two biggest
 
                 $this->real_row = $real_row;
                 echo "\nrow: $real_row\n";
