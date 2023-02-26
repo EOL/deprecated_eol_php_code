632ee6b5297d8eae96ee9859d5a659a1fc784bc6
eliagbayani <eagbayani@eol.org>
2023-02-26 17:41:01 +0800

    TSV reader

---
+++ lib/connectors/TSVReaderAPI.php
@@ -0,0 +1,46 @@
+<?php
+namespace php_active_record;
+/* connector: [xxx.php]
+---------------------------------------------------------------------------
+*/
+class TSVReaderAPI
+{
+    function __construct($folder = null, $query = null)
+    {
+    }
+    function read_tsv($tsv_file, $task)
+    {   $i = 0; $str = "";
+        foreach(new FileIterator($tsv_file) as $line => $row) { $i++;
+            // $row = Functions::conv_to_utf8($row);
+            if($i == 1) $fields = explode("\t", $row);
+            else {
+                if(!$row) continue;
+                $tmp = explode("\t", $row);
+                $rec = array(); $k = 0;
+                foreach($fields as $field) { $rec[$field] = $tmp[$k]; $k++; }
+                $rec = array_map('trim', $rec);
+                // print_r($rec); exit("\nelix1\n");
+
+                $pageID = $rec['pageID'];
+                if($task == "comma_sep_pageID") {
+                    $str .= "$pageID, ";
+                    if($i % 15 == 0) $str .= "\n";
+                }
+                elseif($task == "IDcorrections") {
+                    // elseif($page_id == 70351) return self::fix_further($page_id, $canonical, "Q10295328");
+                    $newID = $rec['newID'];
+                    $row = 'elseif($page_id == '.$pageID.') return self::fix_further($page_id, $canonical, "'.$newID.'");';
+                    echo "\n$row";
+
+                }
+
+                
+
+
+                // if($i >= 20) break; //debug
+            }
+        } //end foreach()
+        echo "\n[$str]\n";
+    }
+}
+?>
---
+++ update_resources/connectors/tsv_reader.php
@@ -0,0 +1,25 @@
+<?php
+namespace php_active_record;
+/* https://eol-jira.bibalex.org/browse/COLLAB-1006 */
+include_once(dirname(__FILE__) . "/../../config/environment.php");
+$timestart = time_elapsed();
+$GLOBALS['ENV_DEBUG'] = true; //orig value should be -> false ... especially in eol-archive server
+
+require_library('connectors/TSVReaderAPI');
+$func = new TSVReaderAPI();
+$path = "/Users/eliagbayani/Desktop/COLLAB-1006/z_from_Katja/";
+// $tsv_file = "3_1051_doi.org_10.1073_pnas.1907847116_fixedOnWikiData.txt";
+$tsv_file = "3_1051_doi.org_10.1073_pnas.1907847116_IDcorrections.txt";
+// $tsv_file = "3_1051_doi.org_10.1073_pnas.1907847116_remove.txt";
+
+$tsv_file = $path.$tsv_file;
+$func->read_tsv($tsv_file, "comma_sep_pageID");
+$func->read_tsv($tsv_file, "IDcorrections");
+
+$elapsed_time_sec = time_elapsed() - $timestart;
+echo "\n\n";
+echo "\n elapsed time = " . $elapsed_time_sec/60 . " minutes";
+echo "\n elapsed time = " . $elapsed_time_sec/60/60 . " hours";
+echo "\n elapsed time = " . $elapsed_time_sec/60/60/24 . " days";
+echo "\n Done processing.\n";
+?>
