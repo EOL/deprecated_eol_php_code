43cb5c901e9ab008b12ca6b3e7c706b3a7620163
eliagbayani <eagbayani@eol.org>
2023-03-06 22:26:02 +0800

    Update mov_taxaRef_toMOF.php

--- update_resources/connectors/mov_taxaRef_toMOF.php
+++ update_resources/connectors/mov_taxaRef_toMOF.php
@@ -3,19 +3,27 @@ namespace php_active_record;
 /* Adjustment request from here:
 https://eol-jira.bibalex.org/browse/DATA-1919?focusedCommentId=67376&page=com.atlassian.jira.plugin.system.issuetabpanels:comment-tabpanel#comment-67376
 Adjustment means, removing the taxa reference and move it to MoF for that taxa.
+
+php update_resources/connectors/mov_taxaRef_toMOF.php _ '{"resource_id":"Brazilian_Flora"}'
 */
 
 include_once(dirname(__FILE__) . "/../../config/environment.php");
 $timestart = time_elapsed();
 
-// /* 1st client: Brazilian Flora: https://opendata.eol.org/dataset/brazilian-flora/resource/04e94dff-d997-4e3f-946c-2c4bf5173256
-$resource_id = 'Brazilian_Flora'; //destination DWCA filename
-$dwca_file = 'https://editors.eol.org/eol_php_code/applications/content_server/resources/BF.tar.gz'; //normal operation
-// $dwca_file = 'http://localhost/eol_php_code/applications/content_server/resources_3/BF.tar.gz'; //during dev.
+// /*
+// print_r($argv);
+$params['jenkins_or_cron']  = @$argv[1]; //not needed here
+$params                     = json_decode(@$argv[2], true);
+$resource_id = @$params['resource_id']; //destination DWCA filename
 // */
 
-/* 2nd client: xxx
-*/
+if($resource_id == 'Brazilian_Flora') { // 1st client: Brazilian Flora: https://opendata.eol.org/dataset/brazilian-flora/resource/04e94dff-d997-4e3f-946c-2c4bf5173256
+    // /* 
+    $dwca_file = 'https://editors.eol.org/eol_php_code/applications/content_server/resources/BF.tar.gz'; //normal operation
+    // $dwca_file = 'http://localhost/eol_php_code/applications/content_server/resources_3/BF.tar.gz'; //during dev.
+    // */
+}
+else exit("Resource ID not defined.");
 
 process_resource_url($dwca_file, $resource_id, $timestart);
 
