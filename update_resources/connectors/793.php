91a0bff2b4f5601dd0761ed05506a4a8e4095c57
eliagbayani <eagbayani@eol.org>
2023-04-05 11:56:13 +0800

    stats

--- update_resources/connectors/793.php
+++ update_resources/connectors/793.php
@@ -8,9 +8,9 @@ http://rs.tdwg.org/dwc/terms/taxon:Total        : 22173
 http://purl.org/dc/dcmitype/StillImage          : 122322
 http://rs.tdwg.org/dwc/terms/measurementorfact  : 22171
 
-793	Thursday 2019-02-21 03:40:21 PM	{"measurement_or_fact.tab"         :22171,"media_resource.tab":179124,"occurrence.tab"         :22171,"taxon.tab":22173}
-793	Sunday 2019-02-24 10:13:06 PM	{"measurement_or_fact_specific.tab":23343,"media_resource.tab":179124,"occurrence_specific.tab":22171,"taxon.tab":22173}
-
+793	Thursday 2019-02-21 03:40:21 PM	{"measurement_or_fact.tab"         :22171, "media_resource.tab":179124, "occurrence.tab"         :22171, "taxon.tab":22173}
+793	Sunday 2019-02-24 10:13:06 PM	{"measurement_or_fact_specific.tab":23343, "media_resource.tab":179124, "occurrence_specific.tab":22171, "taxon.tab":22173}
+793	Tue 2023-04-04 12:45:13 PM	    {"measurement_or_fact_specific.tab":23343, "media_resource.tab":179124, "occurrence_specific.tab":22171, "taxon.tab":22173, "time_elapsed":false}
 */
 include_once(dirname(__FILE__) . "/../../config/environment.php");
 require_library('connectors/FemoraleAPI');
