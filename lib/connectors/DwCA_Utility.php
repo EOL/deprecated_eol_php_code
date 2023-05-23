1feba7c224812a34c708a23702a42744f3003d2d
eliagbayani <eagbayani@eol.org>
2023-05-23 17:40:46 +0800

    Update DwCA_Utility.php

--- lib/connectors/DwCA_Utility.php
+++ lib/connectors/DwCA_Utility.php
@@ -116,7 +116,7 @@ class DwCA_Utility
         if(Functions::is_production()) echo "\nProduction running...\n";
         else                           echo "\nLocal running...\n";
         
-        if(in_array($this->resource_id, array("170_final", "BF", "cites_taxa"))) $info = self::start(false, array("timeout" => 172800, 'expire_seconds' => 60*60*24*30)); //1 month expire
+        if(in_array($this->resource_id, array("170_final", "BF", "cites_taxa", "727"))) $info = self::start(false, array("timeout" => 172800, 'expire_seconds' => 60*60*24*30)); //1 month expire
         elseif(in_array($this->resource_id, array("wikimedia_comnames", "71_new", "368_removed_aves", "itis_2019-08-28", "itis_2020-07-28", 
             "itis_2020-12-01", "itis_2022-02-28_all_nodes", "368_final"))) $info = self::start(false, array("timeout" => 172800, 'expire_seconds' => 0)); //expires now
         elseif(in_array($this->resource_id, array("wiki_en_report"))) $info = self::start(false, array("timeout" => 172800, 'expire_seconds' => 0)); //expires now
