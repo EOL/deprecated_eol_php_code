2131b58cae788239ca032487a0fdff792f357c92
eliagbayani <eagbayani@eol.org>
2023-05-30 16:37:39 +0800

    Update PolytraitsNewAPI.php

--- lib/connectors/PolytraitsNewAPI.php
+++ lib/connectors/PolytraitsNewAPI.php
@@ -69,7 +69,7 @@ class PolytraitsNewAPI
                     }
                 }
             }
-            // break; //debug only
+            break; //debug only
             // if($pageID >= 2) break; //debug only
         } //end while()
     }
@@ -77,7 +77,7 @@ class PolytraitsNewAPI
     {
         $obj = self::get_name_info($rek['sciname']); //print_r($obj); exit;
         self::write_taxon($obj);
-        return;
+        // return;
         $url = str_ireplace('TAXON_ID', $obj->taxonID, $this->service['trait info']);
         if($json = Functions::lookup_with_cache($url, $this->download_options)) {
             $traits = json_decode($json);
@@ -384,49 +384,6 @@ class PolytraitsNewAPI
     // =========================================================================================
     // ========================================================================================= copied template below
     // =========================================================================================
-    private function main_x()
-    {
-        $options = $this->download_options;
-        $options['expire_seconds'] = 60*60*24*30*3; //3 months
-        $csv_file = Functions::save_remote_file_to_local($this->service['plant_list'], $options);
-        $out = shell_exec("wc -l ".$csv_file); echo "$out";
-        $i = 0;
-        $file = Functions::file_open($csv_file, "r");
-        while(!feof($file)) {
-            $row = fgetcsv($file);
-            if(!$row) break;
-            // print_r($row);
-            $i++; if(($i % 2000) == 0) echo "\n$i";
-            if($i == 1) {
-                $fields = $row;
-                // $fields = self::fill_up_blank_fieldnames($fields); // copied template
-                $count = count($fields);
-            }
-            else { //main records
-                $values = $row;
-                if($count != count($values)) { //row validation - correct no. of columns
-                    // print_r($values); print_r($rec);
-                    echo("\nWrong CSV format for this row.\n");
-                    continue;
-                }
-                $k = 0; $rec = array();
-                foreach($fields as $field) {
-                    $rec[$field] = $values[$k];
-                    $k++;
-                }
-                $rec = array_map('trim', $rec); //important step
-                // print_r($fields); print_r($rec); exit;
-                if($rec['Synonym Symbol']) { //meaning it is a synonym
-                    self::write_synonym($rec);
-                    continue;
-                }
-                self::process_rec($rec);
-            }
-            // if($i > 10) break; //debug only
-            // break;
-        }
-        unlink($csv_file);
-    }
     function lookup_profile($symbol)
     {
         $url = $this->serviceUrls->plantsServicesUrl.'PlantProfile?symbol='.$symbol;
@@ -485,38 +442,6 @@ class PolytraitsNewAPI
         }
         else exit("\ncannot lookup\n");
     }
-    private function get_images($obj)
-    {
-        $url = $this->serviceUrls->plantsServicesUrl.'PlantImages?plantId='.$obj->Id;
-        // https://plantsservices.sc.egov.usda.gov/api/PlantImages?plantId=15309
-        if($json = Functions::lookup_with_cache($url, $this->download_options)) {
-            $imgs = json_decode($json); //print_r($objs); exit("\n111\n");
-            echo " | No. of images: ".count($imgs);
-            return $imgs;
-            /*[1] => stdClass Object(
-                [ImageID] => 80
-                [StandardSizeImageLibraryPath] => /ImageLibrary/standard/abes_002_shp.jpg
-                [ThumbnailSizeImageLibraryPath] => /ImageLibrary/thumbnail/abes_002_thp.jpg
-                [LargeSizeImageLibraryPath] => /ImageLibrary/large/abes_002_lhp.jpg
-                [OriginalSizeImageLibraryPath] => /ImageLibrary/original/abes_002_php.jpg
-                [Copyright] => 1            --- should not be 1
-                [CommonName] => Pedro Acevedo-Rodriguez
-                [Title] => 
-                [ImageCreationDate] => 
-                [Collection] => 
-                [InstitutionName] => Smithsonian Institution, Department of Botany
-                [ImageLocation] => United States, Virgin Islands, Saint John Co.
-                [Comment] => 
-                [EmailAddress] => RUSSELLR@si.edu
-                [LiteratureTitle] => 
-                [LiteratureYear] => 0
-                [LiteraturePlace] => 
-                [ProvidedBy] => Smithsonian Institution, Department of Botany
-                [ScannedBy] => 
-            )*/
-        }
-
-    }
     
     private function write_synonym($rec)
     {   /*Array( e.g. synonym record
@@ -558,95 +483,6 @@ class PolytraitsNewAPI
         if(!isset($this->vernacular_name_ids[$vernacular_id])) {
            $this->vernacular_name_ids[$vernacular_id] = '';
            $this->archive_builder->write_object_to_file($v);
-        }
-    }
-    private function parse_sciname($name_str)
-    {   // e.g. "<i>Abutilon abutiloides</i> (Jacq.) Garcke ex Hochr."
-        // <i>Achnatherum occidentale</i> (Thurb.) Barkworth ssp. <i>californicum</i> (Merr. & Burtt Davy) Barkworth
-        if(preg_match_all("/<i>(.*?)<\/i>/ims", $name_str, $arr)) {
-
-            if(count($arr[1]) == 1) {
-                $sciname = trim($arr[1][0]);
-                // echo "\n[$name_str]\n[".$arr[1]."]\n"; exit; //good debug
-                $author = strip_tags($name_str);
-                $author = trim(str_replace($sciname, "", $author));
-                return array('sciname' => $sciname, 'author' => $author);    
-            }
-            elseif(count($arr[1]) > 1) {
-                /* ver 1
-                $sciname = implode(" ", $arr[1]); echo (" >2 words -[$sciname]- ");
-                $author = "";
-                // ----- get author of subspecies or variety e.g. "<i>Achnatherum occidentale</i> (Thurb.) Barkworth ssp. <i>californicum</i> (Merr. & Burtt Davy) Barkworth"
-                $last = end($arr[1]);
-                if(preg_match("/<i>".$last."<\/i>(.*?)elix/ims", $name_str."elix", $arr2)) {
-                    $author = trim($arr2[1]); // echo "\n[".$author."]\n";
-                }
-                // -----
-                return array('sciname' => $sciname, 'author' => $author);    
-                */
-                // /* ver 2
-                return array('sciname' => Functions::remove_whitespace(strip_tags($name_str)), 'author' => '');    
-                // */
-            }
-            else exit("\ninvestigate here...\n");
-
-        }
-        else {
-            // exit("\nNo italics\n$name_str\n");
-            return array('sciname' => $name_str, 'author' => '');    
-        }
-    }
-    private function get_available_image_path($img)
-    {   /*
-        [StandardSizeImageLibraryPath] => /ImageLibrary/standard/abli_001_shp.jpg
-        [LargeSizeImageLibraryPath] => /ImageLibrary/large/abli_001_lhp.jpg
-        [OriginalSizeImageLibraryPath] => /ImageLibrary/original/abli_001_php.jpg
-        */
-        if($val = trim($img->LargeSizeImageLibraryPath)) return $val;
-        if($val = trim($img->StandardSizeImageLibraryPath)) return $val;
-        if($val = trim($img->OriginalSizeImageLibraryPath)) return $val;
-        return false;
-    }
-    private function create_media_archive($taxid, $imgs)
-    {   //print_r($imgs); //exit;
-        // return;
-        foreach($imgs as $img) {
-            if($img->Copyright) continue; //proceed only if not copyrighted
-            // /*
-            $image_file_path = self::get_available_image_path($img);
-            if(!$image_file_path) continue; //blank path
-            // */
-            // print_r($img);
-            $mr = new \eol_schema\MediaResource();
-            // if($reference_ids) $mr->referenceID = implode("; ", $reference_ids);
-            if($agent_ids = self::format_agents($img)) $mr->agentID = implode("; ", $agent_ids);
-            $mr->taxonID                = $taxid;
-            $mr->identifier             = $img->ImageID;
-            $mr->type                   = "http://purl.org/dc/dcmitype/StillImage";
-            $mr->format                 = Functions::get_mimetype($image_file_path);
-            // $mr->furtherInformationURL  = '';
-            $mr->description            = self::format_description($img);
-            $mr->UsageTerms             = 'http://creativecommons.org/publicdomain/zero/1.0/'; //'http://creativecommons.org/licenses/publicdomain/'; //'http://creativecommons.org/licenses/by-nc-sa/3.0/';
-            $mr->Owner                  = $img->ProvidedBy;
-            // /*
-            $rights = "<p>This image is not copyrighted and may be freely used for any purpose. Please credit the artist, original publication if applicable, and the USDA-NRCS PLANTS Database. The following format is suggested and will be appreciated:</p>";
-            if($val = $img->CommonName) {
-                $rights .= "<p>$val @ USDA-NRCS PLANTS Database</p>"; 
-            }
-            $rights .= "<p>If you cite PLANTS in a bibliography, please use the following: USDA, NRCS. [insert current year here]. PLANTS Database (https://plants.sc.egov.usda.gov/, [insert current date here]). National Plant Data Team, Greensboro, NC 27401-4901 USA.</p>";
-            $mr->rights = Functions::remove_whitespace($rights);
-            // */
-
-            $tmp = $this->serviceUrls->imageLibraryUrl . $image_file_path;
-            $mr->accessURI = str_replace("/ImageLibrary/ImageLibrary/", "/ImageLibrary/", $tmp);
-            // https://plants.sc.egov.usda.gov/ImageLibrary/large/abam_016_lvp.jpg
-
-            // $mr->Rating                 = '';
-            if(!isset($this->object_ids[$mr->identifier])) {
-                $this->archive_builder->write_object_to_file($mr);
-                $this->object_ids[$mr->identifier] = '';            
-                @$this->image_cap[$mr->taxonID]++;
-            }    
         }
     }
     private function format_description($img)
