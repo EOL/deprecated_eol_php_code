ba04be20ad271c7cfa0bd4511ae424e40f85b4e2
eliagbayani <eagbayani@eol.org>
2023-06-01 17:05:28 +0800

    Update dwca_lookup.php

--- update_resources/connectors/dwca_lookup.php
+++ update_resources/connectors/dwca_lookup.php
@@ -9,19 +9,21 @@ ini_set("memory_limit","4000M"); // trying for the dwh_try3.zip, didn't work yet
 $GLOBALS['ENV_DEBUG'] = true;
 //===========================================================================================new - start -- handles cmdline params
 
-// /* //utility - useful when generating higherClassification
-$dwca_file = "http://localhost/eol_php_code/applications/content_server/resources_3/polytraits_new.tar.gz";
+// /* 1. First client
+$dwca_file = "https://editors.eol.org/eol_php_code/applications/content_server/resources/Polytraits.tar.gz";
 $resource_id = "nothing here";
+$params['row_type'] = 'http://rs.tdwg.org/dwc/terms/measurementorfact';
+$params['column'] = 'http://rs.tdwg.org/dwc/terms/measurementType';
+$download_options = array("timeout" => 172800, 'expire_seconds' => 60*60*24*1); //1 day cache
+// */
+
 $func = new DwCA_Utility($resource_id, $dwca_file);
-$download_options = array("timeout" => 172800, 'expire_seconds' => 60*60*24*1); //60*60*24*1
 /* a utility - works OK
 $func->count_records_in_dwca($download_options); //works OK
 */
-$params['row_type'] = 'http://rs.tdwg.org/dwc/terms/measurementorfact';
-$params['column'] = 'http://rs.tdwg.org/dwc/terms/measurementType';
-$func->lookup_value_in_dwca($download_options, $params);
+$unique_values = $func->lookup_values_in_dwca($download_options, $params); //get unique values of a column in any table in a DwCA
+print_r($unique_values);
 unset($func);
-// */
 
 $elapsed_time_sec = time_elapsed() - $timestart;
 echo "\n\n";
