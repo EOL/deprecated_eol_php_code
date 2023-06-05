db21c970bb2d5ef518cc4f6039a887d8603d4d40
eliagbayani <eagbayani@eol.org>
2023-06-05 15:48:08 +0800

    Update show_build_status.php

--- applications/taxonomic_validation/show_build_status.php
+++ applications/taxonomic_validation/show_build_status.php
@@ -43,12 +43,14 @@ else {
     /* Summary message */
     // /*
     // $final_archive_gzip_url = CONTENT_RESOURCE_LOCAL_PATH . "MarineGEO_sie/" . $params['uuid'] . ".xls";
-    $final_archive_gzip_url = CONTENT_RESOURCE_LOCAL_PATH . "Trait_Data_Import/" . $params['uuid'] . ".tar.gz";
+    // $final_archive_gzip_url = CONTENT_RESOURCE_LOCAL_PATH . "Trait_Data_Import/" . $params['uuid'] . ".tar.gz";
+    $final_archive_gzip_url = CONTENT_RESOURCE_LOCAL_PATH . "Taxonomic_Validation/" . $params['uuid'] . ".tar.gz";
     if(file_exists($final_archive_gzip_url)) {
         
         // /* NEW: to accommodate Filename_ID implementation
         if($Filename_ID = @$params['Filename_ID']) {
-            $rename_to = CONTENT_RESOURCE_LOCAL_PATH . "Trait_Data_Import/" . $params['Filename_ID'] . ".tar.gz";
+            // $rename_to = CONTENT_RESOURCE_LOCAL_PATH . "Trait_Data_Import/" . $params['Filename_ID'] . ".tar.gz";
+            $rename_to = CONTENT_RESOURCE_LOCAL_PATH . "Taxonomic_Validation/" . $params['Filename_ID'] . ".tar.gz";
             Functions::file_rename($final_archive_gzip_url, $rename_to);
             $final_archive_gzip_url = $rename_to;
             $hash_post = $Filename_ID;
