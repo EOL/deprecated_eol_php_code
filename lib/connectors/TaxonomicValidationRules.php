cc64e58142b759923a1854c6317ab35198103c1e
eliagbayani <eagbayani@eol.org>
2023-06-15 08:49:54 +0800

    Update TaxonomicValidationRules.php

--- lib/connectors/TaxonomicValidationRules.php
+++ lib/connectors/TaxonomicValidationRules.php
@@ -2,9 +2,9 @@
 namespace php_active_record;
 /*
 /extra/gnparser/gnparser/bin/gnparser -V
-version: v1.6.3
-+ /extra/gnparser/gnparser172/gnparser -V
-version: v1.7.2
+    version: v1.6.3
+/extra/gnparser/gnparser172/gnparser -V
+    version: v1.7.2
 */
 class TaxonomicValidationRules
 {
