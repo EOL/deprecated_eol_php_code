fba015677c2800745f85d88aeceb6457517cb1fd
eliagbayani <eagbayani@eol.org>
2023-08-02 23:13:38 +0800

    Update CKAN_API_Access.php

--- lib/connectors/CKAN_API_Access.php
+++ lib/connectors/CKAN_API_Access.php
@@ -34,7 +34,7 @@ class CKAN_API_Access
     function generate_ckan_id_name_list()
     {   $final = array();
         $options = $this->download_options;
-        $options['expire_seconds'] = 60*60*24*1; //1 day expires
+        $options['expire_seconds'] = 0; //60*60*24*1; //1 day expires
         if($json = Functions::lookup_with_cache($this->api_package_list, $options)) {
             $packages = json_decode($json);
             // print_r($packages); exit;
