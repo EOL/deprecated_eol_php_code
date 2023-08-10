b77cb3da5cda08d4402b3ac270408f147f40a500
eliagbayani <eagbayani@eol.org>
2023-08-10 18:10:56 +0800

    Update wikipedia_revision.php

--- update_resources/connectors/wikipedia_revision.php
+++ update_resources/connectors/wikipedia_revision.php
@@ -3,15 +3,17 @@ namespace php_active_record;
 /**/
 include_once(dirname(__FILE__) . "/../../config/environment.php");
 // ini_set('memory_limit','7096M');
-$GLOBALS['ENV_DEBUG'] = false; //true;
+$GLOBALS['ENV_DEBUG'] = true; //false; //true;
 $timestart = time_elapsed();
 
-$title      = "Ocean sunfish"; //"Ocean sunfish" en; //"Atlantic cod"; //"Mola mola" es ; //;
-$language   = "en"; //"en";
+$title      = "એશિયાઇ_સિંહ"; //"Ocean sunfish"; //"Ocean sunfish" en; //"Atlantic cod"; //"Mola mola" es ; //;
+$language   = "gu"; //"en";
+
+// $title = "Ocean sunfish"; $language = "en";
 $options    = array('resource_id' => 'wikipedia_revisions', 'expire_seconds' => 60*60*24*10, //10 days cache
                     'download_wait_time' => 1000000, 'timeout' => 10800, 'download_attempts' => 1);
 $params = array();
-$params['title'] = $title;
+$params['title'] = urlencode($title);
 $params['language'] = $language;
 
 require_library('connectors/WikipediaRevisionsAPI');
