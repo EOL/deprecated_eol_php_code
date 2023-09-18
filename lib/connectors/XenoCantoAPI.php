423e54f00e03162db5a90b5530aea73aee5764aa
eliagbayani <eagbayani@eol.org>
2023-09-18 21:49:55 +0800

    Update XenoCantoAPI.php

--- lib/connectors/XenoCantoAPI.php
+++ lib/connectors/XenoCantoAPI.php
@@ -20,12 +20,26 @@ class XenoCantoAPI
         $this->recorders_list   = $this->domain.'/contributors?q=all';
         $this->recorder_url     = "https://xeno-canto.org/contributor/"; //append the recorder id e.g. "NQMGMOJOHV"
         $this->sound_file_url   = "https://xeno-canto.org/sounds/uploaded/"; //first part of the accessURI
+        $this->debug = array();
     }
     function start()
     {
         $this->recorders_info = self::buildup_recorders_list();
+        // Rosendo Manuel Fraga ---> found in all members list page, https://xeno-canto.org/contributors?q=all
+        // Rosendo Fraga
+        $this->recorders_info["Rosendo Fraga"] = "JQSYKZHEMB"; //written only as "Rosendo Fraga", in media object 
+        $this->recorders_info["Lars Lachmann"] = "HEYJSRUDZZ";
+        $this->recorders_info["Juan Pablo Culasso"] = $this->recorders_info["juan Pablo Culasso"];
+        $this->recorders_info["Nunes D´Acosta"] = $this->recorders_info["Juvêncio da Costa Nunes Neto"];
+        $this->recorders_info["Rafael Martos Martins"] = "VXDVESQDET";
+        $this->recorders_info["Joao Menezes"] = "BEYMZHZHIB";
+        $this->recorders_info["João Menezes"] = "BEYMZHZHIB";        
+        $this->recorders_info["Elisa M. Huanca Plata - Universidad Técnica de Oruro"] = "GHVLJLRSAL";
+        $this->recorders_info["Elisa Huanca"] = "GHVLJLRSAL";
+        $this->recorders_info["James Lidster"] = "NRUIFMFTXY";
         // print_r($this->recorders_info); exit;
         self::main(); //main operation
+        print_r($this->debug);
     }
     function main()
     {   
@@ -80,7 +94,7 @@ class XenoCantoAPI
                     }
                     $i++;
                     // if($i >= 10) break;
-                    break;
+                    // break;
                 }
             }
             else echo "\nnothing found...\n";
@@ -104,6 +118,31 @@ class XenoCantoAPI
                 $orig_rec['family'] = ucfirst(strtolower($arr[1]));
             }
             $orig_rec['taxonID'] = strtolower(str_replace(" ", "-", $orig_rec['sciname']));
+
+
+            // /* --------------- special search for recorders info
+            // <a href="https://xeno-canto.org/contributor/NRUIFMFTXY">James Lidster</a>
+            if(preg_match_all("/xeno\-canto\.org\/contributor\/(.*?)<\/a>/ims", $html, $arr)) { // print_r($arr[1]); exit;
+                /* Array(
+                    [0] => YTUXOCTUEM'>Frank Lambert
+                    [1] => XKXDFWNSPA'>Jeremy Hegge
+                    [2] => XKXDFWNSPA'>Jeremy Hegge
+                    [3] => DNKBTPCMSQ'>Derek Solomon
+                    [4] => HMBDEWEZVS'>Tony Archer
+                    [5] => SGLTZLDXYI'>Lynette Rudman
+                )*/
+                foreach($arr[1] as $str) {
+                    if(preg_match("/elicha(.*?)\'/ims", "elicha".$str, $arr2)) {
+                        $index = $arr2[1];
+                        if(preg_match("/>(.*?)elicha/ims", $str."elicha", $arr2)) {
+                            $name = $arr2[1];
+                            $this->recorders_info[$name] = $index;
+                        }
+                    }
+                }
+            }
+            // exit("\n-wip-\n");
+            // --------------- end */
             return $orig_rec;
         }
     }
@@ -124,7 +163,7 @@ class XenoCantoAPI
                         $rek['identifier']              = $r->id;
                         $rek['taxonID']                 = $rec['taxonID'];
                         if($val = $r->rec) {
-                            $rek['agentID'] = self::format_agent_id($val);
+                            $rek['agentID'] = self::format_agent_id($val, $r);
                             $rek['Owner'] = $val;
                         }
                         $rek['accessURI']               = self::format_accessURI($r, $rek['agentID']);
@@ -177,7 +216,7 @@ class XenoCantoAPI
             return false;
         }
     }
-    private function format_agent_id($recorder_name)
+    private function format_agent_id($recorder_name, $r)
     {
         $possible_names = array($recorder_name, $recorder_name." †"); //did this because some names are RIP e.g. "Tony Archer †"
         foreach($possible_names as $possible) {
@@ -190,7 +229,20 @@ class XenoCantoAPI
                 if($agent_ids = self::create_agents(array($rec))) return implode("; ", $agent_ids);
             }
         }
+        print_r($r);
         exit("\nInvestigate: Recorder name not initialized: [$recorder_name]\n");
+        $this->debug["Recorder name not initialized"][$recorder_name] = '';
+
+        /* For those recorders not found in members list */
+        $rec = array();
+        $rec['identifier'] = str_replace(" ", "_", strtolower($recorder_name));
+        $rec['fullName'] = $recorder_name;
+        $rec['role'] = "recorder";
+        $rec['homepage'] = '';
+        if($agent_ids = self::create_agents(array($rec))) return implode("; ", $agent_ids);
+
+
+
     }
     private function create_agents($agents)
     {
