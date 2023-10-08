11500ba258a17507f9f3b1dccd1377cb16ce827a
eliagbayani <eagbayani@eol.org>
2023-10-08 17:39:39 +0800

    startstop nodes WIP

---
+++ lib/connectors/CypherQueryAPI_StartStop.php
@@ -0,0 +1,338 @@
+<?php
+namespace php_active_record;
+/* connector: [cypher.php]
+---------------------------------------------------------------------------
+Cypher info:
+https://github.com/EOL/eol_website/blob/master/doc/api-access.md
+check if api using cypher is available:
+- login to eol.org
+- paste in browser: https://eol.org/service/cypher?query=MATCH%20(n:Trait)%20RETURN%20n%20LIMIT%201;
+*/
+class CypherQueryAPI_StartStop
+{
+    function __construct($folder = null)
+    {
+        $this->download_options = array(
+            'resource_id'        => 'cypher_query',  //resource_id here is just a folder name in cache
+            'expire_seconds'     => 60*60*24*30, //maybe 1 month to expire
+            'download_wait_time' => 750000, 'timeout' => 60*3, 'download_attempts' => 1, 'delay_in_minutes' => 0.5);
+
+        $this->expire_seconds_4cypher_query = false; //60*60*24; //1 day expires. Default false, other value on demand.
+        $this->expire_seconds_4cypher_query = $this->download_options['expire_seconds'];
+
+        if(Functions::is_production()) $this->download_options['cache_path'] = "/extra/eol_php_cache/";
+        else                           $this->download_options['cache_path'] = "/Volumes/Crucial_2TB/eol_cache/";      //used in Functions.php for all general cache
+        $this->main_path = $this->download_options['cache_path'].$this->download_options['resource_id']."/";
+        if(!is_dir($this->main_path)) mkdir($this->main_path);
+
+        $this->basename = "cypher_".date('Y_m_d_His');
+        $this->per_page = 500; //100;       //per page with DISTINCT
+        $this->per_page_2 = 1000;           //per page without DISTINCT
+        $this->debug = array();
+    }
+    private function initialize_path($input)
+    {
+        $this->report_path = CONTENT_RESOURCE_LOCAL_PATH."reports/cypher/";
+        if(!is_dir($this->report_path)) mkdir($this->report_path);
+        // exit("\n[".json_encode($input)."]\n");
+        // exit("\n".strlen(json_encode($input))."\n");
+        $tmp = md5(json_encode($input));
+        $this->report_path .= "$tmp/";
+        if(!is_dir($this->report_path)) mkdir($this->report_path);
+    }
+    function query_trait_db($input)
+    {        
+        if($input['params']['source'] == "https://doi.org/10.1007/s13127-017-0350-6") $this->with_DISTINCT_YN = false;
+
+        print_r($input); //exit;
+        self::initialize_path($input);
+        // /* report filename
+        $this->tsv_file = $this->report_path."/".$input["trait kind"]."_qry.tsv";
+        // */
+
+        if($val = @$input["per_page"]) $this->per_page = $val;
+        if(isset($input["per_page"])) unset($input["per_page"]); // unset so that initial queries made won't get wasted. Where there is no $input["per_page"] yet.
+
+        $skip = 0;
+        while(true) {
+            $input['skip'] = $skip;
+            $input['limit'] = $this->per_page;
+            $input = self::query_maker($input);
+            $filename = self::generate_path_filename($input); // exit("\n[$filename\n");
+            $json = self::retrieve_trait_data($input, $filename);
+            $obj = json_decode($json); //print_r($obj); return; //exit("\nstop query muna\n");
+            if($total = count(@$obj->data)) {
+                // print_r($obj); exit; //good debug
+                self::write_tsv($obj, $filename, $skip);
+            }
+            print("\n No. of rows: ".$total." | $skip | row#: ".@$this->real_row."\n");
+            $skip += $this->per_page;
+            if($total < $this->per_page) break;
+            // break; //debug only
+            // if($skip == 2000) break; //debug only
+        }
+        print("\n-----Processing ends-----\n");
+        // print_r($input); //good debug
+        print("\nReport file: ".$this->tsv_file."\n");
+    }
+    private function query_maker($input)
+    {
+        $skip = $input['skip'];
+        $limit = $input['limit'];
+        if($input['type'] == "wikidata_base_qry_citation") {
+            $citation = urlencode($input['params']['citation']);
+            // /* new
+            if(    $input['trait kind'] == 'trait') {
+                $qry = 'MATCH (t:Trait)<-[:trait]-(p:Page), ';
+            $qry .= '(t)-[:predicate]->(pred:Term)
+            WHERE t.citation = "'.$citation.'"
+            OPTIONAL MATCH (t)-[:object_term]->(obj:Term)
+            OPTIONAL MATCH (t)-[:units_term]->(units:Term)
+            OPTIONAL MATCH (t)-[:lifestage_term]->(stage:Term)
+            OPTIONAL MATCH (t)-[:sex_term]->(sex:Term)
+            OPTIONAL MATCH (t)-[:statistical_method_term]->(stat:Term)
+            OPTIONAL MATCH (t)-[:metadata]->(ref:MetaData)-[:predicate]->(:Term {name:"reference"})
+            RETURN DISTINCT p.canonical, p.page_id, pred.name, stage.name, sex.name, stat.name, obj.name, t.measurement, units.name, t.source, t.citation, ref.literal
+            ORDER BY p.canonical 
+            SKIP '.$skip.' LIMIT '.$limit;    
+            }
+            elseif($input['trait kind'] == 'inferred_trait') {
+                $qry = 'MATCH (t:Trait)<-[:inferred_trait]-(p:Page), ';
+            $qry .= '(t)-[:predicate]->(pred:Term)
+            WHERE t.citation = "'.$citation.'"
+            OPTIONAL MATCH (t)-[:object_term]->(obj:Term)
+            OPTIONAL MATCH (t)-[:units_term]->(units:Term)
+            OPTIONAL MATCH (t)-[:lifestage_term]->(stage:Term)
+            OPTIONAL MATCH (t)-[:sex_term]->(sex:Term)
+            OPTIONAL MATCH (t)-[:statistical_method_term]->(stat:Term)
+            OPTIONAL MATCH (t)-[:metadata]->(ref:MetaData)-[:predicate]->(:Term {name:"reference"})
+            RETURN DISTINCT p.canonical, p.page_id, t.eol_pk, p.rank, pred.name, stage.name, sex.name, stat.name, obj.name, t.measurement, units.name, t.source, t.citation, ref.literal
+            ORDER BY p.canonical 
+            SKIP '.$skip.' LIMIT '.$limit;
+            } //t.eol_pk, p.rank,
+            // */
+        }
+        elseif($input['type'] == "wikidata_base_qry_source") { //exit("\ngoes here...\n");
+            // /* new block
+            if($input['params']['source'] == "https://doi.org/10.1007/s13127-017-0350-6") $obj_name_uri = "obj.name, obj.uri,"; //pnas
+            else                                                                          $obj_name_uri = "obj.name,"; //orig
+            // */
+    
+            $source = urlencode($input['params']['source']);
+            // $qry = 'MATCH (t:Trait)<-[:trait|inferred_trait]-(p:Page),
+            if(    $input['trait kind'] == 'trait') { //exit("\ngoes here2...\n");
+                $qry = 'MATCH (t:Trait)<-[:trait]-(p:Page), ';
+            // /* ORIG CACHED
+            $qry .= '(t)-[:predicate]->(pred:Term)
+            WHERE t.source = "'.$source.'"
+            OPTIONAL MATCH (t)-[:object_term]->(obj:Term)
+            OPTIONAL MATCH (t)-[:units_term]->(units:Term)
+            OPTIONAL MATCH (t)-[:lifestage_term]->(stage:Term)
+            OPTIONAL MATCH (t)-[:sex_term]->(sex:Term)
+            OPTIONAL MATCH (t)-[:statistical_method_term]->(stat:Term)
+            OPTIONAL MATCH (t)-[:metadata]->(ref:MetaData)-[:predicate]->(:Term {name:"reference"})
+            RETURN DISTINCT p.canonical, p.page_id, pred.name, stage.name, sex.name, stat.name, '.$obj_name_uri.' t.measurement, units.name, t.source, t.citation, ref.literal
+            ORDER BY p.canonical 
+            SKIP '.$skip.' LIMIT '.$limit;
+            // */
+            }
+            elseif($input['trait kind'] == 'inferred_trait') {
+                $qry = 'MATCH (t:Trait)<-[:inferred_trait]-(p:Page), ';
+            // /* ORIG CACHED - recently added t.eol_pk, p.rank
+
+            if($this->with_DISTINCT_YN) {
+            $qry .= '(t)-[:predicate]->(pred:Term)
+            WHERE t.source = "'.$source.'"
+            OPTIONAL MATCH (t)-[:object_term]->(obj:Term)
+            OPTIONAL MATCH (t)-[:units_term]->(units:Term)
+            OPTIONAL MATCH (t)-[:lifestage_term]->(stage:Term)
+            OPTIONAL MATCH (t)-[:sex_term]->(sex:Term)
+            OPTIONAL MATCH (t)-[:statistical_method_term]->(stat:Term)
+            OPTIONAL MATCH (t)-[:metadata]->(ref:MetaData)-[:predicate]->(:Term {name:"reference"})
+            RETURN DISTINCT p.canonical, p.page_id, t.eol_pk, p.rank, pred.name, stage.name, sex.name, stat.name, obj.name, t.measurement, units.name, t.source, t.citation, ref.literal
+            ORDER BY p.canonical 
+            SKIP '.$skip.' LIMIT '.$limit;
+            // */ //t.eol_pk, p.rank,    
+            }
+            else { //print_r($input); exit("\ngoes here\n"); //good debug
+                $qry .= '(t)-[:predicate]->(pred:Term)
+                WHERE t.source = "'.$source.'"
+                OPTIONAL MATCH (t)-[:object_term]->(obj:Term)
+                OPTIONAL MATCH (t)-[:units_term]->(units:Term)
+                OPTIONAL MATCH (t)-[:lifestage_term]->(stage:Term)
+                OPTIONAL MATCH (t)-[:sex_term]->(sex:Term)
+                OPTIONAL MATCH (t)-[:statistical_method_term]->(stat:Term)
+                OPTIONAL MATCH (t)-[:metadata]->(ref:MetaData)-[:predicate]->(:Term {name:"reference"})
+                RETURN p.canonical, p.page_id, t.eol_pk, p.rank, pred.name, stage.name, sex.name, stat.name, '.$obj_name_uri.' t.measurement, units.name, t.source, t.citation, ref.literal
+                ORDER BY p.canonical 
+                SKIP '.$skip.' LIMIT '.$limit; // no DISTINCT
+                // */ //t.eol_pk, p.rank,    
+            }
+
+            }
+
+        }
+        elseif($input['type'] == "traits_stop_at") {
+            $qry = 'MATCH (t)-[:metadata]->(stopNode:MetaData)-[:predicate]->(stop:Term {name:"stops at"})
+            RETURN DISTINCT t.eol_pk, stop.name SKIP '.$skip.' LIMIT '.$limit;
+        }
+
+        elseif($input['type'] == "wikidata_base_qry_resourceID") {
+            $this->with_DISTINCT_YN = false;
+
+            $resource_id = urlencode($input['params']['resource_id']);
+            if($input['trait kind'] == 'trait') {
+                $qry = 'MATCH (t:Trait)<-[:trait]-(p:Page), (t)-[:supplier]->(:Resource {resource_id: '.$resource_id.'}), ';
+                $qry .= '(t)-[:predicate]->(pred:Term)
+                OPTIONAL MATCH (t)-[:object_term]->(obj:Term)
+                OPTIONAL MATCH (t)-[:units_term]->(units:Term)
+                OPTIONAL MATCH (t)-[:lifestage_term]->(stage:Term)
+                OPTIONAL MATCH (t)-[:sex_term]->(sex:Term)
+                OPTIONAL MATCH (t)-[:statistical_method_term]->(stat:Term)
+                OPTIONAL MATCH (t)-[:metadata]->(ref:MetaData)-[:predicate]->(:Term {name:"reference"})
+                RETURN p.canonical, p.page_id, pred.name, stage.name, sex.name, stat.name, obj.name, obj.uri, t.measurement, units.name, t.source, t.citation, ref.literal
+                ORDER BY p.canonical 
+                SKIP '.$skip.' LIMIT '.$limit;
+            }
+            elseif($input['trait kind'] == 'inferred_trait') {
+                $qry = 'MATCH (t:Trait)<-[:inferred_trait]-(p:Page), (t)-[:supplier]->(:Resource {resource_id: '.$resource_id.'}), ';
+                $qry .= '(t)-[:predicate]->(pred:Term)
+                OPTIONAL MATCH (t)-[:object_term]->(obj:Term)
+                OPTIONAL MATCH (t)-[:units_term]->(units:Term)
+                OPTIONAL MATCH (t)-[:lifestage_term]->(stage:Term)
+                OPTIONAL MATCH (t)-[:sex_term]->(sex:Term)
+                OPTIONAL MATCH (t)-[:statistical_method_term]->(stat:Term)
+                OPTIONAL MATCH (t)-[:metadata]->(ref:MetaData)-[:predicate]->(:Term {name:"reference"})
+                RETURN p.canonical, p.page_id, t.eol_pk, p.rank, pred.name, stage.name, sex.name, stat.name, obj.name, obj.uri, t.measurement, units.name, t.source, t.citation, ref.literal
+                ORDER BY p.canonical 
+                SKIP '.$skip.' LIMIT '.$limit;
+            }
+        }
+
+        else exit("\nERROR: Undefiend query.\n");
+        $input['query'] = $qry;
+        return $input;
+    }
+    private function retrieve_trait_data($input, $filename)
+    {   
+        // /* a security block that prevents from creating blank cache files
+        if(file_exists($filename)) {
+            if(filesize($filename) == 0) unlink($filename); //this means this file was left hanging after program is terminated (ctrl+c).
+        }
+        // */
+
+        if(file_exists($filename)) {
+            debug("\nCypher cache already exists. [$filename]\n");
+            
+            // $this->download_options['expire_seconds'] = 60; //debug only - force assign --- test success
+            
+            $file_age_in_seconds = time() - filemtime($filename);
+            if($file_age_in_seconds < $this->expire_seconds_4cypher_query) return self::retrieve_json($filename); //not yet expired
+            if($this->expire_seconds_4cypher_query === false)              return self::retrieve_json($filename); //doesn't expire
+            
+            debug("\nCache expired. Will run cypher now...\n");
+            self::run_cypher_query($input, $filename);
+            return self::retrieve_json($filename);
+        }
+        else {
+            debug("\nRun cypher query...\n");
+            self::run_cypher_query($input, $filename);
+            return self::retrieve_json($filename);
+        }
+    }
+    private function retrieve_json($filename)
+    {
+        $json = file_get_contents($filename);
+        return $json; //json_decode($json, true);
+    }
+    private function run_cypher_query($input, $filename)
+    {   /*
+        Array(
+            [params] => Array(
+                    [citation] => the quick brown fox
+                )
+            [type] => wikidata_base_qry_citation
+        )
+        Array(
+            [params] => Array(
+                    [source] => https://doi.org/10.1111/j.1469-185X.1984.tb00411.x
+                )
+            [type] => wikidata_base_qry_source
+        )*/
+        $json = self::run_query($input['query']);
+        $WRITE = Functions::file_open($filename, "w");
+        fwrite($WRITE, $json); fclose($WRITE);
+        debug("\nSaved OK [$filename]\n");
+    }
+    private function run_query($qry)
+    {
+        $in_file = DOC_ROOT."/temp/".$this->basename.".in";
+        $WRITE = Functions::file_open($in_file, "w");
+        fwrite($WRITE, $qry); fclose($WRITE);
+        $destination = DOC_ROOT."temp/".$this->basename.".out.json";
+        /* worked in eol-archive but may need to add: /bin/cat instead of just 'cat'
+        $cmd = 'wget -O '.$destination.' --header "Authorization: JWT `cat '.DOC_ROOT.'temp/api.token`" https://eol.org/service/cypher?query="`cat '.$in_file.'`"';
+        */
+
+        /* worked OK in MacStudio terminal
+        $cmd = 'wget -O '.$destination.' --header "Authorization: JWT `/bin/cat '.DOC_ROOT.'temp/api.token`" https://eol.org/service/cypher?query="`/bin/cat '.$in_file.'`"';
+           worked OK in MacStudio terminal and jenkins
+        $cmd = '/opt/homebrew/bin/wget -O '.$destination.' --header "Authorization: JWT `/bin/cat '.DOC_ROOT.'temp/api.token`" https://eol.org/service/cypher?query="`/bin/cat '.$in_file.'`"';
+        */
+        $cmd = WGET_PATH.' -O '.$destination.' --header "Authorization: JWT `/bin/cat '.DOC_ROOT.'temp/api.token`" https://eol.org/service/cypher?query="`/bin/cat '.$in_file.'`"';
+        
+        // $cmd .= ' 2>/dev/null'; //this will throw away the output
+        $secs = 60*2; echo "\nSleep $secs secs..."; sleep($secs); echo " Continue...\n"; //delay 2 seconds
+        $output = shell_exec($cmd); //$output here is blank since we ended command with '2>/dev/null' --> https://askubuntu.com/questions/350208/what-does-2-dev-null-mean
+        // echo "\nTerminal out: [$output]\n"; //good debug
+        $json = file_get_contents($destination);
+        unlink($in_file);
+        unlink($destination);
+        // $obj = json_decode($json);
+        // return @$obj->data[0][0];
+        return $json;
+    }
+    private function generate_path_filename($input)
+    {
+        // print_r($input); //exit("\nthis is to be md5 hashed\n"); //good debug
+        $main_path = $this->main_path;
+        $md5 = md5(json_encode($input));
+        $cache1 = substr($md5, 0, 2);
+        $cache2 = substr($md5, 2, 2);
+        if(!file_exists($main_path . $cache1))           mkdir($main_path . $cache1);
+        if(!file_exists($main_path . "$cache1/$cache2")) mkdir($main_path . "$cache1/$cache2");
+        $filename = $main_path . "$cache1/$cache2/$md5.json";
+        return $filename;
+    }
+    private function write_tsv($obj, $filename, $skip)
+    {
+        // print_r($obj); exit("\nelix\n".$this->with_DISTINCT_YN."\n");
+        if($skip == 0) {
+            /* working but moved up
+            $base = pathinfo($filename, PATHINFO_FILENAME); //e.g. "e54dbf6839f325a6a0d5095e82bc5e70"
+            $this->tsv_file = $this->report_path."/".$base.".tsv"; //working but moved up
+            */
+            $WRITE = Functions::file_open($this->tsv_file, "w");
+            fwrite($WRITE, implode("\t", $obj->columns)."\n"); 
+        }
+        else $WRITE = Functions::file_open($this->tsv_file, "a");
+        
+        foreach($obj->data as $rec) {
+            // /* new block for without DISTINCT --- working
+            if(isset($this->with_DISTINCT_YN)) { //bec this func is being used by others e.g. get_traits_stop_at()
+                if(!$this->with_DISTINCT_YN) {
+                    $json_rec = json_encode($rec);
+                    $md5 = md5($json_rec);
+                    if(isset($this->unique_row[$md5])) continue;
+                    else $this->unique_row[$md5] = '';
+                }    
+            }
+            // */
+            // print_r($rec);
+            fwrite($WRITE, implode("\t", $rec)."\n");
+            // print("-[".$rec[0]."]-[".$rec[9]."]");   // just a visual record lookup during runtime.
+        }
+        fclose($WRITE);
+    }
+}
+?>
---
+++ update_resources/connectors/cypher_start_stop.php
@@ -0,0 +1,73 @@
+<?php
+namespace php_active_record;
+/* 
+Katja's task:
+https://docs.google.com/document/d/1OV7M5h-L7C4x5_B2YTKwimyJ68-pFQ9RjQx-Sqq1eDA/edit?userstoinvite=jen.hammock@gmail.com&sharingaction=manageaccess&role=writer 
+*/
+include_once(dirname(__FILE__) . "/../../config/environment.php");
+$timestart = time_elapsed();
+$GLOBALS['ENV_DEBUG'] = true; //orig value should be -> false ... especially in eol-archive server
+
+/* copied template - not used but works OK
+====================================================
+// print_r($argv);
+$params['jenkins_or_cron']   = @$argv[1]; //irrelevant here
+$params['json']              = @$argv[2]; //useful here
+$arr = json_decode($params['json'], true);
+====================================================
+*/
+require_library('connectors/CypherQueryAPI_StartStop');
+$resource_id = 'eol';
+$func = new CypherQueryAPI_StartStop();
+
+/* with resource_id --- query has an error
+$input = array();
+$input["params"] = array("resource_id" => $resource_id);
+$input["type"] = "wikidata_base_qry_resourceID";
+$input["per_page"] = 500; // 500 worked ok
+$input["trait kind"] = "inferred_trait";
+$func->query_trait_db($input);
+*/
+/*
+$input = array();
+$input["params"] = array();
+$input["type"] = "traits_stop_at";
+$input["per_page"] = 500; // 500 worked ok
+$arr = $func->get_traits_stop_at($input);
+*/
+
+/*
+Saved OK [/Volumes/Crucial_2TB/eol_cache/cypher_query/b7/17/b7172623b0095c0271e738f4eebc6fbc.json]
+Saved OK [/Volumes/Crucial_2TB/eol_cache/cypher_query/60/69/6069cfe15ad7643e22e33ee52149f6d8.json]
+ No. of rows: 0
+*/
+
+/*
+https://query.wikidata.org/sparql?query=SELECT ?s WHERE {VALUES ?id {"3393129"} ?s wdt:P1566 ?id }
+https://stackoverflow.com/questions/74244994/query-multiple-geonameids-in-sparql-query-on-wikidata
+*/
+
+// /* for individual resource IDs --- working OK
+$input = array();
+/* 1st client
+$input["params"] = array("resource_id" => 753); // 753-Flora do Brasil | 822-Kubitzki
+$input["type"] = "wikidata_base_qry_resourceID";
+*/
+
+// /* 2nd client
+$input["params"] = array("source" => "https://doi.org/10.1007/s13127-017-0350-6"); // 
+$input["type"] = "wikidata_base_qry_source";
+// */
+
+$input["per_page"] = 1000;
+$input["trait kind"] = "trait"; //print_r($input); exit;
+$func->query_trait_db($input);
+// */
+
+$elapsed_time_sec = time_elapsed() - $timestart;
+echo "\n\n";
+echo "\n elapsed time = " . $elapsed_time_sec/60 . " minutes";
+echo "\n elapsed time = " . $elapsed_time_sec/60/60 . " hours";
+echo "\n elapsed time = " . $elapsed_time_sec/60/60/24 . " days";
+echo "\n Done processing.\n";
+?>
