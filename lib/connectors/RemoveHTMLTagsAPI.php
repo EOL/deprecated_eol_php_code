8cc017458342ac0e521df739ba23dfdcdef6cc04
eliagbayani <eagbayani@eol.org>
2023-10-19 11:04:09 +0800

    remove html tags

--- lib/connectors/RemoveHTMLTagsAPI.php
+++ lib/connectors/RemoveHTMLTagsAPI.php
@@ -9,13 +9,14 @@ class RemoveHTMLTagsAPI
     }
     public static function remove_html_tags($str)
     {
+        $orig_str = $str;
         // step 1: replace <a> tags
         if(preg_match_all("/<a (.*?)<\/a>/ims", $str, $arr)) { // print_r($arr[1]);
             /*Array(
                 [0] => href='http://eol.org/page/173'>jumps over
             )*/
             if($lines = $arr[1]) {
-                foreach($lines as $line) { echo "\n[$line]\n";
+                foreach($lines as $line) { //echo "\n[$line]\n";
                     $href = false;
                     $link_txt = false;
                     if(preg_match("/href=\'(.*?)\'/ims", $line, $arr2)) {
@@ -27,17 +28,44 @@ class RemoveHTMLTagsAPI
                         if(preg_match("/\">(.*?)elicha/ims", $line."elicha", $arr3)) $link_txt = $arr3[1];
                     }
                     else continue;
+                    $href = trim($href);
+                    $link_txt = trim($link_txt);
                     if($href && $link_txt) {
-                        echo "\n[$href][$link_txt]\n";
+                        // echo "\n[$href][$link_txt]\n";
+                        $str = self::remove_anchor_tags($href, $link_txt, $str, $line);
                     }
-                }
+                } //end foreach()
             }
 
         }
-
+        $str = strip_tags($str);
+        return $str;
         // $left = '<table role="presentation">'; $right = '</table>';
         // $desc = self::remove_all_in_between_inclusive($left, $right, $desc);
 
+    }
+    private static function remove_anchor_tags($href, $link_txt, $str, $line)
+    {   /* [http://eol.org/page/173] [jumps over] */
+        echo "\nline: [$line]\n";
+        echo "\n[$href][$link_txt]\n";
+        $last_char = substr($link_txt, -1);
+        echo "\nlast char: [$last_char]\n";
+        if(self::is_a_letter($last_char)) { echo "\nLetter OK\n";
+            $target = "$link_txt ($href)";
+        }
+        else  { echo "\nNot a letter\n";
+            $link_txt = substr($link_txt,0,strlen($link_txt)-1);
+            $target = "$link_txt ($href)$last_char";
+        }
+
+        $line = "<a $line</a>";
+        $str = str_replace($line, $target, $str);
+        return $str;
+    }
+    private static function is_a_letter($char)
+    {
+        if(preg_match('/[a-zA-Z]/', $char)) return true;
+        return false;
     }
 
     public static function remove_all_in_between_inclusive($left, $right, $html, $includeRight = true)
--- update_resources/connectors/remove_html_tags.php
+++ update_resources/connectors/remove_html_tags.php
@@ -4,9 +4,13 @@ include_once(dirname(__FILE__) . "/../../config/environment.php");
 $timestart = time_elapsed();
 
 require_library('connectors/RemoveHTMLTagsAPI');
-$func = new RemoveHTMLTagsAPI();
-$str = "the quick brows fox <a href='http://eol.org/page/173'>jumps over</a> the lazy dog.";
-$func->remove_html_tags($str);
+// $func = new RemoveHTMLTagsAPI();
+$str = "the quick brows fox <a href='http://eol.org/page/173'>jumps over,</a> the lazy dog.";
+// $func->remove_html_tags($str);
+
+$new = RemoveHTMLTagsAPI::remove_html_tags($str);
+echo "\norig: [$str]\n";
+echo "\nnew: [$new]\n";
 
 $elapsed_time_sec = time_elapsed() - $timestart;
 echo "\n\n";
