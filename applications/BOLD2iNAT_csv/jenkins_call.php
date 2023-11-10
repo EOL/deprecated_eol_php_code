0f9ce75f2c5bb0f6e9b8314e4b8642e697fa84f4
eliagbayani <eagbayani@eol.org>
2023-11-10 08:03:19 +0800

    hide debug

--- applications/BOLD2iNAT_csv/jenkins_call.php
+++ applications/BOLD2iNAT_csv/jenkins_call.php
@@ -3,11 +3,11 @@ require_once("../../../LiteratureEditor/Custom/lib/Functions.php");
 require_once("../../../FreshData/controllers/other.php");
 require_once("../../../FreshData/controllers/freshdata.php");
 
-// /* during development
+/* during development
 ini_set('error_reporting', E_ALL);
 ini_set('display_errors', true);
 $GLOBALS['ENV_DEBUG'] = true; //set to true when debugging
-// */
+*/
 
 $ctrler = new freshdata_controller(array());
 $job_name = 'xls2dwca_job';
