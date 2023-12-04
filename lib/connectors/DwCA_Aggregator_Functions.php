6623feb31f09908a2de41f027afb1669f12b956c
eliagbayani <eagbayani@eol.org>
2023-12-04 20:48:42 +0800

    TreatmentBank better text for mining

--- lib/connectors/DwCA_Aggregator_Functions.php
+++ lib/connectors/DwCA_Aggregator_Functions.php
@@ -7,19 +7,23 @@ class DwCA_Aggregator_Functions
     function shorten_bibliographicCitation($meta, $bibliographicCitation)
     {   // exit("\n$meta->file_uri\n");
         // "/Volumes/AKiTiO4/eol_php_code_tmp/dir_07285//media.txt"
-        $eml_file = str_ireplace("media.txt", "eml.xml", $meta->file_uri);
-        if($xml = simplexml_load_file($eml_file)) { // print_r($xml);
-            if($t = $xml->additionalMetadata->metadata->plaziMods) {
-                $mods = $t->children("http://www.loc.gov/mods/v3"); // xmlns:mods="http://www.loc.gov/mods/v3"
-                // echo "\n[".$mods->mods->typeOfResource."]\n"; //prints e.g. "text"
-                $subset = trim((string) $mods->mods->relatedItem->part->detail->title);
-                if($subset) {
-                    // echo "\nmay subset:\n[".$subset."]\n"; //prints the subset of the bibliographicCitation --- good debug
-                    $shortened = str_ireplace("($subset)", "", $bibliographicCitation);
-                    $shortened = Functions::remove_whitespace($shortened);
-                    if($shortened) return $shortened;
+        $eml_file = str_ireplace("media.txt", "eml.xml", $meta->file_uri);      //for extension: http://eol.org/schema/media/Document
+        $eml_file = str_ireplace("description.txt", "eml.xml", $eml_file);      //for extension: http://rs.gbif.org/terms/1.0/Description
+        // exit("\n[$eml_file]\n");
+        if(is_file($eml_file)) {
+            if($xml = simplexml_load_file($eml_file)) { // print_r($xml);
+                if($t = $xml->additionalMetadata->metadata->plaziMods) {
+                    $mods = $t->children("http://www.loc.gov/mods/v3"); // xmlns:mods="http://www.loc.gov/mods/v3"
+                    // echo "\n[".$mods->mods->typeOfResource."]\n"; //prints e.g. "text"
+                    $subset = trim((string) $mods->mods->relatedItem->part->detail->title);
+                    if($subset) {
+                        // echo "\nmay subset:\n[".$subset."]\n"; //prints the subset of the bibliographicCitation --- good debug
+                        $shortened = str_ireplace("($subset)", "", $bibliographicCitation);
+                        $shortened = Functions::remove_whitespace($shortened);
+                        if($shortened) return $shortened;
+                    }
                 }
-            }
+            }    
         }
         // exit("\nstop muna\n");
         return $bibliographicCitation;
@@ -52,6 +56,27 @@ class DwCA_Aggregator_Functions
             }
         }
         return $html;
+    }
+    function let_media_document_go_first_over_description($index)
+    {   /* Array(
+            [0] => http://rs.tdwg.org/dwc/terms/taxon
+            [1] => http://rs.tdwg.org/dwc/terms/occurrence
+            [2] => http://rs.gbif.org/terms/1.0/description
+            [3] => http://rs.gbif.org/terms/1.0/distribution
+            [4] => http://eol.org/schema/media/document
+            [5] => http://rs.gbif.org/terms/1.0/multimedia
+            [6] => http://eol.org/schema/reference/reference
+            [7] => http://rs.gbif.org/terms/1.0/vernacularname
+        ) */
+        $media_document = "http://eol.org/schema/media/document";;
+        $description = "http://rs.gbif.org/terms/1.0/description";
+        if(in_array($media_document, $index) && in_array($description, $index)) {
+            $arr = array_diff($index, array($description));
+            $arr[] = $description;
+            $arr = array_values($arr); //reindex key
+            return $arr;
+        }
+        else return $index;
     }
 }
 ?>
