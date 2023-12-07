a44eeca5049c5f5ab4b4f20968bb0526fa7155fc
eliagbayani <eagbayani@eol.org>
2023-12-07 23:33:36 +0800

    cont. wikipedia en textmining

--- lib/connectors/Pensoft2EOLAPI.php
+++ lib/connectors/Pensoft2EOLAPI.php
@@ -404,17 +404,17 @@ class Pensoft2EOLAPI extends Functions_Pensoft
             
             // if($taxonID != 'Q1000262') continue; //debug only
             
-            /* debug only --- range ranges caching cache
+            // /* debug only --- range ranges caching cache
             if($this->param['resource_id'] == "617_ENV") { //total 1135562 objects in media tab 'TreatmentBank_ENV $m = 378521;
                                                            //total 841539                       '617_ENV'          $m = 284000;
                 $m = 284000; # can run 3 connectors. Comment 2 rows and un-comment 1 row.
                 // if($i >= 1 &&    $i < $m) {}
                 // if($i >= $m &&   $i < $m*2) {}
                 // if($i >= $m*2 && $i < $m*3) {}
-                if($i >= 640000 && $i < 720000) {}
+                if($i >= 480000) {}
                 else continue; 
             }
-            */
+            // */
             
             // print_r($this->allowed_subjects); exit;
             if(self::valid_record($rec)) {
