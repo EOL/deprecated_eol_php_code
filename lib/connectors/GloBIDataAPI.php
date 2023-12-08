6524d52a8286f19e146776c08c590eda7eee7bf6
eliagbayani <eagbayani@eol.org>
2023-12-08 14:56:00 +0800

    Update GloBIDataAPI.php

--- lib/connectors/GloBIDataAPI.php
+++ lib/connectors/GloBIDataAPI.php
@@ -177,7 +177,7 @@ class GloBIDataAPI extends Globi_Refuted_Records
         $OR = self::get_orig_reverse_uri();
         $i = 0;
         foreach(new FileIterator($meta->file_uri) as $line => $row) {
-            $i++; if(($i % 200000) == 0) echo "\n".number_format($i);
+            $i++; if(($i % 500000) == 0) echo "\n".number_format($i);
             if($meta->ignore_header_lines && $i == 1) continue;
             if(!$row) continue;
             $row = Functions::conv_to_utf8($row); //possibly to fix special chars
@@ -668,7 +668,7 @@ class GloBIDataAPI extends Globi_Refuted_Records
         echo "\nprocess_occurrence [$what]\n";
         $i = 0;
         foreach(new FileIterator($meta->file_uri) as $line => $row) {
-            $i++; if(($i % 200000) == 0) echo "\n".number_format($i);
+            $i++; if(($i % 500000) == 0) echo "\n".number_format($i);
             if($meta->ignore_header_lines && $i == 1) continue;
             if(!$row) continue;
             $row = Functions::conv_to_utf8($row); //possibly to fix special chars
@@ -751,7 +751,7 @@ class GloBIDataAPI extends Globi_Refuted_Records
         echo "\nprocess_taxon [$what]\n";
         $i = 0;
         foreach(new FileIterator($meta->file_uri) as $line => $row) {
-            $i++; if(($i % 200000) == 0) echo "\n".number_format($i);
+            $i++; if(($i % 500000) == 0) echo "\n".number_format($i);
             if($meta->ignore_header_lines && $i == 1) continue;
             if(!$row) continue;
             $row = Functions::conv_to_utf8($row); //possibly to fix special chars
@@ -892,7 +892,7 @@ class GloBIDataAPI extends Globi_Refuted_Records
         echo "\nprocess_reference [$what]\n";
         $i = 0;
         foreach(new FileIterator($meta->file_uri) as $line => $row) {
-            $i++; if(($i % 200000) == 0) echo "\n".number_format($i);
+            $i++; if(($i % 500000) == 0) echo "\n".number_format($i);
             if($meta->ignore_header_lines && $i == 1) continue;
             if(!$row) continue;
             $row = Functions::conv_to_utf8($row); //possibly to fix special chars
