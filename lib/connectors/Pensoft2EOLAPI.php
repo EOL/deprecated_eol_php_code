068e0ea83ddd6978d1b7424699d8726eb6c5ebc2
eliagbayani <eagbayani@eol.org>
2023-12-13 23:39:06 +0800

    Update Pensoft2EOLAPI.php

--- lib/connectors/Pensoft2EOLAPI.php
+++ lib/connectors/Pensoft2EOLAPI.php
@@ -404,23 +404,24 @@ class Pensoft2EOLAPI extends Functions_Pensoft
             
             // if($taxonID != 'Q1000262') continue; //debug only
             
-            // /* debug only --- range ranges caching cache
-            if($this->param['resource_id'] == "617_ENV") { //total 841539 objects in media tab '617_ENV'
-                $m = 841539/3; # can run 3 connectors. Comment 2 rows and un-comment 1 row.
-                // if($i >= 1 &&    $i < $m) {}
-                // if($i >= $m &&   $i < $m*2) {}
-                // if($i >= $m*2 && $i < $m*3) {}
-                if($i >= 480000) {}
-                else continue; 
-            }
+            /* debug only --- range ranges caching cache
+            // if($this->param['resource_id'] == "617_ENV") { //total 841539 objects in media tab '617_ENV'
+            //     $m = 841539/3; # can run 3 connectors. Comment 2 rows and un-comment 1 row.
+            //     // if($i >= 1 &&    $i < $m) {}
+            //     // if($i >= $m &&   $i < $m*2) {}
+            //     // if($i >= $m*2 && $i < $m*3) {}
+            //     if($i >= 480000) {}
+            //     else continue; 
+            // }
             if($this->param['resource_id'] == "TreatmentBank_ENV") { //total 1135562 objects in media tab ' $m = 2,108,851
-                $m = 2108851/3; # can run 3 connectors. Comment 2 rows and un-comment 1 row.
-                // if($i >= 1 &&    $i < $m) {}
-                // if($i >= $m &&   $i < $m*2) {}
+                $m = 2108851/4; # can run 3 connectors. Comment 2 rows and un-comment 1 row.
+                if($i >= 1 &&    $i < $m) {}
+                if($i >= $m &&   $i < $m*2) {}
                 if($i >= $m*2 && $i < $m*3) {}
+                if($i >= $m*3 && $i < $m*4) {}
                 else continue; 
             }
-            // */
+            */
             
             // print_r($this->allowed_subjects); exit;
             if(self::valid_record($rec)) {
