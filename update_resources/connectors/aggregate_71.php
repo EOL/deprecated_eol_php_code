7a428daca2c1a5e4329a2ccc2859c9cc923ccab1
eliagbayani <eagbayani@eol.org>
2023-12-31 13:18:48 +0800

    Update aggregate_71.php

--- update_resources/connectors/aggregate_71.php
+++ update_resources/connectors/aggregate_71.php
@@ -8,7 +8,8 @@ $timestart = time_elapsed();
 require_library('connectors/DwCA_Aggregator_Functions');
 require_library('connectors/DwCA_Aggregator');
 
-$resource_id = 71;
+$resource_id = 71; 			//orig
+$resource_id = "71_test";	//during investigation
 $preferred_rowtypes = array("http://rs.tdwg.org/dwc/terms/taxon", "http://rs.gbif.org/terms/1.0/vernacularname", "http://eol.org/schema/agent/agent", "http://eol.org/schema/media/document");
 $func = new DwCA_Aggregator($resource_id);
 $filenames = array('71_1of6', '71_2of6', '71_3of6', '71_4of6', '71_5of6', '71_6of6'); # 71_1of6.tar.gz
