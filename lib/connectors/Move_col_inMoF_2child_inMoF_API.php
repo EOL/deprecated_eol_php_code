1814afb787d80e4a973998de0cd1f147de6cea89
eliagbayani <eagbayani@eol.org>
2024-01-27 02:36:12 +0800

    move col in MoF as child in MoF

---
+++ lib/connectors/Move_col_inMoF_2child_inMoF_API.php
@@ -0,0 +1,182 @@
+<?php
+namespace php_active_record;
+/* connector: [called from DwCA_Utility.php, which is called from move_col_inMoF_2child_inMoF.php] */
+class Move_col_inMoF_2child_inMoF_API
+{
+    function __construct($archive_builder, $resource_id)
+    {
+        $this->resource_id = $resource_id;
+        $this->archive_builder = $archive_builder;
+    }
+    /*================================================================= STARTS HERE ======================================================================*/
+    function start($info)
+    {           
+        $tables = $info['harvester']->tables;
+        /* For TRY database these MoF columns will become child records in MoF: SampleSize and bodyPart */
+
+        self::process_measurementorfact($tables['http://rs.tdwg.org/dwc/terms/measurementorfact'][0], 'build_info_list');
+        
+        if(isset($this->debug)) print_r($this->debug);
+    }
+    private function write_child($measurementType, $measurementValue, $parentMeasurementID)
+    {
+        $m2 = new \eol_schema\MeasurementOrFact_specific();
+        $rek = array();
+        $rek['http://rs.tdwg.org/dwc/terms/measurementID'] = md5("$measurementType|$measurementValue|$parentMeasurementID");
+        $rek['http://rs.tdwg.org/dwc/terms/measurementType'] = $measurementType;
+        $rek['http://rs.tdwg.org/dwc/terms/measurementValue'] = $measurementValue;
+        $rek['http://eol.org/schema/parentMeasurementID'] = $parentMeasurementID;
+        $uris = array_keys($rek);
+        foreach($uris as $uri) {
+            $field = pathinfo($uri, PATHINFO_BASENAME);
+            $m2->$field = $rek[$uri];
+        }
+        if(!isset($this->measurementIDs[$m2->measurementID])) {
+            $this->measurementIDs[$m2->measurementID] = '';
+            $this->archive_builder->write_object_to_file($m2);
+        }
+    }
+    /*================================================================== ENDS HERE =======================================================================*/
+    // below this point are copied templates... 
+    function start_x($info)
+    {           
+        $tables = $info['harvester']->tables;
+        // if(in_array($this->resource_id, array('some resource id'))) {
+        if(true) {
+            self::fix_child_records_if_needed($tables);
+        }
+        
+        if(isset($this->debug)) print_r($this->debug);
+    }
+    private function fix_child_records_if_needed($tables)
+    {
+        self::process_measurementorfact($tables['http://rs.tdwg.org/dwc/terms/measurementorfact'][0], 'build_info_list');
+            // generates $this->oID_with_True_mOfTaxon_mID[$occurrenceID] = $measurementID
+        self::process_measurementorfact($tables['http://rs.tdwg.org/dwc/terms/measurementorfact'][0], 'check_all_rows');
+    }
+    private function process_measurementorfact($meta, $what)
+    {   //print_r($meta);
+        echo "\nprocess_measurementorfact...[$what]\n"; $i = 0;
+        foreach(new FileIterator($meta->file_uri) as $line => $row) {
+            $i++; if(($i % 100000) == 0) echo "\n".number_format($i);
+            if($meta->ignore_header_lines && $i == 1) continue;
+            if(!$row) continue;
+            // $row = Functions::conv_to_utf8($row); //possibly to fix special chars. but from copied template
+            $tmp = explode("\t", $row);
+            $rec = array(); $k = 0;
+            // print_r($meta->fields);
+            foreach($meta->fields as $field) {
+                if(!$field['term']) continue;
+                $rec[$field['term']] = $tmp[$k]; //put "@" as @$tmp[$k] during development
+                $k++;
+            }
+            $rec = array_map('trim', $rec); //print_r($rec); exit("\nstop muna\n");
+            /*Array(
+                [http://rs.tdwg.org/dwc/terms/measurementID] => 48e062565510ab5a6ed2e92680f56c08_try
+                [http://rs.tdwg.org/dwc/terms/occurrenceID] => TRY_Adenogramma glomerata
+                [http://eol.org/schema/measurementOfTaxon] => TRUE
+                [http://rs.tdwg.org/dwc/terms/measurementType] => http://purl.obolibrary.org/obo/FLOPO_0900022
+                [http://rs.tdwg.org/dwc/terms/measurementValue] => http://purl.obolibrary.org/obo/FLOPO_0900044
+                [http://rs.tdwg.org/dwc/terms/measurementUnit] => 
+                [http://rs.tdwg.org/dwc/terms/measurementAccuracy] => 
+                [http://rs.tdwg.org/dwc/terms/measurementMethod] => literature and database review
+                [http://rs.tdwg.org/dwc/terms/measurementRemarks] => 
+                [http://purl.org/dc/terms/source] => https://www.try-db.org/TryWeb/dp.php
+                [http://purl.org/dc/terms/bibliographicCitation] => Kattge, J., S. Daz, S. Lavorel, I. C. Prentice, P. Leadley, G. Bnisch, E. Garnier, M. Westoby, P. B. Reich, I. J. Wright, J. H. C. Cornelissen, C. Violle, S. P. Harrison, P. M. Van Bodegom, M. Reichstein, B. J. Enquist, N. A. Soudzilovskaia, D. D. Ackerly, M. Anand, O. Atkin, M. Bahn, T. R. Baker, D. Baldocchi, R. Bekker, C. C. Blanco, B. Blonder, W. J. Bond, R. Bradstock, D. E. Bunker, F. Casanoves, J. Cavender-Bares, J. Q. Chambers, F. S. Chapin, J. Chave, D. Coomes, W. K. Cornwell, J. M. Craine, B. H. Dobrin, L. Duarte, W. Durka, J. Elser, G. Esser, M. Estiarte, W. F. Fagan, J. Fang, F. Fernndez-Mndez, A. Fidelis, B. Finegan, O. Flores, H. Ford, D. Frank, G. T. Freschet, N. M. Fyllas, R. V Gallagher, W. A. Green, A. G. Gutierrez, T. Hickler, S. Higgins, J. G. Hodgson, A. Jalili, S. Jansen, C. Joly, A. J. Kerkhoff, D. Kirkup, K. Kitajima, M. Kleyer, S. Klotz, J. M. H. Knops, K. Kramer, I. Khn, H. Kurokawa, D. Laughlin, T. D. Lee, M. Leishman, F. Lens, T. Lenz, S. L. Lewis, J. Lloyd, J. Llusi, F. Louault, S. Ma, M. D. Mahecha, P. Manning, T. Massad, B. Medlyn, J. Messier, A. T. Moles, S. C. Mller, K. Nadrowski, S. Naeem, . Niinemets, S. Nllert, A. Nske, R. Ogaya, J. Oleksyn, V. G. Onipchenko, Y. Onoda, J. Ordoez, G. Overbeck, W. A. Ozinga, S. Patio, S. Paula, J. G. Pausas, J. Peuelas, O. L. Phillips, V. Pillar, H. Poorter, L. Poorter, P. Poschlod, A. Prinzing, R. Proulx, A. Rammig, S. Reinsch, B. Reu, L. Sack, B. Salgado-Negret, J. Sardans, S. Shiodera, B. Shipley, A. Siefert, E. Sosinski, J.-F. Soussana, E. Swaine, N. Swenson, K. Thompson, P. Thornton, M. Waldram, E. Weiher, M. White, S. White, S. J. Wright, B. Yguel, S. Zaehle, A. E. Zanne, and C. Wirth. 2011. TRY - a global database of plant traits. Global Change Biology 17:29052935
+                [http://eol.org/schema/reference/referenceID] => 
+                [http://eol.org/schema/terms/meanlog10] => 
+                [http://eol.org/schema/terms/SDlog10] => 
+                [http://eol.org/schema/terms/SampleSize] => 
+                [http://eol.org/schema/terms/bodyPart] => 
+            )*/
+            $measurementID = @$rec['http://rs.tdwg.org/dwc/terms/measurementID'];
+            $occurrenceID = $rec['http://rs.tdwg.org/dwc/terms/occurrenceID'];
+            $measurementOfTaxon = strtolower($rec['http://eol.org/schema/measurementOfTaxon']);
+            $measurementType = $rec['http://rs.tdwg.org/dwc/terms/measurementType'];
+            $measurementValue = $rec['http://rs.tdwg.org/dwc/terms/measurementValue'];
+
+            $SampleSize = $rec['http://eol.org/schema/terms/SampleSize'];
+            $bodyPart = $rec['http://eol.org/schema/terms/bodyPart'];
+
+            //===========================================================================================================================================================
+            if($what == 'build_info_list') {
+                if($SampleSize) $this->mID_with_SampleSize[$measurementID] = $SampleSize;
+                if($bodyPart) $this->mID_with_bodyPart[$measurementID] = $bodyPart;
+            }
+            //===========================================================================================================================================================
+            if($what == 'check_all_rows') {
+                /*
+                child record in MoF:
+                    - doesn't have: occurrenceID | measurementOfTaxon
+                    - has parentMeasurementID
+                    - has also a unique measurementID, as expected.
+                minimum cols on a child record in MoF
+                    - measurementID
+                    - measurementType
+                    - measurementValue
+                    - parentMeasurementID
+                */
+                if($measurementValue = @$this->mID_with_SampleSize[$measurementID]) {
+                    $measurementType = "http://eol.org/schema/terms/SampleSize";
+                    $parentMeasurementID = $measurementID;
+                    self::write_child($measurementType, $measurementValue, $parentMeasurementID);
+                }
+
+
+                
+                if($measurementOfTaxon != 'true') { // criteria to fix child record
+                    if($parentMeasurementID = @$this->oID_with_True_mOfTaxon_mID[$occurrenceID]) {
+                        $m2 = new \eol_schema\MeasurementOrFact_specific();
+                        $rek = array();
+                        $rek['http://rs.tdwg.org/dwc/terms/measurementID'] = md5("$measurementType|$measurementValue|$measurementID|$parentMeasurementID");
+                        $rek['http://rs.tdwg.org/dwc/terms/measurementType'] = $measurementType;
+                        $rek['http://rs.tdwg.org/dwc/terms/measurementValue'] = $measurementValue;
+                        $rek['http://eol.org/schema/parentMeasurementID'] = $parentMeasurementID;
+                        $uris = array_keys($rek);
+                        foreach($uris as $uri) {
+                            $field = pathinfo($uri, PATHINFO_BASENAME);
+                            $m2->$field = $rek[$uri];
+                        }
+                        $this->archive_builder->write_object_to_file($m2);
+                        @$this->debug['fxMoFchild']++;
+                    }
+                    else {
+                        print_r($rec);
+                        exit("\nparentID not found.\n");
+                    }
+                }
+                else $m = self::write_MoF_rec($rec); // rest of the un-changed carry-over MoF records
+            }
+            //===========================================================================================================================================================
+            // if($i >= 10) break; //debug only
+        }
+    }
+    private function write_MoF_rec($rec)
+    {
+        if(@$rec['http://eol.org/schema/parentMeasurementID']) $rec['http://eol.org/schema/measurementOfTaxon'] = ''; //means a child record
+        
+        $m = new \eol_schema\MeasurementOrFact_specific();
+        $uris = array_keys($rec);
+        foreach($uris as $uri) {
+            $field = pathinfo($uri, PATHINFO_BASENAME);
+            $m->$field = $rec[$uri];
+        }
+
+        // /* add measurementID if missing
+        if(!isset($m->measurementID)) {
+            $m->measurementID = Functions::generate_measurementID($m, $this->resource_id); //3rd param is optional. If blank then it will consider all properties of the extension
+        }
+        // */
+
+        // /* measurementValue should not be blank
+        if(!$m->measurementValue) return $m;
+        // */
+        
+        if(!isset($this->measurementIDs[$m->measurementID])) {
+            $this->measurementIDs[$m->measurementID] = '';
+            $this->archive_builder->write_object_to_file($m);
+        }
+        return $m;
+    }
+}
+?>
---
+++ update_resources/connectors/move_col_inMoF_2child_inMoF.php
@@ -0,0 +1,75 @@
+<?php
+namespace php_active_record;
+/* This is generic way to move a column(s) in MoF to a child record in MoF.
+first client: Try database: https://opendata.eol.org/dataset/try/resource/c55b9051-4125-4c36-ab54-cb56477a8746
+                            https://eol-jira.bibalex.org/browse/DATA-1766?focusedCommentId=67791&page=com.atlassian.jira.plugin.system.issuetabpanels:comment-tabpanel#comment-67791
+
+                            Basically moving these columns in MoF SampleSize and bodyPart to children records in MoF
+
+php update_resources/connectors/move_col_inMoF_2child_inMoF.php _ '{"resource_id": "try_dbase_2024_meta_recoded", "resource": "move_MoF_col_2childMoF", "resource_name": "Try DB MoF update"}'
+*/
+
+include_once(dirname(__FILE__) . "/../../config/environment.php");
+$timestart = time_elapsed();
+// $GLOBALS['ENV_DEBUG'] = true;
+
+// print_r($argv);
+$params['jenkins_or_cron'] = @$argv[1]; //not needed here
+$param                     = json_decode(@$argv[2], true);
+$resource_id = $param['resource_id'];
+print_r($param);
+
+$extension = ".tar.gz";
+if($resource_id == "some_resource_id") $extension = ".zip"; //if the source DwCA is not .tar.gz but something else.
+
+// /*
+if(Functions::is_production()) $dwca_file = CONTENT_RESOURCE_LOCAL_PATH . "/$resource_id" . $extension; //".tar.gz";
+else                           $dwca_file = 'http://localhost/eol_php_code/applications/content_server/resources_3/'.$resource_id.$extension;  //'.tar.gz';
+// */
+
+// /* ---------- customize here ----------
+    if($resource_id == 'try_dbase_2024_meta_recoded')   $resource_id = "try_dbase_2024_final";
+elseif($resource_id == 'the source')                    $resource_id = "final dwca"; //add other resources here...
+else exit("\nERROR: resource_id not yet initialized. Will terminate.\n");
+// ----------------------------------------*/
+process_resource_url($dwca_file, $resource_id, $param);
+
+
+$elapsed_time_sec = time_elapsed() - $timestart;
+echo "\n\n";
+echo "elapsed time = " . $elapsed_time_sec/60 . " minutes \n";
+echo "elapsed time = " . $elapsed_time_sec/60/60 . " hours \n";
+echo "\nDone processing.\n";
+
+function process_resource_url($dwca_file, $resource_id, $param)
+{
+    require_library('connectors/DwCA_Utility');
+    $func = new DwCA_Utility($resource_id, $dwca_file, $param);
+    $preferred_rowtypes = array(); //best to set this to array() and just set $excluded_rowtypes
+
+    if($resource_id == "try_dbase_2024_final") {
+        // /* main operation. MoF will be updated in ResourceUtility. taxon, occurrence and reference will be updated in DwCA_Utility's built-in carry-over
+        $excluded_rowtypes = array("http://rs.tdwg.org/dwc/terms/measurementorfact");
+        // "http://eol.org/schema/reference/reference"
+        // "http://rs.tdwg.org/dwc/terms/taxon"
+        // "http://rs.tdwg.org/dwc/terms/occurrence"
+        // */
+        // /* during dev only -- comment in real operation
+        $excluded_rowtypes = array("http://rs.tdwg.org/dwc/terms/measurementorfact", "http://rs.tdwg.org/dwc/terms/occurrence", "http://eol.org/schema/reference/reference", "http://rs.tdwg.org/dwc/terms/taxon");
+        // */
+    }
+    else exit("\n[$resource_id]: resource_id not yet set up.\n");
+
+    $func->convert_archive($preferred_rowtypes, $excluded_rowtypes);
+    Functions::finalize_dwca_resource($resource_id, false, true); //3rd param true means you can delete working folder
+    
+    /* copied template
+    New: important to check if all parents have entries.
+    require_library('connectors/DWCADiagnoseAPI');
+    $func = new DWCADiagnoseAPI();
+    $undefined_parents = $func->check_if_all_parents_have_entries($resource_id, true); //2nd param true means output will write to text file
+    echo "\nTotal undefined parents:" . count($undefined_parents)."\n"; unset($undefined_parents);
+    recursive_rmdir(CONTENT_RESOURCE_LOCAL_PATH.$resource_id."/"); //we can now delete folder after check_if_all_parents_have_entries() - DWCADiagnoseAPI
+    */
+}
+?>
